4527d60e139774133c59187c3fb68d8da0825359
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-06-07 22:23:50 +0300

    In progress.

--- Source/Build/Scripts/Build WxWidgets Release Only.bat
+++ Source/Build/Scripts/Build WxWidgets Release Only.bat
@@ -2,12 +2,6 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-:: Prepare variables.
-
-call "%SCRIPT_HOME%\Set Artifact Paths.bat"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-
 dotnet msbuild /t:BuildWxWidgets /p:ReleaseOnlyWxWidgetsBuild=true "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
