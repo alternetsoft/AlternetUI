9cc91827cb471d5422dbbc55a39c70ed2d89c98f
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-06-07 15:49:17 +0300

    In progress.

--- Source/Build/Scripts/Build WxWidgets Release Only.bat
+++ Source/Build/Scripts/Build WxWidgets Release Only.bat
@@ -8,7 +8,7 @@ call "%SCRIPT_HOME%\Set Artifact Paths.bat"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
-dotnet msbuild /t:BuildWxWidgets "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+dotnet msbuild /t:BuildWxWidgets /p:ReleaseOnlyWxWidgetsBuild=true "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
