0ecc6005026c6eff1c4b62b6c14446bac5e6db5b
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-06-08 17:09:08 +0300

    In progress.

---
+++ Source/Alternet.UI/Xaml/XamlX/Ast/XamlDocument.cs
@@ -0,0 +1,14 @@
+#nullable disable
+using System.Collections.Generic;
+
+namespace XamlX.Ast
+{
+#if !XAMLX_INTERNAL
+    public
+#endif
+    class XamlDocument
+    {
+        public IXamlAstNode Root { get; set; }
+        public Dictionary<string, string> NamespaceAliases { get; set; } = new Dictionary<string, string>();
+    }
+}
---
+++ Source/Alternet.UI/Xaml/XamlX/Transform/Transformers/ResolveContentPropertyTransformer.cs
@@ -0,0 +1,63 @@
+#nullable disable
+using System;
+using System.Linq;
+using XamlX.Ast;
+using XamlX.TypeSystem;
+
+namespace XamlX.Transform.Transformers
+{
+#if !XAMLX_INTERNAL
+    public
+#endif
+    class ResolveContentPropertyTransformer : IXamlAstTransformer
+    {
+        public IXamlAstNode Transform(AstTransformationContext context, IXamlAstNode node)
+        {
+            if (node is XamlAstObjectNode ni)
+            {
+                XamlAstXamlPropertyValueNode propertyNode = null;
+                
+                for (var c = ni.Children.Count - 1; c >= 0; c--)
+                {
+                    var child = ni.Children[c];
+                    if (child is IXamlAstValueNode valueNode)
+                    {
+                        if (propertyNode == null)
+                        {
+                            var contentProperty = context.Configuration.FindContentProperty(ni.Type.GetClrType());
+                            if (contentProperty != null)
+                                propertyNode = new XamlAstXamlPropertyValueNode(ni,
+                                    new XamlAstClrProperty(ni, contentProperty, context.Configuration),
+                                    new IXamlAstValueNode[0]);
+                            else
+                            {
+                                var adders = XamlTransformHelpers.FindPossibleAdders(context, ni.Type.GetClrType());
+                                if (adders.Count == 0)
+                                    throw new XamlParseException(
+                                        $"No Content property or any Add methods found for type {ni.Type.GetClrType().GetFqn()}",
+                                        child);
+                                propertyNode = new XamlAstXamlPropertyValueNode(ni, new XamlAstClrProperty(ni,
+                                        "Content", ni.Type.GetClrType(), null,
+                                        adders.Select(a => new XamlDirectCallPropertySetter(a)
+                                        {
+                                            BinderParameters = {AllowMultiple = true}
+                                        })),
+                                    new IXamlAstValueNode[0]);
+                            }
+                        }
+                        // We are going in reverse order, so insert at the beginning
+                        propertyNode.Values.Insert(0, valueNode);
+                        ni.Children.RemoveAt(c);
+                    }
+                    
+                }
+
+                if (propertyNode != null)
+                    ni.Children.Add(propertyNode);
+
+            }
+
+            return node;
+        }
+    }
+}
