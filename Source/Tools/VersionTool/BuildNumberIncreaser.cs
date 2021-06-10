aaf707e7de6a98543579515f384c019617f0e613
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-06-10 17:07:25 +0300

    In progress.

---
+++ Source/Tools/VersionTool/BuildNumberIncreaser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace VersionTool
+{
+    internal class BuildNumberIncreaser
+    {
+        public static void IncreaseBuildNumber(string versionFilePath)
+        {
+            Console.WriteLine($"Increasing build number...");
+
+            var doc = XDocument.Load(versionFilePath);
+
+            XNamespace ns = "http://schemas.microsoft.com/developer/msbuild/2003";
+            var informationalVersion = doc.Descendants(ns + "Project").Descendants(ns + "PropertyGroup").Descendants(ns + "InformationalVersion").Single();
+            TryIncreaseBuildNumber(informationalVersion, TryIncreaseBuildNumberInInformationalVersion);
+
+            var packageVersion = doc.Descendants(ns + "Project").Descendants(ns + "PropertyGroup").Descendants(ns + "Version").Single();
+            TryIncreaseBuildNumber(packageVersion, TryIncreaseBuildNumberInPackageVersion);
+
+            doc.Save(versionFilePath);
+        }
+
+        private static string TryIncreaseBuildNumber(Regex regex, string value)
+        {
+            var match = regex.Match(value);
+            if (!match.Success)
+                return value;
+
+            if (!match.Groups.TryGetValue("build", out var group))
+                return value;
+            if (!int.TryParse(group.Value, out var buildNumber))
+                return value;
+
+            var nextBuildNumber = buildNumber + 1;
+            return ReplaceNamedGroup("build", nextBuildNumber.ToString(), match);
+        }
+
+        private static string TryIncreaseBuildNumberInInformationalVersion(string value) =>
+            TryIncreaseBuildNumber(new Regex(@"^\d+\.\d+\.\d+ \(\d+\.\d+\.\d+ \w+ build (?<build>\d+)\)$"), value);
+
+        private static string TryIncreaseBuildNumberInPackageVersion(string value) =>
+            TryIncreaseBuildNumber(new Regex(@"^\d+\.\d+\.\d+-\w+\.(?<build>\d+)$"), value);
+
+        private static string ReplaceNamedGroup(string groupName, string replacement, Match m)
+        {
+            string capture = m.Value;
+            capture = capture.Remove(m.Groups[groupName].Index - m.Index, m.Groups[groupName].Length);
+            capture = capture.Insert(m.Groups[groupName].Index - m.Index, replacement);
+            return capture;
+        }
+
+        private static void TryIncreaseBuildNumber(XElement informationalVersion, Func<string, string> increaser)
+        {
+            var oldInformationalVersionValue = informationalVersion.Value;
+            informationalVersion.Value = increaser(informationalVersion.Value);
+            if (oldInformationalVersionValue != informationalVersion.Value)
+                Console.WriteLine($"Changing from {oldInformationalVersionValue} to {informationalVersion.Value}");
+        }
+    }
+}
---
+++ Source/Tools/VersionTool/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace VersionTool
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                CheckArguments(args);
+                BuildNumberIncreaser.IncreaseBuildNumber(LocateVersionFile());
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void CheckArguments(string[] args)
+        {
+            if (args.Length != 1 || !args[0].Equals("increase-build", StringComparison.OrdinalIgnoreCase))
+                throw new Exception("Usage: VersionTool.exe increase-build");
+        }
+
+        private static string LocateVersionFile()
+        {
+            var repoRoot = Path.Combine(
+                Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? throw new Exception(),
+                "../../../../../../");
+
+            var versionFilePath = Path.GetFullPath(Path.Combine(repoRoot, @"Source\Mastering\Version\Version.props"));
+            if (!File.Exists(versionFilePath))
+                throw new InvalidOperationException("Cannot locate Version.props file at" + versionFilePath);
+            return versionFilePath;
+        }
+    }
+}
