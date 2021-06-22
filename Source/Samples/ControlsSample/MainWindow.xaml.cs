d7d6c9ccb5fe165c6f0bbc109a3383ea65acd305
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-06-22 16:39:04 +0300

    Update MainWindow.xaml.cs

--- Source/Samples/ControlsSample/MainWindow.xaml.cs
+++ Source/Samples/ControlsSample/MainWindow.xaml.cs
@@ -43,6 +43,9 @@ namespace ControlsSample
             panel2.Children.Add(new TextBlock() { Text = "125 [50..200]", Margin = new Thickness(0, 10, 0, 5) });
             panel2.Children.Add(new ProgressBar() { Minimum = 50, Maximum = 200, Value = 125, Margin = new Thickness(0, 0, 0, 5) });
 
+            panel2.Children.Add(new TextBlock() { Text = "Custom height, 50 [0..100]", Margin = new Thickness(0, 10, 0, 5) });
+            panel2.Children.Add(new ProgressBar() { Value = 50, Margin = new Thickness(0, 0, 0, 5), Height = 25 });
+
             panel.Children.Add(groupBox1);
 
             groupBoxPage.Children.Add(panel);
