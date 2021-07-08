913e7900d5222675ae2d729d7f69cd9e0d584fbb
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-07-08 16:49:45 +0300

    In progress.

--- Source/Alternet.UI/Controls/ListBoxSelectionMode.cs
+++ Source/Alternet.UI/Controls/ListBoxSelectionMode.cs
@@ -1,8 +1,19 @@
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Specifies the selection behavior of a list box.
+    /// </summary>
+    /// <remarks>This enumeration is used by the <see cref="ListBox"/> class.</remarks>
     public enum ListBoxSelectionMode
     {
+        /// <summary>
+        /// Only one item can be selected.
+        /// </summary>
         Single,
+        
+        /// <summary>
+        /// Multiple items can be selected.
+        /// </summary>
         Multiple
     }
 }
--- Source/Alternet.UI/Controls/ListControl.cs
+++ Source/Alternet.UI/Controls/ListControl.cs
@@ -3,13 +3,26 @@ using System.Globalization;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Provides a common implementation of members for the ListBox and ComboBox classes.
+    /// </summary>
     public abstract class ListControl : Control
     {
         // todo: copy DataSource, DisplayMember, ValueMember etc implementation from WinForms
 
+        /// <summary>
+        /// Gets the items of the <see cref="ListControl"/>.
+        /// </summary>
+        /// <value>An <see cref="Collection{Object}"/> representing the items in the <see cref="ListControl"/>.</value>
+        /// <remarks>This property enables you to obtain a reference to the list of items that are currently stored in the <see cref="ListControl"/>.
+        /// With this reference, you can add items, remove items, and obtain a count of the items in the collection.</remarks>
         [Content]
         public Collection<object> Items { get; } = new Collection<object> { ThrowOnNullItemAddition = true };
 
+        /// <summary>
+        /// Returns the text representation of the specified item.
+        /// </summary>
+        /// <param name="item">The object from which to get the contents to display.</param>
         public string GetItemText(object item)
         {
             return item switch
