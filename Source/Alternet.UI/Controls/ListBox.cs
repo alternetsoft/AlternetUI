3aca0a0684d8ec2bc41ad6407d3ed9a0b01c414b
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-07-08 11:14:04 +0300

    Update ListBox.cs

--- Source/Alternet.UI/Controls/ListBox.cs
+++ Source/Alternet.UI/Controls/ListBox.cs
@@ -4,10 +4,61 @@ using System.Linq;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Represents a control to display a list of items.
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="ListBox"/> control enables you to display a list of items to the user that the user can select by clicking.
+    /// A <see cref="ListBox"/> control can provide single or multiple selections using the <see cref="SelectionMode"/> property.
+    /// The <see cref="Control.BeginUpdate"/> and <see cref="Control.EndUpdate"/> methods enable
+    /// you to add a large number of items to the ListBox without the control being repainted each time an item is added to the list.
+    /// The <see cref="ListControl.Items"/>, <see cref="SelectedItems"/>, and <see cref="SelectedIndices"/> properties provide access to the three
+    /// collections that are used by the <see cref="ListBox"/>.
+    /// </remarks>
     public class ListBox : ListControl
     {
-        HashSet<int> selectedIndices = new HashSet<int>();
+        private HashSet<int> selectedIndices = new HashSet<int>();
 
+        private ListBoxSelectionMode selectionMode = ListBoxSelectionMode.Single;
+
+        /// <summary>
+        /// Occurs when the <see cref="SelectedIndex"/> property or the <see cref="SelectedIndices"/> collection has changed.
+        /// </summary>
+        /// <remarks>
+        /// You can create an event handler for this event to determine when the selected index in the <see cref="ListBox"/> has been changed.
+        /// This can be useful when you need to display information in other controls based on the current selection in the <see cref="ListBox"/>.
+        /// <para>
+        /// You can use the event handler for this event to load the information in the other controls. If the <see cref="SelectionMode"/> property
+        /// is set to <see cref="ListBoxSelectionMode.Multiple"/>, any change to the <see cref="SelectedIndices"/> collection,
+        /// including removing an item from the selection, will raise this event.
+        /// </para>
+        /// </remarks>
+        public event EventHandler? SelectionChanged;
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="SelectionMode"/> property changes.
+        /// </summary>
+        public event EventHandler? SelectionModeChanged;
+
+        /// <summary>
+        /// Gets a collection that contains the zero-based indexes of all currently selected items in the <see cref="ListBox"/>.
+        /// </summary>
+        /// <value>
+        /// An <see cref="IReadOnlyList{T}"/> containing the indexes of the currently selected items in the control.
+        /// If no items are currently selected, an empty <see cref="IReadOnlyList{T}"/> is returned.
+        /// </value>
+        /// <remarks>
+        /// For a multiple-selection <see cref="ListBox"/>, this property returns a collection containing the indexes to all items that are selected
+        /// in the <see cref="ListBox"/>. For a single-selection <see cref="ListBox"/>, this property returns a collection containing a
+        /// single element containing the index of the only selected item in the <see cref="ListBox"/>.
+        /// <para>
+        /// The <see cref="ListBox"/> class provides a number of ways to reference selected items. Instead of using the <see cref="SelectedIndices"/>
+        /// property to obtain the index position of the currently selected item in a single-selection <see cref="ListBox"/>, you
+        /// can use the <see cref="SelectedIndex"/> property. If you want to obtain the item that is currently selected in the <see cref="ListBox"/>,
+        /// instead of the index position of the item, use the <see cref="SelectedItem"/> property. In addition,
+        /// you can use the <see cref="SelectedItems"/> property if you want to obtain all the selected items in a multiple-selection <see cref="ListBox"/>.
+        /// </para>
+        /// </remarks>
         public IReadOnlyList<int> SelectedIndices
         {
             get
@@ -19,7 +70,7 @@ namespace Alternet.UI
             set
             {
                 CheckDisposed();
-                
+
                 ClearSelectedCore();
 
                 bool changed = false;
@@ -32,36 +83,6 @@ namespace Alternet.UI
                 if (changed)
                     InvokeSelectionChanged(EventArgs.Empty);
             }
-        }
-
-        public void ClearSelected()
-        {
-            ClearSelectedCore();
-            InvokeSelectionChanged(EventArgs.Empty);
-        }
-
-        private void ClearSelectedCore()
-        {
-            selectedIndices.Clear();
-        }
-
-        public void SetSelected(int index, bool value)
-        {
-            var changed = SetSelectedCore(index, value);
-
-            if (changed)
-                InvokeSelectionChanged(EventArgs.Empty);
-        }
-
-        private bool SetSelectedCore(int index, bool value)
-        {
-            bool changed;
-            if (value)
-                changed = selectedIndices.Add(index);
-            else
-                changed = selectedIndices.Remove(index);
-            
-            return changed;
         }
 
         public int? SelectedIndex
@@ -87,7 +108,7 @@ namespace Alternet.UI
             get
             {
                 CheckDisposed();
-                
+
                 if (SelectedIndex == null)
                     return null;
 
@@ -110,23 +131,15 @@ namespace Alternet.UI
             }
         }
 
-        public void InvokeSelectionChanged(EventArgs e)
+        public IReadOnlyList<object> SelectedItems
         {
-            if (e == null)
-                throw new ArgumentNullException(nameof(e));
+            get
+            {
+                CheckDisposed();
 
-            OnSelectionChanged(e);
-            SelectionChanged?.Invoke(this, e);
+                return SelectedIndices.Select(x => Items[x]).ToArray();
+            }
         }
-
-        protected virtual void OnSelectionChanged(EventArgs e)
-        {
-        }
-
-        public event EventHandler? SelectionChanged;
-
-        public event EventHandler? SelectionModeChanged;
-        ListBoxSelectionMode selectionMode = ListBoxSelectionMode.Single;
 
         public ListBoxSelectionMode SelectionMode
         {
@@ -148,6 +161,56 @@ namespace Alternet.UI
 
                 SelectionModeChanged?.Invoke(this, EventArgs.Empty);
             }
+        }
+
+        /// <summary>
+        /// Unselects all items in the ListBox.
+        /// </summary>
+        /// <remarks>
+        /// Calling this method is equivalent to setting the <see cref="SelectedIndex"/> property to <c>null</c>.
+        /// You can use this method to quickly unselect all items in the list.
+        /// </remarks>
+        public void ClearSelected()
+        {
+            ClearSelectedCore();
+            InvokeSelectionChanged(EventArgs.Empty);
+        }
+
+        public void SetSelected(int index, bool value)
+        {
+            var changed = SetSelectedCore(index, value);
+
+            if (changed)
+                InvokeSelectionChanged(EventArgs.Empty);
+        }
+
+        public void InvokeSelectionChanged(EventArgs e)
+        {
+            if (e == null)
+                throw new ArgumentNullException(nameof(e));
+
+            OnSelectionChanged(e);
+            SelectionChanged?.Invoke(this, e);
+        }
+
+        protected virtual void OnSelectionChanged(EventArgs e)
+        {
+        }
+
+        private void ClearSelectedCore()
+        {
+            selectedIndices.Clear();
+        }
+
+        private bool SetSelectedCore(int index, bool value)
+        {
+            bool changed;
+            if (value)
+                changed = selectedIndices.Add(index);
+            else
+                changed = selectedIndices.Remove(index);
+
+            return changed;
         }
     }
 }
