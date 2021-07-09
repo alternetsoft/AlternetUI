603adb460e9566f854eb4b342f1b0010153849b9
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-07-09 14:11:01 +0300

    In progress.

--- Source/Alternet.UI/Drawing/DrawingContext.cs
+++ Source/Alternet.UI/Drawing/DrawingContext.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Describes a drawing surface.
+    /// </summary>
     public class DrawingContext : IDisposable
     {
         private Native.DrawingContext dc;
@@ -14,42 +16,67 @@ namespace Alternet.UI
             this.dc = dc;
         }
 
+        /// <summary>
+        /// Fills the interior of a rectangle specified by a <see cref="RectangleF"/> structure.
+        /// </summary>
         public void FillRectangle(RectangleF rectangle, Color color)
         {
             dc.FillRectangle(rectangle, color);
         }
 
+        /// <summary>
+        /// Draws a rectangle specified by a <see cref="RectangleF"/> structure.
+        /// </summary>
         public void DrawRectangle(RectangleF rectangle, Color color)
         {
             dc.DrawRectangle(rectangle, color);
         }
 
+        /// <summary>
+        /// Draws the specified text string at the specified location.
+        /// </summary>
         public void DrawText(string text, PointF origin, Color color)
         {
             dc.DrawText(text, origin, color);
         }
 
+        /// <summary>
+        /// Measures the specified string.
+        /// </summary>
         public SizeF MeasureText(string text)
         {
             return dc.MeasureText(text);
         }
 
+        /// <summary>
+        /// Pushes the specified <see cref="Transform"/> onto the drawing context.
+        /// </summary>
         public void PushTransform(Transform value)
         {
             dc.PushTransform(value.Translation);
         }
 
+        /// <summary>
+        /// Pops the last transform operation that was pushed onto the drawing context.
+        /// </summary>
         public void Pop()
         {
             dc.Pop();
         }
 
+        /// <summary>
+        /// Releases all resources used by the <see cref="DrawingContext"/> object.
+        /// </summary>
         public void Dispose()
         {
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Releases the unmanaged resources used by the <see cref="DrawingContext"/> and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing"></param>
         protected virtual void Dispose(bool disposing)
         {
             if (!isDisposed)
--- Source/Alternet.UI/Drawing/Transform.cs
+++ Source/Alternet.UI/Drawing/Transform.cs
@@ -2,15 +2,28 @@ using System.Drawing;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Defines functionality that enables transformations in a 2-D plane.
+    /// </summary>
     public struct Transform
     {
         // todo
 
         private Transform(SizeF translation) => Translation = translation;
 
+        /// <summary>
+        /// Gets the translation part of the transform.
+        /// </summary>
         public SizeF Translation { get; }
 
+        /// <summary>
+        /// Creates a new transform from a translation value.
+        /// </summary>
         public static Transform FromTranslation(SizeF translation) => new Transform(translation);
+
+        /// <summary>
+        /// Creates a new transform from a translation value.
+        /// </summary>
         public static Transform FromTranslation(PointF translation) => new Transform(new SizeF(translation.X, translation.Y));
     }
 }
