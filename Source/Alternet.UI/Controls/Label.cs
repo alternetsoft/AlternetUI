2878fae5bc482c0f8563436d21e8b49c1e0e9342
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-07-09 09:35:40 +0300

    In progress.

--- Source/Alternet.UI/Controls/Label.cs
+++ Source/Alternet.UI/Controls/Label.cs
@@ -2,10 +2,31 @@ using System;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Represents a text label control.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="Label" /> controls are typically used to provide descriptive text for a control.
+    /// For example, you can use a <see cref="Label" /> to add descriptive text for a <see cref="TextBox"/> control to inform the
+    /// user about the type of data expected in the control. <see cref="Label" /> controls can also be used
+    /// to add descriptive text to a <see cref="Window"/> to provide the user with helpful information.
+    /// For example, you can add a <see cref="Label" /> to the top of a <see cref="Window"/> that provides instructions
+    /// to the user on how to input data in the controls on the form. <see cref="Label" /> controls can be
+    /// also used to display run time information on the status of an application. For example,
+    /// you can add a <see cref="Label" /> control to a form to display the status of each file as a list of files is processed.
+    /// </remarks>
     public class Label : Control
     {
         private string? text;
 
+        /// <summary>
+        /// Occurs when the value of the <see cref="Text"/> property changes.
+        /// </summary>
+        public event EventHandler? TextChanged;
+
+        /// <summary>
+        /// Gets or sets the text displayed on this label.
+        /// </summary>
         public string? Text
         {
             get
@@ -21,23 +42,25 @@ namespace Alternet.UI
                     return;
 
                 text = value;
-                InvokeTextChanged(EventArgs.Empty);
+                RaiseTextChanged(EventArgs.Empty);
             }
         }
 
-        public event EventHandler? TextChanged;
+        /// <summary>
+        /// Called when the value of the <see cref="Text"/> property changes.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+        protected virtual void OnTextChanged(EventArgs e)
+        {
+        }
 
-        private void InvokeTextChanged(EventArgs e)
+        private void RaiseTextChanged(EventArgs e)
         {
             if (e == null)
                 throw new ArgumentNullException(nameof(e));
 
             OnTextChanged(e);
             TextChanged?.Invoke(this, e);
-        }
-
-        protected virtual void OnTextChanged(EventArgs e)
-        {
         }
     }
 }
--- Source/Alternet.UI/Controls/TextBox.cs
+++ Source/Alternet.UI/Controls/TextBox.cs
@@ -2,10 +2,35 @@ using System;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Represents a control that can be used to display or edit unformatted text.
+    /// </summary>
+    /// <remarks>
+    /// With the <see cref="TextBox"/> control, the user can enter text in an application.
+    /// </remarks>
     public class TextBox : Control
     {
         private string text = "";
 
+        private bool editControlOnly = false;
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="Text"/> property changes.
+        /// </summary>
+        public event EventHandler? TextChanged;
+
+        /// <summary>
+        /// todo: use BorderStyle for this purpose later.
+        /// </summary>
+        public event EventHandler? EditControlOnlyChanged;
+
+        /// <summary>
+        /// Gets or sets the text contents of the text box.
+        /// </summary>
+        /// <value>A string containing the text contents of the text box. The default is an empty string ("").</value>
+        /// <remarks>
+        /// Getting this property returns a string copy of the contents of the text box. Setting this property replaces the contents of the text box with the specified string.
+        /// </remarks>
         public string Text
         {
             get
@@ -21,14 +46,13 @@ namespace Alternet.UI
                     return;
 
                 text = value;
-                InvokeTextChanged(EventArgs.Empty);
+                RaiseTextChanged(EventArgs.Empty);
             }
         }
 
-        public event EventHandler? TextChanged;
-
-        private bool editControlOnly = false;
-
+        /// <summary>
+        /// todo: use BorderStyle for this purpose later.
+        /// </summary>
         public bool EditControlOnly
         {
             get
@@ -48,9 +72,11 @@ namespace Alternet.UI
             }
         }
 
-        public event EventHandler? EditControlOnlyChanged;
-
-        public void InvokeTextChanged(EventArgs e)
+        /// <summary>
+        /// Raises the <see cref="TextChanged"/> event and calls <see cref="OnTextChanged(EventArgs)"/>.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+        public void RaiseTextChanged(EventArgs e)
         {
             if (e == null)
                 throw new ArgumentNullException(nameof(e));
@@ -59,10 +85,17 @@ namespace Alternet.UI
             TextChanged?.Invoke(this, e);
         }
 
+        /// <summary>
+        /// Called when the value of the <see cref="Text"/> property changes.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
         protected virtual void OnTextChanged(EventArgs e)
         {
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         protected override ControlHandler CreateHandler()
         {
             if (EditControlOnly)
