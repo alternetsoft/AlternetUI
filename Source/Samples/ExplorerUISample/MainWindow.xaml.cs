7f36f94246164f9ccf3b1ac6bcf7dc4db8fba685
Yevgeni Zolotko <yevgeni.zolotko@alternetsoft.com>
2021-08-09 19:35:30 +0300

    In progress.

--- Source/Samples/ExplorerUISample/MainWindow.xaml.cs
+++ Source/Samples/ExplorerUISample/MainWindow.xaml.cs
@@ -14,8 +14,32 @@ namespace ExplorerUISample
                 throw new InvalidOperationException();
             new XamlLoader().LoadExisting(xamlStream, this);
 
-            //option1RadioButton = (RadioButton)FindControl("option1RadioButton");
-            //option1RadioButton.CheckedChanged += Option1RadioButton_CheckedChanged;
+            var listView = (ListView)FindControl("listView");
+            var date = DateTime.Now.ToShortDateString();
+            listView.Items.Add(new ListViewItem(new[] {"July Report 1", "1K", date}, 0));
+            listView.Items.Add(new ListViewItem(new[] {"06.21 M&A Meeting Memo", "1.5K", date}, 1));
+            listView.Items.Add(new ListViewItem(new[] {"RTC Chart - Mary", "12M", date}, 2));
+            listView.Items.Add(new ListViewItem(new[] {"3rd quarter results - Mary", "1M", date}, 2));
+
+            var treeView = (TreeView)FindControl("treeView");
+            var maryM = new TreeViewItem("MaryM", 3);
+            maryM.Items.Add(new TreeViewItem("Docs", 3));
+            maryM.Items.Add(new TreeViewItem("New Reports", 3));
+            maryM.Items.Add(new TreeViewItem("Misc", 3));
+            var meetings = new TreeViewItem("Meetings", 3);
+            meetings.Items.Add(new TreeViewItem("May", 3));
+            meetings.Items.Add(new TreeViewItem("June", 3));
+            meetings.Items.Add(new TreeViewItem("July", 3));
+            maryM.Items.Add(meetings);
+            treeView.Items.Add(maryM);
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+
+            if (Visible)
+                new ProgressWindow().Show();
         }
     }
 }
