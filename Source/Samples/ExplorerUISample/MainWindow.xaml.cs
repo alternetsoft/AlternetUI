89d4b227efd4e24d623469bc595dcd8950bef099
Yevgeni Zolotko <yevgeni.zolotko@alternetsoft.com>
2021-08-09 19:54:19 +0300

    In progress.

--- Source/Samples/ExplorerUISample/MainWindow.xaml.cs
+++ Source/Samples/ExplorerUISample/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using Image = Alternet.UI.Image;
 
 namespace ExplorerUISample
 {
@@ -17,21 +20,26 @@ namespace ExplorerUISample
             var listView = (ListView)FindControl("listView");
             var date = DateTime.Now.ToShortDateString();
             listView.Items.Add(new ListViewItem(new[] {"July Report 1", "1K", date}, 0));
-            listView.Items.Add(new ListViewItem(new[] {"06.21 M&A Meeting Memo", "1.5K", date}, 1));
-            listView.Items.Add(new ListViewItem(new[] {"RTC Chart - Mary", "12M", date}, 2));
-            listView.Items.Add(new ListViewItem(new[] {"3rd quarter results - Mary", "1M", date}, 2));
+            listView.Items.Add(new ListViewItem(new[] {"06.21 M&A Meeting Memo", "1.5K", date}, 2));
+            listView.Items.Add(new ListViewItem(new[] {"RTC Chart - Mary", "12M", date}, 3));
+            listView.Items.Add(new ListViewItem(new[] {"3rd quarter results - Mary", "1M", date}, 3));
 
+            const int FolderImageIndex = 1;
             var treeView = (TreeView)FindControl("treeView");
-            var maryM = new TreeViewItem("MaryM", 3);
-            maryM.Items.Add(new TreeViewItem("Docs", 3));
-            maryM.Items.Add(new TreeViewItem("New Reports", 3));
-            maryM.Items.Add(new TreeViewItem("Misc", 3));
-            var meetings = new TreeViewItem("Meetings", 3);
-            meetings.Items.Add(new TreeViewItem("May", 3));
-            meetings.Items.Add(new TreeViewItem("June", 3));
-            meetings.Items.Add(new TreeViewItem("July", 3));
+            var maryM = new TreeViewItem("MaryM", FolderImageIndex);
+            maryM.Items.Add(new TreeViewItem("Docs", FolderImageIndex));
+            maryM.Items.Add(new TreeViewItem("New Reports", FolderImageIndex));
+            maryM.Items.Add(new TreeViewItem("Misc", FolderImageIndex));
+            var meetings = new TreeViewItem("Meetings", FolderImageIndex);
+            meetings.Items.Add(new TreeViewItem("May", FolderImageIndex));
+            meetings.Items.Add(new TreeViewItem("June", FolderImageIndex));
+            meetings.Items.Add(new TreeViewItem("July", FolderImageIndex));
             maryM.Items.Add(meetings);
             treeView.Items.Add(maryM);
+
+            var imageList = LoadImageList();
+            treeView.ImageList = imageList;
+            listView.SmallImageList = imageList;
         }
 
         protected override void OnVisibleChanged(EventArgs e)
@@ -40,6 +48,21 @@ namespace ExplorerUISample
 
             if (Visible)
                 new ProgressWindow().Show();
+        }
+
+        private static ImageList LoadImageList()
+        {
+            var smallImageList = new ImageList();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var allResourceNames = assembly.GetManifestResourceNames();
+            var allImageResourceNames = allResourceNames.Where(x => x.StartsWith("ExplorerUISample.Resources.")).ToArray();
+            Image LoadImage(string name) => new Image(assembly.GetManifestResourceStream(name) ?? throw new Exception());
+
+            foreach (var name in allImageResourceNames)
+                smallImageList.Images.Add(LoadImage(name));
+
+            return smallImageList;
         }
     }
 }
