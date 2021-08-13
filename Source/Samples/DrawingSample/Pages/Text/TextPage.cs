d11ae3db7901ac11c3275770b666e7d6c3e30622
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-08-13 12:19:41 +0300

    Update TextPage.cs

--- Source/Samples/DrawingSample/Pages/Text/TextPage.cs
+++ Source/Samples/DrawingSample/Pages/Text/TextPage.cs
@@ -1,7 +1,9 @@
 ﻿using Alternet.UI;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using Font = Alternet.UI.Font;
 using FontFamily = Alternet.UI.FontFamily;
 
 namespace DrawingSample
@@ -10,7 +12,11 @@ namespace DrawingSample
     {
         private const string LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nSuspendisse tincidunt orci vitae arcu congue commodo.\nProin fermentum rhoncus dictum.";
 
-        private Alternet.UI.Font[]? fonts;
+        private Paragraph[]? paragraphs;
+
+        private static Font fontInfoFont = new Alternet.UI.Font(FontFamily.GenericMonospace, 8);
+        private static Color fontInfoColor = Color.Black;
+
         private float fontSize = 10;
 
         public override string Name => "Text Lines";
@@ -21,46 +27,74 @@ namespace DrawingSample
             set
             {
                 fontSize = value;
-                InvalidateFonts();
+                InvalidatParagraphs();
+            }
+        }
+
+        class Paragraph : IDisposable
+        {
+            public Paragraph(Font font, string genericFamilyName)
+            {
+                Font = font;
+                GenericFamilyName = genericFamilyName;
+                FontInfo = $"Generic {GenericFamilyName}: {Font.Name}, {Font.SizeInPoints}pt";
+            }
+
+            public Font Font { get; }
+            public string GenericFamilyName { get; }
+            public string FontInfo { get; }
+
+            public void Dispose()
+            {
+                Font.Dispose();
             }
         }
 
         public override void Draw(DrawingContext dc, RectangleF bounds)
         {
-            if (fonts == null)
-                fonts = CreateFonts().ToArray();
+            if (paragraphs == null)
+                paragraphs = CreateParagraphs().ToArray();
 
             var color = Color.MidnightBlue;
             float lighten = 10;
 
+            float x = 20;
             float y = 20;
-            foreach (var font in fonts)
+            foreach (var paragraph in paragraphs)
             {
-                dc.DrawText(LoremIpsum, new PointF(20, y), font, color);
-                y += dc.MeasureText(LoremIpsum, font).Height + 20;
+                dc.DrawText(paragraph.FontInfo, new PointF(x, y), fontInfoFont, fontInfoColor);
+                y += dc.MeasureText(paragraph.FontInfo, fontInfoFont).Height + 3;
+
+                dc.DrawText(LoremIpsum, new PointF(20, y), paragraph.Font, color);
+                y += dc.MeasureText(LoremIpsum, paragraph.Font).Height + 20;
+
                 var c = new Skybrud.Colors.RgbColor(color.R, color.G, color.B).Lighten(lighten).ToRgb();
                 color = Color.FromArgb(c.R, c.G, c.B);
             }
         }
 
-        private void InvalidateFonts()
+        private void InvalidatParagraphs()
         {
-            if (fonts != null)
+            if (paragraphs != null)
             {
-                foreach (var font in fonts)
-                    font.Dispose();
-                fonts = null;
+                foreach (var paragraph in paragraphs)
+                    paragraph.Dispose();
+                paragraphs = null;
             }
 
             Canvas?.Update();
         }
 
-        private IEnumerable<Alternet.UI.Font> CreateFonts()
+        private IEnumerable<Paragraph> CreateParagraphs()
         {
-            //yield return Control.DefaultFont;
-            yield return new Alternet.UI.Font(FontFamily.GenericSerif, FontSize);
-            yield return new Alternet.UI.Font(FontFamily.GenericSansSerif, FontSize);
-            yield return new Alternet.UI.Font(FontFamily.GenericMonospace, FontSize);
+            Paragraph CreateParagraph(GenericFontFamily genericFamily) =>
+                new Paragraph(
+                    new Font(new FontFamily(genericFamily), FontSize),
+                    genericFamily.ToString());
+
+            yield return CreateParagraph(GenericFontFamily.Serif);
+            yield return CreateParagraph(GenericFontFamily.SansSerif);
+            yield return CreateParagraph(GenericFontFamily.Monospace);
         }
 
         protected override Control CreateSettingsControl() => new TextPageSettings(this);
