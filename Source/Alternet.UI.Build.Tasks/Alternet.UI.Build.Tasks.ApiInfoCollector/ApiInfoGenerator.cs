f1889e3839095f813784e9c812233bf3f25e8cc8
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-09-01 11:38:18 +0300

    In progress.

---
+++ Source/Alternet.UI.Build.Tasks/Alternet.UI.Build.Tasks.ApiInfoCollector/ApiInfoGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Alternet.UI.Build.Tasks.ApiInfoCollector
+{
+    internal static class ApiInfoGenerator
+    {
+        public static XDocument Generate(Assembly assembly)
+        {
+            var document = new XDocument();
+            var root = new XElement("ApiInfo");
+            GenerateAssembly(assembly, root);
+            document.Add(root);
+            return document;
+        }
+
+        private static void GenerateAssembly(Assembly assembly, XElement parentElement)
+        {
+            var assemblyElement = new XElement("Assembly");
+            assemblyElement.Add(new XAttribute("Name", assembly.FullName));
+            foreach (var type in assembly.GetExportedTypes())
+                GenerateType(type, assemblyElement);
+
+            parentElement.Add(assemblyElement);
+        }
+
+        private static void GenerateType(Type type, XElement parentElement)
+        {
+            var typeElement = new XElement("Type");
+            typeElement.Add(new XAttribute("Name", type.FullName));
+            foreach (var @event in type.GetEvents(BindingFlags.Public | BindingFlags.Instance))
+                GenerateEvent(@event, typeElement);
+
+            parentElement.Add(typeElement);
+        }
+
+        private static void GenerateEvent(EventInfo @event, XElement parentElement)
+        {
+            var eventElement = new XElement("Event");
+            eventElement.Add(new XAttribute("Name", @event.Name));
+            parentElement.Add(eventElement);
+        }
+    }
+}
---
+++ Source/Alternet.UI.Build.Tasks/Alternet.UI.Build.Tasks.ApiInfoCollector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Alternet.UI.Build.Tasks.ApiInfoCollector
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Alternet.UI.Build.Tasks.ApiInfoCollector.exe <input-assembly-file-path> <output-xml-file-path>");
+                return 1;
+            }
+
+            try
+            {
+                var xmlDocument = ApiInfoGenerator.Generate(Assembly.LoadFrom(args[0]));
+                
+                var outPath = args[1];
+                var outPathDirectory = Path.GetDirectoryName(outPath);
+                if (!Directory.Exists(outPathDirectory))
+                    Directory.CreateDirectory(outPathDirectory);
+                xmlDocument.Save(outPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unhandled exception: " + e);
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
