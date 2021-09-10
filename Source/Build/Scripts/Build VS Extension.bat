d0c5ad56601c97fe958dfcd60ea2de9a251282f5
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-09-10 11:12:46 +0300

    Revert "Update Build VS Extension.bat"
    This reverts commit 93a7f2fa84b73916ee20a6b739dbb4100f9b1d6d.

--- Source/Build/Scripts/Build VS Extension.bat
+++ Source/Build/Scripts/Build VS Extension.bat
@@ -33,11 +33,7 @@ if not !ERRORLEVEL! EQU 0 (
 set /p FOUND_MSBUILD_PATH_VS_2019= < tmpFile 
 del tmpFile 
 
-"%FOUND_MSBUILD_PATH_VS_2019%" /restore "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-
-"%FOUND_MSBUILD_PATH_VS_2019%" /t:Clean,Build /p:Configuration=Release "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
+"%FOUND_MSBUILD_PATH_VS_2019%" /restore /t:Clean,Build /p:Configuration=Release "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
@@ -47,11 +43,7 @@ if not !ERRORLEVEL! EQU 0 (
 set /p FOUND_MSBUILD_PATH_VS_2019= < tmpFile 
 del tmpFile 
 
-"%FOUND_MSBUILD_PATH_VS_2019%" /restore "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-    
-"%FOUND_MSBUILD_PATH_VS_2019%" /t:Clean,Build /p:Configuration=Release "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
+"%FOUND_MSBUILD_PATH_VS_2019%" /restore /t:Clean,Build /p:Configuration=Release "%SCRIPT_HOME%\..\..\Integration\VisualStudio\Alternet.UI.Integration.VisualStudio\Alternet.UI.Integration.VisualStudio.csproj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
     
