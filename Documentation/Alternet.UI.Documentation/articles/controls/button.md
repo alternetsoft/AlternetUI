61a970d7656f393a6d8f931a2cdc91e693007b9b
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-09-27 21:08:19 +0300

    In progress.

---
+++ Documentation/Alternet.UI.Documentation/articles/controls/button.md
@@ -0,0 +1,1 @@
+# This is a conceptual article about Button
--- Source/Alternet.UI/Controls/Button.cs
+++ Source/Alternet.UI/Controls/Button.cs
@@ -7,6 +7,7 @@ namespace Alternet.UI
     /// <remarks>
     /// A <see cref="Button"/> can be clicked by using the mouse or keyboard if the button has focus.
     /// </remarks>
+    /// <seealso href="~/articles/controls/button">Button Concepts</seealso>
     public class Button : ButtonBase
     {
     }
