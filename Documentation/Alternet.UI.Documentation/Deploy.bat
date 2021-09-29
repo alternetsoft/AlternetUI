2f4b64f2a1ebbd24be173f3241c6e9dd33fd7e4a
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-09-29 18:40:43 +0300

    Update Deploy.bat

--- Documentation/Alternet.UI.Documentation/Deploy.bat
+++ Documentation/Alternet.UI.Documentation/Deploy.bat
@@ -1,4 +1,5 @@
+copy templates\alternet\404-not-found.html site\*.*
 call az login
-#call az storage blob upload-batch --account-name alternetuidocs -s ./site -d $web
+call az storage blob upload-batch --account-name alternetuidocs -s ./site -d $web
 call az cdn endpoint purge -g Alternet -n alternet-ui-docs-cdn --profile-name alternet-ui-docs-cdn --content-paths /*
 
