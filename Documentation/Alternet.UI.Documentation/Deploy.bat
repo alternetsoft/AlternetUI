ee52d7addb16761184c0b2095f10dbf4fc30b81f
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-09-29 15:49:53 +0300

    In progress.

---
+++ Documentation/Alternet.UI.Documentation/Deploy.bat
@@ -0,0 +1,4 @@
+call az login
+#call az storage blob upload-batch --account-name alternetuidocs -s ./site -d $web
+call az cdn endpoint purge -g Alternet -n alternet-ui-docs-cdn --profile-name alternet-ui-docs-cdn --content-paths /*
+
