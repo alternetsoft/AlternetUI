7178b21c1ec71e103677ce24712d29e487c74be2
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-10-21 17:53:31 +0300

    Update Set Build Number.sh

--- Source/Build/Scripts/Set Build Number.sh
+++ Source/Build/Scripts/Set Build Number.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 SCRIPT_HOME=$(cd "$(dirname "$0")"; pwd -P)
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-dotnet run --project "$SCRIPT_HOME/../../Tools/VersionTool/VersionTool.csproj" -- set-build-number $1
+dotnet run --project "$SCRIPT_HOME/../../Tools/Versioning/Alternet.UI.VersionTool.Cli/Alternet.UI.VersionTool.Cli.csproj" -- set-build-number $1
