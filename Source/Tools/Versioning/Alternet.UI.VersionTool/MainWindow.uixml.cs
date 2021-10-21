882790e2ac7cd2aeaad2a0e3f8d884f6c7f3ea83
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-10-21 18:11:18 +0300

    In progress.

---
+++ Source/Tools/Versioning/Alternet.UI.VersionTool/MainWindow.uixml.cs
@@ -0,0 +1,12 @@
+using Alternet.UI;
+
+namespace Alternet.UI.VersionTool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
