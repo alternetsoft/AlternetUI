91fb3a47a05d45c3a39574110ca4fd6cc026423b
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-10-26 17:52:08 +0300

    Update XmlPatcher.cs

--- Source/Tools/Versioning/Alternet.UI.Versioning/Utils/XmlPatcher.cs
+++ Source/Tools/Versioning/Alternet.UI.Versioning/Utils/XmlPatcher.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
@@ -11,8 +13,8 @@ namespace Alternet.UI.Versioning
         public static void PatchAttribute(string filePath, string elementSelector, XName attributeName, string newValue)
         {
             XDocument document;
-            using (var fs = File.OpenRead(filePath))
-                document = XDocument.Load(fs);
+            using (var stream = File.OpenRead(filePath))
+                document = XDocument.Load(stream);
 
             var element = document.XPathSelectElement(elementSelector);
 
@@ -21,7 +23,15 @@ namespace Alternet.UI.Versioning
 
             element.SetAttributeValue(attributeName, newValue);
 
-            document.Save(filePath);
+            using (var writer = XmlWriter.Create(
+                filePath,
+                new XmlWriterSettings
+                {
+                    OmitXmlDeclaration = true,
+                    Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false),
+                    Indent = true
+                }))
+                document.Save(writer);
         }
     }
 }
