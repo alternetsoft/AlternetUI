ace1133d358e3897895998d43ad1190aa700ac77
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2021-11-04 17:39:30 +0200

    In progress.

--- Source/Build/Scripts/Deploy.bat
+++ Source/Build/Scripts/Deploy.bat
@@ -20,6 +20,7 @@ if not !ERRORLEVEL! EQU 0 (
 
 set NuGetApiKey=oy2iknibdcdvlz5ztp3ae34lrw65b5uebhs2dcvfwrlitq
 set GitHubApiKey=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
+set AzureDevOpsAccessToken=4u542gllvtm2sl3zkuquchj6ma4aexme5qykv7dyozkrmz26cora
 
 :: NuGet packages.
 
@@ -38,6 +39,17 @@ if not !ERRORLEVEL! EQU 0 (
 call "%SCRIPT_HOME%\DeployPublicSource.bat" %PublicExamplesSources% %PublicRepo%\Examples %PublicRepo%\ExamplesCommitMessage.txt %GitHubApiKey% alternet-ui-examples
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
+
+:: VS Extensions
+
+set VSIXPublisherTool=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe
+set VSIXPublishManifest2019=%SCRIPT_HOME%\..\..\Integration\VisualStudio\Publish\extension.VS2019.manifest.json
+set VSIXPublishManifest2022=%SCRIPT_HOME%\..\..\Integration\VisualStudio\Publish\extension.VS2022.manifest.json
+set VSIXPackagePath2019=%PublishRoot%\Alternet.UI.Integration.VisualStudio.VS2019*.vsix
+set VSIXPackagePath2022=%PublishRoot%\Alternet.UI.Integration.VisualStudio.VS2022*.vsix
+
+for %%i in (%VSIXPackagePath2019%) do "%VSIXPublisherTool%" publish -payload "%%i" -publishManifest "%VSIXPublishManifest2019%" -personalAccessToken %AzureDevOpsAccessToken%
+for %%i in (%VSIXPackagePath2022%) do "%VSIXPublisherTool%" publish -payload "%%i" -publishManifest "%VSIXPublishManifest2022%" -personalAccessToken %AzureDevOpsAccessToken%
 
 :: Clean Up
 RD /S /Q "%PublicRepo%"
