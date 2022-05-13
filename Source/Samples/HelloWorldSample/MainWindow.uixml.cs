0c9261bb59179699afa493550628056c26615263
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-05-13 12:33:06 +0200

    In progress.

--- Source/Samples/HelloWorldSample/MainWindow.uixml.cs
+++ Source/Samples/HelloWorldSample/MainWindow.uixml.cs
@@ -8,6 +8,11 @@ namespace HelloWorldSample
         public MainWindow()
         {
             InitializeComponent();
+
+            var child = new Window();
+            child.Children.Add(new Button { Text = "Hello!" });
+            child.Owner = this;
+            child.Show();
         }
 
         private void HelloButton_Click(object? sender, EventArgs e)
