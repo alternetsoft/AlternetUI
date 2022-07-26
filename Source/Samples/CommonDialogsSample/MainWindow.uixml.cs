6eddccfa7a70cf69a1c332fe386b8140ec3468f8
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-07-26 21:29:33 +0200

    In progress.

--- Source/Samples/CommonDialogsSample/MainWindow.uixml.cs
+++ Source/Samples/CommonDialogsSample/MainWindow.uixml.cs
@@ -12,7 +12,35 @@ namespace CommonDialogsSample
 
         private void ShowOpenFileDialogButton_Click(object sender, System.EventArgs e)
         {
-            
+            var dialog = new OpenFileDialog();
+            var result = dialog.ShowModal();
+
+            if (result == ModalResult.Accepted)
+                MessageBox.Show("Accepted, FileName = " + dialog.FileName, "Dialog Result");
+            else
+                MessageBox.Show(result.ToString(), "Dialog Result");
+        }
+
+        private void ShowSaveFileDialogButton_Click(object sender, System.EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            var result = dialog.ShowModal();
+
+            if (result == ModalResult.Accepted)
+                MessageBox.Show("Accepted, FileName = " + dialog.FileName, "Dialog Result");
+            else
+                MessageBox.Show(result.ToString(), "Dialog Result");
+        }
+
+        private void ShowSelectDirectoryDialogButton_Click(object sender, System.EventArgs e)
+        {
+            var dialog = new SelectDirectoryDialog();
+            var result = dialog.ShowModal();
+
+            if (result == ModalResult.Accepted)
+                MessageBox.Show("Accepted, FileName = " + dialog.DirectoryName, "Dialog Result");
+            else
+                MessageBox.Show(result.ToString(), "Dialog Result");
         }
     }
 }
