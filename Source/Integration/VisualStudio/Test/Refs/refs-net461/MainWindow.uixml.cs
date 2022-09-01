b5413cd1a073b8e629c6173201fac0ece76b3650
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-09-01 12:11:05 +0200

    Added VS Integration test solution.

---
+++ Source/Integration/VisualStudio/Test/Refs/refs-net461/MainWindow.uixml.cs
@@ -0,0 +1,12 @@
+using Alternet.UI;
+
+namespace test_net461
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
