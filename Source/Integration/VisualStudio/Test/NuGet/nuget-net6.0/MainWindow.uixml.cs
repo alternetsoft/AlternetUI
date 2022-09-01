65e7e4165000ef5811ed995d7691739c5fdfa499
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-09-01 14:46:01 +0200

    More VS Integration Tests

---
+++ Source/Integration/VisualStudio/Test/NuGet/nuget-net6.0/MainWindow.uixml.cs
@@ -0,0 +1,12 @@
+using Alternet.UI;
+
+namespace nuget_net6._0
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Integration/VisualStudio/Test/NuGet/nuget-netcoreapp3.1/MainWindow.uixml.cs
@@ -0,0 +1,12 @@
+using Alternet.UI;
+
+namespace nuget_netcoreapp3._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
