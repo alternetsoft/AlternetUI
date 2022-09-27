4ee55d3e071cbbe52745f23424e90bef6bd4df82
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-09-27 15:49:21 +0200

    In progress.

--- Source/Alternet.UI/Application/ThreadException/ThreadExceptionDetailsWindow.cs
+++ Source/Alternet.UI/Application/ThreadException/ThreadExceptionDetailsWindow.cs
@@ -12,11 +12,16 @@
             // Cannot use UIXML in the Alternet.UI assembly itself, so populate the controls from code.
 
             BeginInit();
+            Title = "Exception Details";
             Padding = new Thickness(10);
             MinimizeEnabled = false;
             MaximizeEnabled = false;
             StartLocation = WindowStartLocation.CenterOwner;
             AlwaysOnTop = true;
+
+            var mainGrid = new Grid();
+            mainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            mainGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
             var detailsTextBox = new TextBox
             {
@@ -24,7 +29,19 @@
                 ReadOnly = true,
                 Multiline = true
             };
-            Children.Add(detailsTextBox);
+            mainGrid.Children.Add(detailsTextBox);
+
+            var closeButton = new Button
+            {
+                Text = "&Close",
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+            closeButton.Click += (o, e) => Close();
+            mainGrid.Children.Add(closeButton);
+            Grid.SetRow(closeButton, 1);
+
+            Children.Add(mainGrid);
             EndInit();
         }
     }
