fec00763d9a5fe6627be9883dc1c65a227928f7d
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-11-09 15:21:36 +0100

    In progress.

--- Source/NativeApi/NativeApi/Api/TreeView/TreeView.cs
+++ Source/NativeApi/NativeApi/Api/TreeView/TreeView.cs
@@ -1,4 +1,5 @@
-﻿using ApiCommon;
+﻿using Alternet.Drawing;
+using ApiCommon;
 using System;
 
 namespace NativeApi.Api
@@ -28,6 +29,38 @@ namespace NativeApi.Api
         public void ClearSelected() => throw new Exception();
 
         public void SetSelected(IntPtr item, bool value) => throw new Exception();
+
+        public bool ShowLines { get; set; }
+
+        public bool ShowRootLines { get; set; }
+
+        public bool ShowExpandButtons { get; set; }
+
+        public IntPtr TopItem { get; }
+
+        public bool FullRowSelect { get; set; }
+
+        public bool AllowLabelEdit { get; set; }
+
+        public void ExpandAll() => throw new Exception();
+        
+        public void CollapseAll() => throw new Exception();
+
+        public IntPtr FocusedItem { get; set; }
+
+        public IntPtr ItemHitTest(Point point) => throw new Exception();
+        public TreeViewHitTestLocations GetHitTestResultLocations(IntPtr hitTestResult) => throw new Exception();
+        public IntPtr GetHitTestResultItem(IntPtr hitTestResult) => throw new Exception();
+        public void FreeHitTestResult(IntPtr hitTestResult) => throw new Exception();
+
+        public bool IsItemSelected(IntPtr item) => throw new Exception();
+
+        public void BeginLabelEdit(IntPtr item) => throw new Exception();
+        public void EndLabelEdit(IntPtr item) => throw new Exception();
+        public void ExpandAllChildren(IntPtr item) => throw new Exception();
+        public void CollapseAllChildren(IntPtr item) => throw new Exception();
+        public void EnsureVisible(IntPtr item) => throw new Exception();
+        public void ScrollIntoView(IntPtr item) => throw new Exception();
 
         public event NativeEventHandler<TreeViewItemEventData>? ItemExpanded { add => throw new Exception(); remove => throw new Exception(); }
         public event NativeEventHandler<TreeViewItemEventData>? ItemCollapsed { add => throw new Exception(); remove => throw new Exception(); }
