c20d6a4da232901068c430cec59b2b9f17023a23
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-12-14 20:14:09 +0100

    In progress.

---
+++ Source/Samples/CustomControlsSample/Gauge/GaugeControl.cs
@@ -0,0 +1,15 @@
+﻿using Alternet.UI;
+using CustomControlsSample.Gauge;
+
+namespace CustomControlsSample
+{
+    public class GaugeControl : ProgressBar
+    {
+        public new GaugeHandler Handler => (GaugeHandler)base.Handler;
+
+        protected override ControlHandler CreateHandler()
+        {
+            return new GaugeHandler();
+        }
+    }
+}
---
+++ Source/Samples/CustomControlsSample/Gauge/GaugeHandler.cs
@@ -0,0 +1,46 @@
+﻿#nullable disable
+
+using Alternet.Drawing;
+using Alternet.UI;
+using System;
+using System.ComponentModel;
+
+namespace CustomControlsSample.Gauge
+{
+    public class GaugeHandler : ProgressBarHandler
+    {
+        protected override bool NeedsPaint => true;
+
+        public override bool IsIndeterminate { get; set; }
+        public override ProgressBarOrientation Orientation { get; set; }
+
+        public override Size GetPreferredSize(Size availableSize)
+        {
+            return new Size(50, 50);
+        }
+
+        protected override void OnAttach()
+        {
+            base.OnAttach();
+            UserPaint = true;
+            Control.ValueChanged += Control_ValueChanged;
+        }
+
+        protected override void OnDetach()
+        {
+            Control.ValueChanged -= Control_ValueChanged;
+
+            base.OnDetach();
+        }
+
+        private void Control_ValueChanged(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        public override void OnPaint(DrawingContext dc)
+        {
+
+        }
+    }
+}
