6e84543dfd2080678c2e7f3014ddbc1410ea50c9
yevgeni-zolotko <yevgeni.zolotko@alternetsoft.com>
2022-12-16 16:24:41 +0100

    In progress.

--- Source/Samples/CustomControlsSample/MainWindow.uixml.cs
+++ Source/Samples/CustomControlsSample/MainWindow.uixml.cs
@@ -13,8 +13,6 @@ namespace CustomControlsSample
             InitializeComponent();
 
             DataContext = new Data();
-
-            //UpdateText();
         }
 
         class Data : INotifyPropertyChanged
@@ -37,60 +35,6 @@ namespace CustomControlsSample
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
-        }
-
-        protected override void OnClosing(WindowClosingEventArgs e)
-        {
-            //if (allowCloseWindowCheckBox == null)
-            //    return;
-
-            //if (!allowCloseWindowCheckBox.IsChecked)
-            //{
-            //    MessageBox.Show("Closing the window is not allowed. Set the check box to allow.", "Closing Not Allowed");
-            //    e.Cancel = true;
-            //}
-
-            base.OnClosing(e);
-        }
-
-        private void Option1RadioButton_CheckedChanged(object? sender, EventArgs e)
-        {
-//            MessageBox.Show(option1RadioButton.IsChecked.ToString(), "Option 1");
-        }
-
-        private void BlueButton_Click(object? sender, EventArgs e)
-        {
-//            SetBrush(Brushes.LightBlue);
-        }
-
-        private void GreenButton_Click(object? sender, EventArgs e)
-        {
-  //          SetBrush(Brushes.LightGreen);
-        }
-
-        private void RedButton_Click(object? sender, EventArgs e)
-        {
-      //      SetBrush(Brushes.Pink);
-        }
-
-        //private void SetBrush(Brush b) => customDrawnControl!.Brush = /*customCompositeControl!.Brush =*/ b;
-
-        private void TextBox_TextChanged(object? sender, TextChangedEventArgs e)
-        {
-            //UpdateText();
-        }
-
-        //private void UpdateText()
-        //{
-        //    if (customDrawnControl == null)
-        //        return;
-
-        //    customDrawnControl!.Text = /*customCompositeControl!.Text =*/ textBox!.Text;
-        //}
-
-        private void ColorPicker_ValueChanged(object sender, System.EventArgs e)
-        {
-            //MessageBox.Show("New Color Value: " + colorPicker.Value);
         }
     }
 }
