03b74dc7f87b8024946c2addaf7b22a79224576d
dmitry-medvedev <64202511+dmitry-medvedev@users.noreply.github.com>
2023-02-27 12:21:35 +1100

    Implement CheckListBox #4

---
+++ Source/NativeApi/NativeApi/Api/CheckListBox.cs
@@ -0,0 +1,16 @@
+﻿using ApiCommon;
+using System;
+
+namespace NativeApi.Api
+{
+    public class CheckListBox : ListBox
+    {
+        public event EventHandler? CheckedChanged { add => throw new Exception(); remove => throw new Exception(); }
+
+        public int[] CheckedIndices { get => throw new Exception(); }
+
+        public void ClearChecked() => throw new Exception();
+
+        public void SetChecked(int index, bool value) => throw new Exception();
+    }
+}
