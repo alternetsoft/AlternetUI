cac145c93f53c0484fcf258769e5e94b4462efdc
generalloki <general.loki.2018@gmail.com>
2023-06-06 14:37:46 +0300

    Implement WebBrowser control #8 (improved setup scripts)

--- Source/Build/Scripts/Build WxWidgets Debug No Download.bat
+++ Source/Build/Scripts/Build WxWidgets Debug No Download.bat
@@ -2,7 +2,7 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-dotnet msbuild /t:BuildWxWidgetsWithoutDownload /p:DebugWxWidgetsBuild=true "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+dotnet msbuild /t:BuildWxWidgetsWithoutDownload /p:DebugOnlyWxWidgetsBuild=true "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
