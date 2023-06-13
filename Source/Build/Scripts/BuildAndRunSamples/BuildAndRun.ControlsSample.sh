61eff00ee1679c3f63662bbb8d746db42a7a3f94
generalloki <general.loki.2018@gmail.com>
2023-06-13 07:22:43 +0300

    Add sh for build and run samples

--- Source/Build/Scripts/BuildAndRunSamples/BuildAndRun.ControlsSample.sh
+++ Source/Build/Scripts/BuildAndRunSamples/BuildAndRun.ControlsSample.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd ../../../Samples/ControlsSample
-dotnet build --property WarningLevel=0
-dotnet run --property WarningLevel=0 --framework net6.0
-popd
-
+./BuildAndRunSample.sh ControlsSample
 
 
---
+++ Source/Build/Scripts/BuildAndRunSamples/BuildAndRun.EmployeeFormSample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
+./BuildAndRunSample.sh EmployeeFormSample
+
+
