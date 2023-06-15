a95adde5d4d342dbaea008a300b7f35f25252945
generalloki <general.loki.2018@gmail.com>
2023-06-15 14:40:47 +0300

    StyleCop related changes

--- Source/Alternet.UI/Controls/WebBrowser/Enums/WebBrowserNavigationAction.cs
+++ Source/Alternet.UI/Controls/WebBrowser/Enums/WebBrowserNavigationAction.cs
@@ -6,28 +6,24 @@ using System.Threading.Tasks;
 
 namespace Alternet.UI
 {
-    
     /// <summary>
     ///     Navigation action types.
     /// </summary>
     public enum WebBrowserNavigationAction
     {
-        
         /// <summary>
         ///     No navigation action.
         /// </summary>
         None,
-        
+
         /// <summary>
         ///     The navigation was started by the user.
         /// </summary>
         User,
-        
+
         /// <summary>
         ///     The navigation was started but not by the user.
         /// </summary>
-        Other
-        
-    };
-    
+        Other,
+    }
 }
