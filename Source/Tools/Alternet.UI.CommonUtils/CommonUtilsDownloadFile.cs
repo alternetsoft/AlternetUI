cb74e00fee5d143a6e1a47344c29479261cc1336
generalloki <general.loki.2018@gmail.com>
2023-06-18 19:56:23 +0300

    DownloadFile with progress action

---
+++ Source/Tools/Alternet.UI.CommonUtils/CommonUtilsDownloadFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public static partial class CommonUtils
+    {
+        public static async Task DownloadFile(string docUrl, string filePath, EventHandler<float> progressChanged)
+        {
+            var client = new HttpClient();
+
+            var progress = new Progress<float>();
+            progress.ProgressChanged += progressChanged;
+
+            using (var file = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                await client.DownloadDataAsync(docUrl, file, progress);
+        }
+
+        public static void DownloadFileProgressChangedToConsole(object sender, float progress)
+        {
+            Console.Write("\r{0}%   ", progress.ToString("0.00"));
+        }
+    }
+
+}
