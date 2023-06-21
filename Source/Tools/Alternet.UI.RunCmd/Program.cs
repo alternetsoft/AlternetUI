5e793474fd7b1e82a020602afb3dd34dc12360e2
generalloki <general.loki.2018@gmail.com>
2023-06-21 10:51:28 +0300

    Added RunCmd project

---
+++ Source/Tools/Alternet.UI.RunCmd/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Alternet.UI;
+
+Console.WriteLine("Alternet.UI.RunCmd");
+Console.WriteLine("Copyright © 2023 AlterNET Software");
+
+CommonUtils.ParseCmdLine(args);
+
+Console.WriteLine($"Arguments: {CommonUtils.ToString(args)}");
+Console.WriteLine();
+
+
+
