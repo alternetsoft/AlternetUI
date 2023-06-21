c75cc94e84ec63cb648537e8e2e6a8df1fac0370
generalloki <general.loki.2018@gmail.com>
2023-06-21 10:53:00 +0300

    Improved installation scripts

--- Install.bat
+++ Install.bat
@@ -2,11 +2,11 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
-    set FOUND_MSBUILD_PATH=%%i)
+:: for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+::    set FOUND_MSBUILD_PATH=%%i)
+::/p:CppMsBuildPath="%FOUND_MSBUILD_PATH%" 
 
-
-dotnet msbuild /p:CppMsBuildPath="%FOUND_MSBUILD_PATH%" /t:InstallAlternetUI "%SCRIPT_HOME%\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+dotnet msbuild /t:InstallAlternetUI "%SCRIPT_HOME%\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
