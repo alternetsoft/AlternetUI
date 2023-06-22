8ad9c9c049b7e9db114ec13a07400ffff3f324a2
generalloki <general.loki.2018@gmail.com>
2023-06-22 13:05:44 +0300

    Update Install.Clean.All.Projects.bat

--- Install.Scripts/Install.Clean.All.Projects.bat
+++ Install.Scripts/Install.Clean.All.Projects.bat
@@ -2,4 +2,4 @@ call "Tools\DoForAllProjects.bat" "Tools\CleanProject.bat"
 
 rmdir /s /q "..\Source\Alternet.UI.Pal\bin"
 rmdir /s /q "..\Source\Alternet.UI.Pal\obj"
-rmdir /s /q "..\Source\Build\Alternet.UI.Pal\bin"
+:: rmdir /s /q "..\Source\Build\Alternet.UI.Pal\bin"
