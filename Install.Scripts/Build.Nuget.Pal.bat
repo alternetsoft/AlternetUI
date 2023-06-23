2691964209b79d6fdbb16a8bdab2e6401d76e28f
generalloki <general.loki.2018@gmail.com>
2023-06-23 08:30:35 +0300

    Improved installation scripts (Nuget related)

--- Install.Scripts/Build.Nuget.Pal.bat
+++ Install.Scripts/Build.Nuget.Pal.bat
@@ -2,7 +2,8 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-dotnet msbuild /t:Nuget "%SCRIPT_HOME%\..\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+dotnet msbuild /p:SignPassword=%1 /t:Nuget "%SCRIPT_HOME%\..\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
