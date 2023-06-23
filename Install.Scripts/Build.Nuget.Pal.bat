4c8fc19236d5bdeee9440e27ca3a8b093236d223
generalloki <general.loki.2018@gmail.com>
2023-06-23 08:11:35 +0300

    Improved installation scripts

--- Install.Scripts/Build.Nuget.Pal.bat
+++ Install.Scripts/Build.Nuget.Pal.bat
@@ -2,7 +2,7 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-dotnet msbuild /t:InstallAlternetUI "%SCRIPT_HOME%\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
+dotnet msbuild /t:Nuget "%SCRIPT_HOME%\..\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
