684e6b2c5963a0412f8aec2452ea44451ce56a45
generalloki <general.loki.2018@gmail.com>
2023-06-24 03:36:31 +0300

    Removed old installation scripts

--- Old/ShAndBat/MSW.1.Download.WxWidgets.Bin.bat
+++
@@ -1,9 +0,0 @@
-SETLOCAL EnableDelayedExpansion
-
-set SCRIPT_HOME=%~dp0.
-
-dotnet msbuild /t:DownloadAndExtractWxWidgetsBin "%SCRIPT_HOME%\..\Alternet.UI.Pal\Alternet.UI.Pal.proj"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-
-exit /b 0
--- Old/ShAndBat/Test.LinuxOrMacOs.Build.WxWidgets.Debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-SCRIPT_HOME=$(cd "$(dirname "$0")"; pwd -P)
-
-# Build
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-dotnet msbuild /t:BuildWxWidgets /p:DebugWxWidgetsBuild=true $SCRIPT_HOME/../Alternet.UI.Pal/Alternet.UI.Pal.proj
