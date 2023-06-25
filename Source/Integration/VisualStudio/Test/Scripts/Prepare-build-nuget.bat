e78d7fa54d66a8de970e388c46e7d0505998a1d5
generalloki <general.loki.2018@gmail.com>
2023-06-25 18:18:38 +0300

    Fixed Vsix compilation problems

--- Source/Integration/VisualStudio/Test/Scripts/Prepare-build-nuget.bat
+++ Source/Integration/VisualStudio/Test/Scripts/Prepare-build-nuget.bat
@@ -9,10 +9,6 @@ dotnet build "%NuGetRoot%\nuget-net461\nuget-net461.csproj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
-dotnet build "%NuGetRoot%\nuget-netcoreapp3.1\nuget-netcoreapp3.1.csproj"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-
 dotnet build "%NuGetRoot%\nuget-net6.0\nuget-net6.0.csproj"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
--- Source/Integration/VisualStudio/Test/Scripts/Prepare.bat
+++ Source/Integration/VisualStudio/Test/Scripts/Prepare.bat
@@ -6,10 +6,6 @@ call "%ScriptHome%\Prepare-net461.bat"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
 
-call "%ScriptHome%\Prepare-netcoreapp3.1.bat"
-if not !ERRORLEVEL! EQU 0 (
-    exit /b !ERRORLEVEL!)
-
 call "%ScriptHome%\Prepare-net6.0.bat"
 if not !ERRORLEVEL! EQU 0 (
     exit /b !ERRORLEVEL!)
