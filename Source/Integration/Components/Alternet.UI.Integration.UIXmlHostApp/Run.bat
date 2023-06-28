6b260e21c671ff75c841b0209f51b84d64e2a5b0
generalloki <general.loki.2018@gmail.com>
2023-06-28 08:20:49 +0300

    Update Run.bat

--- Source/Integration/Components/Alternet.UI.Integration.UIXmlHostApp/Run.bat
+++ Source/Integration/Components/Alternet.UI.Integration.UIXmlHostApp/Run.bat
@@ -1,3 +1,3 @@
-pushd bin\Debug\net461\
+pushd bin\Debug\net462\
 Alternet.UI.Integration.UIXmlHostApp.exe --transport tcp-bson://127.0.0.1:30243/ --session-id 123 --method alternet-ui-remote MyApp.exe
 popd
