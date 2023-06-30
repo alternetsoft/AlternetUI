7a7bac129a7ed346381637614198eb6bce56fb60
generalloki <general.loki.2018@gmail.com>
2023-06-30 07:09:08 +0300

    Update BuildAndRunSample.sh

--- Install.Scripts/BuildAndRunSamples/BuildAndRunSample.sh
+++ Install.Scripts/BuildAndRunSamples/BuildAndRunSample.sh
@@ -2,7 +2,14 @@
 set -euo pipefail
 
 pushd ../../Source/Samples/$1
-#dotnet clean
-dotnet build --nologo --property WarningLevel=0
+
+
+rm -rf bin/Debug/*.*
+rm -rf bin/Release/*.*
+rm -rf obj/Debug/*.*
+rm -rf obj/Release/*.*
+rm -rf obj/*.*
+
+dotnet build --nologo --property WarningLevel=0 --framework net6.0
 dotnet run --nologo --property WarningLevel=0 --framework net6.0
 popd
