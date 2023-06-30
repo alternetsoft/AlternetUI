ac52c21b768c34fbd1b1067e2cd978d2d7272ff0
generalloki <general.loki.2018@gmail.com>
2023-06-30 14:27:49 +0300

    Update Program.cs

--- Source/Tools/Alternet.UI.RunCmd/Program.cs
+++ Source/Tools/Alternet.UI.RunCmd/Program.cs
@@ -54,3 +54,60 @@ if (CommonUtils.CmdLineExecCommands == "waitEnter")
     Console.ReadLine();
     return;
 }
+
+
+void DeleteBinObjFiles(string path)
+{
+    path = Path.GetFullPath(path);
+
+    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".csproj") || s.EndsWith(".vcxproj"));
+
+    foreach (string projFile in files)
+    {
+        var projPath = Path.GetDirectoryName(projFile);
+
+        if(Path.GetFileName(projFile) == "Alternet.UI.RunCmd.csproj")
+        {
+            continue;
+        }
+
+        var projPathBin = Path.Combine(projPath!, "bin");
+        var projPathObj = Path.Combine(projPath!, "obj");
+        var filesToDelete = new List<string>();
+
+        if (Directory.Exists(projPathBin))
+        {
+            var projPathBinFiles = Directory.EnumerateFiles(projPathBin, "*.*", SearchOption.AllDirectories);
+            filesToDelete.AddRange(projPathBinFiles);
+        }
+        
+        if (Directory.Exists(projPathObj))
+        {
+            var projPathObjFiles = Directory.EnumerateFiles(projPathObj, "*.*", SearchOption.AllDirectories);
+            filesToDelete.AddRange(projPathObjFiles);
+        }
+        
+        foreach (var s in filesToDelete)
+        {
+            Console.WriteLine("Deleting file: " + s);
+            try
+            {
+                File.Delete(s);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("WARNING. Error deleting file: " + s);
+            }
+        }
+            
+    }
+}
+
+if (CommonUtils.CmdLineExecCommands == "deleteBinFolders")
+{
+    string path = Path.Combine(
+        CommonUtils.GetAppFolder(),"..", "..", "..", "..", "..");
+    DeleteBinObjFiles(path);
+    return;
+}
