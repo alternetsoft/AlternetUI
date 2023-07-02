2a6cd9fe696fe3fbd1ec002ff8b8f2358e21023f
generalloki <general.loki.2018@gmail.com>
2023-07-02 08:01:07 +0300

    Fixed Border control painting on Linux

--- Source/Alternet.UI/Controls/Border.cs
+++ Source/Alternet.UI/Controls/Border.cs
@@ -5,15 +5,57 @@ namespace Alternet.UI
     /// <summary>
     /// Draws a border, background, or both around another control.
     /// </summary>
-    public class Border : Control
+    public class Border : UserPaintControl
     {
+        private static Pen? defaultBorderPen = null;
+        private static double defaultBorderWidth = 1;
+        private static Color defaultBorderColor = Color.Gray;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Border"/> class.
         /// </summary>
         public Border()
         {
-            BorderBrush = Brushes.Gray;
-            Padding = new Thickness(2);
+            Padding = new Thickness(defaultBorderWidth);
+        }
+
+        public static Pen? DefaultBorderPen
+        {
+            get => defaultBorderPen;
+            set => defaultBorderPen = value;
+        }
+
+        public static double DefaultBorderWidth
+        {
+            get => defaultBorderWidth;
+            set
+            {
+                defaultBorderWidth = value;
+                if (defaultBorderPen != null)
+                    defaultBorderPen.Width = value;
+            }
+        }
+
+        public static Color DefaultBorderColor
+        {
+            get => defaultBorderColor;
+            set
+            {
+                defaultBorderColor = value;
+                if (defaultBorderPen != null)
+                    defaultBorderPen.Color = value;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (defaultBorderPen == null)
+                defaultBorderPen = new Pen(defaultBorderColor, defaultBorderWidth);
+
+            base.OnPaint(e);
+            var dc = e.DrawingContext;
+
+            dc.DrawRectangle(defaultBorderPen, ClientRectangle);
         }
     }
 }
---
+++ Source/Alternet.UI/Controls/UserPaintControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    public class UserPaintControl : Control
+    {
+        public UserPaintControl()
+            : base()
+        {
+            UserPaint = true;
+        }
+
+        public virtual Rect ClientRectangle
+        {
+            get
+            {
+                var size = ClientSize;
+                return new (0,0, size.Width-1,size.Height-1);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+        }
+    }
+
+}
