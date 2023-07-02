cba18674e1c0ce3a338ac61cbfa888aa3fc61c18
generalloki <general.loki.2018@gmail.com>
2023-07-02 08:01:14 +0300

    Update CustomDrawControl.cs

--- Source/Samples/ControlsTest/CustomDrawControl.cs
+++ Source/Samples/ControlsTest/CustomDrawControl.cs
@@ -9,56 +9,32 @@ using Alternet.UI;
 
 namespace ControlsTest
 {
-    public class CustomDrawControl : Control
+    public class CustomDrawControl : UserPaintControl
     {
+        private readonly Control leftControl = new();
+        private readonly Control fillControl = new();
+
         public CustomDrawControl()
+            : base()
         {
+            leftControl.Bounds = new(0, 0, 50, 50);
+            fillControl.Bounds = new(0, 60, 50, 50);
+            leftControl.Background = new SolidBrush(Color.Green);
+            fillControl.Background = new SolidBrush(Color.Red);
         }
 
-        protected override ControlHandler CreateHandler()
+        protected override void OnPaint(PaintEventArgs e)
         {
-            return new CustomHandler();
+            var dc = e.DrawingContext;
+            var bounds = ClientRectangle;
+            LayoutFactory.PerformLayoutLeftFill(this, leftControl, fillControl);
+
+            var brush = this.Background;
+            if (brush != null)
+                dc.FillRectangle(brush, bounds);
+            dc.FillRectangle(leftControl.Background!, leftControl.Bounds);
+            dc.FillRectangle(fillControl.Background!, fillControl.Bounds);
         }
 
-        public class CustomHandler : ControlHandler<CustomDrawControl>
-        {
-            private readonly Control leftControl = new ();
-            private readonly Control fillControl = new ();
-
-            public CustomHandler()
-                : base()
-            {
-                leftControl.Bounds = new (0, 0, 50, 50);
-                fillControl.Bounds = new (0, 0, 50, 50);
-                leftControl.Background = new SolidBrush(Color.Green);
-                fillControl.Background = new SolidBrush(Color.Red);
-            }
-
-            protected override bool NeedsPaint => true;
-
-            public override void OnPaint(DrawingContext dc)
-            {
-                var bounds = ClientRectangle;
-                LayoutFactory.PerformLayoutLeftFill(Control, leftControl, fillControl);
-
-                var brush = Control.Background;
-                if (brush != null)
-                    dc.FillRectangle(brush, bounds);
-                dc.FillRectangle(leftControl.Background!, leftControl.Bounds);
-                dc.FillRectangle(fillControl.Background!, fillControl.Bounds);
-            }
-
-            protected override void OnAttach()
-            {
-                base.OnAttach();
-
-                UserPaint = true;
-            }
-
-            protected override void OnDetach()
-            {
-                base.OnDetach();
-            }
-        }
     }
 }
