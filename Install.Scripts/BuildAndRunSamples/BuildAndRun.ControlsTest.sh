794a762d4894c964d89d5f3236bd1b9400f554f2
generalloki <general.loki.2018@gmail.com>
2023-07-05 19:40:21 +0300

    Update BuildAndRun.ControlsTest.sh

--- Install.Scripts/BuildAndRunSamples/BuildAndRun.ControlsTest.sh
+++ Install.Scripts/BuildAndRunSamples/BuildAndRun.ControlsTest.sh
@@ -2,6 +2,13 @@
 set -euo pipefail
 
 pushd ../../Source/Samples/ControlsTest
+
+rm -rf bin/Debug/*.*
+rm -rf bin/Release/*.*
+rm -rf obj/Debug/*.*
+rm -rf obj/Release/*.*
+rm -rf obj/*.*
+
 dotnet build --property WarningLevel=0
 dotnet run --property WarningLevel=0 --framework net6.0
 popd
