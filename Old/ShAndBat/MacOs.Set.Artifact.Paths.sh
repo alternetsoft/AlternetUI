aa1f2398d71141784c284f6fd3fb7f7262b9e41e
generalloki <general.loki.2018@gmail.com>
2023-07-08 16:42:16 +0300

    Removed old install scripts which are no more needed

--- Old/ShAndBat/MacOs.Set.Artifact.Paths.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-SCRIPT_HOME=$(cd "$(dirname "$0")"; pwd -P)
-
-export UI_ARTIFACTS_PATH=$SCRIPT_HOME/../../../Publish/Artifacts/Build/macOS/UI/
-export UI_PAL_ARTIFACTS_PATH="$UI_ARTIFACTS_PATH/PAL"
