e6e27bba6ab9fcaa9e9df9aecf66a9010e40d194
generalloki <general.loki.2018@gmail.com>
2023-07-18 10:48:35 +0300

    StyleCop for Thickness.cs

--- Source/Alternet.UI/Thickness.cs
+++ Source/Alternet.UI/Thickness.cs
@@ -1,12 +1,14 @@
 using System;
-using Alternet.Drawing;
 using System.Globalization;
+using Alternet.Drawing;
 
 namespace Alternet.UI
 {
     /// <summary>
-    /// Thickness is a value type used to describe the thickness of frame around a rectangle.
-    /// It contains four doubles each corresponding to a side: Left, Top, Right, Bottom.
+    /// Thickness is a value type used to describe the thickness of frame around
+    /// a rectangle.
+    /// It contains four doubles each corresponding to a side:
+    /// Left, Top, Right, Bottom.
     /// </summary>
     public struct Thickness : IEquatable<Thickness>
     {
@@ -28,7 +30,8 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// This constructor builds a Thickness with the specified number of pixels on each side.
+        /// This constructor builds a Thickness with the specified number of
+        /// pixels on each side.
         /// </summary>
         /// <param name="left">The thickness for the left side.</param>
         /// <param name="top">The thickness for the top side.</param>
@@ -54,52 +57,76 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Gets the combined padding information in the form of a <see cref="Drawing.Size"/>.
+        ///     Returns whether all values on every side are equal.
         /// </summary>
-        /// <value>A <see cref="Drawing.Size"/> containing the padding information.</value>
+        public readonly bool IsUniform =>
+            (left == right) && (top == bottom) && (left == top);
+
+        /// <summary>
+        ///     Returns whether all values on every side are positive
+        ///     (greater than 0).
+        /// </summary>
+        public readonly bool IsPositive =>
+            (left > 0) && (right > 0) && (top > 0) && (bottom > 0);
+
+        /// <summary>
+        /// Gets the combined padding information in the form of a
+        /// <see cref="Drawing.Size"/>.
+        /// </summary>
+        /// <value>A <see cref="Drawing.Size"/> containing the padding
+        /// information.</value>
         /// <remarks>
-        /// The <see cref="Horizontal"/> property corresponds to the <see cref="Size.Width"/> property,
-        /// and the <see cref="Vertical"/> property corresponds to the <see cref="Size.Height"/> property.
+        /// The <see cref="Horizontal"/> property corresponds to the
+        /// <see cref="Size.Width"/> property,
+        /// and the <see cref="Vertical"/> property corresponds to the
+        /// <see cref="Size.Height"/> property.
         /// </remarks>
-        public Size Size => new Size(Horizontal, Vertical);
+        public readonly Size Size => new(Horizontal, Vertical);
 
         /// <summary>
         /// Gets the combined padding for the right and left edges.
         /// </summary>
-        /// <value>Gets the sum, of the <see cref="Left"/> and <see cref="Right"/> padding values.</value>
-        public double Horizontal => left + right;
+        /// <value>Gets the sum, of the <see cref="Left"/> and
+        /// <see cref="Right"/> padding values.</value>
+        public readonly double Horizontal => left + right;
 
         /// <summary>
         /// Gets the combined padding for the top and bottom edges.
         /// </summary>
-        /// <value>Gets the sum, of the <see cref="Top"/> and <see cref="Bottom"/> padding values.</value>
-        public double Vertical => top + bottom;
+        /// <value>Gets the sum, of the <see cref="Top"/> and
+        /// <see cref="Bottom"/> padding values.</value>
+        public readonly double Vertical => top + bottom;
 
-        /// <summary>This property is the Length on the thickness' left side</summary>
+        /// <summary>
+        /// This property is the Length on the thickness' left side
+        /// </summary>
         public double Left
         {
-            get { return left; }
+            readonly get { return left; }
             set { left = value; }
         }
 
         /// <summary>This property is the Length on the thickness' top side</summary>
         public double Top
         {
-            get { return top; }
+            readonly get { return top; }
             set { top = value; }
         }
 
-        /// <summary>This property is the Length on the thickness' right side</summary>
+        /// <summary>
+        /// This property is the Length on the thickness' right side
+        /// </summary>
         public double Right
         {
-            get { return right; }
+            readonly get { return right; }
             set { right = value; }
         }
 
-        /// <summary>This property is the Length on the thickness' bottom side</summary>
+        /// <summary>This property is the Length on the thickness' bottom
+        /// side</summary>
         public double Bottom
         {
-            get { return bottom; }
+            readonly get { return bottom; }
             set { bottom = value; }
         }
 
@@ -108,15 +135,43 @@ namespace Alternet.UI
         /// </summary>
         /// <param name="t1">first Thickness to compare</param>
         /// <param name="t2">second Thickness to compare</param>
-        /// <returns>True if all sides of the Thickness are equal, false otherwise</returns>
-        //  SEEALSO
+        /// <returns>True if all sides of the Thickness are equal, false
+        /// otherwise</returns>
         public static bool operator ==(Thickness t1, Thickness t2)
         {
-            return ((t1.left == t2.left || (double.IsNaN(t1.left) && double.IsNaN(t2.left)))
-                    && (t1.top == t2.top || (double.IsNaN(t1.top) && double.IsNaN(t2.top)))
-                    && (t1.right == t2.right || (double.IsNaN(t1.right) && double.IsNaN(t2.right)))
-                    && (t1.bottom == t2.bottom || (double.IsNaN(t1.bottom) && double.IsNaN(t2.bottom)))
-                    );
+            static bool EqualOrNaN(double a1, double a2)
+            {
+                var result = a1 == a2 || (double.IsNaN(a1) && double.IsNaN(a2));
+                return result;
+            }
+
+            if (!EqualOrNaN(t1.left, t2.left))
+                return false;
+            if (!EqualOrNaN(t1.right, t2.right))
+                return false;
+            if (!EqualOrNaN(t1.top, t2.top))
+                return false;
+            return EqualOrNaN(t1.bottom, t2.bottom);
+        }
+
+        /// <summary>
+        /// Overloaded operator to compare two Thicknesses for inequality.
+        /// </summary>
+        /// <param name="t1">first Thickness to compare</param>
+        /// <param name="t2">second Thickness to compare</param>
+        /// <returns>False if all sides of the Thickness are equal, true
+        /// otherwise</returns>
+        public static bool operator !=(Thickness t1, Thickness t2)
+        {
+            return !(t1 == t2);
+        }
+
+        public void Inflate(double value)
+        {
+            left += value;
+            top += value;
+            right += value;
+            bottom += value;
         }
 
         /// <summary>
@@ -141,61 +196,46 @@ namespace Alternet.UI
         {
             value = new Thickness();
 
-            using (var tokenizer = new StringTokenizer(s, CultureInfo.InvariantCulture))
+            using var tokenizer =
+                new StringTokenizer(s, CultureInfo.InvariantCulture);
+            if (tokenizer.TryReadSingle(out var a))
             {
-                if (tokenizer.TryReadSingle(out var a))
+                if (tokenizer.TryReadSingle(out var b))
                 {
-                    if (tokenizer.TryReadSingle(out var b))
+                    if (tokenizer.TryReadSingle(out var c))
                     {
-                        if (tokenizer.TryReadSingle(out var c))
+                        if (tokenizer.TryReadSingle(out var d))
                         {
-                            if (tokenizer.TryReadSingle(out var d))
-                            {
-                                value = new Thickness(a, b, c, d);
-                                return true;
-                            }
-
-                            return false;
+                            value = new Thickness(a, b, c, d);
+                            return true;
                         }
 
-                        value = new Thickness(a, b);
-                        return true;
+                        return false;
                     }
 
-                    value = new Thickness(a);
+                    value = new Thickness(a, b);
                     return true;
                 }
 
-                return false;
+                value = new Thickness(a);
+                return true;
             }
+
+            return false;
         }
 
         /// <summary>
-        /// Overloaded operator to compare two Thicknesses for inequality.
-        /// </summary>
-        /// <param name="t1">first Thickness to compare</param>
-        /// <param name="t2">second Thickness to compare</param>
-        /// <returns>False if all sides of the Thickness are equal, true otherwise</returns>
-        //  SEEALSO
-        public static bool operator !=(Thickness t1, Thickness t2)
-        {
-            return (!(t1 == t2));
-        }
-
-        /// <summary>
-        /// This function compares to the provided object for type and value equality.
+        /// This function compares to the provided object for type and
+        /// value equality.
         /// </summary>
         /// <param name="obj">Object to compare</param>
-        /// <returns>True if object is a Thickness and all sides of it are equal to this Thickness'.</returns>
-        public override bool Equals(object? obj)
+        /// <returns>True if object is a Thickness and all sides of it are equal
+        /// to this Thickness.</returns>
+        public override readonly bool Equals(object? obj)
         {
-            if (obj is Thickness)
-            {
-                Thickness otherObj = (Thickness)obj;
-                return (this == otherObj);
-            }
-
-            return (false);
+            if (obj is Thickness otherObj)
+                return this == otherObj;
+            return false;
         }
 
         /// <summary>
@@ -204,9 +244,9 @@ namespace Alternet.UI
         /// <param name="thickness">Thickness instance to compare.</param>
         /// <returns><c>true</c>if this Thickness instance has the same value
         /// and unit type as thickness.</returns>
-        public bool Equals(Thickness thickness)
+        public readonly bool Equals(Thickness thickness)
         {
-            return (this == thickness);
+            return this == thickness;
         }
 
         /// <summary>
@@ -215,14 +255,15 @@ namespace Alternet.UI
         /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
-            return left.GetHashCode() ^ top.GetHashCode() ^ right.GetHashCode() ^ bottom.GetHashCode();
+            return left.GetHashCode() ^ top.GetHashCode() ^
+                right.GetHashCode() ^ bottom.GetHashCode();
         }
 
         /// <summary>
         /// Converts this Thickness object to a string.
         /// </summary>
         /// <returns>String conversion.</returns>
-        public override string ToString()
+        public override readonly string ToString()
         {
             return $"({left}, {top}, {right}, {bottom})";
         }
