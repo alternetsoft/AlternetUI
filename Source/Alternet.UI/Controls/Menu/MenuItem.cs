e18e37d6f7b704f54484a7c9f4b34d598290a64c
generalloki <general.loki.2018@gmail.com>
2023-07-27 10:12:48 +0300

    StyleCop

--- Source/Alternet.UI/Controls/Menu/MenuItem.cs
+++ Source/Alternet.UI/Controls/Menu/MenuItem.cs
@@ -8,39 +8,84 @@ namespace Alternet.UI
     public class MenuItem : Menu, ICommandSource
     {
         /// <summary>
+        /// Defines a <see cref="DependencyProperty"/> field for the
+        /// <see cref="Command"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                "Command",
+                typeof(ICommand),
+                typeof(MenuItem),
+                new FrameworkPropertyMetadata(
+                    null,
+                    new PropertyChangedCallback(OnCommandChanged)));
+
+        /// <summary>
+        /// Defines a <see cref="DependencyProperty"/> field for the
+        /// <see cref="CommandParameter"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register(
+                "CommandParameter",
+                typeof(object),
+                typeof(MenuItem),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Defines a <see cref="DependencyProperty"/> field for the
+        /// <see cref="CommandTarget"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register(
+                "CommandTarget",
+                typeof(IInputElement),
+                typeof(MenuItem),
+                new PropertyMetadata(null));
+
+        private KeyGesture? shortcut;
+        private string text = string.Empty;
+        private MenuItemRole? role;
+        private bool preCommandEnabledValue = true;
+        private bool @checked;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref='MenuItem'/> class.
         /// </summary>
-        public MenuItem() :
-            this(string.Empty)
+        public MenuItem()
+            : this(string.Empty)
         {
-
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref='MenuItem'/> class with the specified text for the menu item.
+        /// Initializes a new instance of the <see cref='MenuItem'/> class with
+        /// the specified text for the menu item.
         /// </summary>
-        public MenuItem(string text) :
-            this(text, null, null)
+        public MenuItem(string text)
+            : this(text, null, null)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref='MenuItem'/> class with the specified text for the menu item.
+        /// Initializes a new instance of the <see cref='MenuItem'/> class with
+        /// the specified text for the menu item.
         /// </summary>
-        public MenuItem(string text, KeyGesture shortcut) :
-            this(text, null, shortcut)
+        public MenuItem(string text, KeyGesture shortcut)
+            : this(text, null, shortcut)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref='MenuItem'/> class with the specified text and action for the menu item.
+        /// Initializes a new instance of the <see cref='MenuItem'/> class with
+        /// the specified text and action for the menu item.
         /// </summary>
-        public MenuItem(string text, EventHandler onClick) :
-            this(text, onClick, null)
+        public MenuItem(string text, EventHandler onClick)
+            : this(text, onClick, null)
         {
         }
+
         /// <summary>
-        /// Initializes a new instance of the <see cref='MenuItem'/> class with a specified properties for the menu item.
+        /// Initializes a new instance of the <see cref='MenuItem'/> class with
+        /// a specified properties for the menu item.
         /// </summary>
         public MenuItem(string text, EventHandler? onClick, KeyGesture? shortcut)
         {
@@ -50,21 +95,33 @@ namespace Alternet.UI
                 Click += onClick;
         }
 
-        /// <inheritdoc />
-        public override void RaiseClick(EventArgs e)
-        {
-            base.RaiseClick(e);
-            CommandHelpers.ExecuteCommandSource(this);
-        }
+        /// <summary>
+        /// Occurs when the <see cref="Text"/> property changes.
+        /// </summary>
+        public event EventHandler? TextChanged;
 
-        string text = string.Empty;
+        /// <summary>
+        /// Occurs when the <see cref="Shortcut"/> property changes.
+        /// </summary>
+        public event EventHandler? ShortcutChanged;
+
+        /// <summary>
+        /// Occurs when the <see cref="Checked"/> property changes.
+        /// </summary>
+        public event EventHandler? CheckedChanged;
+
+        /// <summary>
+        /// Occurs when the <see cref="Role"/> property changes.
+        /// </summary>
+        public event EventHandler? RoleChanged;
 
         /// <summary>
         /// Gets or sets a value indicating the caption of the menu item.
         /// </summary>
         /// <remarks>
         /// Setting this property to "-" makes the item a separator.
-        /// Use underscore (<c>_</c>) symbol before a character to make it an access key.
+        /// Use underscore (<c>_</c>) symbol before a character to make it
+        /// an access key.
         /// </remarks>
         public string Text
         {
@@ -87,14 +144,8 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Occurs when the <see cref="Text"/> property changes.
-        /// </summary>
-        public event EventHandler? TextChanged;
-
-        KeyGesture? shortcut;
-
-        /// <summary>
-        /// Gets or sets a value indicating the shortcut key associated with the menu item.
+        /// Gets or sets a value indicating the shortcut key associated with
+        /// the menu item.
         /// </summary>
         public KeyGesture? Shortcut
         {
@@ -117,24 +168,21 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Occurs when the <see cref="Shortcut"/> property changes.
-        /// </summary>
-        public event EventHandler? ShortcutChanged;
-
-        MenuItemRole? role;
-
-        /// <summary>
-        /// Gets or sets this menu item's role, which is used to automate macOS-specific standard items layout.
+        /// Gets or sets this menu item's role, which is used to automate
+        /// macOS-specific standard items layout.
         /// </summary>
         /// <remarks>
         /// <para>
-        /// Menu items roles provide a mechanism for automatically adjusting certain menu items to macOS conventions.
-        /// For example, "About", "Exit", and "Preferences" items should be placed in the application menu on macOS, and have a standard shortcuts.
+        /// Menu items roles provide a mechanism for automatically adjusting
+        /// certain menu items to macOS conventions.
+        /// For example, "About", "Exit", and "Preferences" items should be
+        /// placed in the application menu on macOS, and have a standard shortcuts.
         /// For more information, see <see cref="MenuItemRoles"/> class members.
         /// </para>
         /// <para>
         /// Setting <see cref="Role"/> to <see langword="null"/>
-        /// has the same effect as the <see cref="MenuItemRoles.Auto"/> value. The <see langword="null"/> is the default value of this property.
+        /// has the same effect as the <see cref="MenuItemRoles.Auto"/> value.
+        /// The <see langword="null"/> is the default value of this property.
         /// </para>
         /// </remarks>
         public MenuItemRole? Role
@@ -158,14 +206,8 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Occurs when the <see cref="Role"/> property changes.
-        /// </summary>
-        public event EventHandler? RoleChanged;
-
-        bool @checked;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether a check mark appears next to the text of the menu item.
+        /// Gets or sets a value indicating whether a check mark appears next
+        /// to the text of the menu item.
         /// </summary>
         public bool Checked
         {
@@ -194,16 +236,39 @@ namespace Alternet.UI
             set { SetValue(CommandProperty, value); }
         }
 
-        /// <summary>
-        /// Defines a <see cref="DependencyProperty"/> field for the <see cref="Command"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty CommandProperty =
-            DependencyProperty.Register("Command", typeof(ICommand), typeof(MenuItem), new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnCommandChanged)));
+        /// <inheritdoc/>
+        public object? CommandParameter
+        {
+            get { return (object?)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
 
-        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        /// <inheritdoc/>
+        public IInputElement? CommandTarget
+        {
+            get { return (IInputElement?)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+
+        /// <inheritdoc />
+        public override void RaiseClick(EventArgs e)
+        {
+            base.RaiseClick(e);
+            CommandHelpers.ExecuteCommandSource(this);
+        }
+
+        /// <inheritdoc/>
+        protected override ControlHandler CreateHandler()
+        {
+            return GetEffectiveControlHandlerHactory().CreateMenuItemHandler(this);
+        }
+
+        private static void OnCommandChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
         {
            MenuItem b = (MenuItem)d;
-            b.OnCommandChanged((ICommand)e.OldValue, (ICommand)e.NewValue);
+           b.OnCommandChanged((ICommand)e.OldValue, (ICommand)e.NewValue);
         }
 
         private void OnCommandChanged(ICommand oldCommand, ICommand newCommand)
@@ -227,11 +292,11 @@ namespace Alternet.UI
             }
         }
 
-        bool preCommandEnabledValue = true;
-
         private void UnhookCommand(ICommand command)
         {
-            CanExecuteChangedEventManager.RemoveHandler(command, OnCanExecuteChanged);
+            CanExecuteChangedEventManager.RemoveHandler(
+                command,
+                OnCanExecuteChanged);
             UpdateCanExecute();
         }
 
@@ -250,43 +315,6 @@ namespace Alternet.UI
         {
             if (Command != null)
                 Enabled = CommandHelpers.CanExecuteCommandSource(this);
-        }
-
-        /// <inheritdoc/>
-        public object? CommandParameter
-        {
-            get { return (object?)GetValue(CommandParameterProperty); }
-            set { SetValue(CommandParameterProperty, value); }
-        }
-
-        /// <summary>
-        /// Defines a <see cref="DependencyProperty"/> field for the <see cref="CommandParameter"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty CommandParameterProperty =
-            DependencyProperty.Register("CommandParameter", typeof(object), typeof(MenuItem), new PropertyMetadata(null));
-
-        /// <inheritdoc/>
-        public IInputElement? CommandTarget
-        {
-            get { return (IInputElement?)GetValue(CommandTargetProperty); }
-            set { SetValue(CommandTargetProperty, value); }
-        }
-
-        /// <summary>
-        /// Defines a <see cref="DependencyProperty"/> field for the <see cref="CommandTarget"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty CommandTargetProperty =
-            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(MenuItem), new PropertyMetadata(null));
-
-        /// <summary>
-        /// Occurs when the <see cref="Checked"/> property changes.
-        /// </summary>
-        public event EventHandler? CheckedChanged;
-
-        /// <inheritdoc/>
-        protected override ControlHandler CreateHandler()
-        {
-            return GetEffectiveControlHandlerHactory().CreateMenuItemHandler(this);
         }
     }
 }
