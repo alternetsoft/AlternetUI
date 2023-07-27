ed0e1fd2a0e20ad7b9a49fbfb03d384aca239991
generalloki <general.loki.2018@gmail.com>
2023-07-27 08:17:03 +0300

    StyleCop

--- Source/Alternet.UI/Controls/Grid/GridUnitType.cs
+++ Source/Alternet.UI/Controls/Grid/GridUnitType.cs
@@ -3,19 +3,22 @@
 namespace Alternet.UI
 {
     /// <summary>
-    /// GridUnitType enum is used to indicate what kind of value the 
+    /// GridUnitType enum is used to indicate what kind of value the
     /// GridLength is holding.
     /// </summary>
     public enum GridUnitType
     {
         /// <summary>
-        /// The value indicates that content should be calculated without constraints. 
+        /// The value indicates that content should be calculated
+        /// without constraints.
         /// </summary>
         Auto = 0,
+
         /// <summary>
         /// The value is expressed as a pixel.
         /// </summary>
         Pixel,
+
         /// <summary>
         /// The value is expressed as a weighted proportion of available space.
         /// </summary>
