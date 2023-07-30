38e59fb0add8df9ded709c1019c7b36fe0907a52
generalloki <general.loki.2018@gmail.com>
2023-07-30 17:01:12 +0300

    Added Multiline TextBox to ControlsSample

--- Source/NativeApi/NativeApi/Api/TextBox.cs
+++ Source/NativeApi/NativeApi/Api/TextBox.cs
@@ -1,13 +1,16 @@
-﻿using ApiCommon;
+﻿#pragma warning disable
+using ApiCommon;
 using System;
 
 namespace NativeApi.Api
 {
     public class TextBox : Control
     {
-        public event EventHandler? TextChanged { add => throw new Exception(); remove => throw new Exception(); }
+        public event EventHandler? TextChanged { 
+            add => throw new Exception(); remove => throw new Exception(); }
 
-        public string Text { get => throw new Exception(); set => throw new Exception(); }
+        public string Text { 
+            get => throw new Exception(); set => throw new Exception(); }
 
         public bool EditControlOnly { get; set; }
 
