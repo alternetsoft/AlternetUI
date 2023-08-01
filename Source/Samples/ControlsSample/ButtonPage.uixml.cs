108b42a092e9d3bbc53e7e180f92ac22da77550a
generalloki <general.loki.2018@gmail.com>
2023-08-01 19:42:18 +0300

    Improved ControlsSample/Button page
    Added ComboBoxes for Font Name, Font Size, Text Color, back Color

--- Source/Samples/ControlsSample/ButtonPage.uixml.cs
+++ Source/Samples/ControlsSample/ButtonPage.uixml.cs
@@ -1,4 +1,8 @@
-﻿using Alternet.UI;
+﻿using Alternet.Drawing;
+using Alternet.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ControlsSample
 {
@@ -14,6 +18,16 @@ namespace ControlsSample
             ApplyDisabled();
             ApplyImage();
             ApplyDefault();
+
+            comboBoxFontName.Items.AddRange(FontFamily.FamiliesNamesAscending);
+
+            comboBoxFontSize.Items.AddRange(
+                new object[] { 8, 9, 10, 11, 12, 14, 18, 24, 30, 36, 48, 60, 72, 96 });
+
+            var knownColors = Color.GetKnownColors();
+            var colorsNames = knownColors.Select(x => x.Name).ToArray();
+            comboBoxTextColor.Items.AddRange(colorsNames);
+            comboBoxBackColor.Items.AddRange(colorsNames);
         }
 
         public IPageSite? Site
@@ -47,10 +61,7 @@ namespace ControlsSample
             object? sender,
             System.EventArgs e)
         {
-            if(boldCheckBox.IsChecked)
-                button.Font = Control.DefaultFont.AsBold;
-            else
-                button.Font = Control.DefaultFont;
+            ApplyFont();
         }
 
         private void ApplyDisabled()
@@ -67,7 +78,8 @@ namespace ControlsSample
         {
             if (button == null)
                 return;
-            button.StateImages = imageCheckBox.IsChecked ? ResourceLoader.ButtonImages : new ControlStateImages();
+            button.StateImages = imageCheckBox.IsChecked ? 
+                ResourceLoader.ButtonImages : new ControlStateImages();
         }
 
         private void DefaultCheckBox_CheckedChanged(object sender, System.EventArgs e)
@@ -83,6 +95,86 @@ namespace ControlsSample
         private void Button_Click(object sender, System.EventArgs e)
         {
             site?.LogEvent("Button: Click");
+        }
+
+        private void ApplyFont()
+        {
+            if (site == null)
+                return;
+
+            object? fontNameItem = comboBoxFontName.SelectedItem;
+            object? fontSizeItem = comboBoxFontSize.SelectedItem;
+
+            FontStyle fontStyle = boldCheckBox.IsChecked ?
+                FontStyle.Bold : FontStyle.Regular;
+
+            double fontSize = fontSizeItem == null ?
+                Font.Default.SizeInPoints : Double.Parse(fontSizeItem.ToString());
+
+            string fontName = fontNameItem == null ?
+                Font.Default.Name : (string)fontNameItem;
+
+            Font font = new(fontName, fontSize, fontStyle);
+
+            site?.LogEvent("Button: Font changed to " + font.Name + ", "
+                + font.SizeInPoints);
+
+            button.Font = font;
+            button.PerformLayout();
+        }
+
+        private void ApplyTextColor()
+        {
+            if (site == null)
+                return;
+
+            object? item = comboBoxTextColor.SelectedItem;
+
+            if (item == null)
+                return;
+
+            site?.LogEvent("Button: Text Color changed to " + item);
+
+            Color newColor = Color.FromName(item.ToString());
+
+            button.Foreground = new SolidBrush(newColor);
+        }
+
+        private void ApplyBackColor()
+        {
+            if (site == null)
+                return;
+
+            object? item = comboBoxBackColor.SelectedItem;
+
+            if (item == null)
+                return;
+
+            site?.LogEvent("Button: Back Color changed to " + item);
+
+            Color newColor = Color.FromName(item.ToString());
+
+            button.Background = new SolidBrush(newColor);
+        }
+
+        private void ComboBoxFontName_Changed(object? sender, EventArgs e)
+        {
+            ApplyFont();
+        }
+
+        private void ComboBoxFontSize_Changed(object? sender, EventArgs e)
+        {
+            ApplyFont();
+        }
+
+        private void ComboBoxTextColor_Changed(object? sender, EventArgs e)
+        {
+            ApplyTextColor();
+        }
+
+        private void ComboBoxBackColor_Changed(object? sender, EventArgs e)
+        {
+            ApplyBackColor();
         }
     }
 }
