45a193f319aedaabd1db835649213664d6785b73
generalloki <general.loki.2018@gmail.com>
2023-08-04 11:06:48 +0300

    Add FontWeight documentation

--- Source/Alternet.UI/Drawing/Font/FontWeight.cs
+++ Source/Alternet.UI/Drawing/Font/FontWeight.cs
@@ -6,31 +6,106 @@ using System.Threading.Tasks;
 
 namespace Alternet.Drawing
 {
+    /// <summary>
+    /// Refers to the density of a typeface, in terms of the
+    /// lightness or heaviness of the strokes.
+    /// </summary>
     [Flags]
     public enum FontWeight
     {
+        /// <summary>
+        /// Font weight is not specified or is invalid.
+        /// </summary>
         Invalid = 0,
 
+        /// <summary>
+        /// Specifies a "Thin" font weight (100).
+        /// </summary>
         Thin = 100,
 
+        /// <summary>
+        /// Specifies an "Extra-light" font weight (200).
+        /// </summary>
         ExtraLight = 200,
 
+        /// <summary>
+        /// 
+        /// </summary>
+        UltraLight = ExtraLight,
+
+        /// <summary>
+        /// Specifies a "Light" font weight (300).
+        /// </summary>
         Light = 300,
 
+        /// <summary>
+        /// Specifies a "Normal" font weight (400).
+        /// </summary>
         Normal = 400,
 
+        /// <summary>
+        /// Specifies a "Regular" font weight (400).
+        /// </summary>
+        Regular = Normal,
+
+        /// <summary>
+        /// Specifies a "Medium" font weight.
+        /// </summary>
         Medium = 500,
 
+        /// <summary>
+        /// Specifies a "Semi-bold" font weight (600).
+        /// </summary>
         SemiBold = 600,
 
+        /// <summary>
+        /// Specifies a "Demi-bold" font weight (600).
+        /// </summary>
+        DemiBold = SemiBold,
+
+        /// <summary>
+        /// Specifies a "Bold" font weight (700).
+        /// </summary>
         Bold = 700,
 
+        /// <summary>
+        /// Specifies an "Extra-bold" font weight (800).
+        /// </summary>
         ExtraBold = 800,
 
+        /// <summary>
+        /// Specifies an "Ultra-bold" font weight (800).
+        /// </summary>
+        UltraBold = ExtraBold,
+
+        /// <summary>
+        /// Specifies a "Heavy" font weight (900).
+        /// </summary>
         Heavy = 900,
 
+        /// <summary>
+        /// Specifies a "Black" font weight (900).
+        /// </summary>
+        Black = Heavy,
+
+        /// <summary>
+        /// Specifies an "Extra-black" font weight (950).
+        /// </summary>
+        ExtraBlack = 950,
+
+        /// <summary>
+        /// Specifies an "Ultra-black" font weight (950).
+        /// </summary>
+        UltraBlack = ExtraBlack,
+
+        /// <summary>
+        /// Specifies an "Extra-heavy" font weight (100).
+        /// </summary>
         ExtraHeavy = 1000,
 
+        /// <summary>
+        /// Maximal possible font weight.
+        /// </summary>
         Max = ExtraHeavy,
     }
 }
