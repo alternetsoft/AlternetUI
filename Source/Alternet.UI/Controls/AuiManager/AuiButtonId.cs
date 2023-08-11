32e6d2f08b5ad86a1f54c09799dc52345b0f985c
generalloki <general.loki.2018@gmail.com>
2023-08-11 08:12:50 +0300

    Implementing AuiManager

---
+++ Source/Alternet.UI/Controls/AuiManager/AuiButtonId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public enum AuiButtonId
+    {
+        Close = 101,
+
+        MaximizeRestore = 102,
+
+        Minimize = 103,
+
+        Pin = 104,
+
+        Options = 105,
+
+        WindowList = 106,
+
+        Left = 107,
+
+        Right = 108,
+
+        Up = 109,
+
+        Down = 110,
+
+        Custom1 = 201,
+
+        Custom2 = 202,
+
+        Custom3 = 203,
+    }
+}
---
+++ Source/Alternet.UI/Controls/AuiManager/AuiPaneInsertLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public enum AuiPaneInsertLevel
+    {
+        Pane = 0,
+
+        Row = 1,
+
+        Dock = 2,
+    }
+}
