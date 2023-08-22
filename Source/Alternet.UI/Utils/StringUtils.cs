1a3b03fd91fbfb83a431546ede02136c4a293c7a
generalloki <general.loki.2018@gmail.com>
2023-08-22 10:07:44 +0300

    Add StringUtils.ComparerObjectUsingToString

--- Source/Alternet.UI/Utils/StringUtils.cs
+++ Source/Alternet.UI/Utils/StringUtils.cs
@@ -9,7 +9,34 @@ namespace Alternet.UI
     /// <summary>
     /// Contains <see cref="string"/> related static methods.
     /// </summary>
-    internal static class StringUtils
+    public static class StringUtils
     {
+        private static IComparer<object>? comparerObjectUsingToString;
+
+        /// <summary>
+        /// Returns <see cref="IComparer{T}"/> which converts objects to strings using
+        /// <see cref="object.ToString"/> and uses
+        /// <see cref="string.Compare(string?, string?, StringComparison)"/> for comparing.
+        /// </summary>
+        public static IComparer<object> ComparerObjectUsingToString
+        {
+            get
+            {
+                comparerObjectUsingToString ??= new ComparerUsingToString<object>();
+                return comparerObjectUsingToString;
+            }
+        }
+
+        private class ComparerUsingToString<T> : IComparer<T>
+            where T : class
+        {
+            public int Compare(T? x, T? y)
+            {
+                var s1 = x?.ToString();
+                var s2 = y?.ToString();
+                var result = string.Compare(s1, s2, StringComparison.CurrentCulture);
+                return result;
+            }
+        }
     }
 }
