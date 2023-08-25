7f9b85f6744150a65a9afd9a023208bec29e17d2
generalloki <general.loki.2018@gmail.com>
2023-08-25 13:46:11 +0300

    Update ErrorMessages.cs

--- Source/Alternet.UI/ErrorMessages.cs
+++ Source/Alternet.UI/ErrorMessages.cs
@@ -2,6 +2,10 @@ namespace Alternet.UI
 {
     internal static class ErrorMessages
     {
-        public const string CurrentApplicationIsNotSet = "Current application instance is not set. Create a new Application instance to set it.";
+        public const string CurrentApplicationIsNotSet = "Current application instance is not set";
+        public const string CannotDisposeImmutableObject = "Cannot dispose an immutable object.";
+        public const string OnlySolidBrushInstancesSupported =
+            "Only SolidBrush instances are supported.";
+        public const string CannotChangeImmutableObject = "Cannot change an immutable object.";
     }
 }
