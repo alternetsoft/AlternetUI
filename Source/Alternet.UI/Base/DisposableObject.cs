51186ee2f2a87eecbdbecd1014374865b58981e5
generalloki <general.loki.2018@gmail.com>
2023-09-03 08:45:40 +0300

    Create DisposableObject.cs

---
+++ Source/Alternet.UI/Base/DisposableObject.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    // https://learn.microsoft.com/ru-ru/dotnet/api/system.idisposable?view=net-7.0
+    internal abstract class DisposableObject : IDisposable
+    {
+        private readonly bool disposeHandle;
+        private bool disposed = false;
+        private IntPtr handle;
+
+        public DisposableObject(IntPtr handle = default, bool disposeHandle = true)
+        {
+            this.handle = handle;
+            this.disposeHandle = disposeHandle;
+        }
+
+        // Use C# finalizer syntax for finalization code.
+        // This finalizer will run only if the Dispose method
+        // does not get called.
+        // It gives your base class the opportunity to finalize.
+        // Do not provide finalizer in types derived from this class.
+        ~DisposableObject()
+        {
+            // Do not re-create Dispose clean-up code here.
+            // Calling Dispose(disposing: false) is optimal in terms of
+            // readability and maintainability.
+            Dispose(disposing: false);
+        }
+
+        public IntPtr Handle { get => handle; }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Dispose(bool disposing) executes in two distinct scenarios.
+        // If disposing equals true, the method has been called directly
+        // or indirectly by a user's code. Managed and unmanaged resources
+        // can be disposed.
+        // If disposing equals false, the method has been called by the
+        // runtime from inside the finalizer and you should not reference
+        // other objects. Only unmanaged resources can be disposed.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                // If disposing equals true, dispose all managed
+                // and unmanaged resources.
+                if (disposing)
+                    DisposeManagedResources();
+
+                // Call the appropriate methods to clean up
+                // unmanaged resources here.
+                // If disposing is false,
+                // only the following code is executed.
+                if(handle != default && disposeHandle)
+                {
+                    DisposeUnmanagedResources();
+                    handle = default;
+                }
+
+                // Note disposing has been done.
+                disposed = true;
+            }
+        }
+
+        protected virtual void DisposeManagedResources()
+        {
+        }
+
+        protected abstract void DisposeUnmanagedResources();
+    }
+}
