1c234e5000bfc37f6a7e05beb71de63e17e63d7a
generalloki <general.loki.2018@gmail.com>
2023-09-05 13:42:52 +0300

    Create SRCategoryAttribute.cs

---
+++ Source/Alternet.UI/Attributes/SRCategoryAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal sealed class SRCategoryAttribute : CategoryAttribute
+    {
+        public SRCategoryAttribute(string category)
+            : base(category)
+        {
+        }
+
+        protected override string GetLocalizedString(string value)
+        {
+            return SR.Get(value);
+        }
+    }
+}
