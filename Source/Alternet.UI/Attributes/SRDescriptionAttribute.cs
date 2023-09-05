cb973dbd8bed8d7faa346d1ff897a6916ad10948
generalloki <general.loki.2018@gmail.com>
2023-09-05 14:30:34 +0300

    Create SRDescriptionAttribute.cs

---
+++ Source/Alternet.UI/Attributes/SRDescriptionAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal sealed class SRDescriptionAttribute : DescriptionAttribute
+    {
+        private bool replaced;
+
+        public SRDescriptionAttribute(string description)
+            : base(description)
+        {
+        }
+
+        public override string Description
+        {
+            get
+            {
+                if (!replaced)
+                {
+                    replaced = true;
+                    DescriptionValue = SR.Get(base.Description);
+                }
+
+                return base.Description;
+            }
+        }
+    }
+}
