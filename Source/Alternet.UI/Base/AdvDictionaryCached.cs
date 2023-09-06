52882ca4decdd6cb4683169895756c10bc9719a1
generalloki <general.loki.2018@gmail.com>
2023-09-06 05:46:35 +0300

    Create AdvDictionaryCached.cs

---
+++ Source/Alternet.UI/Base/AdvDictionaryCached.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    internal class AdvDictionaryCached<TKey, TValue> : AdvDictionary<TKey, TValue>
+        where TKey : class
+    {
+        private TKey? lastKey;
+        private TValue? lastValue;
+
+        public TValue GetOrCreateCached(TKey key, Func<TValue> func)
+        {
+            if (lastKey == key)
+                return lastValue!;
+            var result = GetOrCreate(key, func);
+            lastKey = key;
+            lastValue = result;
+            return result;
+        }
+
+        public new bool Remove(TKey key)
+        {
+            ClearLastKey();
+            return base.Remove(key);
+        }
+
+        public new void Clear()
+        {
+            ClearLastKey();
+            base.Clear();
+        }
+
+        private void ClearLastKey()
+        {
+            lastKey = default;
+            lastValue = default;
+        }
+    }
+}
