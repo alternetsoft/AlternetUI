e3d69c6f7796efb11e15e39a2a5c52065a3d5d06
generalloki <general.loki.2018@gmail.com>
2023-09-06 17:20:47 +0300

    Update Point.cs

--- Source/Alternet.UI/Drawing/Point.cs
+++ Source/Alternet.UI/Drawing/Point.cs
@@ -17,21 +17,24 @@ namespace Alternet.Drawing
     */
 
     /// <summary>
-    /// Represents an ordered pair of x and y coordinates that define a point in a two-dimensional plane.
+    /// Represents an ordered pair of x and y coordinates that define a point in a
+    /// two-dimensional plane.
     /// </summary>
     [Serializable]
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct Point : IEquatable<Point>
     {
         /// <summary>
-        /// Creates a new instance of the <see cref='Drawing.Point'/> class with member data left uninitialized.
+        /// Creates a new instance of the <see cref='Drawing.Point'/> class with member data
+        /// left uninitialized.
         /// </summary>
         public static readonly Point Empty;
         private double x; // Do not rename (binary serialization)
         private double y; // Do not rename (binary serialization)
 
         /// <summary>
-        /// Initializes a new instance of the <see cref='Drawing.Point'/> class with the specified coordinates.
+        /// Initializes a new instance of the <see cref='Drawing.Point'/> class with the
+        /// specified coordinates.
         /// </summary>
         public Point(double x, double y)
         {
@@ -62,7 +65,7 @@ namespace Alternet.Drawing
 
             Point value;
 
-            String firstToken = th.NextTokenRequired();
+            string firstToken = th.NextTokenRequired();
 
             value = new Point(
                 Convert.ToDouble(firstToken, formatProvider),
@@ -83,7 +86,7 @@ namespace Alternet.Drawing
         /// <returns>
         /// A string representation of this object.
         /// </returns>
-        internal string ConvertToString(string format, IFormatProvider provider)
+        internal readonly string ConvertToString(string format, IFormatProvider provider)
         {
             // Helper to get the numeric list separator for a given culture.
             char separator = TokenizerHelper.GetNumericListSeparator(provider);
@@ -95,15 +98,18 @@ namespace Alternet.Drawing
                 Y);
         }
 
-        /* TODO: uncommment when Double System.Numerics is availble. See https://github.com/dotnet/runtime/issues/24168
+        /* TODO: uncommment when Double System.Numerics is availble.
+         * See https://github.com/dotnet/runtime/issues/24168
         /// <summary>
-        /// Creates a new <see cref="System.Numerics.Vector2"/> from this <see cref="System.Drawing.PointF"/>.
+        /// Creates a new <see cref="System.Numerics.Vector2"/> from
+        /// this <see cref="System.Drawing.PointF"/>.
         /// </summary>
         public Vector2 ToVector2() => new Vector2(x, y);
 
 
         /// <summary>
-        /// Converts the specified <see cref="Drawing.Point"/> to a <see cref="System.Numerics.Vector2"/>.
+        /// Converts the specified <see cref="Drawing.Point"/> to a
+        /// <see cref="System.Numerics.Vector2"/>.
         /// </summary>
         public static explicit operator Vector2(Point point) => point.ToVector2();*/
 
@@ -132,7 +138,8 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
-        /// Converts the specified <see cref="System.Numerics.Vector2"/> to a <see cref="Drawing.Point"/>.
+        /// Converts the specified <see cref="System.Numerics.Vector2"/> to a
+        /// <see cref="Drawing.Point"/>.
         /// </summary>
         public static explicit operator Point(Vector2 vector) => new Point(vector);
 
@@ -142,7 +149,8 @@ namespace Alternet.Drawing
         public static Point operator +(Point pt, Int32Size sz) => Add(pt, sz);
 
         /// <summary>
-        /// Translates a <see cref='Drawing.Point'/> by the negative of a given <see cref='Drawing.Int32Size'/> .
+        /// Translates a <see cref='Drawing.Point'/> by the negative of a given
+        /// <see cref='Drawing.Int32Size'/> .
         /// </summary>
         public static Point operator -(Point pt, Int32Size sz) => Subtract(pt, sz);
 
@@ -152,19 +160,23 @@ namespace Alternet.Drawing
         public static Point operator +(Point pt, Size sz) => Add(pt, sz);
 
         /// <summary>
-        /// Translates a <see cref='Drawing.Point'/> by the negative of a given <see cref='Drawing.Size'/> .
+        /// Translates a <see cref='Drawing.Point'/> by the negative of a given
+        /// <see cref='Drawing.Size'/> .
         /// </summary>
         public static Point operator -(Point pt, Size sz) => Subtract(pt, sz);
 
         /// <summary>
-        /// Compares two <see cref='Drawing.Point'/> objects. The result specifies whether the values of the
+        /// Compares two <see cref='Drawing.Point'/> objects. The result specifies whether
+        /// the values of the
         /// <see cref='Drawing.Point.X'/> and <see cref='Drawing.Point.Y'/> properties of the two
         /// <see cref='Drawing.Point'/> objects are equal.
         /// </summary>
-        public static bool operator ==(Point left, Point right) => left.X == right.X && left.Y == right.Y;
+        public static bool operator ==(Point left, Point right) => left.X == right.X &&
+            left.Y == right.Y;
 
         /// <summary>
-        /// Compares two <see cref='Drawing.Point'/> objects. The result specifies whether the values of the
+        /// Compares two <see cref='Drawing.Point'/> objects. The result specifies whether
+        /// the values of the
         /// <see cref='Drawing.Point.X'/> or <see cref='Drawing.Point.Y'/> properties of the two
         /// <see cref='Drawing.Point'/> objects are unequal.
         /// </summary>
@@ -173,12 +185,14 @@ namespace Alternet.Drawing
         /// <summary>
         /// Translates a <see cref='Drawing.Point'/> by a given <see cref='Drawing.Int32Size'/> .
         /// </summary>
-        public static Point Add(Point pt, Int32Size sz) => new Point(pt.X + sz.Width, pt.Y + sz.Height);
+        public static Point Add(Point pt, Int32Size sz) => new(pt.X + sz.Width, pt.Y + sz.Height);
 
         /// <summary>
-        /// Translates a <see cref='Drawing.Point'/> by the negative of a given <see cref='Drawing.Int32Size'/> .
+        /// Translates a <see cref='Drawing.Point'/> by the negative of a given
+        /// <see cref='Drawing.Int32Size'/> .
         /// </summary>
-        public static Point Subtract(Point pt, Int32Size sz) => new Point(pt.X - sz.Width, pt.Y - sz.Height);
+        public static Point Subtract(Point pt, Int32Size sz) =>
+            new(pt.X - sz.Width, pt.Y - sz.Height);
 
         /// <summary>
         /// Translates a <see cref='Drawing.Point'/> by a given <see cref='Drawing.Size'/> .
@@ -186,29 +200,34 @@ namespace Alternet.Drawing
         public static Point Add(Point pt, Size sz) => new Point(pt.X + sz.Width, pt.Y + sz.Height);
 
         /// <summary>
-        /// Translates a <see cref='Drawing.Point'/> by the negative of a given <see cref='Drawing.Size'/> .
+        /// Translates a <see cref='Drawing.Point'/> by the negative of a given
+        /// <see cref='Drawing.Size'/> .
         /// </summary>
-        public static Point Subtract(Point pt, Size sz) => new Point(pt.X - sz.Width, pt.Y - sz.Height);
+        public static Point Subtract(Point pt, Size sz) => new(pt.X - sz.Width, pt.Y - sz.Height);
 
         /// <summary>
         /// Determines whether the specified object is equal to the current object.
         /// </summary>
         /// <param name="obj">The object to compare with the current object.</param>
-        /// <returns><c>true</c> if the specified object is equal to the current object; otherwise, <c>false</c>.</returns>
-        public override readonly bool Equals([NotNullWhen(true)] object? obj) => obj is Point && Equals((Point)obj);
+        /// <returns><c>true</c> if the specified object is equal to the current object;
+        /// otherwise, <c>false</c>.</returns>
+        public override readonly bool Equals([NotNullWhen(true)] object? obj) =>
+            obj is Point && Equals((Point)obj);
 
         /// <summary>
         /// Indicates whether the current object is equal to another object of the same type.
         /// </summary>
         /// <param name="other">An object to compare with this object.</param>
-        /// <returns><c>true</c> if the current object is equal to other; otherwise, <c>false</c>.</returns>
+        /// <returns><c>true</c> if the current object is equal to other; otherwise,
+        /// <c>false</c>.</returns>
         public readonly bool Equals(Point other) => this == other;
 
         /// <summary>
         /// Serves as the default hash function.
         /// </summary>
         /// <returns>A hash code for the current object.</returns>
-        public override readonly int GetHashCode() => HashCode.Combine(X.GetHashCode(), Y.GetHashCode());
+        public override readonly int GetHashCode() =>
+            HashCode.Combine(X.GetHashCode(), Y.GetHashCode());
 
         /// <summary>
         /// Returns a string that represents the current object.
