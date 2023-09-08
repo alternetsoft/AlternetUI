28dd7a16b1255c4f2bcf83e49167bf97c87bfb82
generalloki <general.loki.2018@gmail.com>
2023-09-08 10:35:35 +0300

    Update DisposableObject.cs

--- Source/Alternet.UI/Base/DisposableObject.cs
+++ Source/Alternet.UI/Base/DisposableObject.cs
@@ -7,46 +7,82 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     // https://learn.microsoft.com/ru-ru/dotnet/api/system.idisposable?view=net-7.0
-    internal abstract class DisposableObject : IDisposable
+
+    /// <summary>
+    /// Provides a mechanism for releasing managed and unmanaged resources.
+    /// </summary>
+    public abstract class DisposableObject : IDisposable
     {
         private readonly bool disposeHandle;
         private bool disposed = false;
         private IntPtr handle;
 
-        public DisposableObject(IntPtr handle = default, bool disposeHandle = true)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisposableObject"/> class.
+        /// </summary>
+        /// <param name="handle">Handle to unmanaged resources.</param>
+        /// <param name="disposeHandle">Specifies whether to dispose handle using
+        /// <see cref="DisposeUnmanagedResources"/>.</param>
+        public DisposableObject(IntPtr handle, bool disposeHandle)
         {
             this.handle = handle;
             this.disposeHandle = disposeHandle;
         }
 
-        // Use C# finalizer syntax for finalization code.
-        // This finalizer will run only if the Dispose method
-        // does not get called.
-        // It gives your base class the opportunity to finalize.
-        // Do not provide finalizer in types derived from this class.
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisposableObject"/> class.
+        /// </summary>
+        public DisposableObject()
+        {
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="DisposableObject"/> class.
+        /// </summary>
         ~DisposableObject()
         {
+            // Use C# finalizer syntax for finalization code.
+            // This finalizer will run only if the Dispose method
+            // does not get called.
+            // It gives your base class the opportunity to finalize.
+            // Do not provide finalizer in types derived from this class.
+
             // Do not re-create Dispose clean-up code here.
             // Calling Dispose(disposing: false) is optimal in terms of
             // readability and maintainability.
             Dispose(disposing: false);
         }
 
+        /// <summary>
+        /// Gets whether object is disposed.
+        /// </summary>
+        public bool IsDisposed => disposed;
+
+        /// <summary>
+        /// Gets handle to unmanaged resources.
+        /// </summary>
         public IntPtr Handle { get => handle; }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
 
-        // Dispose(bool disposing) executes in two distinct scenarios.
-        // If disposing equals true, the method has been called directly
-        // or indirectly by a user's code. Managed and unmanaged resources
-        // can be disposed.
-        // If disposing equals false, the method has been called by the
-        // runtime from inside the finalizer and you should not reference
-        // other objects. Only unmanaged resources can be disposed.
+        /// <summary>
+        /// Disposes object's resources.
+        /// </summary>
+        /// <remarks>
+        /// Dispose(bool disposing) executes in two distinct scenarios.
+        /// If disposing equals <c>true</c>, the method has been called directly
+        /// or indirectly by a user's code. Managed and unmanaged resources
+        /// can be disposed.
+        /// If disposing equals <c>false</c>, the method has been called by the
+        /// runtime from inside the finalizer and you should not reference
+        /// other objects. Only unmanaged resources can be disposed.
+        /// </remarks>
+        /// <param name="disposing">Disposing scenario</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!this.disposed)
@@ -71,10 +107,16 @@ namespace Alternet.UI
             }
         }
 
+        /// <summary>
+        /// Override to dispose managed resources.
+        /// </summary>
         protected virtual void DisposeManagedResources()
         {
         }
 
+        /// <summary>
+        /// Override to dispose <see cref="Handle"/> or other unmanaged resources.
+        /// </summary>
         protected virtual void DisposeUnmanagedResources()
         {
         }
