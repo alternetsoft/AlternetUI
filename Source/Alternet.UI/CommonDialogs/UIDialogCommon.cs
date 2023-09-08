30e47bb078cdd9ad90327d6c3f6b192bb8d1dbe4
generalloki <general.loki.2018@gmail.com>
2023-09-08 10:36:34 +0300

    Add UIDialog

---
+++ Source/Alternet.UI/CommonDialogs/UIDialogCommon.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the base class used for displaying dialog windows inherited from
+    /// <see cref="Window"/> on the screen.
+    /// </summary>
+    public class UIDialogCommon : DisposableObject
+    {
+        private string? title;
+        private Window? window;
+
+        /// <summary>
+        /// Gets <see cref="Window"/> instance which implements dialog window.
+        /// </summary>
+        public Window DialogWindow
+        {
+            get
+            {
+                if(window == null)
+                {
+                    window = CreateDialogWindow();
+                    UpdateTitle();
+                }
+
+                return window;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the dialog window title.
+        /// </summary>
+        public virtual string? Title
+        {
+            get
+            {
+                if(window == null)
+                    return title;
+                return window.Title;
+            }
+
+            set
+            {
+                title = value;
+                UpdateTitle();
+            }
+        }
+
+        /// <summary>
+        /// Runs a dialog window.
+        /// </summary>
+        /// <returns>
+        /// <see cref="ModalResult.Accepted"/> if the user clicks OK in the dialog window;
+        /// otherwise, <see cref="ModalResult.Canceled"/>.
+        /// </returns>
+        public virtual ModalResult ShowModal()
+        {
+            return DialogWindow.ShowModal();
+        }
+
+        /// <summary>
+        /// Runs a dialog window with the specified owner.
+        /// </summary>
+        /// <param name="owner">
+        /// A window that will own the modal dialog.
+        /// </param>
+        /// <returns>
+        /// <see cref="ModalResult.Accepted"/> if the user clicks OK in the dialog window;
+        /// otherwise, <see cref="ModalResult.Canceled"/>.
+        /// </returns>
+        public virtual ModalResult ShowModal(Window? owner)
+        {
+            return DialogWindow.ShowModal(owner);
+        }
+
+        /// <summary>
+        /// Creates <see cref="Window"/> instance which implements dialog window.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual Window CreateDialogWindow()
+        {
+            return new UIDialogWindow();
+        }
+
+        /// <inheritdoc/>
+        protected override void DisposeManagedResources()
+        {
+            if (window == null)
+                return;
+            window.Dispose();
+            window = null;
+        }
+
+        private void UpdateTitle()
+        {
+            if (window == null || title == null)
+                return;
+            window.Title = title;
+        }
+    }
+}
---
+++ Source/Alternet.UI/CommonDialogs/UIDialogWindow.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Base class for dialog windows.
+    /// </summary>
+    public class UIDialogWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIDialogWindow"/> class.
+        /// </summary>
+        public UIDialogWindow()
+        {
+            ShowInTaskbar = false;
+            MinimizeEnabled = false;
+            MaximizeEnabled = false;
+            StartLocation = WindowStartLocation.CenterScreen;
+            Size = new (600, 400);
+
+            ButtonPanel.Children.Add(OkButton);
+            ButtonPanel.Children.Add(CancelButton);
+            ButtonPanel.Children.Add(ApplyButton);
+
+            LayoutPanel.SetDock(ButtonPanel, DockStyle.Bottom);
+            LayoutPanel.SetDock(DataPanel, DockStyle.Fill);
+            Children.Add(MainPanel);
+            MainPanel.Children.Add(DataPanel);
+            MainPanel.Children.Add(ButtonPanel);
+        }
+
+        /// <summary>
+        /// Gets or sets default button margin for the Ok, Cancel and Apply buttons.
+        /// </summary>
+        public static Thickness DefaultButtonMargin { get; set; } = new(5);
+
+        public Panel DataPanel { get; } = new()
+        {
+        };
+
+        public LayoutPanel MainPanel { get; } = new()
+        {
+        };
+
+        /// <summary>
+        /// Gets Ok button.
+        /// </summary>
+        public Button OkButton { get; } = new()
+        {
+            Text = CommonStrings.Default.ButtonOk,
+            Margin = DefaultButtonMargin,
+            IsDefault = true,
+        };
+
+        /// <summary>
+        /// Gets Cancel button.
+        /// </summary>
+        public Button CancelButton { get; } = new()
+        {
+            Text = CommonStrings.Default.ButtonCancel,
+            Margin = DefaultButtonMargin,
+            IsCancel = true,
+        };
+
+        /// <summary>
+        /// Gets Apply button.
+        /// </summary>
+        public Button ApplyButton { get; } = new()
+        {
+            Margin = DefaultButtonMargin,
+            Text = CommonStrings.Default.ButtonApply,
+        };
+
+        /// <summary>
+        /// Gets panel with Ok, Cancel and Apply buttons.
+        /// </summary>
+        public HorizontalStackPanel ButtonPanel { get; } = new()
+        {
+        };
+    }
+}
