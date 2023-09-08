12aa9aa52f324b06296414e429e2443895acc6ba
generalloki <general.loki.2018@gmail.com>
2023-09-08 10:35:44 +0300

    Create CommonStrings.cs

---
+++ Source/Alternet.UI/Localization/CommonStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    internal class CommonStrings
+    {
+        public static CommonStrings Default { get; set; } = new();
+
+        public string ButtonOk { get; set; } = "Ok";
+
+        public string ButtonCancel { get; set; } = "Cancel";
+
+        public string ButtonApply { get; set; } = "Apply";
+    }
+}
