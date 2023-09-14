8328b98ece18a5cb2634da099804b66f64c6898b
generalloki <general.loki.2018@gmail.com>
2023-09-14 16:42:49 +0300

    Add svg for collection editor

--- Source/Alternet.UI/Utils/SvgUtils.cs
+++ Source/Alternet.UI/Utils/SvgUtils.cs
@@ -19,13 +19,38 @@ namespace Alternet.UI
         /// <summary>
         /// Gets or sets url used to load "plus" svg image used in "Add" toolbar buttons.
         /// </summary>
-        public static string UrlImagePlus { get; set; } =
-            string.Format(ResTemplate, "plus");
+        public static string UrlImagePlus { get; set; } = GetImageUrl("plus");
 
         /// <summary>
         /// Gets or sets url used to load "minus" svg image used in "Remove" toolbar buttons.
         /// </summary>
-        public static string UrlImageMinus { get; set; } =
-            string.Format(ResTemplate, "minus");
+        public static string UrlImageMinus { get; set; } = GetImageUrl("minus");
+
+        /// <summary>
+        /// Gets or sets url used to load svg image used in "Ok" toolbar buttons.
+        /// </summary>
+        public static string UrlImageOk { get; set; } = GetImageUrl("check");
+
+        /// <summary>
+        /// Gets or sets url used to load svg image used in "Cancel" toolbar buttons.
+        /// </summary>
+        public static string UrlImageCancel { get; set; } = GetImageUrl("xmark");
+
+        /// <summary>
+        /// Gets or sets url used to load svg image used in "Add child" toolbar buttons.
+        /// </summary>
+        public static string UrlImageAddChild { get; set; } = GetImageUrl("alternet-add-child");
+
+        /// <summary>
+        /// Gets or sets url used to load svg image used in "Remove All" toolbar buttons.
+        /// </summary>
+        public static string UrlImageRemoveAll { get; set; } = GetImageUrl("eraser");
+
+        /// <summary>
+        /// Gets or sets url used to load svg image used in "Apply" toolbar buttons.
+        /// </summary>
+        public static string UrlImageApply { get; set; } = GetImageUrl("square-check");
+
+        private static string GetImageUrl(string name) => string.Format(ResTemplate, name);
     }
 }
