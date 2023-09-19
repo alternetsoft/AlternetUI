dfb636c28220767a38f9884c742107b478ced452
generalloki <general.loki.2018@gmail.com>
2023-09-19 07:15:35 +0300

    Moved to Interfaces folder

---
+++ Source/Alternet.UI/Interfaces/IEnumerableTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Extends <see cref="IEnumerable"/> interface so tree-like structures can be enumerated.
+    /// </summary>
+    public interface IEnumerableTree : IEnumerable
+    {
+        /// <summary>
+        /// Enumerates children of the specified item.
+        /// </summary>
+        /// <param name="item">Item id.</param>
+        /// <returns></returns>
+        IEnumerable? GetChildren(object item);
+
+        /// <summary>
+        /// Gets data associated with the item.
+        /// </summary>
+        /// <param name="item">Item id.</param>
+        object? GetData(object item);
+    }
+}
---
+++ Source/Alternet.UI/Interfaces/IEnumerableTree{T}.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Exposes the enumerator, which supports a iteration over a collection with
+    /// tree-like structure of a specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+    public interface IEnumerableTree<T> : IEnumerableTree, IEnumerable<T>
+    {
+        /// <inheritdoc cref="IEnumerableTree.GetChildren"/>
+        IEnumerable<T>? GetChildren(T item);
+    }
+}
