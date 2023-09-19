d4096ada95eaa0f02340f8b05a22841fb8464eca
generalloki <general.loki.2018@gmail.com>
2023-09-19 09:01:34 +0300

    Create BaseObject.cs

---
+++ Source/Alternet.UI/Base/BaseObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Base class with properties and methods common to all Alternet.UI objects.
+    /// </summary>
+    public class BaseObject
+    {
+    }
+}
