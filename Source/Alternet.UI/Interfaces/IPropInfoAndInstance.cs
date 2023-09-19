53007093b7e1106eb1bbc6cb86411186c09ce36e
generalloki <general.loki.2018@gmail.com>
2023-09-19 07:15:43 +0300

    Create IPropInfoAndInstance.cs

---
+++ Source/Alternet.UI/Interfaces/IPropInfoAndInstance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains object and property information.
+    /// </summary>
+    public interface IPropInfoAndInstance
+    {
+        /// <summary>
+        /// Gets or sets objects instance in which property is contained.
+        /// </summary>
+        object? Instance { get; set; }
+
+        /// <summary>
+        /// Gets or sets property information.
+        /// </summary>
+        PropertyInfo? PropInfo { get; set; }
+    }
+}
