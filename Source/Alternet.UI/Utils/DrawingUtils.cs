0b26490b9b8c18faf310251e164a22550100ca7b
generalloki <general.loki.2018@gmail.com>
2023-09-20 06:46:27 +0300

    Create DrawingUtils.cs

---
+++ Source/Alternet.UI/Utils/DrawingUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains static methods related to drawing.
+    /// </summary>
+    internal static class DrawingUtils
+    {
+    }
+}
