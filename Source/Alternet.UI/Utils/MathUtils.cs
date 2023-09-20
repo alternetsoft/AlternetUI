8defa6d77e9cc78f657c2a66c8bdc04bc452e4ee
generalloki <general.loki.2018@gmail.com>
2023-09-20 06:46:31 +0300

    Create MathUtils.cs

---
+++ Source/Alternet.UI/Utils/MathUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains <see cref="Math"/> related static methods.
+    /// </summary>
+    public static class MathUtils
+    {
+        /// <summary>
+        /// Calculates distance between two points.
+        /// </summary>
+        /// <param name="x1">X coordinate of the first point.</param>
+        /// <param name="y1">Y coordinate of the first point.</param>
+        /// <param name="x2">X coordinate of the second point.</param>
+        /// <param name="y2">Y coordinate of the second point.</param>
+        /// <returns><see cref="double"/> value with distance between point (x1, y1) and
+        /// point (x2, y2).</returns>
+        public static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        /// <summary>
+        /// Calculates distance between two points.
+        /// </summary>
+        /// <param name="p1">First point.</param>
+        /// <param name="p2">Second point.</param>
+        /// <returns><see cref="double"/> value with distance between point <paramref name="p1"/>
+        /// and point <paramref name="p2"/>.</returns>
+        public static double GetDistance(Point p1, Point p2)
+        {
+            return GetDistance(p1.X, p1.Y, p2.X, p2.Y);
+        }
+
+        /// <summary>
+        /// Compares distance between two points and specified value.
+        /// </summary>
+        /// <param name="p1">First point.</param>
+        /// <param name="p2">Second point.</param>
+        /// <param name="value">Value to compare distance with.</param>
+        /// <returns><c>true</c> if distance between two points is less
+        /// than <paramref name="value"/>; <c>false</c> otherwise.</returns>
+        public static bool DistanceIsLess(Point p1, Point p2, double value)
+        {
+            return (Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2)) < Math.Pow(value, 2);
+        }
+    }
+}
