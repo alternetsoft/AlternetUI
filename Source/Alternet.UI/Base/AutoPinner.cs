77b62d97cbd26ae58053aa61a7e6e72559e13cda
generalloki <general.loki.2018@gmail.com>
2023-09-25 02:43:08 +0300

    Create AutoPinner.cs

---
+++ Source/Alternet.UI/Base/AutoPinner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    internal class AutoPinner : IDisposable
+    {
+        private GCHandle pinnedObject;
+
+        public AutoPinner(object obj)
+        {
+            pinnedObject = GCHandle.Alloc(obj, GCHandleType.Pinned);
+        }
+
+        public static implicit operator IntPtr(AutoPinner ap)
+        {
+            return ap.pinnedObject.AddrOfPinnedObject();
+        }
+
+        public void Dispose()
+        {
+            pinnedObject.Free();
+        }
+    }
+}
