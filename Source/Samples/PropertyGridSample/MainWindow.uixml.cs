43294a7307d3b075bdfdf8d8c7bde353f2087866
generalloki <general.loki.2018@gmail.com>
2023-09-25 13:10:54 +0300

    Update MainWindow.uixml.cs

--- Source/Samples/PropertyGridSample/MainWindow.uixml.cs
+++ Source/Samples/PropertyGridSample/MainWindow.uixml.cs
@@ -45,7 +45,7 @@ namespace PropertyGridSample
             KnownColorStrings.Default.Custom = "Custom...";
 
             // Sample localization of color name
-            KnownColorStrings.Default.Black = "black";
+            KnownColorStrings.Default.Azure = "azure color";
 
             // Sample localization of Enum property values.
             var localizableEnum = PropertyGrid.GetChoices<BrushType>();
