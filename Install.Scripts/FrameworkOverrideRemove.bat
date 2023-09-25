02f926294421fc3a49a41ff8a6ce8e2234bdd9d9
generalloki <general.loki.2018@gmail.com>
2023-09-25 03:06:50 +0300

    Create FrameworkOverrideRemove.bat

---
+++ Install.Scripts/FrameworkOverrideRemove.bat
@@ -0,0 +1,12 @@
+SETLOCAL EnableDelayedExpansion
+
+set SCRIPT_HOME=%~dp0.
+set SOURCE_DIR=%SCRIPT_HOME%\..\Source
+
+pushd "%SOURCE_DIR%\Version\"
+del FrameworksOverride.props
+popd
+
+pushd "%SOURCE_DIR%\Samples\CommonData\"
+del FrameworksOverride.props
+popd
