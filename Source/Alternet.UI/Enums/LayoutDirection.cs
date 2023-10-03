6d48c129b63411b4e4e003c8434bda0caec0e883
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-03 07:59:07 +0300

    Create LayoutDirection.cs

---
+++ Source/Alternet.UI/Enums/LayoutDirection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// This is the layout direction stored returned by <see cref="Application.LayoutDirection"/>
+    /// and other methods for RTL(right-to-left) languages support.
+    /// </summary>
+    public enum LayoutDirection
+    {
+        /// <summary>
+        /// Unknown layout direction. Use default.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Layout direction is left-to-right.
+        /// </summary>
+        LeftToRight,
+
+        /// <summary>
+        /// Layout direction is right-to-left.
+        /// </summary>
+        RightToLeft,
+    }
+}
