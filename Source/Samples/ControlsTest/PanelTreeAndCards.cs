cab22dbc6f608207e330b0e9b3ef61a766e8cc0d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-06 08:49:13 +0300

    Create PanelTreeAndCards.cs

---
+++ Source/Samples/ControlsTest/PanelTreeAndCards.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public class PanelTreeAndCards : PanelAuiManager
+    {
+        private readonly CardPanel cardPanel = new();
+
+        public PanelTreeAndCards()
+        {
+            LeftTreeView.Required();
+            Manager.AddPane(cardPanel, CenterPane);
+
+            LeftTreeView.SelectionChanged += LeftTreeView_SelectionChanged;
+            Manager.Update();
+        }
+
+        public CardPanel CardPanel => cardPanel;
+
+        public void Add(string title, Func<Control> fn)
+        {
+            var index = cardPanel.Add(title, fn);
+            var item = LeftTreeView.Add(title);
+            item.Tag = index;
+        }
+
+        public void Add(string title, Control control)
+        {
+            var index = cardPanel.Add(title, control);
+            var item = LeftTreeView.Add(title);
+            item.Tag = index;
+        }
+
+        private void LeftTreeView_SelectionChanged(object? sender, System.EventArgs e)
+        {
+            var tag = LeftTreeView.SelectedItem?.Tag;
+            cardPanel.SetActiveCard(tag as int?);
+        }
+    }
+}
