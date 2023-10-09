2ee10154f291c491720c91f2b93aec206814b9d7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-09 17:45:56 +0300

    ControlsTest minor changes

--- Source/Samples/ControlsTest/MainTestWindow.uixml.cs
+++ Source/Samples/ControlsTest/MainTestWindow.uixml.cs
@@ -14,6 +14,7 @@ namespace ControlsTest
         static MainTestWindow()
         {
             AuiNotebook.DefaultCreateStyle = AuiNotebookCreateStyle.Top;
+            Test.DoTests();
         }
 
         public MainTestWindow()
---
+++ Source/Samples/ControlsTest/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI;
+
+namespace ControlsTest
+{
+    internal class Test
+    {
+        public static void DoTests()
+        {
+        }
+
+        public static void LogManifestResourceNames()
+        {
+            var assembly = typeof(Test).Assembly;
+
+            var resources = assembly?.GetManifestResourceNames();
+
+            if (resources is null)
+                return;
+
+            foreach(var item in resources)
+            {
+                LogUtils.LogToFile(item);
+            }
+        }
+    }
+}
