fac5f97e32c99193db33f7c915c154b766bad53f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-21 18:40:16 +0300

    Create Animation.cs

---
+++ Source/NativeApi/NativeApi/Api/Animation.cs
@@ -0,0 +1,18 @@
+﻿#pragma warning disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeApi.Api
+{
+    // https://docs.wxwidgets.org/3.2/classwx_animation_ctrl.html
+    public class Animation
+    {
+    }
+}
+
+/*
+
+ */
