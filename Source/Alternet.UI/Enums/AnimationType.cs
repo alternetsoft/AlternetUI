4a58a127ece2130b7c88fccb2d1bb0fb2680957c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-22 10:56:28 +0300

    Create AnimationType.cs

---
+++ Source/Alternet.UI/Enums/AnimationType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines animation type.
+    /// </summary>
+    public enum AnimationType
+    {
+        /// <summary>
+        /// Invalid animation.
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// Gif animation.
+        /// </summary>
+        Gif,
+
+        /// <summary>
+        /// Ani animation.
+        /// </summary>
+        Ani,
+
+        /// <summary>
+        /// Any animation.
+        /// </summary>
+        Any,
+    }
+}
