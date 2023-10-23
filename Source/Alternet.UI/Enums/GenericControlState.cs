d02c83426663956a70f84b63962b1a2b68d7836b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-23 13:49:42 +0300

    Create GenericControlState.cs

---
+++ Source/Alternet.UI/Enums/GenericControlState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines different <see cref="Control"/> states.
+    /// </summary>
+    public enum GenericControlState
+    {
+        /// <summary>
+        /// Normal state.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Mouse is over the control.
+        /// </summary>
+        Hovered,
+
+        /// <summary>
+        /// The left mouse button was clicked on the control and not yet released.
+        /// </summary>
+        Pressed,
+
+        /// <summary>
+        /// Control is disabled.
+        /// </summary>
+        Disabled,
+
+        /// <summary>
+        /// Control is focused.
+        /// </summary>
+        Focused,
+    }
+}
