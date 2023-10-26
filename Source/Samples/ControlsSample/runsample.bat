db27d654504d47c664a568a558a940bb4b404896
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-26 15:38:13 +0300

    Update runsample.bat

--- Source/Samples/ControlsSample/runsample.bat
+++ Source/Samples/ControlsSample/runsample.bat
@@ -1,6 +1,6 @@
 pushd %1
-dotnet build --nologo --property WarningLevel=0
-start /b dotnet run --nologo --property WarningLevel=0 --framework net6.0
+dotnet build /p:Platform=x64 --arch x64 --nologo --property WarningLevel=0 --framework net6.0
+start /b dotnet run /p:Platform=x64 --arch x64 --nologo --property WarningLevel=0 --framework net6.0
 popd
 
-
+:: set /p input= Type any input
