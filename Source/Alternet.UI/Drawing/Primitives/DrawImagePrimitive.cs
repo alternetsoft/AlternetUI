06a11c829cfaffae16e0c66f07cb2e6dd898ae01
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-10-28 10:49:17 +0300

    Add DrawPrimitive

---
+++ Source/Alternet.UI/Drawing/Primitives/DrawImagePrimitive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.Drawing
+{
+    internal class DrawImagePrimitive : DrawPrimitive
+    {
+    }
+}
+
+/*
+
+DrawImage(Image image, Point origin) 
+
+DrawImage(Image image, Rect destinationRect)
+
+DrawImage(Image image, Rect destinationRect, Rect sourceRect)
+
+
+ 
+ */
---
+++ Source/Alternet.UI/Drawing/Primitives/DrawTextPrimitive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.Drawing
+{
+    internal class DrawTextPrimitive : DrawPrimitive
+    {
+    }
+}
+
+/*
+ 
+DrawText(string text, Font font, Brush brush, Point origin) // default format
+
+DrawText(string text, Font font, Brush brush, Point origin, TextFormat format)
+
+DrawText(string text, Font font, Brush brush, Rect bounds)
+
+DrawText(
+            string text,
+            Font font,
+            Brush brush,
+            Rect bounds,
+            TextFormat format)
+
+Size MeasureText(string text, Font font)
+
+Size MeasureText(string text, Font font, double maximumWidth)
+
+Size MeasureText(string text, Font font, double maximumWidth, TextFormat format)
+ */
