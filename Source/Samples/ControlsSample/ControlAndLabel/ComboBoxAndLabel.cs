5305f2e67353c1f55764d9335dd99a77ba7d562a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-03 09:42:20 +0200

    ControlAndLabel

---
+++ Source/Samples/ControlsSample/ControlAndLabel/ComboBoxAndLabel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public class ComboBoxAndLabel : ControlAndLabel
+    {
+        /// <inheritdoc/>
+        protected override Control CreateControl() => new ComboBox();
+    }
+}
---
+++ Source/Samples/ControlsSample/ControlAndLabel/ControlAndLabel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements labeled control.
+    /// </summary>
+    /// <remarks> This is an abstract class. You can use <see cref="TextBoxAndLabel"/>,
+    /// <see cref="ComboBoxAndLabel"/> or derive from <see cref="ControlAndLabel"/>
+    /// in order to implement your own custom labeled control.</remarks>
+    public abstract class ControlAndLabel : StackPanel
+    {
+        private readonly Label label = new()
+        {
+
+        };
+
+        private readonly PictureBox errorPicture = new()
+        {
+
+        };
+
+        private Control mainControl;
+
+        public ControlAndLabel()
+            : base()
+        {
+            label.Parent = this;
+            mainControl = CreateControl();
+            mainControl.Parent = this;
+            errorPicture.Parent = this;
+        }
+
+        public Label Label => label;
+
+        public PictureBox ErrorPicture => errorPicture;
+
+        protected abstract Control CreateControl();
+    }
+}
