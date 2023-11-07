f100b9f0343d70f841633d92d84e12376cc37ce9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-07 11:07:03 +0200

    Create BitmapType.cs

---
+++ Source/Alternet.UI/Enums/BitmapType.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public enum BitmapType
+    {
+        Invalid = 0,
+
+        Bmp,
+
+        BmpResource,
+
+        Resource = BmpResource,
+
+        Ico,
+
+        IcoResource,
+
+        Cur,
+
+        CurResource,
+
+        Xbm,
+
+        XbmData,
+
+        Xpm,
+
+        XpmData,
+
+        Tiff,
+
+        TiffResource,
+
+        Gif,
+
+        GifResource,
+
+        Png,
+
+        PngResource,
+
+        Jpeg,
+
+        JpegResource,
+
+        Pnm,
+
+        PnmResource,
+
+        Pcx,
+
+        PcxResource,
+
+        Pict,
+
+        PictResource,
+
+        Icon,
+
+        IconResource,
+
+        Ani,
+
+        Iff,
+
+        Tga,
+
+        MacCursor,
+
+        MacCursorResource,
+
+        Max,
+
+        Any = 50,
+    }
+}
