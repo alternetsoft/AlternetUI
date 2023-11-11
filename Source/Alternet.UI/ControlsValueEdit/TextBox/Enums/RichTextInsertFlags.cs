866aed78b05de88465004dbf474516ded99a5a9b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-11 07:48:33 +0200

    Create RichTextInsertFlags.cs

---
+++ Source/Alternet.UI/ControlsValueEdit/TextBox/Enums/RichTextInsertFlags.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Flags for object insertion in <see cref="RichTextBox"/>.
+    /// </summary>
+    [Flags]
+    public enum RichTextInsertFlags
+    {
+        /// <summary>
+        /// No flags are specified.
+        /// </summary>
+        None = 0x00,
+
+        /// <summary>
+        /// Insert with previous paragraph style.
+        /// </summary>
+        WithPreviousParagraphStyle = 0x01,
+
+        /// <summary>
+        /// Insert interactive.
+        /// </summary>
+        Interactive = 0x02,
+    }
+}
