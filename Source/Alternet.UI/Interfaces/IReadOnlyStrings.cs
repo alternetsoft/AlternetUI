39905033fa513ceb437a2c30c4752d2b181736f3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-12 23:48:05 +0200

    Create IReadOnlyStrings.cs

---
+++ Source/Alternet.UI/Interfaces/IReadOnlyStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public interface IReadOnlyStrings
+    {
+        /// <summary>
+        /// Gets the number of elements.
+        /// </summary>
+        /// <returns>
+        /// The number of elements contained in the <see cref="IReadOnlyStrings"/>.
+        /// </returns>
+        int Count { get; }
+
+        /// <summary>
+        /// Gets the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns>The element at the specified index.</returns>
+        string? this[int index] { get; }
+    }
+}
