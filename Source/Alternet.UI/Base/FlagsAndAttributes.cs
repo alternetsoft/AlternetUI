d88b8cd7e17740fbbfbd3b4478f23f97b6167ba9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-13 15:59:57 +0200

    Create FlagsAndAttributes.cs

---
+++ Source/Alternet.UI/Base/FlagsAndAttributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    internal abstract class FLagsAndAttributes
+    {
+        public abstract bool HasFlag(string name);
+
+        public abstract void AddFlag(string name);
+
+        public abstract void RemoveFlag(string name);
+
+        public abstract void ToggleFlag(string name);
+
+        public abstract bool HasAttribute(string name);
+
+        public abstract void RemoveAttribute(string name);
+
+        public abstract void SetAttribute(string name, object? value);
+
+        public abstract object? GetAttribute(string name);
+
+        public abstract T GetAttribute<T>(string name, T defaultValue);
+    }
+}
