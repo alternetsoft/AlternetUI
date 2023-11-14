6a51066fdde1a4f531d0b162697cec20c46eab0e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-14 13:27:40 +0200

    Create WindowDeveloperTools.cs

---
+++ Source/Alternet.UI/CommonWindows/WindowDeveloperTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Developer tools window with additional debug related features.
+    /// </summary>
+    internal class WindowDeveloperTools : Window
+    {
+        private readonly PanelDeveloperTools panel = new()
+        {
+            SuggestedSize = new(800, 600),
+        };
+
+        public WindowDeveloperTools()
+            : base()
+        {
+            StartLocation = WindowStartLocation.CenterScreen;
+            Title = "Developer Tools";
+            panel.Parent = this;
+            panel.Update();
+            SetSizeToContent();
+        }
+    }
+}
