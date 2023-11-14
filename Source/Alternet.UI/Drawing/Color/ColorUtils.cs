32b9d8c819db8b06e16789aca48744d39da82c4e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-14 15:38:14 +0200

    ColorUtils.IsDarkBackground

--- Source/Alternet.UI/Drawing/Color/ColorUtils.cs
+++ Source/Alternet.UI/Drawing/Color/ColorUtils.cs
@@ -246,6 +246,20 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
+        /// Returns true if <paramref name="backgroundColor"/> is darker
+        /// than <paramref name="foregroundColor"/>.
+        /// </summary>
+        public static bool IsDarkBackground(Color foregroundColor, Color backgroundColor)
+        {
+            // The threshold here is rather arbitrary, but it seems that using just
+            // inequality would be wrong as it could result in false positivies.
+            if (foregroundColor.IsOk && backgroundColor.IsOk)
+                return foregroundColor.GetLuminance() - backgroundColor.GetLuminance() > 0.2;
+            else
+                return SystemSettings.IsUsingDarkBackground;
+        }
+
+        /// <summary>
         /// Converts <see cref="SystemSettingsColor"/> to <see cref="KnownColor"/>.
         /// </summary>
         /// <param name="color">Color value.</param>
