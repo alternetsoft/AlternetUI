a17fe6eb9037aa502400f910edf874a399b4a344
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-14 13:27:35 +0200

    Create PanelDeveloperTools.cs

---
+++ Source/Alternet.UI/CommonPanels/PanelDeveloperTools.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI.Localization;
+
+namespace Alternet.UI
+{
+    internal class PanelDeveloperTools : PanelAuiManager
+    {
+        private readonly LogListBox mainLogListBox = new()
+        {
+            HasBorder = false,
+        };
+
+        public PanelDeveloperTools()
+            : base()
+        {
+            DefaultRightPaneBestSize = new(150, 200);
+            DefaultRightPaneMinSize = new(150, 200);
+
+            mainLogListBox.Parent = CenterNotebook;
+            mainLogListBox.ContextMenu.Required();
+            mainLogListBox.MenuItemShowDevTools?.SetEnabled(false);
+            mainLogListBox.BindApplicationLog();
+            ActionsControl.Required();
+            var mainLogPage = CenterNotebook.AddPage(
+                mainLogListBox,
+                CommonStrings.Default.NotebookTabTitleOutput);
+            InitActions();
+            DebugUtils.HookExceptionEvents();
+        }
+
+        private void InitActions()
+        {
+            AddAction("Log system settings", SystemSettings.Log);
+            AddAction("Log font families", LogUtils.LogFontFamilies);
+            AddAction("Log system fonts", SystemSettings.LogSystemFonts);
+            AddAction("Log display info", Display.Log);
+        }
+    }
+}
