aded293c127a04934a6b5302c397d3c53720e3f8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-14 09:43:34 +0200

    Update runsample.bat

--- Source/Samples/ControlsSample/runsample.bat
+++ Source/Samples/ControlsSample/runsample.bat
@@ -1,3 +1,4 @@
+ECHO ===================
 pushd %1
 dotnet build /p:Platform=x64 --arch x64 --nologo --property WarningLevel=0 --framework net6.0
 start /b dotnet run /p:Platform=x64 --arch x64 --nologo --property WarningLevel=0 --framework net6.0
