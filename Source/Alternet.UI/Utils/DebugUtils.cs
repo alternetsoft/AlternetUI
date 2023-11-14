68873f4329409c3f1ca1efb1dc2bbb181cbce2f7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-14 13:28:23 +0200

    Update DebugUtils.cs

--- Source/Alternet.UI/Utils/DebugUtils.cs
+++ Source/Alternet.UI/Utils/DebugUtils.cs
@@ -13,17 +13,47 @@ namespace Alternet.UI
     public static class DebugUtils
     {
         private static bool insideUnhandledException;
+        private static bool hookedExceptionEvents;
+        private static WindowDeveloperTools? devToolsWindow;
 
         /// <summary>
         /// Hooks exception events for the debug purposes.
         /// </summary>
         public static void HookExceptionEvents()
         {
+            if (hookedExceptionEvents)
+                return;
             var a = Application.Current;
             a.ThreadException += Application_ThreadException;
             a.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
             AppDomain.CurrentDomain.UnhandledException +=
                 CurrentDomain_UnhandledException;
+            hookedExceptionEvents = true;
+        }
+
+        /// <summary>
+        /// Shows developer tools window.
+        /// </summary>
+        public static void ShowDeveloperTools()
+        {
+            if(devToolsWindow is null)
+            {
+                devToolsWindow = new();
+                devToolsWindow.Closing += DevToolsWindow_Closing;
+                devToolsWindow.Disposed += DevToolsWindow_Disposed;
+            }
+
+            devToolsWindow.Show();
+
+            static void DevToolsWindow_Closing(object? sender, WindowClosingEventArgs e)
+            {
+                (sender as Window)?.Hide();
+                e.Cancel = true;
+            }
+
+            void DevToolsWindow_Disposed(object? sender, EventArgs e)
+            {
+            }
         }
 
         private static void HandleException(Exception e)
