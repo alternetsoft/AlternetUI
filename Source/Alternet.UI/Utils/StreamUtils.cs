34ad57deafa02c0152b53f6250c1e9f22da3ba33
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-15 07:04:05 +0200

    Update StreamUtils.cs

--- Source/Alternet.UI/Utils/StreamUtils.cs
+++ Source/Alternet.UI/Utils/StreamUtils.cs
@@ -10,7 +10,29 @@ namespace Alternet.UI
     /// <summary>
     /// Contains <see cref="Stream"/> related static methods.
     /// </summary>
-    internal static class StreamUtils
+    public static class StreamUtils
     {
+        /// <summary>
+        /// Reads <see cref="string"/> from <see cref="Stream"/> using <see cref="Encoding.UTF8"/>
+        /// encoding.
+        /// </summary>
+        /// <param name="stream">Stream with data.</param>
+        public static string StringFromStream(Stream stream)
+        {
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+
+        /// <summary>
+        /// Writes <see cref="string"/> to <see cref="Stream"/> using <see cref="Encoding.UTF8"/>
+        /// encoding.
+        /// </summary>
+        /// <param name="stream">Stream with data.</param>
+        /// <param name="s">String with data.</param>
+        public static void StringToStream(Stream stream, string s)
+        {
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.Write(s);
+        }
     }
 }
