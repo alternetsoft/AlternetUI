d52ecfd7a5670fec70489a49dad14ab5852b8ddc
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-16 03:37:26 +0200

    Create TextEllipsizeType.cs

---
+++ Source/Alternet.UI/Enums/TextEllipsizeType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates text replacement methods when the text width exceed the
+    /// container's width.
+    /// </summary>
+    public enum TextEllipsizeType
+    {
+        /// <summary>
+        /// No replacement of the text with an ellipsis is done when the text width exceed the
+        /// container's width.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Replace the end of the text with an ellipsis when the text width exceed the
+        /// container's width.
+        /// </summary>
+        End,
+
+        /// <summary>
+        /// Replace the beginning of the text with an ellipsis when the text width exceed the
+        /// container's width.
+        /// </summary>
+        Start,
+
+        /// <summary>
+        /// Replace the middle of the text with an ellipsis when the text width exceed the
+        /// container's width.
+        /// </summary>
+        Middle,
+    }
+}
