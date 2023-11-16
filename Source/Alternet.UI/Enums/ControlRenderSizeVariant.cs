4bc34c93c22f60fa9f74ce256d2c45f81ff9e8f0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-16 10:14:30 +0200

    Create ControlRenderSizeVariant.cs

---
+++ Source/Alternet.UI/Enums/ControlRenderSizeVariant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates different rendering sizes of the control.
+    /// </summary>
+    public enum ControlRenderSizeVariant
+    {
+        /// <summary>
+        /// Normal size
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Smaller size (about 25 % smaller than normal)
+        /// </summary>
+        Small,
+
+        /// <summary>
+        /// Mini size (about 33 % smaller than normal)
+        /// </summary>
+        Mini,
+
+        /// <summary>
+        /// Large size (about 25 % larger than normal)
+        /// </summary>
+        Large,
+    }
+}
