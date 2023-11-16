0aef8b63da4dc08f4310fd97397b7d4b2fc829df
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-16 04:07:10 +0200

    Create IAuiToolbarItem.cs

---
+++ Source/Alternet.UI/Controls/AuiManager/IAuiToolbarItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides access to methods and properties of the <see cref="AuiToolbar"/> item.
+    /// </summary>
+    internal interface IAuiToolbarItem
+    {
+    }
+}
