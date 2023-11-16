c6c219dfb29f1c87988b06192dd3733b47c4ac32
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-16 15:00:46 +0200

    IFontAndColor

---
+++ Source/Alternet.UI/Controls/BaseControls/FontAndColor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    internal class FontAndColor
+    {
+        internal class ControlStaticDefaultFontAndColor : IReadOnlyFontAndColor
+        {
+            private readonly ControlId controlType;
+            private readonly ControlRenderSizeVariant renderSize;
+
+            public ControlStaticDefaultFontAndColor(
+                ControlId controlType,
+                ControlRenderSizeVariant renderSize = ControlRenderSizeVariant.Normal)
+            {
+                this.controlType = controlType;
+                this.renderSize = renderSize;
+            }
+
+            public Color? BackgroundColor =>
+                Control.GetClassDefaultAttributesBgColor(controlType, renderSize);
+
+            public Color? ForegroundColor =>
+                Control.GetClassDefaultAttributesFgColor(controlType, renderSize);
+
+            public Font? Font =>
+                Control.GetClassDefaultAttributesFont(controlType, renderSize);
+        }
+
+        internal class ControlDefaultFontAndColor : IReadOnlyFontAndColor
+        {
+            private readonly Control control;
+
+            public ControlDefaultFontAndColor(Control control)
+            {
+                this.control = control;
+            }
+
+            public Color? BackgroundColor => control.GetDefaultAttributesBgColor();
+
+            public Color? ForegroundColor => control.GetDefaultAttributesFgColor();
+
+            public Font? Font => control.GetDefaultAttributesFont();
+        }
+    }
+}
---
+++ Source/Alternet.UI/Interfaces/IReadOnlyFontAndColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains methods to get background color, foreground color and font.
+    /// </summary>
+    public interface IReadOnlyFontAndColor
+    {
+        /// <summary>
+        /// Gets background color.
+        /// </summary>
+        Color? BackgroundColor { get; }
+
+        /// <summary>
+        /// Gets foreground color.
+        /// </summary>
+        Color? ForegroundColor { get; }
+
+        /// <summary>
+        /// Gets font.
+        /// </summary>
+        Font? Font { get; }
+    }
+}
