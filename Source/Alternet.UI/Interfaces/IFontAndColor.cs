fe3755791bc99009385b7c633c01aa9771b70a0a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-16 15:59:22 +0200

    Create IFontAndColor.cs

---
+++ Source/Alternet.UI/Interfaces/IFontAndColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains methods to get and set background color, foreground color and font.
+    /// </summary>
+    internal interface IFontAndColor : IReadOnlyFontAndColor
+    {
+        /// <summary>
+        /// Gets or sets background color.
+        /// </summary>
+        new Color? BackgroundColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets foreground color.
+        /// </summary>
+        new Color? ForegroundColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets font.
+        /// </summary>
+        new Font? Font { get; set; }
+    }
+}
