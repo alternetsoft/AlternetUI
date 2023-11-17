5c163824dc871ebf6733e4f1922d8d9e8dc6d1ce
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-17 07:28:36 +0200

    Create GenericTabControl.cs

---
+++ Source/Alternet.UI/CommonPanels/CardPanel/GenericTabControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    internal class GenericTabControl : Control
+    {
+        private readonly CardPanel cardPanel = new();
+        private readonly CardPanelHeader cardPanelHeader = new();
+
+        public GenericTabControl()
+            : base()
+        {
+        }
+
+        public CardPanel CardPanel => cardPanel;
+
+        public CardPanelHeader CardPanelHeader => cardPanelHeader;
+    }
+}
