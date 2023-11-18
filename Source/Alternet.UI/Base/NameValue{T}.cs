efe139dd06bb925eced71cba8d45a9b2685096a9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-18 13:24:00 +0200

    Update NameValue{T}.cs

--- Source/Alternet.UI/Base/NameValue{T}.cs
+++ Source/Alternet.UI/Base/NameValue{T}.cs
@@ -18,7 +18,7 @@ namespace Alternet.UI
         /// <param name="name">A string that contains the name of the <paramref name="value"/>.</param>
         /// <param name="value">An object that contains the content that the
         /// <paramref name="name"/> identifies.</param>
-        public NameValue(string name, T? value = default)
+        public NameValue(string name, T value)
         {
             Name = name;
             Value = value;
@@ -34,6 +34,6 @@ namespace Alternet.UI
         /// Gets and sets the content that the <see cref="Name"/> property value identifies.
         /// </summary>
         /// <value>A value of the <typeparamref name="T"/> type that contains the content.</value>
-        public T? Value { get; set; }
+        public T Value { get; set; }
     }
 }
