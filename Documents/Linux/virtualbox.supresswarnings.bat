301ea1f9ff2415789c739687d38f04a5dcc7edf4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-22 05:26:05 +0200

    Create virtualbox.supresswarnings.bat

---
+++ Documents/Linux/virtualbox.supresswarnings.bat
@@ -0,0 +1,4 @@
+c:
+cd "C:\Program Files\Oracle\VirtualBox\"
+
+VBoxManage setextradata global GUI/SuppressMessages remindAboutMouseIntegrationOff,remindAboutAutoCapture,remindAboutMouseIntegrationOn,remindAboutMouseIntegration
