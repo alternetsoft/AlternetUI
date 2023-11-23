afc277811b7ae1644886c488e80be4dec126c3f3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-23 19:10:34 +0200

    Create BaseChildObject.cs

---
+++ Source/Alternet.UI/Base/BaseChildObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Base class with properties and methods common to all child objects.
+    /// </summary>
+    public class BaseChildObject<T> : BaseObject
+    {
+        private readonly T owner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseChildObject{T}"/> class.
+        /// </summary>
+        public BaseChildObject(T owner)
+        {
+            this.owner = owner;
+        }
+
+        /// <summary>
+        /// Gets owner.
+        /// </summary>
+        [Browsable(false)]
+        public T Owner => owner;
+    }
+}
