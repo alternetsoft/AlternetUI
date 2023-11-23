9a797b3635f34542ced5e1d7ee367a61fb2a76e4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-23 07:05:06 +0200

    HeaderLabel: CreateInnerControl, CreateDefaultInnerControl

--- Source/Alternet.UI/Controls/Labels/HeaderLabel.cs
+++ Source/Alternet.UI/Controls/Labels/HeaderLabel.cs
@@ -11,28 +11,51 @@ namespace Alternet.UI
     /// </summary>
     public class HeaderLabel : CustomLabel
     {
-        private CardPanelHeader headerPanel = new();
+        /// <summary>
+        /// Gets or sets function which creates inner label.
+        /// </summary>
+        /// <remarks>
+        /// Result of this function must support at least <see cref="ITextProperty"/>
+        /// interface.
+        /// </remarks>
+        public static Func<Control> CreateInnerControl = CreateDefaultInnerControl;
+
+        private Control control;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HeaderLabel"/> class.
         /// </summary>
         public HeaderLabel()
         {
-            headerPanel.Parent = this;
+            control = CreateInnerControl?.Invoke() ?? CreateDefaultInnerControl();
+            control.Parent = this;
         }
-        
+
         /// <inheritdoc/>
         public override string Text
         {
             get
             {
-                return headerPanel.Text;
+                return (control as ITextProperty)?.Text ?? string.Empty;
             }
 
             set
             {
-                headerPanel.Text = value;
+                if(control is ITextProperty instance)
+                    instance.Text = value;
             }
+        }
+
+        /// <summary>
+        /// Creates default inner label.
+        /// </summary>
+        public static Control CreateDefaultInnerControl()
+        {
+            var panel = new CardPanelHeader();
+            panel.TabHasBorder = false;
+            panel.Text = string.Empty;
+            panel.SelectFirstTab();
+            return panel;
         }
     }
 }
