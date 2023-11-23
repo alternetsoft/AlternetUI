86c30153e4568a1ba15ec199329c2ad4dcc5c2ea
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-23 05:41:23 +0200

    Create HeaderLabel.cs

---
+++ Source/Alternet.UI/Controls/Labels/HeaderLabel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements label control that looks like header.
+    /// </summary>
+    public class HeaderLabel : CustomLabel
+    {
+        private CardPanelHeader headerPanel = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeaderLabel"/> class.
+        /// </summary>
+        public HeaderLabel()
+        {
+            headerPanel.Parent = this;
+        }
+        
+        /// <inheritdoc/>
+        public override string Text
+        {
+            get
+            {
+                return headerPanel.Text;
+            }
+
+            set
+            {
+                headerPanel.Text = value;
+            }
+        }
+    }
+}
