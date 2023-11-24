73dd0b03df888afe0c6cb70820fe2d7ca6728229
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-24 04:39:39 +0200

    Update ControlStateSettings.cs

--- Source/Alternet.UI/Controls/BaseControls/ControlStateSettings/ControlStateSettings.cs
+++ Source/Alternet.UI/Controls/BaseControls/ControlStateSettings/ControlStateSettings.cs
@@ -7,10 +7,25 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     /// <summary>
-    /// Specifies sets of objects (images, colors, borders) for different control states.
+    /// Specifies sets of objects (images, colors, borders, pens, brushes) for different control states.
     /// </summary>
     public class ControlStateSettings
     {
+        /// <summary>
+        /// Gets or sets <see cref="ControlStateBrushes"/>.
+        /// </summary>
+        public ControlStateBrushes? Backgrounds { get; set; }
+
+        /// <summary>
+        /// Gets or sets <see cref="ControlStateBrushes"/>.
+        /// </summary>
+        public ControlStateBrushes? Foregrounds { get; set; }
+
+        /// <summary>
+        /// Gets or sets <see cref="ControlStatePens"/>.
+        /// </summary>
+        public ControlStatePens? Pens { get; set; }
+
         /// <summary>
         /// Gets or sets <see cref="ControlStateImages"/>.
         /// </summary>
