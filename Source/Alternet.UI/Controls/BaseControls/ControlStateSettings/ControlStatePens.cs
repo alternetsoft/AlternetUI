db2400eab7713f0c6e3dbdec763eb2bcadf714c0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-24 04:17:10 +0200

    Create ControlStatePens.cs

---
+++ Source/Alternet.UI/Controls/BaseControls/ControlStateSettings/ControlStatePens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies a set of <see cref="Pen"/> for different control states.
+    /// </summary>
+    public class ControlStatePens : ControlStateObjects<Pen>
+    {
+        /// <summary>
+        /// Gets <see cref="ControlStatePens"/> with empty state images.
+        /// </summary>
+        public static readonly ControlStatePens Empty = new()
+        {
+            Immutable = true,
+        };
+    }
+}
