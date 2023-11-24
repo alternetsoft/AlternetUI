400380edb71519445f978da9fef4d447082252b3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-24 04:17:07 +0200

    Create ControlStateBrushes.cs

---
+++ Source/Alternet.UI/Controls/BaseControls/ControlStateSettings/ControlStateBrushes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies a set of <see cref="Brush"/> for different control states.
+    /// </summary>
+    public class ControlStateBrushes : ControlStateObjects<Brush>
+    {
+        /// <summary>
+        /// Gets <see cref="ControlStateBrushes"/> with empty state images.
+        /// </summary>
+        public static readonly ControlStateBrushes Empty = new()
+        {
+            Immutable = true,
+        };
+    }
+}
