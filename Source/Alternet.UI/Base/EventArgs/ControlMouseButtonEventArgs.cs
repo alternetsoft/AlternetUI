0024f45ca5c0da2ba975f0013893e2fd3bb726ea
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-26 16:46:02 +0200

    Create ControlMouseButtonEventArgs.cs

---
+++ Source/Alternet.UI/Base/EventArgs/ControlMouseButtonEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the mouse button events.
+    /// </summary>
+    internal class ControlMouseButtonEventArgs : BaseEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ControlMouseButtonEventArgs"/> class.
+        /// </summary>
+        /// <param name="control">Affected control.</param>
+        /// <param name="e">Mouse button event arguments.</param>
+        public ControlMouseButtonEventArgs(Control control, MouseButtonEventArgs e)
+        {
+            Control = control;
+            MouseButtonEventArgs = e;
+        }
+
+        /// <summary>
+        /// Gets affected control.
+        /// </summary>
+        public Control Control { get; }
+
+        /// <summary>
+        /// Gets mouse button event arguments.
+        /// </summary>
+        public MouseButtonEventArgs MouseButtonEventArgs { get; }
+    }
+}
