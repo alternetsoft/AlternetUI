a40eb35579c9da438bddda4f8df1b6d3d3fb37ec
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-11-26 03:57:29 +0200

    Create PopupPropertyGrid.cs

---
+++ Source/Alternet.UI/CommonPopups/PopupPropertyGrid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Popup window with <see cref="PropertyGrid"/> control.
+    /// </summary>
+    public class PopupPropertyGrid : PopupWindow
+    {
+        /// <summary>
+        /// Gets or sets <see cref="ListBox"/> control used in the popup window.
+        /// </summary>
+        [Browsable(false)]
+        public new PropertyGrid MainControl
+        {
+            get => (PropertyGrid)base.MainControl;
+            set => base.MainControl = value;
+        }
+
+        /// <inheritdoc/>
+        public override void HidePopup(ModalResult result)
+        {
+            if (!MainControl.ClearSelection(true))
+                return;
+            base.HidePopup(result);
+        }
+
+        /// <inheritdoc/>
+        protected override Control CreateMainControl()
+        {
+            return new PropertyGrid()
+            {
+                HasBorder = false,
+            };
+        }
+    }
+}
