b86f83a846a09b9b021affd61f88455598098c40
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-01 15:58:37 +0200

    Create BaseEventArgs{T}.cs

---
+++ Source/Alternet.UI/Base/EventArgs/BaseEventArgs{T}.cs
@@ -0,0 +1,25 @@
+﻿namespace Alternet.UI
+{
+    /// <summary>
+    /// Extends <see cref="BaseEventArgs"/> with parameter of <typeparamref name="T"/> type.
+    /// </summary>
+    /// <typeparam name="T">Type of the <see cref="Value"/> property.</typeparam>
+    public class BaseEventArgs<T> : BaseEventArgs
+    {
+        private readonly T value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseEventArgs{T}"/> class.
+        /// </summary>
+        /// <param name="value"></param>
+        public BaseEventArgs(T value)
+        {
+            this.value = value;
+        }
+
+        /// <summary>
+        /// Gets parameter value.
+        /// </summary>
+        public T Value => value;
+    }
+}
