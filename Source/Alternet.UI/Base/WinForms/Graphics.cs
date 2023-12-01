dedc9eed14f25fbba5abac0036b74fbaeb431077
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-01 16:36:50 +0200

    WinForms related dummy classes

---
+++ Source/Alternet.UI/Base/WinForms/Graphics.cs
@@ -0,0 +1,13 @@
+﻿namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Defined in order to make library more compatible with the legacy code.
+    /// </summary>
+    public class Graphics : DrawingContext
+    {
+        internal Graphics(UI.Native.DrawingContext dc)
+            : base(dc)
+        {
+        }
+    }
+}
