dc10a111fcb3960674dd600d35825eb5d935b67d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-01 16:27:49 +0200

    WinForms related dummy classes

---
+++ Source/Alternet.UI/Base/WinForms/IWin32Window.cs
@@ -0,0 +1,9 @@
+﻿namespace Alternet.UI
+{
+    /// <summary>
+    /// Defined in order to make library more compatible with the legacy code.
+    /// </summary>
+    public class IWin32Window : Window
+    {
+    }
+}
