3da6b26b782141bb73ffd366254645b1b670ff36
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-02 09:05:19 +0200

    Update ErrorMessages.cs

--- Source/Alternet.UI/Localization/ErrorMessages.cs
+++ Source/Alternet.UI/Localization/ErrorMessages.cs
@@ -8,95 +8,99 @@ namespace Alternet.UI.Localization
         /// <summary>
         /// Current localizations for error messages.
         /// </summary>
-        public static ErrorMessages Default { get; set; } = new();
+        public static ErrorMessages Default = new();
 
         /// <summary>
         /// Gets or sets error message localization.
         /// </summary>
-        public string CurrentApplicationIsNotSet { get; set; } =
+        public string CurrentApplicationIsNotSet =
             "Current application instance is not set.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ErrorTitle { get; set; } = "Error";
+        public string ErrorTitle = "Error";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string CannotDisposeImmutableObject { get; set; } =
+        public string CannotDisposeImmutableObject =
             "Cannot dispose an immutable object.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string CannotChangeReadOnlyObject { get; set; } =
+        public string CannotChangeReadOnlyObject =
             "Cannot change a readonly object.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string OnlySolidBrushInstancesSupported { get; set; } =
+        public string OnlySolidBrushInstancesSupported =
             "Only SolidBrush instances are supported.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string CannotChangeImmutableObject { get; set; } =
+        public string CannotChangeImmutableObject =
             "Cannot change an immutable object.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string InvalidParameter { get; set; } = "Invalid Parameter.";
+        public string InvalidParameter = "Invalid Parameter.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ParameterIsAlreadySet { get; set; } = "Parameter is already set.";
+        public string ParameterIsAlreadySet = "Parameter is already set.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string EventHasAlreadyBeenHandled { get; set; } =
+        public string EventHasAlreadyBeenHandled =
             "Event has already been handled.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string PropertyIsNull { get; set; } = "Property {0} is null.";
+        public string PropertyIsNull = "Property {0} is null.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string PropertyCannotBeNull { get; set; } = "Property {0} value cannot be null.";
+        public string PropertyCannotBeNull = "Property {0} value cannot be null.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string InvalidStringFormat { get; set; } = "String format is not valid.";
+        public string InvalidStringFormat = "String format is not valid.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationNumberIsExpected { get; set; } = "Number is expected.";
+        public string ValidationNumberIsExpected = "Number is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationUnsignedNumberIsExpected { get; set; } = "Unsigned number is expected.";
+        public string ValidationUnsignedNumberIsExpected = "Unsigned number is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationUnsignedFloatIsExpected { get; set; } = "Unsigned floating point number is expected.";
+        public string ValidationUnsignedFloatIsExpected = "Unsigned floating point number is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationFloatIsExpected { get; set; } = "Floating point number is expected.";
+        public string ValidationFloatIsExpected = "Floating point number is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationHexNumberIsExpected { get; set; } = "Hexadecimal number is expected.";
+        public string ValidationHexNumberIsExpected = "Hexadecimal number is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationRangeFormat { get; set; } = "Range is [{0}..{1}].";
+        public string ValidationRangeFormat = "Range is [{0}..{1}].";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationInvalidFormat { get; set; } = "Invalid format.";
+        public string ValidationInvalidFormat = "Invalid format.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationMinimumLength { get; set; } = "Minimum length is {0} characters.";
+        public string ValidationMinimumLength = "Minimum length is {0} characters.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationMaximumLength { get; set; } = "Maximum length is {0} characters.";
+        public string ValidationMaximumLength = "Maximum length is {0} characters.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationMinMaxLength { get; set; } = "Valid length is {0} to {1} characters.";
+        public string ValidationMinMaxLength = "Valid length is {0} to {1} characters.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationMinimumValue { get; set; } = "Minimum value is {0}.";
+        public string ValidationMinimumValue = "Minimum value is {0}.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationMaximumValue { get; set; } = "Maximum value is {0}.";
+        public string ValidationMaximumValue = "Maximum value is {0}.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationValueIsRequired { get; set; } = "Value is required.";
+        public string ValidationValueIsRequired = "Value is required.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationEMailIsExpected { get; set; } = "E-mail is expected.";
+        public string ValidationEMailIsExpected = "E-mail is expected.";
 
         /// <see cref="CurrentApplicationIsNotSet"/>
-        public string ValidationUrlIsExpected { get; set; } = "Url is expected.";
+        public string ValidationUrlIsExpected = "Url is expected.";
+
+        /// <see cref="CurrentApplicationIsNotSet"/>
+        public string InvalidBoundArgument
+            = "Value of '{1}' is not valid for '{0}'. '{0}' should be greater than {2} and less than or equal to {3}.";
     }
 }
