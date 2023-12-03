00b5522f903eeecca2de2817af6f8fd2c8983cfe
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-03 06:33:44 +0200

    OSUtils.Sleep

---
+++ Source/Alternet.UI/Utils/OSUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// OS related static methods
+    /// </summary>
+    public static class OSUtils
+    {
+        /// <summary>
+        /// Suspends the execution of the current thread for a specified interval.
+        /// </summary>
+        /// <param name="milliSeconds">Specifies time, in milliseconds, for which
+        /// to suspend execution.</param>
+        public static void Sleep(int milliSeconds)
+        {
+            Thread.Sleep(milliSeconds);
+        }
+    }
+}
