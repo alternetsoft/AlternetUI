055b8dc275509a852ac123eac566acd6d0e5b28e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-05 14:07:39 +0200

    Update MessageBoxObj.cpp

--- Source/Alternet.UI.Pal/MessageBoxObj.cpp
+++ Source/Alternet.UI.Pal/MessageBoxObj.cpp
@@ -2,6 +2,38 @@
 
 namespace Alternet::UI
 {
+    int wxMessageBox2(const wxString& message, const wxString& caption, long style,
+        wxWindow* parent)
+    {
+        // add the appropriate icon unless this was explicitly disabled by use of
+        // wxICON_NONE
+        if (!(style & wxICON_NONE) && !(style & wxICON_MASK))
+        {
+            style |= style & wxYES ? wxICON_QUESTION : wxICON_INFORMATION;
+        }
+
+        wxMessageDialog dialog(parent, message, caption, style);
+
+        int ans = dialog.ShowModal();
+        switch (ans)
+        {
+        case wxID_OK:
+            return wxOK;
+        case wxID_YES:
+            return wxYES;
+        case wxID_NO:
+            return wxNO;
+        case wxID_CANCEL:
+            return wxCANCEL;
+        case wxID_HELP:
+            return wxHELP;
+        }
+
+        wxFAIL_MSG(wxT("unexpected return code from wxMessageDialog"));
+
+        return wxCANCEL;
+    }
+
     MessageBoxResult MessageBoxObj::Show(
         Window* owner,
         const string& text,
@@ -14,7 +46,7 @@ namespace Alternet::UI
 
         auto wxOwner = owner != nullptr ? owner->GetWxWindow() : nullptr;
 
-        auto wxResult = wxMessageBox(
+        auto wxResult = wxMessageBox2(
             wxStr(text),
             wxStr(caption.value_or(u"")),
             style,
