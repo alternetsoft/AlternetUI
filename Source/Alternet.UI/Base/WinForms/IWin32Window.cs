e77f2f2659176daf0c61916c64d9187c5f23f662
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-06 07:41:02 +0200

    Update IWin32Window.cs

--- Source/Alternet.UI/Base/WinForms/IWin32Window.cs
+++ Source/Alternet.UI/Base/WinForms/IWin32Window.cs
@@ -1,9 +1,15 @@
-﻿namespace Alternet.UI
+﻿using System;
+
+namespace Alternet.UI
 {
     /// <summary>
     /// Defined in order to make library more compatible with the legacy code.
     /// </summary>
-    public class IWin32Window : Window
+    /// <summary>Provides an interface to expose Win32 HWND handles.</summary>
+    public interface IWin32Window
     {
+        /// <summary>Gets the handle to the window represented by the implementer.</summary>
+        /// <returns>A handle to the window represented by the implementer.</returns>
+        IntPtr Handle { get; }
     }
 }
