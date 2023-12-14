84463a30e7b418a0a12515cf20a0b95664354dca
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-14 17:59:04 +0200

    CustomCursor test project

---
+++ Source/Tests/CustomCursor/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using Alternet.UI;
+using Alternet.Drawing;
+
+namespace MinMaster
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.CreateAndRun(() => new WindowLogListBox(), Init);
+        }
+
+        private static void Init()
+        {
+            Application.Log($"The application started at {DateTime.Now:HH:mm:ss.fff}");
+
+            Bitmap bitmap = new Bitmap();
+
+            var type = typeof(Program);
+
+            var resources = type.Module.Assembly.GetManifestResourceNames();
+            LogUtils.Log(resources);
+
+            var stream = type.Module.Assembly.GetManifestResourceStream(
+                "CustomCursor.CUR.HideWhiteSpace.cur");
+            if (stream is null)
+                return;
+            var result = bitmap.Load(stream, BitmapType.Any);
+            if (!result)
+                Application.Log("Error loading sample cursor");
+
+            var form = Application.FirstWindow<WindowLogListBox>();
+            if (form is null)
+                return;
+            form.ListBox.Cursor = new(bitmap);
+        }
+    }
+}
