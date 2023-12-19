ac4e6dc5fd2efcb5abfe581e10239710585fd547
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-19 16:00:15 +0200

    Update ScrollEventType.cs

--- Source/Alternet.UI/Controls/BaseControls/ScrollBar/ScrollEventType.cs
+++ Source/Alternet.UI/Controls/BaseControls/ScrollBar/ScrollEventType.cs
@@ -15,51 +15,51 @@ namespace Alternet.UI
         /// The scroll thumb was moved a small distance.
         /// The user clicked the left or top scroll arrow,
         /// or pressed the <see cref="Key.Up"/>key.</summary>
-        SmallDecrement,
+        SmallDecrement = 0,
 
         /// <summary>
         /// The scroll thumb was moved a small distance.
         /// The user clicked the right or bottom scroll arrow,
         /// or pressed the <see cref="Key.Down"/> key.
         /// </summary>
-        SmallIncrement,
+        SmallIncrement = 1,
 
         /// <summary>
         /// The scroll thumb was moved a large distance.
         /// The user clicked the scroll bar to the left or above the scroll thumb,
         /// or pressed the <see cref="Key.PageUp"/> key.</summary>
-        LargeDecrement,
+        LargeDecrement = 2,
 
         /// <summary>
         /// The scroll thumb was moved a large distance.
         /// The user clicked the scroll bar to the right or below
         /// the scroll thumb, or pressed the <see cref="Key.PageDown"/> key.
         /// </summary>
-        LargeIncrement,
+        LargeIncrement = 3,
 
         /// <summary>
         /// The scroll thumb was moved.
         /// </summary>
-        ThumbPosition,
+        ThumbPosition = 4,
 
         /// <summary>
         /// The scroll thumb is currently being moved.
         /// </summary>
-        ThumbTrack,
+        ThumbTrack = 5,
 
         /// <summary>
         /// The scroll thumb was moved to the min position.
         /// </summary>
-        First,
+        First = 6,
 
         /// <summary>
         /// The scroll thumb was moved to the max position.
         /// </summary>
-        Last,
+        Last = 7,
 
         /// <summary>
         /// The scroll thumb has stopped moving.
         /// </summary>
-        EndScroll,
+        EndScroll = 8,
     }
 }
