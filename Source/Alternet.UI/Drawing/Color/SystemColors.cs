aa3cceecf511588d8d1adc77d9000d063ba07449
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-25 13:50:25 +0200

    SystemColors made all colors non readonly

--- Source/Alternet.UI/Drawing/Color/SystemColors.cs
+++ Source/Alternet.UI/Drawing/Color/SystemColors.cs
@@ -9,186 +9,186 @@ namespace Alternet.Drawing
         /// of the active window's border.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the
         /// active window's border.</returns>
-        public static readonly Color ActiveBorder = new(KnownColor.ActiveBorder);
+        public static Color ActiveBorder = new(KnownColor.ActiveBorder);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color
         /// of the background of the active window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the
         /// active window's title bar.</returns>
-        public static readonly Color ActiveCaption = new(KnownColor.ActiveCaption);
+        public static Color ActiveCaption = new(KnownColor.ActiveCaption);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color
         /// of the text in the active window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the text in
         /// the active window's title bar.</returns>
-        public static readonly Color ActiveCaptionText = new(KnownColor.ActiveCaptionText);
+        public static Color ActiveCaptionText = new(KnownColor.ActiveCaptionText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color
         /// of the application workspace. </summary>
         /// <returns>A <see cref="Color" /> that is the color of the
         /// application workspace.</returns>
-        public static readonly Color AppWorkspace = new(KnownColor.AppWorkspace);
+        public static Color AppWorkspace = new(KnownColor.AppWorkspace);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the face
         /// color of a 3-D element.</summary>
         /// <returns>A <see cref="Color" /> that is the face color of
         /// a 3-D element.</returns>
-        public static readonly Color ButtonFace = new(KnownColor.ButtonFace);
+        public static Color ButtonFace = new(KnownColor.ButtonFace);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the
         /// highlight color of a 3-D element. </summary>
         /// <returns>A <see cref="Color" /> that is the highlight color
         /// of a 3-D element.</returns>
-        public static readonly Color ButtonHighlight = new(KnownColor.ButtonHighlight);
+        public static Color ButtonHighlight = new(KnownColor.ButtonHighlight);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the shadow
         /// color of a 3-D element. </summary>
         /// <returns>A <see cref="Color" /> that is the shadow color of
         /// a 3-D element.</returns>
-        public static readonly Color ButtonShadow = new(KnownColor.ButtonShadow);
+        public static Color ButtonShadow = new(KnownColor.ButtonShadow);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the
         /// face color of a 3-D element.</summary>
         /// <returns>A <see cref="Color" /> that is the face color of
         /// a 3-D element.</returns>
-        public static readonly Color Control = new(KnownColor.Control);
+        public static Color Control = new(KnownColor.Control);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the shadow color of a 3-D
         /// element. </summary>
         /// <returns>A <see cref="Color" /> that is the shadow color of a 3-D element.</returns>
-        public static readonly Color ControlDark = new(KnownColor.ControlDark);
+        public static Color ControlDark = new(KnownColor.ControlDark);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the dark shadow color
         /// of a 3-D element. </summary>
         /// <returns>A <see cref="Color" /> that is the dark shadow color of a 3-D element.</returns>
-        public static readonly Color ControlDarkDark = new(KnownColor.ControlDarkDark);
+        public static Color ControlDarkDark = new(KnownColor.ControlDarkDark);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the light color of a 3-D
         /// element. </summary>
         /// <returns>A <see cref="Color" /> that is the light color of a 3-D element.</returns>
-        public static readonly Color ControlLight = new(KnownColor.ControlLight);
+        public static Color ControlLight = new(KnownColor.ControlLight);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the highlight color of
         /// a 3-D element. </summary>
         /// <returns>A <see cref="Color" /> that is the highlight color of a 3-D element.</returns>
-        public static readonly Color ControlLightLight = new(KnownColor.ControlLightLight);
-
-        /// <summary>Gets a <see cref="Color" /> structure that is the color of text in a
-        /// 3-D element.</summary>
-        /// <returns>A <see cref="Color" /> that is the color of text in a 3-D element.</returns>
-        public static readonly Color ControlText = new(KnownColor.ControlText);
+        public static Color ControlLightLight = new(KnownColor.ControlLightLight);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the desktop.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the desktop.</returns>
-        public static readonly Color Desktop = new(KnownColor.Desktop);
+        public static Color Desktop = new(KnownColor.Desktop);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the lightest color in the
         /// color gradient of an active window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the lightest color in the color gradient
         /// of an active window's title bar.</returns>
-        public static readonly Color GradientActiveCaption = new(KnownColor.GradientActiveCaption);
+        public static Color GradientActiveCaption = new(KnownColor.GradientActiveCaption);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the lightest color in the
         /// color gradient of an inactive window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the lightest color in the color gradient
         /// of an inactive window's title bar.</returns>
-        public static readonly Color GradientInactiveCaption = new(KnownColor.GradientInactiveCaption);
+        public static Color GradientInactiveCaption = new(KnownColor.GradientInactiveCaption);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of dimmed
         /// text. </summary>
         /// <returns>A <see cref="Color" /> that is the color of dimmed text.</returns>
-        public static readonly Color GrayText = new(KnownColor.GrayText);
+        public static Color GrayText = new(KnownColor.GrayText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the background
         /// of selected items.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background of selected
         /// items.</returns>
-        public static readonly Color Highlight = new(KnownColor.Highlight);
+        public static Color Highlight = new(KnownColor.Highlight);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the text of
         /// selected items.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the text of selected
         /// items.</returns>
-        public static readonly Color HighlightText = new(KnownColor.HighlightText);
+        public static Color HighlightText = new(KnownColor.HighlightText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color used to designate
         /// a hot-tracked item. </summary>
         /// <returns>A <see cref="Color" /> that is the color used to designate a hot-tracked
         /// item.</returns>
-        public static readonly Color HotTrack = new(KnownColor.HotTrack);
+        public static Color HotTrack = new(KnownColor.HotTrack);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of an inactive
         /// window's border.</summary>
         /// <returns>A <see cref="Color" /> that is the color of an inactive window's
         /// border.</returns>
-        public static readonly Color InactiveBorder = new(KnownColor.InactiveBorder);
+        public static Color InactiveBorder = new(KnownColor.InactiveBorder);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the background
         /// of an inactive window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background of an
         /// inactive window's title bar.</returns>
-        public static readonly Color InactiveCaption = new(KnownColor.InactiveCaption);
+        public static Color InactiveCaption = new(KnownColor.InactiveCaption);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the text
         /// in an inactive window's title bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the text in an inactive
         /// window's title bar.</returns>
-        public static readonly Color InactiveCaptionText = new(KnownColor.InactiveCaptionText);
+        public static Color InactiveCaptionText = new(KnownColor.InactiveCaptionText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the background
         /// of a ToolTip.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background of a
         /// ToolTip.</returns>
-        public static readonly Color Info = new (KnownColor.Info);
+        public static Color Info = new (KnownColor.Info);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the text
         /// of a ToolTip.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the text of a ToolTip.</returns>
-        public static readonly Color InfoText = new(KnownColor.InfoText);
+        public static Color InfoText = new(KnownColor.InfoText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of a menu's
         /// background.</summary>
         /// <returns>A <see cref="Color" /> that is the color of a menu's background.</returns>
-        public static readonly Color Menu = new(KnownColor.Menu);
+        public static Color Menu = new(KnownColor.Menu);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the
         /// background of a menu bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background of a menu
         /// bar.</returns>
-        public static readonly Color MenuBar = new(KnownColor.MenuBar);
+        public static Color MenuBar = new(KnownColor.MenuBar);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color used to highlight
         /// menu items when the menu appears as a flat menu.</summary>
         /// <returns>A <see cref="Color" /> that is the color used to highlight menu items
         /// when the menu appears as a flat menu.</returns>
-        public static readonly Color MenuHighlight = new(KnownColor.MenuHighlight);
+        public static Color MenuHighlight = new(KnownColor.MenuHighlight);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of a menu's
         /// text.</summary>
         /// <returns>A <see cref="Color" /> that is the color of a menu's text.</returns>
-        public static readonly Color MenuText = new(KnownColor.MenuText);
+        public static Color MenuText = new(KnownColor.MenuText);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the background
         /// of a scroll bar.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background of a scroll
         /// bar.</returns>
-        public static readonly Color ScrollBar = new(KnownColor.ScrollBar);
+        public static Color ScrollBar = new(KnownColor.ScrollBar);
+
+        /// <summary>Gets a <see cref="Color" /> structure that is the color of
+        /// a window frame.</summary>
+        /// <returns>A <see cref="Color" /> that is the color of a window frame.</returns>
+        public static Color WindowFrame = new(KnownColor.WindowFrame);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the background
         /// in the client area of a window.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the background in the client
         /// area of a window.</returns>
-        public static readonly Color Window = new(KnownColor.Window);
-
-        /// <summary>Gets a <see cref="Color" /> structure that is the color of
-        /// a window frame.</summary>
-        /// <returns>A <see cref="Color" /> that is the color of a window frame.</returns>
-        public static readonly Color WindowFrame = new(KnownColor.WindowFrame);
+        public static Color Window = new(KnownColor.Window);
 
         /// <summary>Gets a <see cref="Color" /> structure that is the color of the text in the
         /// client area of a window.</summary>
         /// <returns>A <see cref="Color" /> that is the color of the text in the client area
         /// of a window.</returns>
-        public static readonly Color WindowText = new(KnownColor.WindowText);
+        public static Color WindowText = new(KnownColor.WindowText);
+
+        /// <summary>Gets a <see cref="Color" /> structure that is the color of text in a
+        /// 3-D element.</summary>
+        /// <returns>A <see cref="Color" /> that is the color of text in a 3-D element.</returns>
+        public static Color ControlText = new(KnownColor.ControlText);
     }
 }
