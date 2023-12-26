a1eac40229879949e001594d69a3b84d406f0dfb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-26 12:17:39 +0200

    Update MethodExtensions.cs

--- Source/Alternet.UI/Utils/MethodExtensions.cs
+++ Source/Alternet.UI/Utils/MethodExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,24 @@ namespace Alternet.UI.Extensions
     /// </summary>
     public static class MethodExtensions
     {
+        /// <summary>
+        /// Gets whether <see cref="DockStyle"/> equals <see cref="DockStyle.Top"/> or
+        /// <see cref="DockStyle.Bottom"/>.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsTopOrBottom(this DockStyle dock)
+        {
+            return dock == DockStyle.Bottom || dock == DockStyle.Top;
+        }
+
+        /// <summary>
+        /// Gets whether <see cref="DockStyle"/> equals <see cref="DockStyle.Left"/> or
+        /// <see cref="DockStyle.Right"/>.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsLeftOrRight(this DockStyle dock)
+        {
+            return dock == DockStyle.Left || dock == DockStyle.Right;
+        }
     }
 }
