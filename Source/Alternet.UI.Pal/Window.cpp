3999c85400fb471707d904522d6b96f426ce452c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-28 11:36:35 +0200

    Disabled Window maximize on Linux

--- Source/Alternet.UI.Pal/Window.cpp
+++ Source/Alternet.UI.Pal/Window.cpp
@@ -517,8 +517,10 @@ namespace Alternet::UI
         if (GetMinimizeEnabled())
             style |= wxMINIMIZE_BOX;
 
+#ifndef __WXGTK__
         if (GetMaximizeEnabled())
             style |= wxMAXIMIZE_BOX;
+#endif
 
         if (GetCloseEnabled())
             style |= wxCLOSE_BOX;
