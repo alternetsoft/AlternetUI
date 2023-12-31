805455f642bf818c4088766365d2959b62a779ca
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2023-12-31 08:07:36 +0200

    SpeedButton

---
+++ Source/Alternet.UI/Controls/BaseControls/SpeedButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements speed button control.
+    /// </summary>
+    public class SpeedButton : PictureBox
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpeedButton"/> class.
+        /// </summary>
+        public SpeedButton()
+        {
+            AcceptsFocusAll = false;
+            ImageStretch = false;
+            Borders ??= new();
+
+            var border = BorderSettings.Default.Clone();
+            border.UniformCornerRadius = 50;
+            border.UniformRadiusIsPercent = true;
+            Borders.SetObject(border, GenericControlState.Hovered);
+            Borders.SetObject(border, GenericControlState.Pressed);
+        }
+    }
+}
