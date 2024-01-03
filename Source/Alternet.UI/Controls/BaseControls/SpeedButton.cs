832bc786f1c9d043a060c16ce53433ca3c5db8d2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-01-03 11:59:57 +0200

    SpeedButton.ClickAction

--- Source/Alternet.UI/Controls/BaseControls/SpeedButton.cs
+++ Source/Alternet.UI/Controls/BaseControls/SpeedButton.cs
@@ -13,6 +13,8 @@ namespace Alternet.UI
     /// </summary>
     public class SpeedButton : PictureBox
     {
+        private Action? clickAction;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SpeedButton"/> class.
         /// </summary>
@@ -27,6 +29,36 @@ namespace Alternet.UI
             border.UniformRadiusIsPercent = true;
             Borders.SetObject(border, GenericControlState.Hovered);
             Borders.SetObject(border, GenericControlState.Pressed);
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="Action"/> which will be executed when
+        /// this control is clicked by the user.
+        /// </summary>
+        [Browsable(false)]
+        public Action? ClickAction
+        {
+            get => clickAction;
+            set
+            {
+                if (clickAction != null)
+                    Click -= OnClickAction;
+                clickAction = value;
+                if (clickAction != null)
+                    Click += OnClickAction;
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnMouseLeftButtonDown(MouseEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            RaiseClick(EventArgs.Empty);
+        }
+
+        private void OnClickAction(object? sender, EventArgs? e)
+        {
+            clickAction?.Invoke();
         }
     }
 }
