3740e5a0aee67336caa54bb91b074102686f135d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-01-10 11:08:41 +0200

    Create GenericToolBarSet.cs

---
+++ Source/Samples/PropertyGridSample/GenericToolBarSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public class GenericToolBarSet : VerticalStackPanel
+    {
+        public GenericToolBarSet()
+        {
+            ToolBarCount = 1;
+        }
+
+        public virtual int ToolBarCount
+        {
+            get
+            {
+                return Children.Count;
+            }
+
+            set
+            {
+                value = Math.Max(value, 1);
+                if (value == ToolBarCount)
+                    return;
+                Children.SetCount(value, CreateToolBar);
+            }
+        }
+
+        public virtual GenericToolBar GetToolBar(int index)
+        {
+            if (ToolBarCount <= index)
+                ToolBarCount = index + 1;
+            return (GenericToolBar)Children[index];
+        }
+
+        protected virtual GenericToolBar CreateToolBar()
+        {
+            GenericToolBar result = new();
+            if(Children.Count > 0)
+            {
+                result.Margin = (0, 4, 0, 0);
+            }
+
+            return result;
+        }
+    }
+}
