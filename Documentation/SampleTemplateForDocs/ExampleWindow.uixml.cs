3d8a202a2ddb654753058df48283f99eb944eb94
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-01-27 09:24:16 +0200

    Documentation

---
+++ Documentation/SampleTemplateForDocs/ExampleWindow.uixml.cs
@@ -0,0 +1,19 @@
+using Alternet.UI;
+using System;
+
+namespace Alternet.UI.Documentation.Examples
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Example1()
+        {
+            #region CSharpCreation
+            #endregion
+        }
+    }
+}
