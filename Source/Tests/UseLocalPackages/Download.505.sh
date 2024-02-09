8914b920de9e81e8cb11473e0dc93e41d2d7ad6d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-02-09 11:42:06 +0200

    Update Download.505.sh

--- Source/Tests/UseLocalPackages/Download.505.sh
+++ Source/Tests/UseLocalPackages/Download.505.sh
@@ -6,9 +6,9 @@ rm -rf LocalPackages/*.snupkg
 
 pushd LocalPackages
 
-wget "https://drive.google.com/uc?export=download&id=1JwhP5nZGqQ7K7YQE8dLwbsNUuvrsHbdP"
-wget "https://drive.google.com/uc?export=download&id=1YV8tVKQJ3FCucbkUuzNY91WISUkLw1pe"
-wget "https://drive.google.com/uc?export=download&id=1qTHthghA3M97O78xLEX_KoMlcEedWVl1"
+wget "https://drive.google.com/uc?export=download&id=1JwhP5nZGqQ7K7YQE8dLwbsNUuvrsHbdP" -O "Alternet.UI.0.9.505-beta.nupkg"
+wget "https://drive.google.com/uc?export=download&id=1YV8tVKQJ3FCucbkUuzNY91WISUkLw1pe" -O "Alternet.UI.0.9.505-beta.snupkg"
+wget "https://drive.google.com/uc?export=download&id=1qTHthghA3M97O78xLEX_KoMlcEedWVl1" -O "Alternet.UI.Pal.0.9.505-beta.nupkg"
 
 popd
 
