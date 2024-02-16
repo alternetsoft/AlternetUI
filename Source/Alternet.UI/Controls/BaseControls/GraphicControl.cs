6b4b6f04c3a9a69f499373b26f7ec02e78adac7a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-02-16 07:47:05 +0200

    Create GraphicControl.cs

---
+++ Source/Alternet.UI/Controls/BaseControls/GraphicControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Descendant of the <see cref="UserPaintControl"/> which doesn't need to have focus.
+    /// An example of <see cref="GraphicControl"/> is <see cref="GenericLabel"/>.
+    /// </summary>
+    public class GraphicControl : UserPaintControl
+    {
+        /// <inheritdoc/>
+        internal override ControlHandler CreateHandler()
+        {
+            return new GraphicControlHandler();
+        }
+
+        internal class GraphicControlHandler : ControlHandler
+        {
+            internal override Native.Control CreateNativeControl()
+            {
+                var result = new Native.Panel
+                {
+                    AcceptsFocusAll = false,
+                };
+                return result;
+            }
+        }
+    }
+}
