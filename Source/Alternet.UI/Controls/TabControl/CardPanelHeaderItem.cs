c8cc5f98ee8a4e8242c8e60bf76375c00926a43f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-02-21 14:10:27 +0200

    CardPanelHeaderItem: Image, ImageSet, DisabledImage

--- Source/Alternet.UI/Controls/TabControl/CardPanelHeaderItem.cs
+++ Source/Alternet.UI/Controls/TabControl/CardPanelHeaderItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Alternet.Drawing;
 
 namespace Alternet.UI
 {
@@ -37,6 +38,62 @@ namespace Alternet.UI
         {
             get => cardControl;
             set => cardControl = value;
+        }
+
+        /// <summary>
+        /// Gets or sets disabled tab image.
+        /// </summary>
+        public virtual Image? DisabledImage
+        {
+            get => HeaderButton?.DisabledImage;
+            set
+            {
+                if (HeaderButton is null)
+                    return;
+                HeaderButton.DisabledImage = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets disabled tab image.
+        /// </summary>
+        public virtual ImageSet? DisabledImageSet
+        {
+            get => HeaderButton?.DisabledImageSet;
+            set
+            {
+                if (HeaderButton is null)
+                    return;
+                HeaderButton.DisabledImageSet = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets tab image.
+        /// </summary>
+        public virtual Image? Image
+        {
+            get => HeaderButton?.Image;
+            set
+            {
+                if(HeaderButton is null)
+                    return;
+                HeaderButton.Image = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets tab image.
+        /// </summary>
+        public virtual ImageSet? ImageSet
+        {
+            get => HeaderButton?.ImageSet;
+            set
+            {
+                if (HeaderButton is null)
+                    return;
+                HeaderButton.ImageSet = value;
+            }
         }
 
         /// <summary>
