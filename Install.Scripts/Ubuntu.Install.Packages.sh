a6e692b76d4fe827de812004ffd02c3b68bc2edb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-02-25 12:15:36 +0200

    Update Ubuntu.Install.Packages.sh

--- Install.Scripts/Ubuntu.Install.Packages.sh
+++ Install.Scripts/Ubuntu.Install.Packages.sh
@@ -21,5 +21,7 @@ sudo apt-get install -y gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gst
 sudo apt-get install -y gstreamer1.0-gl 
 sudo apt-get install -y gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
+sudo apt-get -y install osspd
+
 #wxWidgets Cross-platform C++ GUI toolkit
 #sudo apt-get install -y libwxgtk3.0-gtk3-dev libwxgtk-media3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev
