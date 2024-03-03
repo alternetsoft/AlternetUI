0e17d6103877da537bb4cc5d0cce7b333cf207d7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-03 07:36:43 +0200

    Update CalendarPage.cs

--- Source/Samples/ControlsSample/Pages/DateTime/CalendarPage.cs
+++ Source/Samples/ControlsSample/Pages/DateTime/CalendarPage.cs
@@ -20,78 +20,97 @@ namespace ControlsSample
 
         public CalendarPage()
         {
-            mainPanel.Parent = this;
+            DoInsideLayout(Fn);
+
+            void Fn()
+            {
+
+                mainPanel.Parent = this;
+
+                calendar.Margin = 5;
+                calendar.HorizontalAlignment = HorizontalAlignment.Left;
+                calendar.VerticalAlignment = VerticalAlignment.Top;
+
+                var calendarPanel = mainPanel.AddHorizontalStackPanel();
+
+                calendar.Parent = calendarPanel;
+
+                var optionsPanel = mainPanel.AddHorizontalStackPanel();
+
+                // Allow date range panel
+
+                var rangePanel = optionsPanel.AddVerticalStackPanel();
+                rangePanel.AddButtons(
+                    ("Allow Any Date", RangeAnyDate_Click),
+                    ("Allow <= Tomorrow", RangeTomorrow_Click),
+                    ("Allow >= Yesterday", RangeYesterday_Click),
+                    ("Allow Yesterday..Tomorrow", RangeYesterdayTomorrow_Click)).Margin(5);
+
+                // CheckBoxes panel
+
+                var checkboxPanel = optionsPanel.AddVerticalStackPanel();
+
+                var showHolidaysCheckBox = checkboxPanel.AddCheckBox("Show Holidays")
+                    .BindBoolProp(calendar, nameof(Calendar.ShowHolidays));
+
+                var noMonthChangeCheckBox = checkboxPanel.AddCheckBox("No Month Change")
+                    .BindBoolProp(calendar, nameof(Calendar.NoMonthChange));
+
+                var useGenericCheckBox = checkboxPanel.AddCheckBox("Use Generic")
+                    .BindBoolProp(calendar, nameof(Calendar.UseGeneric));
+
+                var sequentalMonthSelectCheckBox = checkboxPanel.AddCheckBox("Sequental Month Select")
+                    .BindBoolProp(calendar, nameof(Calendar.SequentalMonthSelect));
+                sequentalMonthSelectCheckBox.Enabled = useGenericCheckBox.IsChecked;
+
+                var showSurroundWeeksCheckBox = checkboxPanel.AddCheckBox("Show Surround Weeks")
+                    .BindBoolProp(calendar, nameof(Calendar.ShowSurroundWeeks));
+                showSurroundWeeksCheckBox.Enabled = useGenericCheckBox.IsChecked;
+
+                var weekNumbersCheckBox = checkboxPanel.AddCheckBox("Week Numbers")
+                    .BindBoolProp(calendar, nameof(Calendar.ShowWeekNumbers));
+                checkboxPanel.ChildrenSet.Margin(3);
+
+                // Buttons panel
+
+                var buttonPanel = calendarPanel.AddVerticalStackPanel();
+                var setDayColorsButton = buttonPanel.AddButton("Days (5, 7) style", SetDayColors);
+                setDayColorsButton.Enabled = useGenericCheckBox.IsChecked;
+
+                buttonPanel.AddButton("Mark days (2, 3)", MarkDays);
+                buttonPanel.AddButton("Today", calendar.SelectToday);
+
+                buttonPanel.ChildrenSet.Margin(5);
+
+                // Other initializations
+
+                useGenericCheckBox.BindBoolProp(setDayColorsButton, nameof(Button.Enabled));
+                useGenericCheckBox.BindBoolProp(sequentalMonthSelectCheckBox, nameof(Button.Enabled));
+                useGenericCheckBox.BindBoolProp(showSurroundWeeksCheckBox, nameof(Button.Enabled));
+
+                calendar.RecreateWindow();
+                calendar.PerformLayout();
+
+                useGenericCheckBox.CheckedChanged += Generic_CheckedChanged;
+
+                void Generic_CheckedChanged(object? sender, EventArgs e)
+                {
+                    showHolidaysCheckBox.IsChecked = calendar.ShowHolidays;
+                    noMonthChangeCheckBox.IsChecked = calendar.NoMonthChange;
+                    sequentalMonthSelectCheckBox.IsChecked = calendar.SequentalMonthSelect;
+                    showSurroundWeeksCheckBox.IsChecked = calendar.ShowSurroundWeeks;
+                    weekNumbersCheckBox.IsChecked = calendar.ShowWeekNumbers;
+
+                    if (calendar.UseGeneric)
+                        calendar.BackgroundColor = SystemColors.Window;
+                }
+            }
 
             calendar.SelectionChanged += Calendar_SelectionChanged;
             calendar.PageChanged += Calendar_PageChanged;
             calendar.WeekNumberClick += Calendar_WeekNumberClick;
             calendar.DayHeaderClick += Calendar_DayHeaderClick;
             calendar.DayDoubleClick += Calendar_DayDoubleClick;
-            calendar.Margin = 5;
-            calendar.HorizontalAlignment = HorizontalAlignment.Left;
-            calendar.VerticalAlignment = VerticalAlignment.Top;
-
-            var optionsPanel = mainPanel.AddHorizontalStackPanel();
-
-            // CheckBoxes panel
-            
-            var checkboxPanel = optionsPanel.AddVerticalStackPanel();
-
-            var showHolidaysCheckBox = checkboxPanel.AddCheckBox("Show Holidays")
-                .BindBoolProp(calendar,nameof(Calendar.ShowHolidays));
-            
-            var noMonthChangeCheckBox = checkboxPanel.AddCheckBox("No Month Change")
-                .BindBoolProp(calendar, nameof(Calendar.NoMonthChange));
-            
-            var useGenericCheckBox = checkboxPanel.AddCheckBox("Use Generic")
-                .BindBoolProp(calendar, nameof(Calendar.UseGeneric));
-
-            var sequentalMonthSelectCheckBox = checkboxPanel.AddCheckBox("Sequental Month Select")
-                .BindBoolProp(calendar, nameof(Calendar.SequentalMonthSelect));
-            sequentalMonthSelectCheckBox.Enabled = useGenericCheckBox.IsChecked;
-            
-            var showSurroundWeeksCheckBox = checkboxPanel.AddCheckBox("Show Surround Weeks")
-                .BindBoolProp(calendar, nameof(Calendar.ShowSurroundWeeks));
-            showSurroundWeeksCheckBox.Enabled = useGenericCheckBox.IsChecked;
-            
-            var weekNumbersCheckBox = checkboxPanel.AddCheckBox("Week Numbers")
-                .BindBoolProp(calendar, nameof(Calendar.ShowWeekNumbers));
-            checkboxPanel.ChildrenSet.Margin(5);
-
-            // Buttons panel
-
-            var buttonPanel = optionsPanel.AddVerticalStackPanel();
-            var setDayColorsButton = buttonPanel.AddButton("Days (5, 7) style", SetDayColors);
-            setDayColorsButton.Enabled = useGenericCheckBox.IsChecked;
-
-            buttonPanel.AddButton("Mark days (2, 3)", MarkDays);
-            buttonPanel.AddButton("Today", calendar.SelectToday);
-
-            buttonPanel.ChildrenSet.Margin(5);
-
-            // Allow date range panel
-
-            var rangePanel = optionsPanel.AddGroupBox("Allow Date Range");
-            rangePanel.Layout = LayoutStyle.Vertical;
-
-            rangePanel.AddButton("Any Date", RangeAnyDate_Click);
-            rangePanel.AddButton("<= Tomorrow", RangeTomorrow_Click);
-            rangePanel.AddButton(">= Yesterday", RangeYesterday_Click);
-            rangePanel.AddButton("Yesterday..Tomorrow", RangeYesterdayTomorrow_Click);
-
-            rangePanel.ChildrenSet.Margin(5);
-
-            // Other initializations
-
-            useGenericCheckBox.BindBoolProp(setDayColorsButton, nameof(Button.Enabled));
-            useGenericCheckBox.BindBoolProp(sequentalMonthSelectCheckBox, nameof(Button.Enabled));
-            useGenericCheckBox.BindBoolProp(showSurroundWeeksCheckBox, nameof(Button.Enabled));
-
-            calendar.Parent = mainPanel;
-            calendar.RecreateWindow();
-            calendar.PerformLayout();
-
-            useGenericCheckBox.CheckedChanged += Generic_CheckedChanged;
 
             void MarkDays()
             {
@@ -113,19 +132,6 @@ namespace ControlsSample
                 calendar.SetAttr(7, dateAttr);
                 calendar.Refresh();
             }
-
-            void Generic_CheckedChanged(object? sender, EventArgs e)
-            {
-                showHolidaysCheckBox.IsChecked = calendar.ShowHolidays;
-                noMonthChangeCheckBox.IsChecked = calendar.NoMonthChange;
-                sequentalMonthSelectCheckBox.IsChecked = calendar.SequentalMonthSelect;
-                showSurroundWeeksCheckBox.IsChecked = calendar.ShowSurroundWeeks;
-                weekNumbersCheckBox.IsChecked = calendar.ShowWeekNumbers;
-
-                if (calendar.UseGeneric)
-                    calendar.BackgroundColor = SystemColors.Window;
-            }
-
         }
 
         private void RangeAnyDate_Click()
