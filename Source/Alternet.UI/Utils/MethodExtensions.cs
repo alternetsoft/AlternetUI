bc33cefb4f9573dd8ff2e38ac787053761f39b69
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-05 12:13:08 +0200

    Update MethodExtensions.cs

--- Source/Alternet.UI/Utils/MethodExtensions.cs
+++ Source/Alternet.UI/Utils/MethodExtensions.cs
@@ -28,6 +28,7 @@ namespace Alternet.UI.Extensions
         /// <typeparam name="T">Type of the item</typeparam>
         /// <param name="stack"><see cref="Stack{T}"/> instance.</param>
         /// <param name="items">Items to push.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void PushRange<T>(this Stack<T> stack, IEnumerable<T> items)
         {
             foreach (var item in items)
@@ -38,9 +39,21 @@ namespace Alternet.UI.Extensions
         /// Removes underscore characters ('_') from string.
         /// </summary>
         /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string RemoveUnderscore(this string s)
         {
             return s.Replace("_", string.Empty);
+        }
+
+        /// <summary>
+        /// Trims end of line characters from the string.
+        /// </summary>
+        /// <param name="s">String.</param>
+        /// <returns>String without all end of line characters.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string TrimEndEol(this string s)
+        {
+            return s.TrimEnd('\r', '\n');
         }
 
         /// <summary>
