185ef201969f0d14b0586d44074cebf5fdfdf322
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-06 06:40:30 +0200

    CustomControlsWindow: Add ColorComboBox, etc.

--- Source/Samples/ControlsSample/InternalSamples/CustomControlsSample/CustomControlsWindow.uixml.cs
+++ Source/Samples/ControlsSample/InternalSamples/CustomControlsSample/CustomControlsWindow.uixml.cs
@@ -8,123 +8,74 @@ namespace CustomControlsSample
 {
     internal partial class CustomControlsWindow : Window
     {
-        private readonly Data data = new();
-        private readonly SpeedColorButton colorPicker;
-        private readonly TicTacToeControl ticTacToe;
-        private readonly FancySlider knobControl;
-        private readonly FancyProgressBar gaugeControl;
-        private readonly ListBox logListBox;
+        private readonly SpeedColorButton colorPicker = new()
+        {
+            Value = Color.Red,
+            HorizontalAlignment = HorizontalAlignment.Left,
+            MinimumSize = 32,
+            VerticalAlignment = VerticalAlignment.Center,
+        };
+        private readonly ColorComboBox colorCombo = new()
+        {
+            Value = Color.Red,
+            VerticalAlignment = VerticalAlignment.Center,
+        };
+        private readonly Label colorComboLabel = new("ComboBox:")
+        {
+            VerticalAlignment = VerticalAlignment.Center,
+        };
+        private readonly TicTacToeControl ticTacToe = new()
+        {
+            Margin = 10,
+        };
+        private readonly FancySlider knobControl = new()
+        {
+            Minimum = 0,
+            Maximum = 100,
+            Margin = new Thickness(0, 0, 5, 0),
+        };
+        private readonly FancyProgressBar gaugeControl = new()
+        {
+            Minimum = 0,
+            Maximum = 100,
+            VerticalAlignment = VerticalAlignment.Center,
+            Margin = new Thickness(5, 0, 0, 0),
+        };
 
         public CustomControlsWindow()
         {
-            Name = "MainWindow";
-
             Icon = Application.DefaultIcon;
-
-            DataContext = data;
 
             InitializeComponent();
 
-            colorPicker = new()
+            DoInsideLayout(Fn);
+
+            void Fn()
             {
-                Value = Color.Red,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                MinimumSize = 32,
-                Margin = (0,0,0,5),
-            };
-
-            /*colorPicker.InitSolidBorder();*/
-
-            ticTacToe = new()
-            {
-                Margin = new Thickness(10, 10, 10, 10)
-            };
-
-            knobControl = new()
-            {
-                Minimum = 0,
-                Maximum = 100,
-                Margin = new Thickness(0, 0, 5, 0),
-            };
-
-            gaugeControl = new()
-            {
-                Minimum = 0,
-                Maximum = 100,
-                VerticalAlignment = VerticalAlignment.Center,
-                Margin = new Thickness(5,0,0,0),
-            };
-
-            logListBox = new()
-            {
-                Margin = 5,
-                SuggestedWidth = 150,
-                SuggestedHeight = 100,
-                Visible = false,
-            };
-
-            Binding myBinding = new ("IntValue") 
-            { 
-                Mode = BindingMode.TwoWay,
-            };            
-
-            BindingOperations.SetBinding(
-                gaugeControl, 
-                FancyProgressBar.ValueProperty, 
-                myBinding);
-            BindingOperations.SetBinding(
-                knobControl,
-                FancySlider.ValueProperty,
-                myBinding);
-            data.PropertyChanged += Data_PropertyChanged;
-
-            SuspendLayout();
-            colorPickerStackPanel.Children.Add(colorPicker);
-            ticTacToeStackPanel.Children.Add(ticTacToe);
-            slidersStackPanel.Children.Add(knobControl);
-            slidersStackPanel.Children.Add(gaugeControl);
-
-            logListBox.Parent = ticTacToeStackPanel;
-
-            ResumeLayout(true);
+                nativeColorPicker.SuggestedSize = 32;
+                colorPicker.Parent = colorGroupBox;
+                colorComboLabel.Parent = colorGroupBox;
+                colorCombo.Parent = colorGroupBox;
+                ticTacToe.Parent = ticTacToeStackPanel;
+                knobControl.Parent = slidersStackPanel;
+                gaugeControl.Parent = slidersStackPanel;
+            }
             this.SetSizeToContent();
-
-            Application.Current.LogMessage += Current_LogMessage;            
+            Slider_ValueChanged(null, EventArgs.Empty);
+            knobControl.ValueChanged += Slider_ValueChanged;
         }
 
-        private void Data_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        private void Slider_ValueChanged(object? sender, EventArgs e)
         {
-            intValueLabel.Text = data.IntValue.ToString();
-        }
+            if (sender is not Slider slider)
+                return;
 
-        private void Current_LogMessage(object? sender, LogMessageEventArgs e)
-        {
-            logListBox.Add(e.Message!);
-            logListBox.SelectLastItem();
-        }
+            var v = slider.Value;
 
-        public static bool DisableCustomColorPopup { get; set; } = false;
-
-        class Data : INotifyPropertyChanged
-        {
-            private int intValue = 60;
-
-            public event PropertyChangedEventHandler? PropertyChanged;
-
-            public int IntValue
-            {
-                get => intValue;
-                set
-                {
-                    intValue = value;
-                    OnPropertyChanged();
-                }
-            }
-
-            void OnPropertyChanged([CallerMemberName] string propertyName = "")
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
+            gaugeControl.Value = v;
+            knobControl.Value = v;
+            progressBar1.Value = v;
+            slider1.Value = v;
         }
     }
 }
