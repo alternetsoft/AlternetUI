4902b3b5a0a8491d114de344d431ec54f06feb8a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-10 17:07:49 +0200

    Create ColorListBox.cs

---
+++ Source/Samples/ControlsSample/NewCode/ColorListBox.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// <see cref="ListBox"/> descendant for selecting <see cref="Color"/> values.
+    /// </summary>
+    /// <remarks>
+    /// Items in this control have <see cref="ListControlItem"/> type where
+    /// <see cref="ListControlItem.Value"/> is <see cref="Color"/> and
+    /// <see cref="ListControlItem.Text"/> is label of the color.
+    /// </remarks>
+    internal class ColorListBox : VListBox
+    {
+        /// <summary>
+        /// Gets or sets default painter for the <see cref="ColorListBox"/> items.
+        /// </summary>
+        public static IListBoxItemPainter Painter = new DefaultItemPainter();
+
+        /// <summary>
+        /// Gets or sets method that initializes items in <see cref="ColorListBox"/>.
+        /// </summary>
+        public static Action<ColorListBox>? InitColors = InitDefaultColors;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorListBox"/> class.
+        /// </summary>
+        public ColorListBox()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorListBox"/> class.
+        /// </summary>
+        /// <param name="defaultColors">Specifies whether to add default color items
+        /// to the control.</param>
+        public ColorListBox(bool defaultColors)
+        {
+            Initialize(defaultColors);
+        }
+
+        /// <summary>
+        /// Adds color items to the <see cref="ColorListBox"/>. This is default
+        /// implementation of the initialization method. It is assigned to
+        /// <see cref="InitColors"/> property by default.
+        /// </summary>
+        /// <param name="control">Control to initialize.</param>
+        public static void InitDefaultColors(ColorListBox control)
+        {
+            ListControlUtils.AddColors(control);
+        }
+
+        private void Initialize(bool defaultColors = true)
+        {
+            if (defaultColors)
+            {
+                if (InitColors is not null)
+                    InitColors(this);
+            }
+
+            ItemPainter = Painter;
+        }
+
+        /// <summary>
+        /// Default item painter for the <see cref="ColorListBoxBox"/> items.
+        /// </summary>
+        public class DefaultItemPainter : IListBoxItemPainter
+        {
+            public SizeD GetSize(VListBox sender, int index)
+            {
+                return sender.DefaultMeasureItemSize(index);
+            }
+
+            public void Paint(VListBox sender, ListBoxItemPaintEventArgs e)
+            {
+                object? item = sender.Items[e.ItemIndex];
+
+                if (item is ListControlItem item1)
+                    item = item1.Value;
+
+                var itemColor = (item as Color) ?? Color.White;
+
+                if (!itemColor.IsOk)
+                    itemColor = Color.White;
+
+                var (colorRect, itemRect) = sender.GetItemImageRect(e);
+                e.ClipRectangle = itemRect;
+                sender.DefaultDrawItem(e);
+                ColorComboBox.PaintColorImage(e.Graphics, colorRect, itemColor);
+            }
+        }
+    }
+}
