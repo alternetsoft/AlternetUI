12a96ce3a53bc47a3034b0a0eff5fc56b359d0a8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-11 04:51:32 +0200

    Control.GetUpdateClientRect

--- Source/NativeApi/NativeApi/Api/Controls/Control.cs
+++ Source/NativeApi/NativeApi/Api/Controls/Control.cs
@@ -8,6 +8,8 @@ namespace NativeApi.Api
 {
     public abstract class Control
     {
+        public RectI GetUpdateClientRect() => default;
+
         public static DrawingContext OpenClientDrawingContextForWindow(IntPtr window) => default;
         public static DrawingContext OpenPaintDrawingContextForWindow(IntPtr window) => default;
         public static DrawingContext OpenDrawingContextForDC(IntPtr dc, bool deleteDc) => default;
