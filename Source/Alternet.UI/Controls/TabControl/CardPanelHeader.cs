9e87d9dece2e2333580dcb1095250a8c18ee2f30
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-18 14:26:43 +0200

    TabControl: Increase default tab padding
    This is done because on normal dpi display distance was too small

--- Source/Alternet.UI/Controls/TabControl/CardPanelHeader.cs
+++ Source/Alternet.UI/Controls/TabControl/CardPanelHeader.cs
@@ -34,7 +34,7 @@ namespace Alternet.UI
         /// <summary>
         /// Gets or sets default value for the tab padding.
         /// </summary>
-        public static Thickness DefaultTabPadding = (5, 4, 5, 4);
+        public static Thickness DefaultTabPadding = (6, 5, 6, 5);
 
         /// <summary>
         /// Gets or sets default value of the <see cref="AdditionalSpace"/> property.
