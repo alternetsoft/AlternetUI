71fe695520dceb672dea552ac6b16f1826f4639b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-03-21 14:06:03 +0200

    Create HiddenWindow.cs

---
+++ Source/Alternet.UI/Controls/Window/HiddenWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// This <see cref="Window"/> descendant is always hidden.
+    /// </summary>
+    public class HiddenWindow : Window
+    {
+        /// <inheritdoc/>
+        public override bool Visible
+        {
+            get => base.Visible;
+            set => base.Visible = false;
+        }
+    }
+}
