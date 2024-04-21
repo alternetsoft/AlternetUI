b6ffdeb92775ab3341f567a712622473c8a82427
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-04-21 07:11:37 +0300

    Maui and SharpHook related

---
+++ Source/Alternet.UI.Maui/KeyboardDeviceSharpHook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Alternet.UI;
+
+namespace Alternet.UI.Maui
+{
+    public class KeyboardDeviceSharpHook : KeyboardDevice
+    {
+        public static KeyboardDevice Default = new KeyboardDeviceSharpHook();
+
+        public static KeyStates KeyStates;
+
+        protected override KeyStates GetKeyStatesFromSystem(Key key)
+        {
+            return KeyStates;
+        }
+    }
+}
---
+++ Source/Alternet.UI.Maui/SharpHookFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI;
+using SharpHook;
+
+namespace Alternet.UI.Maui
+{
+    internal class SharpHookFactory
+    {
+        private TaskPoolGlobalHook? hook;
+
+        public SharpHookFactory()
+        {
+            hook = new TaskPoolGlobalHook();
+
+            hook.KeyTyped += Hook_KeyTyped;
+            hook.KeyPressed += Hook_KeyPressed;
+            hook.KeyReleased += Hook_KeyReleased;
+
+            hook.MouseClicked += Hook_MouseClicked;
+            hook.MousePressed += Hook_MousePressed;
+            hook.MouseReleased += Hook_MouseReleased;
+            hook.MouseMoved += Hook_MouseMoved;
+            hook.MouseDragged += Hook_MouseDragged;
+            hook.MouseWheel += Hook_MouseWheel;
+
+            hook.RunAsync();
+        }
+
+        private void Hook_KeyTyped(object? sender, KeyboardHookEventArgs e)
+        {
+            char keyChar = e.Data.KeyChar;
+        }
+
+        private void Hook_KeyPressed(object? sender, KeyboardHookEventArgs e)
+        {
+            SharpHook.Native.KeyCode keyCode = e.Data.KeyCode;
+            SharpHook.Native.ModifierMask mask = e.RawEvent.Mask;
+        }
+
+        private void Hook_KeyReleased(object? sender, KeyboardHookEventArgs e)
+        {
+        }
+
+        private void Hook_MouseClicked(object? sender, MouseHookEventArgs e)
+        {
+        }
+
+        private void Hook_MousePressed(object? sender, MouseHookEventArgs e)
+        {
+        }
+
+        private void Hook_MouseReleased(object? sender, MouseHookEventArgs e)
+        {
+        }
+
+        private void Hook_MouseMoved(object? sender, MouseHookEventArgs e)
+        {
+        }
+
+        private void Hook_MouseDragged(object? sender, MouseHookEventArgs e)
+        {
+        }
+
+        private void Hook_MouseWheel(object? sender, MouseWheelHookEventArgs e)
+        {
+        }
+    }
+}
