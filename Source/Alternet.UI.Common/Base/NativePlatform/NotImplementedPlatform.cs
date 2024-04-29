e514b61d4a844ea55e6125452b95793de834e916
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-04-29 09:58:51 +0300

    Moved BaseMemory to UI.Common dll

--- Source/Alternet.UI.Common/Base/NativePlatform/NotImplementedPlatform.cs
+++ Source/Alternet.UI.Common/Base/NativePlatform/NotImplementedPlatform.cs
@@ -8,5 +8,59 @@ namespace Alternet.UI
 {
     internal class NotImplementedPlatform : NativePlatform
     {
+        public override IntPtr MemoryAlloc(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryAllocLong(ulong size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryCopy(IntPtr dest, IntPtr src, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryCopyLong(IntPtr dest, IntPtr src, ulong count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MemoryFree(IntPtr ptr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryMove(IntPtr dest, IntPtr src, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryMoveLong(IntPtr dest, IntPtr src, ulong count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryRealloc(IntPtr ptr, int newSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemoryReallocLong(IntPtr ptr, ulong newSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemorySet(IntPtr dest, byte fillByte, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntPtr MemorySetLong(IntPtr dest, byte fillByte, ulong count)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
