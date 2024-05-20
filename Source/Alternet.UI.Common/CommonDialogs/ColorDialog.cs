c8815a5389e55ff52a5f566f77ca3d6f637b8f65
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-05-20 08:29:42 +0300

    ColorDialog moved to common dll

---
+++ Source/Alternet.UI.Common/CommonDialogs/ColorDialog.cs
@@ -0,0 +1,42 @@
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Displays a dialog window from which the user can select a color.
+    /// </summary>
+    [ControlCategory("Dialogs")]
+    public class ColorDialog : CommonDialog
+    {
+        /// <summary>
+        /// Gets default <see cref="ColorDialog"/> instance.
+        /// </summary>
+        public static ColorDialog Default = defaultDialog ??= new ColorDialog();
+
+        private static ColorDialog? defaultDialog;
+
+        /// <summary>
+        /// Gets or sets the color selected by the color dialog window.
+        /// </summary>
+        public virtual Color Color
+        {
+            get
+            {
+                CheckDisposed();
+                return ((IColorDialogHandler)Handler).Color;
+            }
+
+            set
+            {
+                CheckDisposed();
+                ((IColorDialogHandler)Handler).Color = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override IDialogHandler CreateHandler()
+        {
+            return NativePlatform.Default.CreateColorDialogHandler(this);
+        }
+    }
+}
