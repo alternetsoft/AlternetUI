caec0912e82170f605b5f4bc4ed4f561f054bf3b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-05-25 07:20:13 +0300

    ControlsSampleDll
    Moved all code from ControlsSample.exe to ControlsSampleDll as we need to use samples and tests not only for WxWidgets platform but also for Maui platform.

---
+++ Source/Samples/ControlsSampleDll/InternalSamples/NinePatchDrawing/NinePatchDrawingWindow.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI;
+
+namespace ControlsSample
+{
+    internal class NinePatchDrawingWindow : Window
+    {
+        private const string ResPrefixBackground = "embres:ControlsSampleDll.Resources.Backgrounds.";
+
+        internal const string backgroundUrl1 = $"{ResPrefixBackground}textured-background2.jpg";
+
+        private const string ResPrefix = "embres:ControlsSampleDll.Resources.NinePatchImages.";
+
+        internal const string imageUrl1 = $"{ResPrefix}NinePatch1.png";
+        internal const string imageUrl2 = $"{ResPrefix}NinePatch2.png";
+
+        internal static Image image1 = new Bitmap(imageUrl1);
+        internal static Image image2 = new Bitmap(imageUrl2);
+
+        internal static Image background1 = new Bitmap(backgroundUrl1);
+
+        private readonly UserControl control = new();
+        private readonly PopupPictureBox picturePopup = new();
+
+        private readonly Button button = new("Draw on screen")
+        {
+            Visible = true,
+        };
+
+        private readonly Button button2 = new("Screen image")
+        {
+            Visible = true,
+        };
+
+        public NinePatchDrawingWindow()
+        {
+            Layout = LayoutStyle.Vertical;
+
+            Title = "Graphics.DrawSlicedImage demo";
+
+            control.VerticalAlignment = VerticalAlignment.Fill;
+            control.Paint += Control_Paint;
+            control.Parent = this;
+
+            Size = (600, 600);
+
+            var buttonPanel = AddHorizontalStackPanel();
+
+            button.Margin = 10;
+            button.VerticalAlignment = VerticalAlignment.Bottom;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+            button.Parent = buttonPanel;
+
+            button2.Margin = 10;
+            button2.VerticalAlignment = VerticalAlignment.Bottom;
+            button2.HorizontalAlignment = HorizontalAlignment.Left;
+            button2.Parent = buttonPanel;
+
+            button2.ClickAction = () =>
+            {
+                Application.Log("Some Linux versions don't support getting screen image.");
+                var bitmap = Image.FromScreen();
+                if (bitmap is not null)
+                {
+                    bitmap?.Rescale((800, 600));
+                    picturePopup.MainControl.Image = bitmap;
+                    picturePopup.ShowPopup(button2);
+                }
+                else
+                    Application.LogError("Screen picture is null");
+            };           
+
+            button.ClickAction = () =>
+            {
+                Application.Log("Some Linux versions don't support drawing on screen.");
+
+                for(int i = 0; i < Display.Count; i++)
+                    DrawOnDisplay(i);
+
+                void DrawOnDisplay(int index)
+                {
+                    var rect = Display.AllScreens[index].Bounds;
+
+                    var dc = WxGraphics.FromScreen();
+
+                    dc.FillRectangleI(Color.White.AsBrush, (rect.Location, (500, 400)));
+
+                    dc.DrawRotatedTextI(
+                        $"Display {index}",
+                        rect.Location + (50, 250),
+                        (Font ?? Control.DefaultFont).Scaled(2.7),
+                        Color.Red,
+                        Color.Empty,
+                        40);
+                }
+            };
+        }
+
+        private void Control_Paint(object? sender, PaintEventArgs e)
+        {
+            // All rectangles in this method are not in dips but in pixels.
+            // Graphics methods with 'I' suffix assume pixels.
+
+            var brush = background1.AsBrush;
+            e.Graphics.FillRectangle(brush, e.ClipRectangle);
+
+            NinePatchImagePaintParams args = new(image1);
+            args.SourceRect = (24, 24, 16, 16);
+            args.PatchRect = RectI.Inflate(args.SourceRect, -2, -2);
+            args.DestRect = (70, 70, 64, 64);
+            e.Graphics.DrawImageSliced(args);
+
+            NinePatchImagePaintParams args2 = new(image2);
+            args2.SourceRect = image2.Bounds;
+            args2.PatchRect = RectI.Inflate(args2.SourceRect, -10, -10);
+            args2.DestRect = (170, 170, 250, 160);
+            e.Graphics.DrawImageSliced(args2);
+
+            e.Graphics.DrawRotatedTextI(
+                "Hello",
+                (190, 250),
+                (Font ?? Control.DefaultFont).Scaled(2.7),
+                Color.Red,
+                Color.Empty,
+                40);
+
+            e.Graphics.BlitI(
+                (450, 200),
+                args2.DestRect.Size,
+                e.Graphics,
+                args2.DestRect.Location,
+                RasterOperationMode.Copy);
+
+            e.Graphics.StretchBlitI(
+                (450, 400),
+                args2.DestRect.Size * 2,
+                e.Graphics,
+                args2.DestRect.Location,
+                args2.DestRect.Size,
+                RasterOperationMode.Copy);
+        }
+    }
+}
---
+++ Source/Samples/ControlsSampleDll/Pages/Other/NotifyIconPage.uixml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Alternet.Drawing;
+using Alternet.UI;
+
+namespace ControlsSample
+{
+    internal partial class NotifyIconPage : Control
+    {
+        public static readonly Image Image = new Bitmap(typeof(NotifyIconPage).Assembly.GetManifestResourceStream(
+            "ControlsSampleDll.Resources.Logo16x16.png") ?? throw new Exception());
+
+        private readonly NotifyIcon? notifyIcon;
+
+        public NotifyIconPage()
+        {
+            InitializeComponent();
+
+            notifyPanel.Visible = NotifyIcon.IsAvailable;
+
+            if (notifyPanel.Visible)
+            {
+                notifyIcon = new NotifyIcon { Icon = Image, Text = notifyIconTextTextBox.Text };
+                notifyIcon.Click += NotifyIcon_Click;
+                notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+                notifyIcon.Menu = new ExampleContextMenu();
+            }
+
+            ControlSet.New(
+                iconTextLabel,
+                tooltipTitleLabel,
+                tooltipMessageLabel,
+                tooltipIconLabel,
+                tooltipKindLabel)
+                .SuggestedWidthToMax();
+
+            tooltipKindComboBox.BindEnumProp(this, nameof(ToolTipKind));
+            tooltipIconComboBox.BindEnumProp(this, nameof(ToolTipIcon));
+
+            showToolTipButton.Click += ShowToolTipButton_Click;
+            hideToolTipButton.Click += HideToolTipButton_Click;
+            resetTitleButton.Click += ResetTitleButton_Click;
+            showSimpleButton.Click += ShowSimpleButton_Click;
+
+            GetColumnGroup(3, true).SuggestedWidthToMax();
+
+            toolTipLabel.Click += ToolTipLabel_Click;
+        }
+
+        private void ToolTipLabel_Click(object? sender, EventArgs e)
+        {
+            Application.Log("ToolTipLabel_Click");
+            tooltipPreview.BackgroundColor = Color.White;
+            toolTipLabel.Text = string.Empty;
+            toolTipLabel.BackgroundColor = Color.White;
+        }
+
+        private void ResetTitleButton_Click(object? sender, EventArgs e)
+        {
+            tooltipTitleTextBox.Text = string.Empty;
+        }
+
+        internal void LogColors()
+        {
+            LogUtils.LogColor("Info", SystemColors.Info);
+            LogUtils.LogColor("SystemSettings.Info", SystemSettings.GetColor(KnownSystemColor.Info));
+            LogUtils.LogColor("InfoText", SystemColors.InfoText);
+            LogUtils.LogColor("SystemSettings.InfoText", SystemSettings.GetColor(KnownSystemColor.InfoText));
+            LogUtils.LogColor("BkColor", RealBackgroundColor);
+            LogUtils.LogColor("FgColor", RealForegroundColor);
+        }
+
+        private void ShowToolTipButton_Click(object? sender, EventArgs e)
+        {
+            RichToolTip.Show(
+                tooltipTitleTextBox.Text,
+                tooltipMessageTextBox.Text,
+                tooltipPreview,
+                ToolTipKind,
+                ToolTipIcon);
+        }
+
+        private void ShowSimpleButton_Click(object? sender, EventArgs e)
+        {
+            RichToolTip.ShowSimple(tooltipMessageTextBox.Text, tooltipPreview);
+        }
+
+        private void HideToolTipButton_Click(object? sender, EventArgs e)
+        {
+            // RichToolTip.Show assigns RichToolTip.Default,
+            // so we can use this variable for hiding the tooltip.
+            var toolTip = RichToolTip.Default;
+            RichToolTip.Default = null;
+            toolTip?.Dispose();
+        }
+
+        public RichToolTipKind ToolTipKind { get; set; } = RichToolTipKind.None;
+
+        public MessageBoxIcon ToolTipIcon { get; set; } = MessageBoxIcon.Warning;
+
+        private void NotifyIcon_DoubleClick(object? sender, EventArgs e)
+        {
+            Application.Log("NotifyIcon: DoubleClick");
+        }
+
+        private void NotifyIcon_Click(object? sender, EventArgs e)
+        {
+            Application.Log("NotifyIcon: Click");
+        }
+
+        private void NotifyIconVisibleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {   
+            if(notifyIcon is not null)
+                notifyIcon.Visible = notifyIconVisibleCheckBox.IsChecked;
+        }
+
+        private void ApplyTextButton_Click(object sender, System.EventArgs e)
+        {
+            if (notifyIcon is not null)
+                notifyIcon.Text = notifyIconTextTextBox.Text;
+        }
+    }
+}
