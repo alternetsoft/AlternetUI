7690d5d92a357254323a519231ff9e06b73528a1
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-05-26 06:35:37 +0300

    IWebBrowser xml comments

--- Source/Alternet.UI.Common/Controls/WebBrowser/Interfaces/IWebBrowser.cs
+++ Source/Alternet.UI.Common/Controls/WebBrowser/Interfaces/IWebBrowser.cs
@@ -8,49 +8,68 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     /// <summary>
-    ///     All methods and properties of the <see cref="WebBrowser"/> control
-    ///     are defined in this interface.
+    /// Declares methods and properties which allow to work with <see cref="WebBrowser"/> control.
     /// </summary>
     public interface IWebBrowser : IWebBrowserLite
     {
+        /// <inheritdoc cref="WebBrowser.Navigated"/>
         event EventHandler<WebBrowserEventArgs> Navigated;
 
+        /// <inheritdoc cref="WebBrowser.Navigating"/>
         event EventHandler<WebBrowserEventArgs> Navigating;
 
+        /// <inheritdoc cref="WebBrowser.Loaded"/>
         event EventHandler<WebBrowserEventArgs> Loaded;
 
+        /// <inheritdoc cref="WebBrowser.Error"/>
         event EventHandler<WebBrowserEventArgs> Error;
 
+        /// <inheritdoc cref="WebBrowser.NewWindow"/>
         event EventHandler<WebBrowserEventArgs> NewWindow;
 
+        /// <inheritdoc cref="WebBrowser.DocumentTitleChanged"/>
         event EventHandler<WebBrowserEventArgs> DocumentTitleChanged;
 
+        /// <inheritdoc cref="WebBrowser.FullScreenChanged"/>
         event EventHandler<WebBrowserEventArgs> FullScreenChanged;
 
+        /// <inheritdoc cref="WebBrowser.ScriptMessageReceived"/>
         event EventHandler<WebBrowserEventArgs> ScriptMessageReceived;
 
+        /// <inheritdoc cref="WebBrowser.ScriptResult"/>
         event EventHandler<WebBrowserEventArgs> ScriptResult;
 
+        /// <inheritdoc cref="WebBrowser.Source"/>
         Uri Source { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.Url"/>
         string Url { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.CanZoomIn"/>
         bool CanZoomIn { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanZoomOut"/>
         bool CanZoomOut { get; }
 
+        /// <inheritdoc cref="WebBrowser.ZoomIn"/>
         void ZoomIn();
 
+        /// <inheritdoc cref="WebBrowser.ZoomOut"/>
         void ZoomOut();
 
+        /// <inheritdoc cref="WebBrowser.NavigateToStream(stream)"/>
         void NavigateToStream(Stream stream);
 
+        /// <inheritdoc cref="WebBrowser.Navigate(Uri)"/>
         void Navigate(Uri source);
 
+        /// <inheritdoc cref="WebBrowser.Navigate(string)"/>
         void Navigate(string urlString);
 
+        /// <inheritdoc cref="WebBrowser.ToInvokeScriptArg"/>
         string? ToInvokeScriptArg(object arg);
 
+        /// <inheritdoc cref="WebBrowser.InvokeScriptAsync"/>
         void InvokeScriptAsync(string scriptName, IntPtr clientData, params object[] args);
     }
 }
--- Source/Alternet.UI.Common/Controls/WebBrowser/Interfaces/IWebBrowserLite.cs
+++ Source/Alternet.UI.Common/Controls/WebBrowser/Interfaces/IWebBrowserLite.cs
@@ -7,117 +7,170 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     /// <summary>
-    ///     Subset of the <see cref="WebBrowser"/> control's methods and properties.
+    /// Subset of the <see cref="WebBrowser"/> control's methods and properties.
     /// </summary>
     public interface IWebBrowserLite
     {
+        /// <inheritdoc cref="WebBrowser.PreferredColorScheme"/>
         WebBrowserPreferredColorScheme PreferredColorScheme { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.Editable"/>
         bool Editable { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.CanGoBack"/>
         bool CanGoBack { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanGoForward"/>
         bool CanGoForward { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanCut"/>
         bool CanCut { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanCopy"/>
         bool CanCopy { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanUndo"/>
         bool CanUndo { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanRedo"/>
         bool CanRedo { get; }
 
+        /// <inheritdoc cref="WebBrowser.IsBusy"/>
         bool IsBusy { get; }
 
+        /// <inheritdoc cref="WebBrowser.CanPaste"/>
         bool CanPaste { get; }
 
+        /// <inheritdoc cref="WebBrowser.ZoomFactor"/>
         float ZoomFactor { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.ZoomType"/>
         WebBrowserZoomType ZoomType { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.Zoom"/>
         WebBrowserZoom Zoom { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.HasSelection"/>
         bool HasSelection { get; }
 
+        /// <inheritdoc cref="WebBrowser.SelectedSource"/>
         string SelectedSource { get; }
 
+        /// <inheritdoc cref="WebBrowser.SelectedText"/>
         string SelectedText { get; }
 
+        /// <inheritdoc cref="WebBrowser.PageSource"/>
         string PageSource { get; }
 
+        /// <inheritdoc cref="WebBrowser.PageText"/>
         string PageText { get; }
 
+        /// <inheritdoc cref="WebBrowser.AccessToDevToolsEnabled"/>
         bool AccessToDevToolsEnabled { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.UserAgent"/>
         string UserAgent { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.ContextMenuEnabled"/>
         bool ContextMenuEnabled { get; set; }
 
+        /// <inheritdoc cref="WebBrowser.Backend"/>
         WebBrowserBackend Backend { get; }
 
+        /// <inheritdoc cref="WebBrowser.GetNativeBackend"/>
         IntPtr GetNativeBackend();
 
+        /// <inheritdoc cref="WebBrowser.DoCommand"/>
         string? DoCommand(string cmdName, params object?[] args);
 
+        /// <inheritdoc cref="WebBrowser.AddUserScript"/>
         bool AddUserScript(string javascript, bool injectDocStart);
 
+        /// <inheritdoc cref="WebBrowser.LoadURL"/>
         void LoadURL(string url);
 
+        /// <inheritdoc cref="WebBrowser.CanSetZoomType"/>
         bool CanSetZoomType(WebBrowserZoomType zoomType);
 
+        /// <inheritdoc cref="WebBrowser.GetCurrentTitle"/>
         string GetCurrentTitle();
 
+        /// <inheritdoc cref="WebBrowser.GetCurrentURL"/>
         string GetCurrentURL();
 
+        /// <inheritdoc cref="WebBrowser.NavigateToString"/>
         void NavigateToString(string html, string? baseUrl = null);
 
+        /// <inheritdoc cref="WebBrowser.GoBack"/>
         bool GoBack();
 
+        /// <inheritdoc cref="WebBrowser.GoForward"/>
         bool GoForward();
 
+        /// <inheritdoc cref="WebBrowser.Stop"/>
         void Stop();
 
+        /// <inheritdoc cref="WebBrowser.ClearSelection"/>
         void ClearSelection();
 
+        /// <inheritdoc cref="WebBrowser.Copy"/>
         void Copy();
 
+        /// <inheritdoc cref="WebBrowser.Paste"/>
         void Paste();
 
+        /// <inheritdoc cref="WebBrowser.Cut"/>
         void Cut();
 
+        /// <inheritdoc cref="WebBrowser.SetVirtualHostNameToFolderMapping"/>
         public void SetVirtualHostNameToFolderMapping(
             string hostName,
             string folderPath,
             WebBrowserHostResourceAccessKind accessKind);
 
+        /// <inheritdoc cref="WebBrowser.ClearHistory"/>
         void ClearHistory();
 
+        /// <inheritdoc cref="WebBrowser.EnableHistory"/>
         void EnableHistory(bool enable = true);
 
+        /// <inheritdoc cref="WebBrowser.Reload"/>
         void Reload();
 
+        /// <inheritdoc cref="WebBrowser.Reload"/>
         void Reload(bool noCache);
 
+        /// <inheritdoc cref="WebBrowser.SelectAll"/>
         void SelectAll();
 
+        /// <inheritdoc cref="WebBrowser.DeleteSelection"/>
         void DeleteSelection();
 
+        /// <inheritdoc cref="WebBrowser.Undo"/>
         void Undo();
 
+        /// <inheritdoc cref="WebBrowser.Redo"/>
         void Redo();
 
+        /// <inheritdoc cref="WebBrowser.Find"/>
         int Find(string text, WebBrowserFindParams? prm = null);
 
+        /// <inheritdoc cref="WebBrowser.FindClearResult"/>
         void FindClearResult();
 
+        /// <inheritdoc cref="WebBrowser.Print"/>
         void Print();
 
+        /// <inheritdoc cref="WebBrowser.RemoveAllUserScripts"/>
         void RemoveAllUserScripts();
 
+        /// <inheritdoc cref="WebBrowser.AddScriptMessageHandler"/>
         bool AddScriptMessageHandler(string name);
 
+        /// <inheritdoc cref="WebBrowser.RemoveScriptMessageHandler"/>
         bool RemoveScriptMessageHandler(string name);
 
+        /// <inheritdoc cref="WebBrowser.RunScriptAsync"/>
         void RunScriptAsync(string javascript, IntPtr? clientData);
     }
 }
