ce21afb9a4bacee78018ab3e86ab2c428525e610
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-02 06:07:46 +0300

    SystemFonts improved
    - All properties are now can be set
    - New props: Default, DefaultMono, Serif, SansSerif
    - New methods: GetFont, SetFont

--- Source/Alternet.UI.Common/Drawing/Font/SystemFonts.cs
+++ Source/Alternet.UI.Common/Drawing/Font/SystemFonts.cs
@@ -17,46 +17,106 @@ namespace Alternet.UI
         private static Font? ansiVar;
         private static Font? system;
         private static Font? deviceDefault;
+        private static Font? sansSerif;
+        private static Font? serif;
 
         /// <summary>
         /// Original equipment manufacturer dependent fixed-pitch font.
         /// </summary>
-        public static Font OemFixed =>
-            oemFixed ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.OemFixed);
+        public static Font OemFixed
+        {
+            get => oemFixed ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.OemFixed);
+            set => oemFixed = value ?? FontFactory.Handler.CreateSystemFont(SystemSettingsFont.OemFixed);
+        }
 
         /// <summary>
         /// Windows fixed-pitch (monospaced) font.
         /// </summary>
-        public static Font AnsiFixed =>
-            ansiFixed ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiFixed);
+        public static Font AnsiFixed
+        {
+            get => ansiFixed ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiFixed);
+            set => ansiFixed = value ?? FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiFixed);
+        }
 
         /// <summary>
         /// Windows variable-pitch (proportional) font.
         /// </summary>
-        public static Font AnsiVar =>
-            ansiVar ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiVar);
+        public static Font AnsiVar
+        {
+            get => ansiVar ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiVar);
+            set => ansiVar = value ?? FontFactory.Handler.CreateSystemFont(SystemSettingsFont.AnsiVar);
+        }
 
         /// <summary>
         /// System font. By default, the system uses the system font to draw menus,
         /// dialog box controls, and text.
         /// </summary>
-        public static Font System =>
-            system ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.System);
+        public static Font System
+        {
+            get => system ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.System);
+            set => system = value ?? FontFactory.Handler.CreateSystemFont(SystemSettingsFont.System);
+        } 
 
         /// <summary>
         /// Device-dependent font.
         /// </summary>
-        public static Font DeviceDefault =>
-            deviceDefault ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.DeviceDefault);
+        public static Font DeviceDefault
+        {
+            get => deviceDefault
+                ??= FontFactory.Handler.CreateSystemFont(SystemSettingsFont.DeviceDefault);
+            set => deviceDefault = value
+                ?? FontFactory.Handler.CreateSystemFont(SystemSettingsFont.DeviceDefault);
+        }
 
         /// <summary>
         /// Default font for user interface objects such as menus and dialog boxes.
+        /// This is the same as <see cref="Font.Default"/>.
         /// </summary>
         /// <remarks>
         /// Note that with modern GUIs nothing guarantees that the same font is used for
         /// all GUI elements, so some controls might use a different font by default.
         /// </remarks>
-        public static Font DefaultGui => Font.Default;
+        public static Font DefaultGui
+        {
+            get => Font.Default;
+            set => Font.Default = value;
+        }
+
+        /// <summary>
+        /// Alias to <see cref="Font.Default"/>.
+        /// </summary>
+        public static Font Default
+        {
+            get => Font.Default;
+            set => Font.Default = value;
+        }
+
+        /// <summary>
+        /// Alias to <see cref="Font.DefaultMono"/>.
+        /// </summary>
+        public static Font DefaultMono
+        {
+            get => Font.DefaultMono;
+            set => Font.DefaultMono = value;
+        }
+
+        /// <summary>
+        /// Gets generic "Serif" font.
+        /// </summary>
+        public static Font Serif
+        {
+            get => serif ??= new Font(FontFamily.GenericSerif, Default.SizeInPoints);
+            set => serif = value ?? new Font(FontFamily.GenericSerif, Default.SizeInPoints);
+        }
+
+        /// <summary>
+        /// Gets generic "Sans serif" font.
+        /// </summary>
+        public static Font SansSerif
+        {
+            get => sansSerif ??= new Font(FontFamily.GenericSansSerif, Default.SizeInPoints);
+            set => sansSerif = value ?? new Font(FontFamily.GenericSansSerif, Default.SizeInPoints);
+        }
 
         /// <summary>
         /// Gets system font.
@@ -78,6 +138,76 @@ namespace Alternet.UI
                 case SystemSettingsFont.DefaultGui:
                 default:
                     return DefaultGui;
+            }
+        }
+
+        /// <summary>
+        /// Sets system font.
+        /// </summary>
+        public static void SetFont(SystemSettingsFont font, Font value)
+        {
+            switch (font)
+            {
+                case SystemSettingsFont.OemFixed:
+                    OemFixed = value;
+                    break;
+                case SystemSettingsFont.AnsiFixed:
+                    AnsiFixed = value;
+                    break;
+                case SystemSettingsFont.AnsiVar:
+                    AnsiVar = value;
+                    break;
+                case SystemSettingsFont.System:
+                    System = value;
+                    break;
+                case SystemSettingsFont.DeviceDefault:
+                    DeviceDefault = value;
+                    break;
+                case SystemSettingsFont.DefaultGui:
+                    DefaultGui = value;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Gets font for the specified <see cref="GenericFontFamily"/>.
+        /// </summary>
+        public static void SetFont(GenericFontFamily font, Font value)
+        {
+            switch (font)
+            {
+                case GenericFontFamily.Default:
+                    Font.Default = value;
+                    break;
+                case GenericFontFamily.SansSerif:
+                    SansSerif = value;
+                    break;
+                case GenericFontFamily.Serif:
+                    Serif = value;
+                    break;
+                case GenericFontFamily.Monospace:
+                    Font.DefaultMono = value;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Gets font for the specified <see cref="GenericFontFamily"/>.
+        /// </summary>
+        public static Font GetFont(GenericFontFamily font)
+        {
+            switch (font)
+            {
+                case GenericFontFamily.Default:
+                case GenericFontFamily.None:
+                default:
+                    return Font.Default;
+                case GenericFontFamily.SansSerif:
+                    return SansSerif;
+                case GenericFontFamily.Serif:
+                    return Serif;
+                case GenericFontFamily.Monospace:
+                    return Font.DefaultMono;
             }
         }
     }
