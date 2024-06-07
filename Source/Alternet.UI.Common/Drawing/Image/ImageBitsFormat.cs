252f40b9b85ceee426f5de29db68c39b9ba622f5
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-07 10:44:52 +0300

    Update ImageBitsFormat.cs

--- Source/Alternet.UI.Common/Drawing/Image/ImageBitsFormat.cs
+++ Source/Alternet.UI.Common/Drawing/Image/ImageBitsFormat.cs
@@ -24,6 +24,41 @@ namespace Alternet.Drawing
 
         public int Alpha;
 
+        /// <summary>
+        /// Tests whether two specified <see cref="ImageBitsFormat"/> structures are equivalent.
+        /// </summary>
+        /// <param name="left">The <see cref="ImageBitsFormat"/> that is to the left
+        /// of the equality operator.</param>
+        /// <param name="right">The <see cref="ImageBitsFormat"/> that is to the right
+        /// of the equality operator.</param>
+        /// <returns><c>true</c> if the two <see cref="ImageBitsFormat"/> structures
+        /// are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(ImageBitsFormat left, ImageBitsFormat right)
+        {
+            return
+                left.BitsPerPixel == right.BitsPerPixel &&
+                left.HasAlpha == right.HasAlpha &&
+                left.SizePixel == right.SizePixel &&
+                left.Red == right.Red &&
+                left.Green == right.Green &&
+                left.Blue == right.Blue &&
+                left.Alpha == right.Alpha;
+        }
+
+        /// <summary>
+        /// Tests whether two specified <see cref="ImageBitsFormat"/> structures are not equivalent.
+        /// </summary>
+        /// <param name="left">The <see cref="ImageBitsFormat"/> that is to the left
+        /// of the equality operator.</param>
+        /// <param name="right">The <see cref="ImageBitsFormat"/> that is to the right
+        /// of the equality operator.</param>
+        /// <returns><c>true</c> if the two <see cref="ImageBitsFormat"/> structures
+        /// are not equal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(ImageBitsFormat left, ImageBitsFormat right)
+        {
+            return !(left == right);
+        }
+
         public readonly void Log(string? sectionName = null)
         {
             var self = this;
@@ -38,6 +73,25 @@ namespace Alternet.Drawing
                 App.LogNameValue("Blue", self.Blue);
                 App.LogNameValue("Alpha", self.Alpha);
             }, sectionName);
+        }
+
+        public readonly override bool Equals(object? obj)
+        {
+            if (obj is not ImageBitsFormat value)
+                return false;
+            return this == value;
+        }
+
+        public readonly override int GetHashCode()
+        {
+            return HashCode.Combine(
+                BitsPerPixel.GetHashCode(),
+                HasAlpha.GetHashCode(),
+                SizePixel.GetHashCode(),
+                Red.GetHashCode(),
+                Green.GetHashCode(),
+                Blue.GetHashCode(),
+                Alpha.GetHashCode());
         }
     }
 }
