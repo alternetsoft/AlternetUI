6c0169c26f988cd6a2333d1ef4705d14a476a1af
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-08 15:18:49 +0300

    Added SkiaSharp mega demo in ControlsSample

--- Source/Alternet.UI.Common/Utils/LogUtils.cs
+++ Source/Alternet.UI.Common/Utils/LogUtils.cs
@@ -28,6 +28,8 @@ namespace Alternet.UI
         public static LogFlags Flags;
 
         private static readonly ICustomFlags EventLoggedFlags = Factory.CreateCustomFlags();
+
+        private static List<(string Name, Action Action)>? registeredLogActions;
         private static int id;
         private static int logUseMaxLength;
 
@@ -755,8 +757,14 @@ namespace Alternet.UI
             Test(KnownSystemColor.MenuHighlight);
         }
 
+        public static void RegisterLogAction(string name, Action action)
+        {
+            registeredLogActions ??= new();
+            registeredLogActions.Add((name, action));
+        }
+
         /// <summary>
-        /// Enumerates log actions.
+        /// Enumerates registered log actions.
         /// </summary>
         public static void EnumLogActions(Action<string, Action> addLogAction)
         {
@@ -803,6 +811,14 @@ namespace Alternet.UI
             addLogAction("Log SKBitmap", LogUtils.LogSkiaBitmap);
             addLogAction("Log Skia mono fonts", LogUtils.LogSkiaMonoFonts);
             addLogAction("Log image bits formats", LogImageBitsFormats);
+
+            if (registeredLogActions is not null)
+            {
+                foreach(var item in registeredLogActions)
+                {
+                    addLogAction(item.Name, item.Action);
+                }
+            }
         }
 
         public static void LogImageBitsFormats()
