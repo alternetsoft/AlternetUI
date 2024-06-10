14ebae62874e90b8d3603bbaabf8d1807b44e99a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-10 10:09:25 +0300

    Remove vector from Port as duplicate of system

--- Source/Alternet.UI.Common/Xaml/Port/Markup.Xaml.Loader/CompilerExtensions/UixmlPortXamlIlLanguageParseIntrinsics.cs
+++ Source/Alternet.UI.Common/Xaml/Port/Markup.Xaml.Loader/CompilerExtensions/UixmlPortXamlIlLanguageParseIntrinsics.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Numerics;
+
 using Alternet.Drawing;
 using Alternet.UI;
 using Alternet.UI.Markup.Xaml.XamlIl.CompilerExtensions.AstNodes;
@@ -79,11 +81,11 @@ namespace Alternet.UI.Markup.Xaml.XamlIl.CompilerExtensions
                 }
             }
             
-            if (type.Equals(types.Vector))
+            /*if (type.Equals(types.Vector))
             {
                 try
                 {
-                    var vector = Port.Vector.Parse(text);
+                    var vector = Vector.Parse(text);
             
                     result = new UixmlPortXamlIlVectorLikeConstantAstNode(node, types, types.Vector, types.VectorFullConstructor,
                         new[] { vector.X, vector.Y });
@@ -94,7 +96,7 @@ namespace Alternet.UI.Markup.Xaml.XamlIl.CompilerExtensions
                 {
                     throw new XamlX.XamlLoadException($"Unable to parse \"{text}\" as a vector", node);
                 }
-            }
+            }*/
             
             if (type.Equals(types.Size))
             {
