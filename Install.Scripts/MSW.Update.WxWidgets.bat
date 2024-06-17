2df66cf5c7353ad5ee16bdf524fcba6e329c974f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-17 06:12:21 +0300

    Update MSW.Update.WxWidgets.bat

--- Install.Scripts/MSW.Update.WxWidgets.bat
+++ Install.Scripts/MSW.Update.WxWidgets.bat
@@ -1,3 +1,8 @@
+:: STEPS: 
+:: 1. Modify WxWidgetsDownloadUrlLocalHost in Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.props 
+:: 2. Copy new WxWidgets from External to localhost and cloud
+
+
 call MSW.Update.WxWidgets.SubTool.1.DownloadAndExtract.bat
 call MSW.Update.WxWidgets.SubTool.2.Defines.bat
 call MSW.Update.WxWidgets.SubTool.3.BuildWithoutDownload.bat
