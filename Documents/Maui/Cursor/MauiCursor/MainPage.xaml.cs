ca28f3842172af964a58178b689dad4307e78ed3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-06-17 04:59:27 +0300

    Maui cursor and keyboard related info

---
+++ Documents/Maui/Cursor/MauiCursor/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiCursor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Hover.IsVisible = true;
+		Hover.SetCustomCursor(CursorIcon.Wait, Hover.Handler?.MauiContext);
+		CursorBehavior.SetCursor(CounterBtn, CursorIcon.SizeAll);
+	}
+}
---
+++ Documents/Maui/Cursor/MauiCursor/Platforms/MacCatalyst/CursorExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace MauiCursor;
+
+using AppKit;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Platform;
+using UIKit;
+
+public static class CursorExtensions
+{
+	public static void SetCustomCursor(this VisualElement visualElement, CursorIcon cursor, IMauiContext? mauiContext)
+	{
+		ArgumentNullException.ThrowIfNull(mauiContext);
+		var view = visualElement.ToPlatform(mauiContext);
+		if (view.GestureRecognizers is not null)
+		{
+			foreach (var recognizer in view.GestureRecognizers.OfType<PointerUIHoverGestureRecognizer>())
+			{
+				view.RemoveGestureRecognizer(recognizer);
+			}
+		}
+
+		view.AddGestureRecognizer(new PointerUIHoverGestureRecognizer(r =>
+		{
+			switch (r.State)
+			{
+				case UIGestureRecognizerState.Began:
+					GetNSCursor(cursor).Set();
+					break;
+				case UIGestureRecognizerState.Ended:
+					NSCursor.ArrowCursor.Set();
+					break;
+			}
+		}));
+	}
+
+	static NSCursor GetNSCursor(CursorIcon cursor)
+	{
+		return cursor switch
+		{
+			CursorIcon.Hand => NSCursor.OpenHandCursor,
+			CursorIcon.IBeam => NSCursor.IBeamCursor,
+			CursorIcon.Cross => NSCursor.CrosshairCursor,
+			CursorIcon.Arrow => NSCursor.ArrowCursor,
+			CursorIcon.SizeAll => NSCursor.ResizeUpCursor,
+			CursorIcon.Wait => NSCursor.OperationNotAllowedCursor,
+			_ => NSCursor.ArrowCursor,
+		};
+	}
+
+	class PointerUIHoverGestureRecognizer(Action<UIHoverGestureRecognizer> action) : UIHoverGestureRecognizer(action);
+}
