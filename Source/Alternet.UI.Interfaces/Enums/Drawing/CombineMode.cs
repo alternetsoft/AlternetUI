93928ff3c993dbf0d0edf9627720fced536f74d5
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-03 17:31:16 +0300

    Update CombineMode.cs

--- Source/Alternet.UI.Interfaces/Enums/Drawing/CombineMode.cs
+++ Source/Alternet.UI.Interfaces/Enums/Drawing/CombineMode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Alternet.UI.Drawing
+namespace Alternet.Drawing
 {
     /// <summary>
     /// Specifies how different regions can be combined.
