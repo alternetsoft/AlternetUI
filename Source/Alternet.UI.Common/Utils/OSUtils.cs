a93fb383c97e74e9bd5f792e5c76ea498db6890a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-08 07:10:18 +0300

    New OSUtils methods
    GetNativeModuleName, BytesToInt16, BytesToInt, CharsToInt

--- Source/Alternet.UI.Common/Utils/OSUtils.cs
+++ Source/Alternet.UI.Common/Utils/OSUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,15 +9,44 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     /// <summary>
-    /// OS related static methods
+    /// Contains static methods and properties related to the operating system and processor.
     /// </summary>
     public static class OSUtils
     {
+        /// <summary>
+        /// Adds an extension to the specified native module name. The extension depends on the
+        /// operating system.
+        /// </summary>
+        /// <param name="nativeModuleNameNoExt">Native module name without an extension.</param>
+        /// <returns></returns>
+        public static string GetNativeModuleName(string nativeModuleNameNoExt)
+        {
+            var result = nativeModuleNameNoExt;
+
+            if (App.IsWindowsOS)
+            {
+                result = $"{result}.dll";
+            }
+            else
+            if (App.IsLinuxOS || App.IsAndroidOS)
+            {
+                result = $"{result}.so";
+            }
+            else
+            if (App.IsMacOS || App.IsIOS)
+            {
+                result = $"{result}.dylib";
+            }
+
+            return result;
+        }
+
         /// <summary>
         /// Suspends the execution of the current thread for a specified interval.
         /// </summary>
         /// <param name="milliSeconds">Specifies time, in milliseconds, for which
         /// to suspend execution.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Sleep(int milliSeconds)
         {
             Thread.Sleep(milliSeconds);
@@ -27,6 +57,7 @@ namespace Alternet.UI
         /// </summary>
         /// <param name="value">Specifies the value to be converted.</param>
         /// <returns>The return value is the low-order word of the specified value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static short LoWord(IntPtr value)
         {
             return (short)value.ToInt32();
@@ -37,27 +68,54 @@ namespace Alternet.UI
         /// </summary>
         /// <param name="value">Specifies the value to be converted.</param>
         /// <returns>The return value is the high-order word of the specified value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static short HiWord(IntPtr value)
         {
             return (short)(value.ToInt32() >> 16);
         }
 
-        internal static short BytesToInt16(byte ch0, byte ch1)
+        /// <summary>
+        /// Converts two bytes to <see cref="short"/> value.
+        /// </summary>
+        /// <param name="ch0">First byte.</param>
+        /// <param name="ch1">Second byte.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static short BytesToInt16(byte ch0, byte ch1)
         {
             int num = ch1;
             num |= ch0 << 8;
             return (short)num;
         }
 
-        internal static int BytesToInt(byte ch0, byte ch1, byte ch2, byte ch3)
+        /// <summary>
+        /// Converts four bytes to <see cref="int"/> value.
+        /// </summary>
+        /// <param name="ch0">First byte.</param>
+        /// <param name="ch1">Second byte.</param>
+        /// <param name="ch2">Third byte.</param>
+        /// <param name="ch3">Fourth byte.</param>
+        /// <returns></returns>
+        public static int BytesToInt(byte ch0, byte ch1, byte ch2, byte ch3)
         {
-            return MmioFOURCC((char)ch3, (char)ch2, (char)ch1, (char)ch0);
+            int num = ch0;
+            num |= (int)((uint)ch1 << 8);
+            num |= (int)((uint)ch2 << 16);
+            return num | (int)((uint)ch3 << 24);
         }
 
-        internal static int MmioFOURCC(char ch0, char ch1, char ch2, char ch3)
+        /// <summary>
+        /// Converts four chars to <see cref="int"/> value.
+        /// Only low byte of the <see cref="char"/> is used in the conversion.
+        /// </summary>
+        /// <param name="ch0">First character.</param>
+        /// <param name="ch1">Second character.</param>
+        /// <param name="ch2">Third character.</param>
+        /// <param name="ch3">Fourth character.</param>
+        /// <returns></returns>
+        public static int CharsToInt(char ch0, char ch1, char ch2, char ch3)
         {
-            int num = 0;
-            num |= ch0;
+            int num = ch0;
             num |= (int)((uint)ch1 << 8);
             num |= (int)((uint)ch2 << 16);
             return num | (int)((uint)ch3 << 24);
