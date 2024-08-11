aedd51f7bc1e2a85dbe499e679641e7983963d7e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-11 09:59:10 +0300

    Create RunAllSamples.sh

---
+++ Source/Samples/RunAllSamples.sh
@@ -0,0 +1,69 @@
+pushd CommonDialogsSample
+/bin/bash run.sh
+popd
+
+pushd ControlsSample
+/bin/bash run.sh
+popd
+
+pushd CustomControlsSample
+/bin/bash run.sh
+popd
+
+pushd DragAndDropSample
+/bin/bash run.sh
+popd
+
+pushd DrawingSample
+/bin/bash run.sh
+popd
+
+pushd EmployeeFormSample
+/bin/bash run.sh
+popd
+
+pushd ExplorerUISample
+/bin/bash run.sh
+popd
+
+pushd HelloWorldSample
+/bin/bash run.sh
+popd
+
+pushd InputSample
+/bin/bash run.sh
+popd
+
+pushd LayoutSample
+/bin/bash run.sh
+popd
+
+pushd MenuSample
+/bin/bash run.sh
+popd
+
+pushd PaintSample
+/bin/bash run.sh
+popd
+
+pushd PreviewSample
+/bin/bash run.sh
+popd
+
+pushd PrintingSample
+/bin/bash run.sh
+popd
+
+pushd PropertyGridSample
+/bin/bash run.sh
+popd
+
+pushd ThreadingSample
+/bin/bash run.sh
+popd
+
+pushd WindowPropertiesSample
+/bin/bash run.sh
+popd
+
+
