6a007d343ce71cc1d39c1547e358932ccc5a2fbe
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-11 09:46:40 +0300

    Create RunAllSamples.bat

---
+++ Source/Samples/RunAllSamples.bat
@@ -0,0 +1,69 @@
+pushd CommonDialogsSample
+call run.bat
+popd
+
+pushd ControlsSample
+call run.bat
+popd
+
+pushd CustomControlsSample
+call run.bat
+popd
+
+pushd DragAndDropSample
+call run.bat
+popd
+
+pushd DrawingSample
+call run.bat
+popd
+
+pushd EmployeeFormSample
+call run.bat
+popd
+
+pushd ExplorerUISample
+call run.bat
+popd
+
+pushd HelloWorldSample
+call run.bat
+popd
+
+pushd InputSample
+call run.bat
+popd
+
+pushd LayoutSample
+call run.bat
+popd
+
+pushd MenuSample
+call run.bat
+popd
+
+pushd PaintSample
+call run.bat
+popd
+
+pushd PreviewSample
+call run.bat
+popd
+
+pushd PrintingSample
+call run.bat
+popd
+
+pushd PropertyGridSample
+call run.bat
+popd
+
+pushd ThreadingSample
+call run.bat
+popd
+
+pushd WindowPropertiesSample
+call run.bat
+popd
+
+
