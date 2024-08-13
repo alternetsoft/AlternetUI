5f40b60eb2f55b9bc3bd2b919d8c68d919f3f8d9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-13 14:48:21 +0300

    SIgnTool

--- Install.Scripts/SignToolVerify.bat
+++ Install.Scripts/SignToolVerify.bat
@@ -4,4 +4,4 @@ set SCRIPT_HOME=%~dp0.
 set SOURCE_DIR=%SCRIPT_HOME%\..\Source
 set DLL=%1
 
-dotnet msbuild /t:DotNetSign /p:DLL_PATH="%DLL%" "%SCRIPT_HOME%\SignTool.proj"
+dotnet msbuild /t:SignVerify /p:DLL_PATH="%DLL%" "%SCRIPT_HOME%\SignTool.proj"
---
+++ Install.Scripts/Tests/Test.SignDll.bat
@@ -0,0 +1,8 @@
+SETLOCAL EnableDelayedExpansion
+
+set SCRIPT_HOME=%~dp0
+set SOURCE_DIR=%SCRIPT_HOME%\..\..\Source
+set SIGNTOOL=%SCRIPT_HOME%\..\SignTool.bat
+
+call "%SIGNTOOL%" "%SOURCE_DIR%\Alternet.UI\bin\Debug\net8.0\Alternet.UI.dll"
+
