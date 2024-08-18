8b6e11eefccf2727ba4f7edba82ccc5c9fe09ded
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-18 17:27:10 +0300

    CustomDemoWindow, CustomInternalSamplesPage

---
+++ Source/Samples/ControlsSampleDll/Custom/CustomDemoWindow.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alternet.UI
+{
+    public class CustomDemoWindow : Window
+    {
+        public static bool AddBuildNumber = true;
+        public static bool LogFocusedControl = false;
+
+        public static bool BindConsole = false;
+
+        private SplittedTreeAndCards? pageContainer;
+        private readonly LogListBox eventsControl;
+        private readonly LayoutPanel splitterPanel;
+        private readonly Control panel;
+        private readonly Splitter splitter = new();
+
+        static CustomDemoWindow()
+        {
+            UixmlLoader.LoadFromResName = FormLoadFromResName;
+            UixmlLoader.LoadFromStream = FormLoadFromStream;
+            UixmlLoader.ReportLoadException = ReportFormLoadException;
+
+            UseDebugBackgroundColor = false;
+
+            ResourceLoader.CustomStreamFromUrl += ResourceLoader_CustomStreamFromUrl;
+            ResourceLoader.CustomStreamFromUrl += ResourceLoader_CustomStreamFromUrl2;
+
+            Control.FocusedControlChanged += (s, e) =>
+            {
+                if (LogFocusedControl)
+                    App.LogReplace($"FocusedControl: {FocusedControl?.GetType()}", "FocusedControl:");
+            };
+
+            LogUtils.RegisterLogAction("Toggle Logging of FocusedControl", () =>
+            {
+                LogFocusedControl = !LogFocusedControl;
+            });
+        }
+
+        public CustomDemoWindow()
+        {
+            SupressEsc = true;
+
+            eventsControl = new()
+            {
+                Dock = DockStyle.Bottom,
+                HasBorder = false,
+                Margin = (0, 10, 0, 0),
+            };
+
+            splitterPanel = new()
+            {
+            };
+
+            panel = new()
+            {
+                Padding = 5,
+            };
+
+            eventsControl.BindApplicationLog();
+
+            if (BindConsole)
+            {
+                ConsoleUtils.BindConsoleOutput();
+                ConsoleUtils.BindConsoleError();
+            }
+
+            DoInsideLayout(Initialize);
+        }
+
+        public static bool FormLoadFromResName(string resName, object obj, UixmlLoader.Flags flags)
+        {
+            return false;
+        }
+
+        public static bool FormLoadFromStream(
+            Stream stream,
+            object obj,
+            string? resName,
+            UixmlLoader.Flags flags)
+        {
+            return false;
+        }
+
+        public static bool ReportFormLoadException(Exception e, string? resName, UixmlLoader.Flags flags)
+        {
+            return false;
+        }
+
+        internal void SetDebugColors()
+        {
+            DebugBackgroundColor(Color.Red, nameof(CustomDemoWindow));
+            pageContainer?.SetDebugColors();
+        }
+
+        public void AddPage(string title, Func<Control> action)
+        {
+            pageContainer?.Add(title, action);
+        }
+
+        public void Initialize()
+        {
+            pageContainer = new(SplittedTreeAndCards.TreeKind.ListBox);
+            panel.Parent = this;
+            Title = $"AlterNET UI Demo";
+
+            if (AddBuildNumber)
+            {
+                Title = $"{Title} {SystemSettings.Handler.GetUIVersion()}";
+            }
+
+            Size = (900, 700);
+            StartLocation = WindowStartLocation.CenterScreen;
+
+            Icon = new("embres:ControlsSampleDll.Sample.ico");
+
+            AddPages();
+
+            LogUtils.DebugLogVersion();
+
+            splitterPanel.Parent = panel;
+            pageContainer.Dock = DockStyle.Fill;
+            splitter.Dock = DockStyle.Bottom;
+            pageContainer.Parent = splitterPanel;
+            splitter.Parent = splitterPanel;
+            eventsControl.Height = 150;
+            eventsControl.Parent = splitterPanel;
+
+            pageContainer.SelectedIndex = 0;
+            pageContainer.ListBox!.HScrollBarVisible = true;
+            pageContainer.LeftControl?.SetFocusIfPossible();
+
+            var logSizeChanged = false;
+
+            if (logSizeChanged)
+            {
+                SizeChanged += MainWindow_SizeChanged;
+                StateChanged += MainWindow_StateChanged;
+            }
+        }
+
+        protected virtual void AddPages()
+        {
+        }
+
+        private void MainWindow_StateChanged(object? sender, EventArgs e)
+        {
+            App.Log($"State changed: {Size}, {this.State}");
+        }
+
+        private void MainWindow_SizeChanged(object? sender, EventArgs e)
+        {
+            App.Log($"Size changed: {Size}, {this.State}");
+        }
+
+        protected Control CreateCustomPage(NameValue<Func<Control>>?[] pages)
+        {
+            TabControl result = new()
+            {
+                Margin = 10,
+            };
+
+            result.ContentPadding = 5;
+
+            result.AddRange(pages);
+            return result;
+        }
+
+        [Browsable(false)]
+        public string? LastEventMessage => eventsControl.LastLogMessage;
+
+        public void LogEventSmart(string? message, string? prefix)
+        {
+            eventsControl.LogReplace(message, prefix);
+        }
+
+        public void LogEvent(string? message)
+        {
+            eventsControl.Log(message);
+        }
+
+        private static void ResourceLoader_CustomStreamFromUrl(object? sender, StreamFromUrlEventArgs e)
+        {
+            if (e.Value is null)
+                return;
+
+            e.Handled = true;
+            e.Result = ResourceLoader.DefaultStreamFromUrl(e.Value);
+        }
+
+        private static void ResourceLoader_CustomStreamFromUrl2(object? sender, StreamFromUrlEventArgs e)
+        {
+            App.Log("CustomStreamFromUrl 2");
+        }
+    }
+}
--- Source/Samples/ControlsSampleDll/MainWindow.cs
+++ Source/Samples/ControlsSampleDll/MainWindow.cs
@@ -7,112 +7,10 @@ using Alternet.UI;
 
 namespace ControlsSample
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : CustomDemoWindow
     {
-        public static bool LogFocusedControl = false;
-
-        public static bool BindConsole = false;
-
-        private SplittedTreeAndCards? pageContainer;
-        private readonly LogListBox eventsControl;
-        private readonly LayoutPanel splitterPanel;
-        private readonly Control panel;
-        private readonly Splitter splitter = new();
-
-        static MainWindow()
+        protected override void AddPages()
         {
-            UixmlLoader.LoadFromResName = FormLoadFromResName;
-            UixmlLoader.LoadFromStream = FormLoadFromStream;
-            UixmlLoader.ReportLoadException = ReportFormLoadException;
-
-            UseDebugBackgroundColor = false;
-
-            ResourceLoader.CustomStreamFromUrl += ResourceLoader_CustomStreamFromUrl;
-            ResourceLoader.CustomStreamFromUrl += ResourceLoader_CustomStreamFromUrl2;
-
-            Control.FocusedControlChanged += (s, e) =>
-            {
-                if(LogFocusedControl)
-                    App.LogReplace($"FocusedControl: {FocusedControl?.GetType()}", "FocusedControl:");
-            };
-
-            LogUtils.RegisterLogAction("Toggle Logging of FocusedControl", () =>
-            {
-                LogFocusedControl = !LogFocusedControl;
-            });
-        }
-
-        public MainWindow()
-        {
-            SupressEsc = true;
-
-            eventsControl = new()
-            {
-                Dock = DockStyle.Bottom,
-                HasBorder = false,
-                Margin = (0, 10, 0, 0),
-            };
-
-            splitterPanel = new()
-            {
-            };
-
-            panel = new()
-            {
-                Padding = 5,
-            };
-
-            eventsControl.BindApplicationLog();
-
-            if (BindConsole)
-            {
-                ConsoleUtils.BindConsoleOutput();
-                ConsoleUtils.BindConsoleError();
-            }
-
-            DoInsideLayout(Initialize);
-        }
-
-        public static bool FormLoadFromResName(string resName, object obj, UixmlLoader.Flags flags)
-        {
-            return false;
-        }
-
-        public static bool FormLoadFromStream(
-            Stream stream,
-            object obj,
-            string? resName,
-            UixmlLoader.Flags flags)
-        {
-            return false;
-        }
-
-        public static bool ReportFormLoadException(Exception e, string? resName, UixmlLoader.Flags flags)
-        {
-            return false;
-        }
-
-        internal void SetDebugColors()
-        {
-            DebugBackgroundColor(Color.Red, nameof(MainWindow));
-            pageContainer?.SetDebugColors();
-        }
-
-        public void Initialize()
-        {
-            pageContainer = new(SplittedTreeAndCards.TreeKind.ListBox);
-            panel.Parent = this;
-            Title = "AlterNET UI Demo";
-            Size = (900, 700);
-            StartLocation = WindowStartLocation.CenterScreen;
-
-            Icon = new("embres:ControlsSampleDll.Sample.ico");
-
-            void AddPage(string title, Func<Control> action)
-            {
-                pageContainer.Add(title, action);
-            }
-
             AddPage("Welcome", CreateWelcomePage);
             AddPage("Text", CreateTextInputPage);
             AddPage("ListBoxes", CreateListControlsPage);
@@ -128,51 +26,6 @@ namespace ControlsSample
             AddPage("TabControl", CreateTabControlPage);
             AddPage("Multimedia", CreateMultimediaPage);
             AddPage("Samples", CreateOtherPage);
-
-            LogUtils.DebugLogVersion();
-
-            splitterPanel.Parent = panel;
-            pageContainer.Dock = DockStyle.Fill;
-            splitter.Dock = DockStyle.Bottom;
-            pageContainer.Parent = splitterPanel;
-            splitter.Parent = splitterPanel;
-            eventsControl.Height = 150;
-            eventsControl.Parent = splitterPanel;
-
-            pageContainer.SelectedIndex = 0;
-            pageContainer.ListBox!.HScrollBarVisible = true;
-            pageContainer.LeftControl?.SetFocusIfPossible();
-
-            var logSizeChanged = false;
-
-            if (logSizeChanged)
-            {
-                SizeChanged += MainWindow_SizeChanged;
-                StateChanged += MainWindow_StateChanged;
-            }
-        }
-
-        private void MainWindow_StateChanged(object? sender, EventArgs e)
-        {
-            App.Log($"State changed: {Size}, {this.State}");
-        }
-
-        private void MainWindow_SizeChanged(object? sender, EventArgs e)
-        {
-            App.Log($"Size changed: {Size}, {this.State}");
-        }
-
-        Control CreateCustomPage(NameValue<Func<Control>>?[] pages)
-        {
-            TabControl result = new()
-            {
-                Margin = 10,
-            };
-
-            result.ContentPadding = 5;
-
-            result.AddRange(pages);
-            return result;
         }
 
         Control CreateListControlsPage()
@@ -319,33 +172,6 @@ namespace ControlsSample
             if (sender is not LinkLabel linkLabel)
                 return;
             LogEvent(linkLabel.Url);
-        }
-
-        [Browsable(false)]
-        public string? LastEventMessage => eventsControl.LastLogMessage;
-
-        public void LogEventSmart(string? message, string? prefix)
-        {
-            eventsControl.LogReplace(message, prefix);
-        }
-
-        public void LogEvent(string? message)
-        {
-            eventsControl.Log(message);
-        }
-
-        private static void ResourceLoader_CustomStreamFromUrl(object? sender, StreamFromUrlEventArgs e)
-        {
-            if (e.Value is null)
-                return;
-
-            e.Handled = true;
-            e.Result = ResourceLoader.DefaultStreamFromUrl(e.Value);
-        }
-
-        private static void ResourceLoader_CustomStreamFromUrl2(object? sender, StreamFromUrlEventArgs e)
-        {
-            App.Log("CustomStreamFromUrl 2");
         }
     }
 }
