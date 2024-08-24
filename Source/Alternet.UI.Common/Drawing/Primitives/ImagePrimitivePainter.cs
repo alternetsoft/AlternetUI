491893c7b23b328eb18f3b2d6c27cee4ba75caa7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-24 07:18:23 +0300

    Move some code from image primitive to base class

--- Source/Alternet.UI.Common/Drawing/Primitives/ImagePrimitivePainter.cs
+++ Source/Alternet.UI.Common/Drawing/Primitives/ImagePrimitivePainter.cs
@@ -24,68 +24,12 @@ namespace Alternet.Drawing
         public ImageSet? ImageSet;
 
         /// <summary>
-        /// Gets or sets destination point where to draw the image.
-        /// </summary>
-        public PointD DestPoint;
-
-        /// <summary>
-        /// Gets or sets destination image size.
-        /// </summary>
-        public SizeD? Size;
-
-        /// <summary>
         /// Gets or sets source rectangle which specifies part of the image to draw.
         /// </summary>
         public RectI? SourceRect = null;
 
-        /// <summary>
-        /// Gets whether or not to stretch this object. Default is <c>true</c>.
-        /// </summary>
-        public bool Stretch = true;
-
-        /// <summary>
-        /// Gets whether or not to center this object vertically. Default is <c>true</c>.
-        /// </summary>
-        public bool CenterVert = true;
-
-        /// <summary>
-        /// Gets whether or not to center this object horizontally. Default is <c>true</c>.
-        /// </summary>
-        public bool CenterHorz = true;
-
-        /// <summary>
-        /// Gets or sets destination rectangle where to draw the image.
-        /// </summary>
-        public virtual RectD DestRect
-        {
-            get
-            {
-                if(Size is null)
-                {
-                    return new RectD(DestPoint, Drawing.SizeD.Empty);
-                }
-                else
-                    return new RectD(DestPoint, Size.Value);
-            }
-
-            set
-            {
-                DestPoint = value.TopLeft;
-                Size = value.Size;
-            }
-        }
-
-        /// <summary>
-        /// Gets whether or not center this object horizontally or vertically.
-        /// </summary>
-        public virtual bool CenterHorzOrVert => CenterHorz || CenterVert;
-
-        /// <summary>
-        /// Draws image specified in the primitive properties.
-        /// </summary>
-        /// <param name="control">Control in which image is painted.</param>
-        /// <param name="dc">Drawing context.</param>
-        public virtual void Draw(Control control, Graphics dc)
+        /// <inheritdoc/>
+        public override void Draw(Control control, Graphics dc)
         {
             var image = Image;
             image ??= ImageSet?.AsImage(ImageSet.DefaultSize);
--- Source/Alternet.UI.Common/Drawing/Primitives/PrimitivePainter.cs
+++ Source/Alternet.UI.Common/Drawing/Primitives/PrimitivePainter.cs
@@ -14,8 +14,69 @@ namespace Alternet.Drawing
     public class PrimitivePainter : BaseObject
     {
         /// <summary>
+        /// Gets or sets destination point where to draw the primitive.
+        /// </summary>
+        public PointD DestPoint;
+
+        /// <summary>
+        /// Gets or sets destination size of the primitive.
+        /// </summary>
+        public SizeD? Size;
+
+        /// <summary>
         /// Gets or sets whether this object is visible.
         /// </summary>
         public bool Visible = true;
+
+        /// <summary>
+        /// Gets whether or not to stretch this object. Default is <c>true</c>.
+        /// </summary>
+        public bool Stretch = true;
+
+        /// <summary>
+        /// Gets whether or not to center this object vertically. Default is <c>true</c>.
+        /// </summary>
+        public bool CenterVert = true;
+
+        /// <summary>
+        /// Gets whether or not to center this object horizontally. Default is <c>true</c>.
+        /// </summary>
+        public bool CenterHorz = true;
+
+        /// <summary>
+        /// Gets or sets destination rectangle where to draw the image.
+        /// </summary>
+        public virtual RectD DestRect
+        {
+            get
+            {
+                if (Size is null)
+                {
+                    return new RectD(DestPoint, Drawing.SizeD.Empty);
+                }
+                else
+                    return new RectD(DestPoint, Size.Value);
+            }
+
+            set
+            {
+                DestPoint = value.TopLeft;
+                Size = value.Size;
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not center this object horizontally or vertically.
+        /// </summary>
+        public virtual bool CenterHorzOrVert => CenterHorz || CenterVert;
+
+        /// <summary>
+        /// Draws primitive on the canvas.
+        /// </summary>
+        /// <param name="control">Control in which primitive is painted.</param>
+        /// <param name="dc">Drawing context.</param>
+        public virtual void Draw(Control control, Graphics dc)
+        {
+        }
     }
 }
