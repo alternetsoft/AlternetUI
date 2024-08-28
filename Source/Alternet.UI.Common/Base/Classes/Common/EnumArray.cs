885231275cf948c1d18843775a32844286df37b6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-08-28 04:49:21 +0300

    Added EnumArray<TKey, TValue>
    Structure with internal array which elements can be accessed using enum values as the indexes.

--- Source/Alternet.UI.Common/Base/Classes/Common/EnumArray.cs
+++ Source/Alternet.UI.Common/Base/Classes/Common/EnumArray.cs
@@ -11,7 +11,7 @@ namespace Alternet.UI
     /// </summary>
     /// <typeparam name="TKey">Type of the enum used as the index in the array.</typeparam>
     /// <typeparam name="TValue">Type of the array elements.</typeparam>
-    internal readonly struct EnumArray<TKey, TValue>
+    public readonly struct EnumArray<TKey, TValue>
         where TKey : struct, Enum, IConvertible
     {
         /// <summary>
@@ -22,11 +22,48 @@ namespace Alternet.UI
         /// <summary>
         /// Initializes a new instance of the <see cref="EnumArray{TKey, TValue}"/> struct.
         /// </summary>
+        public EnumArray()
+            : this(EnumUtils.GetMaxValueUseMax<TKey>())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumArray{TKey, TValue}"/> struct.
+        /// </summary>
         /// <param name="maxKeyValue">Maximal value in the enum.</param>
         public EnumArray(TKey maxKeyValue)
         {
             var m = System.Convert.ToInt32(maxKeyValue);
             Data = new TValue[m + 1];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumArray{TKey, TValue}"/> struct.
+        /// </summary>
+        /// <param name="maxKeyValue">Maximal value in the enum.</param>
+        public EnumArray(int maxKeyValue)
+        {
+            Data = new TValue[maxKeyValue + 1];
+        }
+
+        /// <summary>
+        /// Gets or sets array element.
+        /// </summary>
+        /// <param name="index">Index of the array element.</param>
+        /// <returns></returns>
+        public TValue this[TKey index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                return Data[System.Convert.ToInt32(index)];
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                Data[System.Convert.ToInt32(index)] = value;
+            }
         }
 
         /// <summary>
