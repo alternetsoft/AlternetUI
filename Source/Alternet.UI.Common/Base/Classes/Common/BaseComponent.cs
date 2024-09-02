ffdae420929d9e989de94a9129575d8240ba951c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-09-02 03:58:33 +0300

    BaseComponent is now derived from DisposableObject
    and has Site and Tag properties

--- Source/Alternet.UI.Common/Base/Classes/Common/BaseComponent.cs
+++ Source/Alternet.UI.Common/Base/Classes/Common/BaseComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,18 @@ namespace Alternet.UI
     /// <summary>
     /// Base class for Alternet.UI components.
     /// </summary>
-    public class BaseComponent : BaseObject
+    public class BaseComponent : DisposableObject, IComponent
     {
+        /// <summary>
+        /// Gets or sets the <see cref="ISite"/> associated with the object; or <c>null</c>,
+        /// if the object does not have a site.
+        /// </summary>
+        public virtual ISite? Site { get; set; }
+
+        /// <summary>
+        /// Gets or sets an arbitrary <see cref="object"/> representing some type of user state.
+        /// </summary>
+        /// <value>An arbitrary <see cref="object"/> representing some type of user state.</value>
+        public virtual object? Tag { get; set; }
     }
 }
