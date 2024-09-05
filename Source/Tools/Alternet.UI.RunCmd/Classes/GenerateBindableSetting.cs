874aaa8674734eaa88f1db93984d4f4a1201ffc4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-09-05 11:34:49 +0300

    Bindable properties generator (started)

---
+++ Source/Tools/Alternet.UI.RunCmd/Classes/GenerateBindableSetting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    public class GenerateBindableSetting
+    {
+        public string? RootFolder { get; set; }
+
+        public string? PathToDll { get; set; }
+
+        public string? TypeName { get; set; }
+
+        public string? PathToResult { get; set; }
+
+        public string? ResultTypeName { get; set; }
+
+        public void Prepare()
+        {
+            if(!string.IsNullOrEmpty(RootFolder))
+                RootFolder = Path.GetFullPath(RootFolder);
+            
+            PathToDll = ReplaceParams(PathToDll);
+            PathToResult = ReplaceParams(PathToResult);
+
+            string? ReplaceParams(string? s)
+            {
+                if (s is null)
+                    return null;
+                if (!string.IsNullOrEmpty(RootFolder))
+                {
+                    var result = s.Replace("$(RootFolder)", RootFolder);
+                    return result;
+                }
+
+                return s;
+            }
+        }
+    }
+}
---
+++ Source/Tools/Alternet.UI.RunCmd/Classes/GenerateBindableSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Alternet.Base.Collections;
+
+namespace Alternet.UI
+{
+    public class GenerateBindableSettings
+    {
+        public Collection<GenerateBindableSetting> Items { get; } = new();
+
+        public static void TestSave()
+        {
+            GenerateBindableSettings settings = new();
+
+            GenerateBindableSetting setting = new();
+
+            setting.RootFolder = @"..\..\..\..\..\..\..\AlternetStudio\Source";
+            setting.PathToDll = @"$(RootFolder)\Editor\bin\Debug\net8.0\Alternet.Editor.AlterNetUI.v10.dll";
+            setting.TypeName = "Alternet.Editor.SyntaxEdit";
+            setting.PathToResult = @"$(RootFolder)\Editor.MAUI\Classes\SyntaxEditMaui.Generated.cs";
+            setting.ResultTypeName = "Alternet.Editor.SyntaxEditMaui";
+
+            settings.Items.Add(setting);
+
+            XmlUtils.SerializeToFile(@"e:\GenerateBindableSettings.xml", settings);
+        }
+    }
+}
+
+
+
+
