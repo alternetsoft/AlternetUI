ee91e873f50288d18e22bd0d8fcdb46f8cc91a2f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-09-06 14:02:51 +0300

    Maui related minor changes

---
+++ Source/Alternet.UI.Common/Utils/SystemUtils/AsyncUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains methods and properties related to async execution and tasks.
+    /// </summary>
+    public static class AsyncUtils
+    {
+        /// <summary>
+        /// Runs the specified task synchroniously.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void Sync(Func<Task> func) => Task.Run(func).ConfigureAwait(false);
+
+        /// <summary>
+        /// Runs the specified task synchroniously and returns result.
+        /// </summary>
+        /// <param name="func"></param>
+        public static T Sync<T>(Func<Task<T>> func) => Task.Run(func).Result;
+    }
+}
--- Source/Alternet.UI.Maui/Common/MauiDialogFactoryHandler.cs
+++ Source/Alternet.UI.Maui/Common/MauiDialogFactoryHandler.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Alternet.Drawing;
+using Alternet.UI.Localization;
+
+using Microsoft.Maui.Dispatching;
 
 namespace Alternet.UI
 {
@@ -36,6 +39,14 @@ namespace Alternet.UI
             throw new NotImplementedException();
         }
 
+        /// <param name="message">A (possibly) multiline dialog message above the single line
+        /// <paramref name="prompt"/>.</param>
+        /// <param name="prompt">Single line dialog prompt.</param>
+        /// <param name="caption">Dialog title.</param>
+        /// <param name="value">Default value. Optional. Default is 0.</param>
+        /// <param name="min">A positive minimal value. Optional. Default is 0.</param>
+        /// <param name="max">A positive maximal value. Optional. Default is 100.</param>
+        /// <param name="parent">Dialog parent.</param>
         public long? GetNumberFromUser(
             string message,
             string prompt,
@@ -45,6 +56,31 @@ namespace Alternet.UI
             long max,
             Control? parent)
         {
+            /*
+            var page = MauiApplicationHandler.GetParentPage(parent);
+
+            if (page is null)
+                return null;
+
+            Task<string> ShowDialog()
+            {
+                var result = page.DisplayPromptAsync(
+                                    caption,
+                                    prompt,
+                                    CommonStrings.Default.ButtonOk,
+                                    CommonStrings.Default.ButtonCancel,
+                                    placeholder: message,
+                                    maxLength: -1,
+                                    Microsoft.Maui.Keyboard.Numeric,
+                                    initialValue: value.ToString());
+                return result;
+            }
+
+            string? result = await ShowDialog();
+
+            if (long.TryParse(result, out var longResult))
+                return longResult;
+            */
             return null;
         }
 
