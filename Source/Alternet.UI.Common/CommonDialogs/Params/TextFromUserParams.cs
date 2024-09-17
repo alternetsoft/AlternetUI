37a733e3032a370bc8ef0f72919052b5beff2e01
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-09-17 08:46:51 +0300

    DialogFactory.GetNumberFromUserAsync

--- Source/Alternet.UI.Common/CommonDialogs/Params/TextFromUserParams.cs
+++ Source/Alternet.UI.Common/CommonDialogs/Params/TextFromUserParams.cs
@@ -7,7 +7,7 @@ namespace Alternet.UI
     /// <summary>
     /// Contains properties which allow to customize dialog box which asks a text value from the user.
     /// </summary>
-    public class TextFromUserParams : ValueFromUserParams<string>
+    public class TextFromUserParams : ValueFromUserParams<string?>
     {
     }
 }
