000972da8f848da4e708fd679587f107f6da27ab
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-09-23 09:16:53 +0300

    IImmutableObject

---
+++ Source/Alternet.UI.Common/Interfaces/IImmutableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains properties and methods which allow to get and set immutable state of the object.
+    /// </summary>
+    public interface IImmutableObject : IDisposableObject
+    {
+        /// <summary>
+        /// Gets whether object is immutable (properties can not be changed).
+        /// </summary>
+        bool Immutable { get; }
+
+        /// <summary>
+        /// <inheritdoc cref="ImmutableObject.SetImmutable()"/>
+        /// </summary>
+        void SetImmutable();
+    }
+}
