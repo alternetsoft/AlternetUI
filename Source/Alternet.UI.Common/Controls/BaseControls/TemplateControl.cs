fb4a5f5ad4c38519efe4295755473cede981b7cc
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-11 09:03:48 +0300

    TemplateControl and TemplateUtils

---
+++ Source/Alternet.UI.Common/Controls/BaseControls/TemplateControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Parent class for all template controls.
+    /// </summary>
+    public class TemplateControl : Border
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemplateControl"/> class.
+        /// </summary>
+        public TemplateControl()
+        {
+            Visible = false;
+            HasBorder = false;
+        }
+    }
+}
