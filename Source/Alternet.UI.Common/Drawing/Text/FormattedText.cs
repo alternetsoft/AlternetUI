3afa2d8b5e8df86de2bf4ce65b2473d9f1aed9c9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-12 19:40:56 +0300

    Update FormattedText.cs

--- Source/Alternet.UI.Common/Drawing/Text/FormattedText.cs
+++ Source/Alternet.UI.Common/Drawing/Text/FormattedText.cs
@@ -122,6 +122,19 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
+        /// Gets wrapped text. Each line of the wrapped text has width less than <see cref="MaxWidth"/>
+        /// and doesn't have new line characters.
+        /// </summary>
+        public virtual IEnumerable<string>? WrappedText
+        {
+            get
+            {
+                Prepare();
+                return wrappedText;
+            }
+        }
+
+        /// <summary>
         /// Gets size of the text with applied min and max constraints in device-independent units.
         /// </summary>
         public virtual SizeD RestrictedSize
@@ -135,7 +148,7 @@ namespace Alternet.Drawing
 
         /// <summary>
         /// Gets real size of the text in device-independent units.
-        /// <see cref="MaxWidth"/> and <see cref="MaxHeight"/> are not applied to the result. 
+        /// <see cref="MaxWidth"/> and <see cref="MaxHeight"/> are not applied to the result.
         /// </summary>
         public virtual SizeD RealSize
         {
@@ -147,6 +160,18 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
+        /// Gets of sets horizontal alignment of the whole text block.
+        /// </summary>
+        public virtual HorizontalAlignment BlockHorizontalAlignment { get; set; }
+            = UI.HorizontalAlignment.Stretch;
+
+        /// <summary>
+        /// Gets of sets vertical alignment of the whole text block.
+        /// </summary>
+        public virtual VerticalAlignment BlockVerticalAlignment { get; set; }
+            = UI.VerticalAlignment.Stretch;
+
+        /// <summary>
         /// Gets or sets foreground color. If not specified, black color is used.
         /// </summary>
         public virtual Color? ForegroundColor { get; set; }
@@ -156,7 +181,16 @@ namespace Alternet.Drawing
         /// </summary>
         public virtual Color? BackgroundColor { get; set; }
 
-        private Font SafeFont => Font ?? Control.DefaultFont;
+        /// <summary>
+        /// Gets or sets whether to set clip rectangle when object is painted.
+        /// Default is <c>true</c>.
+        /// </summary>
+        public virtual bool IsClipped { get; set; } = true;
+
+        /// <summary>
+        /// Gets font in a safe way. If <see cref="Font"/> is not specified, returns default font.
+        /// </summary>
+        public virtual Font SafeFont => Font ?? Control.DefaultFont;
 
         /// <inheritdoc/>
         public override void Changed()
@@ -167,11 +201,35 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
+        /// Gets aligned bounding recrangle of the text.
+        /// </summary>
+        /// <param name="scaleFactor">Scale factor.</param>
+        /// <param name="container">Container rectangle.</param>
+        /// <returns></returns>
+        public virtual RectD GetBlockRect(Coord scaleFactor, RectD container)
+        {
+            ScaleFactor = ScaleFactor;
+            Prepare();
+
+            if (wrappedText is null)
+                return container;
+
+            RectD blockRect = (container.Location, RestrictedSize);
+            blockRect = AlignUtils.AlignRectInRect(
+                blockRect,
+                container,
+                BlockHorizontalAlignment,
+                BlockVerticalAlignment);
+
+            return blockRect;
+        }
+
+        /// <summary>
         /// Draws formatted text.
         /// </summary>
         /// <param name="dc">Canvas where drawing is performed.</param>
-        /// <param name="rect">Bounding rectangle.</param>
-        public virtual void Draw(Graphics dc, RectD rect)
+        /// <param name="bounds">Bounding rectangle.</param>
+        public virtual void Draw(Graphics dc, RectD bounds)
         {
             ScaleFactor = dc.ScaleFactor;
             Prepare();
@@ -179,23 +237,20 @@ namespace Alternet.Drawing
             if (wrappedText is null)
                 return;
 
-            PointD location = rect.Location;
+            var blockRect = GetBlockRect(dc.ScaleFactor, bounds);
 
-            dc.PushClip();
-            try
-            {
-                dc.Clip = new Region(rect);
-                dc.DrawText(
-                    wrappedText,
-                    location,
-                    SafeFont,
-                    ForegroundColor ?? Color.Black,
-                    BackgroundColor ?? Color.Empty);
-            }
-            finally
-            {
-                dc.PopClip();
-            }
+            dc.DoInsideClipped(
+                bounds,
+                () =>
+                {
+                    dc.DrawText(
+                        wrappedText,
+                        blockRect.Location,
+                        SafeFont,
+                        ForegroundColor ?? Color.Black,
+                        BackgroundColor ?? Color.Empty);
+                },
+                IsClipped);
 
             /*
                     private TextHorizontalAlignment horizontalAlignment = TextHorizontalAlignment.Left;
@@ -205,7 +260,10 @@ namespace Alternet.Drawing
             */
         }
 
-        private void Prepare()
+        /// <summary>
+        /// Prepares object for painting. You should not call this normally.
+        /// </summary>
+        public virtual void Prepare()
         {
             if (wrappedText is not null)
                 return;
