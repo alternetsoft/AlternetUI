02a941e4b90eddac1026774a6acd2ef852f4a581
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-18 22:19:56 +0300

    TemplateUtils.TemplateRichToolTip

--- Source/Alternet.UI.Common/Controls/Templates/TemplateUtils.cs
+++ Source/Alternet.UI.Common/Controls/Templates/TemplateUtils.cs
@@ -240,15 +240,70 @@ namespace Alternet.UI
             where TLabel : AbstractControl, new()
         {
             var result = new TemplateWithBoldText<TLabel>(prefix, boldText, suffix, hasBorder);
-
             if(fontAndColor is not null)
+                result.AsFontAndColor = fontAndColor;
+            return result;
+        }
+
+        /// <summary>
+        /// Sample template control with text which has a middle part with bold font.
+        /// </summary>
+        public class TemplateRichToolTip<TLabel> : TemplateControl
+            where TLabel : AbstractControl, new()
+        {
+            private readonly Border border = new()
             {
-                result.BackgroundColor = fontAndColor.BackgroundColor;
-                result.ForegroundColor = fontAndColor.ForegroundColor;
-                result.Font = fontAndColor.Font;
+                Layout = LayoutStyle.Horizontal,
+                HasBorder = false,
+            };
+
+            private readonly Grid grid = new()
+            {
+                RowColumnCount = (2, 2),
+            };
+
+            private readonly TLabel titleLabel = new();
+            private readonly TLabel messageLabel = new();
+            private readonly PictureBox pictureBox = new();
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TemplateRichToolTip{T}"/> class.
+            /// </summary>
+            public TemplateRichToolTip()
+            {
+                DoInsideLayout(() =>
+                {
+                    border.Parent = this;
+                    grid.Parent = border;
+
+                    pictureBox.Parent = grid;
+
+                    titleLabel.RowColumn = (0, 1);
+                    titleLabel.Parent = grid;
+
+                    messageLabel.RowColumn = (1, 1);
+                    messageLabel.Parent = grid;
+
+                    SetChildrenUseParentBackColor(true, true);
+                    SetChildrenUseParentForeColor(true, true);
+                    SetChildrenUseParentFont(true, true);
+                });
             }
 
-            return result;
+            /// <summary>
+            /// Gets control which contains title.
+            /// </summary>
+            public AbstractControl TitleLabel => titleLabel;
+
+            /// <summary>
+            /// Gets control which contains message.
+            /// </summary>
+            public AbstractControl MessageLabel => messageLabel;
+
+            /// <summary>
+            /// Gets control which contains image.
+            /// </summary>
+            public PictureBox PictureBox => pictureBox;
         }
 
         /// <summary>
