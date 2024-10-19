ff8f0056b2658f57ede436be35750b935a5fb43c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-19 18:57:32 +0300

    Create LightDarkImage.cs

---
+++ Source/Alternet.UI.Common/Drawing/Image/LightDarkImage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Contains image for the light and dark color themes.
+    /// </summary>
+    public class LightDarkImage
+    {
+        /// <summary>
+        /// Gets or sets image for the light color theme.
+        /// </summary>
+        public Image? Light;
+
+        /// <summary>
+        /// Gets or sets image for the dark color theme.
+        /// </summary>
+        public Image? Dark;
+
+        /// <summary>
+        /// Gets image for the specified light/dark flag.
+        /// </summary>
+        /// <param name="isDark">The light/dark flag for which to get the image.</param>
+        /// <returns></returns>
+        public virtual Image? GetImage(bool isDark)
+        {
+            if (isDark)
+                return Dark;
+            return Light;
+        }
+
+        /// <summary>
+        /// Sets image for the specified light/dark flag.
+        /// </summary>
+        /// <param name="isDark">The light/dark flag for which to set the image.</param>
+        /// <param name="value">New image value.</param>
+        public virtual void SetImage(bool isDark, Image? value)
+        {
+            if (isDark)
+                Dark = value;
+            else
+                Light = value;
+        }
+    }
+}
