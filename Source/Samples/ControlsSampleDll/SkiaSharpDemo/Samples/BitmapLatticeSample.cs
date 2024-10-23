ec8fa54b78d319a6a7de9e1a9c643c586a80a44c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-23 07:51:41 +0300

    Hide SkiaSharpDemo warnings
    appeared when switched to SkiaSharp v3

--- Source/Samples/ControlsSampleDll/SkiaSharpDemo/Samples/BitmapLatticeSample.cs
+++ Source/Samples/ControlsSampleDll/SkiaSharpDemo/Samples/BitmapLatticeSample.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#pragma warning disable
+using System;
 
 using SkiaSharp;
 
--- Source/Samples/ControlsSampleDll/SkiaSharpDemo/Samples/CreateXpsSample.cs
+++ Source/Samples/ControlsSampleDll/SkiaSharpDemo/Samples/CreateXpsSample.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#pragma warning disable
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
