b206dadd85a67e4be9005b4be4b5ea995f63f267
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-29 04:31:36 +0200

    Create InvalidateEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/InvalidateEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the 'Invalidated' event.
+    /// </summary>
+    public class InvalidateEventArgs : BaseEventArgs
+    {
+        private readonly RectD invalidRect;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidateEventArgs" /> class.
+        /// </summary>
+        /// <param name="invalidRect">The <see cref="RectD" /> that contains
+        /// the invalidated window area.
+        /// </param>
+        public InvalidateEventArgs(RectD invalidRect)
+        {
+            this.invalidRect = invalidRect;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="RectD" /> that contains the invalidated window area.
+        /// </summary>
+        /// <returns>The invalidated window area.</returns>
+        public RectD InvalidRect => invalidRect;
+    }
+}
