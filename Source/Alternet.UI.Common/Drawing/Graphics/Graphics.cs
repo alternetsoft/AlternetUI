8d13968a45cc8cea4e135c3f54813cf8d81ad924
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-30 18:34:29 +0200

    Graphics: splitted clip related to separate file

--- Source/Alternet.UI.Common/Drawing/Graphics/Graphics.cs
+++ Source/Alternet.UI.Common/Drawing/Graphics/Graphics.cs
@@ -90,23 +90,13 @@ namespace Alternet.Drawing
         /// <summary>
         /// Gets vertical scale factor.
         /// </summary>
-        public Coord VerrticalScaleFactor
+        public Coord VerticalScaleFactor
         {
             get
             {
                 return GraphicsFactory.ScaleFactorFromDpi(GetDPI().Height);
             }
         }
-
-        /// <summary>
-        /// Gets or sets clipping region.
-        /// </summary>
-        public abstract Region? Clip { get; set; }
-
-        /// <summary>
-        /// Gets whether <see cref="Clip"/> property is assigned.
-        /// </summary>
-        public abstract bool HasClip { get; }
 
         /// <summary>
         /// Gets or sets the interpolation mode associated with this <see cref="Graphics"/>.
@@ -1225,55 +1215,6 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
-        /// Calls the specified action inside temprorary clipped rectangle, so painting outside
-        /// this rectangle is ignored.
-        /// </summary>
-        /// <param name="isClipped">Whether to clip rectangle. Optional. Default is <c>true</c>.</param>
-        /// <param name="rect">Rectangle region to set as clip object.</param>
-        /// <param name="action">Action to call.</param>
-        public virtual void DoInsideClipped(RectD rect, Action action, bool isClipped = true)
-        {
-            if (isClipped)
-            {
-                try
-                {
-                    PushClip();
-                    Clip = new Region(rect);
-                    action();
-                }
-                finally
-                {
-                    PopClip();
-                }
-            }
-            else
-                action();
-        }
-
-        /// <summary>
-        /// Pops a stored clip region state from the stack and sets the current clip region
-        /// to that state.
-        /// </summary>
-        public void PopClip()
-        {
-            clipStack ??= new();
-            Clip = clipStack.Pop();
-        }
-
-        /// <summary>
-        /// Pushes the current state of the clip region on a stack.
-        /// </summary>
-        public void PushClip()
-        {
-            clipStack ??= new();
-
-            if(HasClip)
-                clipStack.Push(Clip);
-            else
-                clipStack.Push(null);
-        }
-
-        /// <summary>
         /// Pushes the current state of the transformation matrix on a stack.
         /// </summary>
         public void Push()
@@ -1291,43 +1232,6 @@ namespace Alternet.Drawing
         /// returns Size(0,0) object.
         /// </returns>
         public abstract SizeI GetDPI();
-
-        /// <summary>
-        /// Destroys the current clipping region so that none of the DC is clipped.
-        /// </summary>
-        public abstract void DestroyClippingRegion();
-
-        /// <summary>
-        /// Sets the clipping region for this device context to the intersection of the
-        /// given region described by the parameters of this method and the previously
-        /// set clipping region.
-        /// </summary>
-        /// <param name="rect">Clipping rectangle.</param>
-        /// <remarks>
-        /// The clipping region is an area to which drawing is restricted. Possible uses
-        /// for the clipping region are for clipping text or for speeding up
-        /// window redraws when only a known area of the screen is damaged.
-        /// </remarks>
-        /// <remarks>
-        /// Calling this function can only make the clipping region
-        /// smaller, never larger.
-        /// You need to call <see cref="DestroyClippingRegion"/> first if you want
-        /// to set the clipping
-        /// region exactly to the region specified.
-        /// If resulting clipping region is empty, then all drawing on the DC is
-        /// clipped out (all changes made by drawing operations are masked out).
-        /// </remarks>
-        public abstract void SetClippingRegion(RectD rect);
-
-        /// <summary>
-        /// Gets the rectangle surrounding the current clipping region.
-        /// If no clipping region is set this function returns the extent of the device context.
-        /// </summary>
-        /// <returns>
-        /// <see cref="RectD"/> filled in with the logical coordinates of the clipping region
-        /// on success, or <see cref="RectD.Empty"/> otherwise.
-        /// </returns>
-        public abstract RectD GetClippingBox();
 
         /// <summary>
         /// Converts point to device-independent units.
