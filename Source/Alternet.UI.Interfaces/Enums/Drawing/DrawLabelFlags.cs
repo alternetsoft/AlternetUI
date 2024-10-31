3718c467feaaaa5494cb269a866eb6596871d8cd
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-10-31 07:22:38 +0200

    Create DrawLabelFlags.cs

---
+++ Source/Alternet.UI.Interfaces/Enums/Drawing/DrawLabelFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Enumerates flags that can be used to customize label painting.
+    /// </summary>
+    [Flags]
+    public enum DrawLabelFlags
+    {
+        /// <summary>
+        /// Text may contain html bold tags.
+        /// </summary>
+        TextHasBold = 1,
+    }
+}
