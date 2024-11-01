04512a82ee7c55c8869b0632f6aabac9790d08d7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-01 09:03:41 +0200

    Splitted Demo\Tests to separate files for each control

---
+++ Source/Samples/PropertyGridSample/Classes/Tests/Tests.ToolBar.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI;
+using Alternet.Base.Collections;
+using Alternet.Drawing;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+
+namespace PropertyGridSample
+{
+    public partial class MainWindow
+    {
+        void InitTestsToolBar()
+        {
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Visible", TestGenericToolBarVisible);
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Enabled", TestGenericToolBarEnabled);
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Delete", TestGenericToolBarDelete);
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Sticky", TestGenericToolBarSticky);
+            PropertyGrid.AddSimpleAction<ToolBar>(
+                "Test Foreground Color",
+                TestGenericToolBarForegroundColor);
+            PropertyGrid.AddSimpleAction<ToolBar>(
+                "Test Background Color",
+                TestGenericToolBarBackgroundColor);
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Font", TestGenericToolBarFont);
+            PropertyGrid.AddSimpleAction<ToolBar>("Test Background", TestGenericToolBarBackground);
+            PropertyGrid.AddSimpleAction<ToolBar>("Reset Background", TestGenericToolBarResetBackground);
+            PropertyGrid.AddSimpleAction<ToolBar>("Clear", TestGenericToolBarClear);
+            PropertyGrid.AddSimpleAction<ToolBar>("Add OK button", TestGenericToolBarAddOk);
+            PropertyGrid.AddSimpleAction<ToolBar>("Add Cancel button", TestGenericToolBarAddCancel);
+            PropertyGrid.AddSimpleAction<ToolBar>("ReInit", TestGenericToolBarReInit);
+        }
+
+        void TestGenericToolBarFont()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.Font = AbstractControl.DefaultFont.Scaled(2);
+        }
+
+        void TestGenericToolBarForegroundColor()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.ForegroundColor = Color.Red;
+        }
+
+        void TestGenericToolBarBackgroundColor()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.Background = null;
+            control.BackgroundColor = Color.DarkOliveGreen;
+        }
+
+        void TestGenericToolBarBackground()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.Background = Color.RebeccaPurple.AsBrush;
+            control.BackgroundColor = null;
+        }
+
+        void TestGenericToolBarClear()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.DeleteAll();
+        }
+
+        void TestGenericToolBarAddOk()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.AddSpeedBtn(KnownButton.OK);
+        }
+
+        void TestGenericToolBarAddCancel()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.AddSpeedBtn(KnownButton.Cancel);
+        }
+
+        void TestGenericToolBarReInit()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.DeleteAll(false);
+            ObjectInit.InitGenericToolBar(control);
+        }
+
+
+        void TestGenericToolBarResetBackground()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            control.Background = null;
+            control.BackgroundColor = null;
+        }
+
+        void TestGenericToolBarVisible()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            var childId = control.GetToolId(1);
+            var value = control.GetToolVisible(childId);
+            value = !value;
+            control.SetToolVisible(childId, value);
+        }
+
+        void TestGenericToolBarEnabled()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            var childId = control.GetToolId(1);
+            var enabled = control.GetToolEnabled(childId);
+            enabled = !enabled;
+            control.SetToolEnabled(childId, enabled);
+        }
+
+        void TestGenericToolBarSticky()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            var childId = control.GetToolId(1);
+            var value = control.GetToolSticky(childId);
+            value = !value;
+            control.SetToolSticky(childId, value);
+        }
+
+        void TestGenericToolBarDelete()
+        {
+            var control = GetSelectedControl<ToolBar>();
+            if (control is null)
+                return;
+            var childId = control.GetToolId(1);
+            control.DeleteTool(childId);
+        }
+    }
+}
---
+++ Source/Samples/PropertyGridSample/Classes/Tests/Tests.TreeView.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alternet.UI;
+using Alternet.Base.Collections;
+using Alternet.Drawing;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+
+namespace PropertyGridSample
+{
+    public partial class MainWindow
+    {
+        void InitTestsTreeView()
+        {
+            AddControlAction<TreeView>("Load png from resources", TestTreeViewLoadPngFromResource);
+            AddControlAction<TreeView>(
+                "Load all small *.png in folder...",
+                TestTreeViewLoadAllPngInFolder);
+            AddControlAction<TreeView>("Load all *.svg in folder...", TestTreeViewLoadAllSvgInFolder);
+            AddControlAction<TreeView>("Load known svg", TestTreeViewLoadKnownSvg);
+        }
+
+        void TestTreeViewLoadPngFromResource(TreeView control)
+        {
+            const string highDpiSuffix = "_HighDpi";
+
+            string[] resNames =
+            [
+                "ClassAlpha",
+                "ConstantAlpha",
+                "DelegateAlpha",
+                "EventAlpha",
+                "FieldAlpha",
+                "GenericParameterAlpha",
+                "InterfaceAlpha",
+                "KeywordAlpha",
+                "LocalOrParameterAlpha",
+                "MethodAlpha",
+                "NamespaceAlpha",
+                "PropertyAlpha",
+                "StructAlpha",
+            ];
+
+            string pathPrefix = "Resources.CodeComletionSymbols.";
+
+            int size = control.HasScaleFactor ? 32 : 16;
+
+            ImageList imgList = new();
+            imgList.ImageSize = size;
+
+            control.RemoveAll();
+            control.ImageList = imgList;
+            int index = 0;
+
+            foreach (var s in resNames)
+            {
+                var resNameHighDpi = $"{pathPrefix}{s}{highDpiSuffix}.png";
+                var resName = $"{pathPrefix}{s}.png";
+                var selectedName = HasScaleFactor ? resNameHighDpi : resName;
+
+                if (!imgList.AddFromAssemblyUrl(typeof(ObjectInit).Assembly, selectedName))
+                    continue;
+
+                TreeViewItem item = new(Path.GetFileName(selectedName), index);
+                control.Add(item);
+                index++;
+            }
+        }
+
+        void TestTreeViewLoadAllPngInFolder(TreeView control)
+        {
+            var dialog = SelectDirectoryDialog.Default;
+
+            dialog.ShowAsync(() =>
+            {
+                int size = 16;
+                if (control.HasScaleFactor)
+                    size = 32;
+
+                ImageList imgList = new();
+                imgList.ImageSize = size;
+
+                control.RemoveAll();
+                control.ImageList = imgList;
+                int index = 0;
+
+                var folder = dialog.DirectoryName;
+                var files = Directory.GetFiles(folder, "*.png");
+
+                Array.Sort(files);
+
+                foreach (var file in files)
+                {
+                    var image = new Bitmap(file);
+                    if (image.Size != (size, size))
+                        continue;
+                    if (!imgList.Add(image))
+                        continue;
+
+                    TreeViewItem item = new(Path.GetFileName(file), index);
+                    control.Add(item);
+                    index++;
+                }
+            });
+        }
+
+        void TestTreeViewLoadKnownSvg(TreeView control)
+        {
+            int size = 32;
+            ImageList imgList = new();
+            imgList.ImageSize = size;
+
+            control.RemoveAll();
+            control.ImageList = imgList;
+            int index = 0;
+
+            control.DoInsideUpdate(() =>
+            {
+                AddImages(KnownSvgImages.GetAllImages());
+                AddImages(KnownColorSvgImages.GetAllImages());
+            });
+
+            void AddImages(IEnumerable<SvgImage> images)
+            {
+                foreach (var svg in images)
+                {
+                    imgList.AddSvg(svg, control.IsDarkBackground);
+
+                    TreeViewItem item = new(svg.Url ?? "<empty url>", index);
+                    control.Add(item);
+                    index++;
+                }
+            }
+        }
+
+        void TestTreeViewLoadAllSvgInFolder(TreeView control)
+        {
+            var dialog = SelectDirectoryDialog.Default;
+
+            dialog.ShowAsync(() =>
+            {
+                int size = 32;
+                ImageList imgList = new();
+                imgList.ImageSize = size;
+
+                control.RemoveAll();
+                control.ImageList = imgList;
+                int index = 0;
+
+                var folder = dialog.DirectoryName;
+                var files = Directory.GetFiles(folder, "*.svg");
+
+                Array.Sort(files);
+
+                foreach (var file in files)
+                {
+                    var svg = new MonoSvgImage(file);
+                    imgList.AddSvg(svg, control.IsDarkBackground);
+
+                    TreeViewItem item = new(Path.GetFileName(file), index);
+                    control.Add(item);
+                    index++;
+                }
+            });
+        }
+    }
+}
