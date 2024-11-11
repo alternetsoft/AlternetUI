d448a6fd9eefad23b6baa7b98e4bf50940d0734a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-11 20:57:22 +0200

    testNewMAUI test project

---
+++ Source/Tests/testnewMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace testnewMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Source/Tests/testnewMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace testnewMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
