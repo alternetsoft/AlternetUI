afa1ad927b9d85d6e5a94d71505c26ebbb26afd6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-12 13:21:02 +0200

    Create ControlAndButtonClickEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Handled/ControlAndButtonClickEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the <see cref="ControlAndButton.ButtonClick"/> event.
+    /// </summary>
+    public class ControlAndButtonClickEventArgs : HandledEventArgs
+    {
+        /// <summary>
+        /// Gets id of the clicked button.
+        /// </summary>
+        public virtual ObjectUniqueId? ButtonId { get; set; }
+    }
+}
