f5bfbe6246376785cbdf77144d967e4d3eb80e9e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-12 17:26:53 +0200

    Create MeasureItemEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/MeasureItemEventArgs.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the measure item events.
+    /// </summary>
+    public class MeasureItemEventArgs : EventArgs
+    {
+        private Graphics graphics;
+        private Coord itemHeight;
+        private Coord itemWidth;
+        private int index;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MeasureItemEventArgs" /> class
+        /// providing a parameter for the item height.
+        /// </summary>
+        /// <param name="graphics">The <see cref="Graphics" /> object being written to.</param>
+        /// <param name="index">The index of the item for which you need the height or width.</param>
+        /// <param name="itemHeight">The height of the item to measure relative to
+        /// the <paramref name="graphics" /> object.</param>
+        public MeasureItemEventArgs(Graphics graphics, int index, int itemHeight)
+        {
+            this.graphics = graphics;
+            this.index = index;
+            this.itemHeight = itemHeight;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MeasureItemEventArgs" /> class.</summary>
+        /// <param name="graphics">The <see cref="Graphics" /> object being written to.</param>
+        /// <param name="index">The index of the item for which you need the height or width.</param>
+        public MeasureItemEventArgs(Graphics graphics, int index)
+        {
+            this.graphics = graphics;
+            this.index = index;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Graphics" /> object to measure against.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Graphics" /> object to use to determine the scale of the item
+        /// you are drawing.
+        /// </returns>
+        public virtual Graphics Graphics
+        {
+            get
+            {
+                return graphics;
+            }
+
+            set
+            {
+                graphics = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the index of the item for which the height and width is needed.
+        /// </summary>
+        /// <returns>
+        /// The index of the item to be measured.
+        /// </returns>
+        public virtual int Index
+        {
+            get
+            {
+                return index;
+            }
+
+            set
+            {
+                index = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the height of the item specified by the
+        /// <see cref="MeasureItemEventArgs.Index"/>.
+        /// </summary>
+        /// <returns>The height of the item measured.</returns>
+        public virtual Coord ItemHeight
+        {
+            get
+            {
+                return itemHeight;
+            }
+
+            set
+            {
+                itemHeight = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the item specified by the
+        /// <see cref="MeasureItemEventArgs.Index" />.
+        /// </summary>
+        /// <returns>The width of the item measured.</returns>
+        public virtual Coord ItemWidth
+        {
+            get
+            {
+                return itemWidth;
+            }
+
+            set
+            {
+                itemWidth = value;
+            }
+        }
+    }
+}
