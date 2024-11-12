8ce1abbf2cdf3341de3dee3315749a8b058fd93b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-12 17:05:39 +0200

    Create ButtonBorderStyle.cs

---
+++ Source/Alternet.UI.Interfaces/Enums/Buttons/ButtonBorderStyle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the border style for a button control.
+    /// </summary>
+    public enum ButtonBorderStyle
+    {
+        /// <summary>
+        /// No border.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// A dotted-line border.
+        /// </summary>
+        Dotted,
+
+        /// <summary>
+        /// A dashed border.
+        /// </summary>
+        Dashed,
+
+        /// <summary>
+        /// A solid border.
+        /// </summary>
+        Solid,
+
+        /// <summary>
+        /// A sunken border.
+        /// </summary>
+        Inset,
+
+        /// <summary>
+        /// A raised border.
+        /// </summary>
+        Outset,
+    }
+}
