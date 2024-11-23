d9d08a52a2403408c36f5c6ec7082610c8b1b86d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-23 09:49:02 +0200

    Don't use net9 terminal logger in the installation

--- RunBuildUI.sh
+++ RunBuildUI.sh
@@ -4,4 +4,4 @@ SCRIPT_HOME=$(cd "$(dirname "$0")"; pwd -P)
 
 # Build
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-dotnet msbuild /t:BuildUI_MacOsOrLinux $SCRIPT_HOME/Source/Build/Alternet.UI.Pal/Alternet.UI.Pal.proj
+dotnet msbuild -tl:off /t:BuildUI_MacOsOrLinux $SCRIPT_HOME/Source/Build/Alternet.UI.Pal/Alternet.UI.Pal.proj
