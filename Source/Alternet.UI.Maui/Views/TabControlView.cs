b7b2edf03e395542920453abbee9f2d813ba3fe7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-11-28 12:58:47 +0200

    Create TabControlView.cs

---
+++ Source/Alternet.UI.Maui/Views/TabControlView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Alternet.UI;
+
+using Microsoft.Maui.Dispatching;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements tab control view on the MAUI platform using <see cref="TabControl"/> control.
+    /// </summary>
+    /// <remarks>
+    /// This controls is implemented for testing purposes. It is better to use native MAUI control
+    /// instead of <see cref="TabControlView"/>.
+    /// </remarks>
+    public partial class TabControlView : ControlView<TabControl>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TabControlView"/> class.
+        /// </summary>
+        public TabControlView()
+        {
+        }
+    }
+}
