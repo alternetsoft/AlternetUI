aba1ae1f3bc4e08bb48f1f26229bae9b16d87b23
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-19 09:40:55 +0200

    Create WindowCloseAction.cs

---
+++ Source/Alternet.UI.Interfaces/Enums/Window/WindowCloseAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates known actions which can be performed when window is closed.
+    /// </summary>
+    public enum WindowCloseAction
+    {
+        /// <summary>
+        /// Window is disposed.
+        /// </summary>
+        Dispose,
+
+        /// <summary>
+        /// Window is hidden.
+        /// </summary>
+        Hide,
+
+        /// <summary>
+        /// Nothing is done.
+        /// </summary>
+        None,
+    }
+}
