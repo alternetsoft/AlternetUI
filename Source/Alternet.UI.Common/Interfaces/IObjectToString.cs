a46a7d6be5a89c47ff6e5daf634386e6aae8a113
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-22 01:34:32 +0200

    IObjectToString: new ToString overload

--- Source/Alternet.UI.Common/Interfaces/IObjectToString.cs
+++ Source/Alternet.UI.Common/Interfaces/IObjectToString.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,5 +57,54 @@ namespace Alternet.UI
         /// <returns>The <see cref="string"/> representation of the <paramref name="value"/>
         /// as specified by <paramref name="format"/> and <paramref name="provider"/>.</returns>
         string? ToString(object value, string? format, IFormatProvider? provider);
+
+        /// <summary>
+        /// Converts the <paramref name="value"/> to its equivalent <see cref="string"/>
+        /// representation, using the specified context and culture information.
+        /// </summary>
+        /// <param name="value">A value for convertion.</param>
+        /// <param name="context">An <see cref="ITypeDescriptorContext"/> that
+        /// provides a format context.</param>
+        /// <param name="culture"><see cref="CultureInfo"/> used for the conversion.
+        /// If null is passed, the current culture is assumed.</param>
+        /// <param name="useInvariantConversion"></param>
+        /// <returns></returns>
+        string? ToString(
+            object value,
+            ITypeDescriptorContext? context,
+            CultureInfo? culture,
+            bool useInvariantConversion);
+
+        /// <summary>
+        /// Conversion arguments.
+        /// </summary>
+        public struct Arguments
+        {
+            /// <summary>
+            /// Gets or sets <see cref="CultureInfo"/> used for the conversion.
+            /// </summary>
+            public CultureInfo? Culture;
+
+            /// <summary>
+            /// Gets or sets <see cref="ITypeDescriptorContext"/> used for the conversion.
+            /// </summary>
+            public ITypeDescriptorContext? Context;
+
+            /// <summary>
+            /// Gets or sets whether to use invariant conversion.
+            /// </summary>
+            public bool UseInvariantConversion;
+
+            /// <summary>
+            /// Gets or sets a standard or custom format string.
+            /// </summary>
+            public string? Format;
+
+            /// <summary>
+            /// Gets or sets an object that supplies culture-specific
+            /// formatting information.
+            /// </summary>
+            public IFormatProvider? FormatProvider;
+        }
     }
 }
