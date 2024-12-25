3e8eeb556eaeb6cc2f8c3ae7fa9ace301db8183c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-25 05:50:06 +0200

    LightDarkColor: improved
    - supports IEquatable<LightDarkColor>
    - add == and != operators
    - add LightOrDark(bool isDark) method
    - add GetHashCode, Equals

--- Source/Alternet.UI.Common/Drawing/Color/LightDarkColor.cs
+++ Source/Alternet.UI.Common/Drawing/Color/LightDarkColor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -16,7 +17,7 @@ namespace Alternet.Drawing
     /// depends on the value specified in <see cref="IsDarkOverride"/> and a result
     /// of <see cref="SystemSettings.IsUsingDarkBackground"/>.
     /// </summary>
-    public class LightDarkColor : Color
+    public class LightDarkColor : Color, IEquatable<LightDarkColor>
     {
         /// <summary>
         /// Gets or sets whether dark or light color is returned
@@ -26,8 +27,8 @@ namespace Alternet.Drawing
         /// </summary>
         public static bool? IsDarkOverride;
 
-        private readonly Color light;
-        private readonly Color dark;
+        private Color light;
+        private Color dark;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LightDarkColor"/> class.
@@ -55,20 +56,76 @@ namespace Alternet.Drawing
         /// <summary>
         /// Gets dark color.
         /// </summary>
-        public Color Dark => dark;
+        public Color Dark
+        {
+            get
+            {
+                return dark;
+            }
+
+            set
+            {
+                dark = value;
+            }
+        }
 
         /// <summary>
         /// Gets light color.
         /// </summary>
-        public Color Light => light;
+        public Color Light
+        {
+            get
+            {
+                return light;
+            }
+
+            set
+            {
+                light = value;
+            }
+        }
 
         /// <inheritdoc/>
         public override string DebugString
         {
             get
             {
-                return $"(IsUsingDarkColor: {IsUsingDarkColor}, Light: {Light.ARGBWeb}, Dark: {Dark.ARGBWeb})";
+                return $"(IsUsingDarkColor: {IsUsingDarkColor}," +
+                    $" Light: {Light.ARGBWeb}, Dark: {Dark.ARGBWeb})";
             }
+        }
+
+        /// <summary>
+        /// Tests whether two specified <see cref="LightDarkColor"/> structures are different.
+        /// </summary>
+        /// <param name="left">The <see cref="LightDarkColor"/> that is to the left
+        /// of the inequality operator.</param>
+        /// <param name="right">The <see cref="LightDarkColor"/> that is to the right
+        /// of the inequality operator.</param>
+        /// <returns><c>true</c> if the two <see cref="LightDarkColor"/> structures
+        /// are different; otherwise, <c>false</c>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(LightDarkColor? left, LightDarkColor? right) => !(left == right);
+
+        /// <summary>
+        /// Tests whether two specified <see cref="LightDarkColor"/> structures are equivalent.
+        /// </summary>
+        /// <param name="left">The <see cref="LightDarkColor"/> that is to the left
+        /// of the equality operator.</param>
+        /// <param name="right">The <see cref="LightDarkColor"/> that is to the right
+        /// of the equality operator.</param>
+        /// <returns><c>true</c> if the two <see cref="LightDarkColor"/> structures
+        /// are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(LightDarkColor? left, LightDarkColor? right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Dark == right.Dark
+                && left.Light == right.Light;
         }
 
         /// <summary>
@@ -90,6 +147,53 @@ namespace Alternet.Drawing
             {
                 IsDarkOverride = savedOverride;
             }
+        }
+
+        /// <summary>
+        /// Gets <see cref="Dark"/> or <see cref="Light"/> color depending on
+        /// <paramref name="isDark"/> parameter value.
+        /// </summary>
+        /// <param name="isDark">Whether to get dark or light color.</param>
+        /// <returns></returns>
+        public Color LightOrDark(bool isDark)
+        {
+            if (isDark)
+                return Dark;
+            else
+                return Light;
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><c>true</c> if the specified object is equal to the
+        /// current object; otherwise, <c>false</c>.</returns>
+        public override bool Equals([NotNullWhen(true)] object? obj) =>
+            obj is LightDarkColor other && Equals(other);
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Dark.GetHashCode(), Light.GetHashCode());
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of
+        /// the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><c>true</c> if the current object is equal to other;
+        /// otherwise, <c>false</c>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(LightDarkColor? other)
+        {
+            if (other is null)
+                return false;
+            return this == other;
         }
 
         /// <inheritdoc/>
