f2a0986edcc71d7109769bd7885eccbf4c0bf437
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-25 08:48:46 +0200

    Create PlessDisplayHandler.cs

---
+++ Source/Alternet.UI.Common/Base/Display/PlessDisplayHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements <see cref="IDisplayHandler"/> interface provider which does nothing.
+    /// </summary>
+    public class PlessDisplayHandler : DisposableObject, IDisplayHandler
+    {
+        /// <inheritdoc/>
+        public virtual bool IsOk => true;
+
+        /// <inheritdoc/>
+        public virtual RectI GetClientArea()
+        {
+            return (0, 0, 1920, 1024);
+        }
+
+        /// <inheritdoc/>
+        public virtual RectI GetGeometry()
+        {
+            return (0, 0, 1920, 1024);
+        }
+
+        /// <inheritdoc/>
+        public virtual string GetName()
+        {
+            return "Primary Display";
+        }
+
+        /// <inheritdoc/>
+        public virtual double GetScaleFactor()
+        {
+            return 1;
+        }
+
+        /// <inheritdoc/>
+        public virtual bool IsPrimary()
+        {
+            return true;
+        }
+    }
+}
