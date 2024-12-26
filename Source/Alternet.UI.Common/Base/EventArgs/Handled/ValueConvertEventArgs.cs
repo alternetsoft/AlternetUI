76d473ebc37b7a8b4f0e163cd46803eb4aa78da0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-26 13:41:24 +0200

    Update ValueConvertEventArgs.cs

--- Source/Alternet.UI.Common/Base/EventArgs/Handled/ValueConvertEventArgs.cs
+++ Source/Alternet.UI.Common/Base/EventArgs/Handled/ValueConvertEventArgs.cs
@@ -8,14 +8,16 @@ namespace Alternet.UI
 {
     /// <summary>
     /// Provides data for the value convertion events. <typeparamref name="TFrom"/> specifies
-    /// type of the source data. <typeparamref name="TResult"/> specifies type of the destination data.
+    /// type of the source data. <typeparamref name="TResult"/> specifies
+    /// type of the destination data.
     /// </summary>
     public class ValueConvertEventArgs<TFrom, TResult> : HandledEventArgs
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ValueConvertEventArgs{TFrom, TResult}"/> class.
+        /// Initializes a new instance of the
+        /// <see cref="ValueConvertEventArgs{TFrom, TResult}"/> class.
         /// </summary>
-        /// <param name="value"></param>
+        /// <param name="value">Value to convert.</param>
         public ValueConvertEventArgs(TFrom value)
         {
             Value = value;
