9b599f40c4cab9ec408779542aa3be6ad1a293b6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-26 19:22:10 +0200

    Create AutoResetAttribute.cs

---
+++ Source/Alternet.UI.Common/Base/Attributes/AutoResetAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies whether field or property can be reset automatically.
+    /// </summary>
+    public class AutoResetAttribute : BaseBoolAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoResetAttribute"/> class.
+        /// </summary>
+        /// <param name="value">Value</param>
+        public AutoResetAttribute(bool value = true)
+            : base(value)
+        {
+        }
+    }
+}
