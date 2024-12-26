f8d42dd90729dd3ec11742e728c1d8de7dfd368f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-26 10:06:34 +0200

    Alternet.UI.WinForms

---
+++ Source/Alternet.UI.WinForms/WinFormsUtils.cs
@@ -0,0 +1,34 @@
+﻿namespace Alternet.UI.WinForms
+{
+    public static class WinFormsUtils
+    {
+        public static Alternet.UI.SystemColorMode ApplicationColorMode
+        {
+            get
+            {
+                return (Alternet.UI.SystemColorMode)Application.ColorMode;
+            }
+
+            set
+            {
+                Application.SetColorMode((System.Windows.Forms.SystemColorMode)value);
+            }
+        }
+
+        public static Alternet.UI.SystemColorMode ApplicationSystemColorMode
+        {
+            get
+            {
+                return (Alternet.UI.SystemColorMode)Application.SystemColorMode;
+            }
+        }
+
+        public static bool ApplicationIsDarkMode
+        {
+            get
+            {
+                return Application.IsDarkModeEnabled;
+            }
+        }
+    }
+}
