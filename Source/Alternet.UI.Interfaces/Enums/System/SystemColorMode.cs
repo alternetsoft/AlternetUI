91beb35ec0d9da4e1075475330dd745de511464a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-26 10:06:00 +0200

    Create SystemColorMode.cs

---
+++ Source/Alternet.UI.Interfaces/Enums/System/SystemColorMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates known system color modes.
+    /// </summary>
+    public enum SystemColorMode
+    {
+        /// <summary>
+        /// Dark mode is or should be disabled.
+        /// </summary>
+        Classic = 0,
+
+        /// <summary>
+        /// The setting for the current system color mode is inherited from the operating system.
+        /// </summary>
+        System = 1,
+
+        /// <summary>
+        /// Dark mode is enabled.
+        /// </summary>
+        Dark = 2,
+    }
+}
