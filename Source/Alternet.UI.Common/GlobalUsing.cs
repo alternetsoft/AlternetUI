dd8947021bef11510bc3d8939d5c12c7f06abbb2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-26 13:40:43 +0200

    global StringToObjectEventArgs, ObjectToStringEventArgs

--- Source/Alternet.UI.Common/GlobalUsing.cs
+++ Source/Alternet.UI.Common/GlobalUsing.cs
@@ -1,6 +1,10 @@
 ﻿global using EnumArrayStateImages
     = Alternet.UI.EnumArray<Alternet.UI.VisualControlState, Alternet.Drawing.Image?>;
 
+global using StringToObjectEventArgs = Alternet.UI.ValueConvertEventArgs<string?, object?>;
+
+global using ObjectToStringEventArgs = Alternet.UI.ValueConvertEventArgs<object?, string?>;
+
 [assembly: Alternet.UI.XmlnsDefinition("http://schemas.alternetsoft.com/ui/2021", "Alternet.UI")]
 
 namespace Alternet.UI.Internal
