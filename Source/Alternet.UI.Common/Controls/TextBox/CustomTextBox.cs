d008ae2408c1f3584f6b8efbe279c5d83b371bd0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-27 02:08:03 +0200

    Update CustomTextBox.cs

--- Source/Alternet.UI.Common/Controls/TextBox/CustomTextBox.cs
+++ Source/Alternet.UI.Common/Controls/TextBox/CustomTextBox.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Alternet.Drawing;
