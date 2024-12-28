43aacaed439633243a2215da39a62bd74dd874cb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-28 16:39:08 +0200

    ConversionUtils: Parse float array methods
    ParseFourFloats, ParseTwoFloats, ParseOneOrTwoOrFourFloats and use them in Parse method of RectD, SizeD, PointD and Thickness

--- Source/Alternet.UI.Common/Utils/ConversionUtils.cs
+++ Source/Alternet.UI.Common/Utils/ConversionUtils.cs
@@ -12,6 +12,133 @@ namespace Alternet.UI
     public static class ConversionUtils
     {
         /// <summary>
+        /// Parses string with 1, 2 or 4 float values separated by the
+        /// comma character or other separator.
+        /// All whitespaces and brackets are trimmed before parsing is done.
+        /// Null is returned if error occurs during parsing or number of parsed floats
+        /// is different from 1, 2 or 4.
+        /// </summary>
+        /// <param name="s">String to parse.</param>
+        /// <param name="formatProvider">Optional <see cref="IFormatProvider"/> with
+        /// string format settings. If not specified, <see cref="App.InvariantEnglishUS"/>
+        /// is used.</param>
+        /// <returns></returns>
+        public static float[]? ParseOneOrTwoOrFourFloats(
+            string? s,
+            IFormatProvider? formatProvider = null)
+        {
+            s = StringUtils.TrimWhitespaceAndBrackets(s);
+
+            using var tokenizer = new StringTokenizer(s, App.InvariantEnglishUS);
+
+            if (tokenizer.TryReadSingle(out var a))
+            {
+                if (tokenizer.TryReadSingle(out var b))
+                {
+                    if (tokenizer.TryReadSingle(out var c))
+                    {
+                        if (tokenizer.TryReadSingle(out var d))
+                            return [a, b, c, d];
+
+                        return null;
+                    }
+
+                    return [a, b];
+                }
+
+                return [a];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string with two float values separated by the comma character or other separator.
+        /// All whitespaces and brackets are trimmed before parsing is done.
+        /// </summary>
+        /// <param name="source">String to parse.</param>
+        /// <param name="formatProvider">Optional <see cref="IFormatProvider"/> with
+        /// string format settings. If not specified, <see cref="App.InvariantEnglishUS"/>
+        /// is used.</param>
+        /// <returns></returns>
+        public static (float, float) ParseTwoFloats(
+            string? source,
+            IFormatProvider? formatProvider = null)
+        {
+            source = StringUtils.TrimWhitespaceAndBrackets(source);
+
+            formatProvider ??= App.InvariantEnglishUS;
+
+            TokenizerHelper th = new(source, formatProvider);
+
+            (float, float) value;
+
+            string firstToken = th.NextTokenRequired();
+
+            // The token will already have had whitespace trimmed so we can do a
+            // simple string compare.
+            if (firstToken == "Empty" || string.IsNullOrEmpty(firstToken))
+            {
+                value = (0, 0);
+            }
+            else
+            {
+                value = (
+                    Convert.ToSingle(firstToken, formatProvider),
+                    Convert.ToSingle(th.NextTokenRequired(), formatProvider));
+            }
+
+            // There should be no more tokens in this string.
+            th.LastTokenRequired();
+
+            return value;
+        }
+
+        /// <summary>
+        /// Parses string with four float values separated by the comma character or other separator.
+        /// All whitespaces and brackets are trimmed before parsing is done.
+        /// </summary>
+        /// <param name="source">String to parse.</param>
+        /// <param name="formatProvider">Optional <see cref="IFormatProvider"/> with
+        /// string format settings. If not specified, <see cref="App.InvariantEnglishUS"/>
+        /// is used.</param>
+        /// <returns></returns>
+        public static (float, float, float, float) ParseFourFloats(
+            string? source,
+            IFormatProvider? formatProvider = null)
+        {
+            source = StringUtils.TrimWhitespaceAndBrackets(source);
+
+            formatProvider ??= App.InvariantEnglishUS;
+
+            TokenizerHelper th = new(source, formatProvider);
+
+            (float, float, float, float) value;
+
+            string firstToken = th.NextTokenRequired();
+
+            // The token will already have had whitespace trimmed so we can do a
+            // simple string compare.
+            if (firstToken == "Empty" || string.IsNullOrEmpty(firstToken))
+            {
+                value = (0, 0, 0, 0);
+            }
+            else
+            {
+                value = (
+                    Convert.ToSingle(firstToken, formatProvider),
+                    Convert.ToSingle(th.NextTokenRequired(), formatProvider),
+                    Convert.ToSingle(th.NextTokenRequired(), formatProvider),
+                    Convert.ToSingle(th.NextTokenRequired(), formatProvider));
+            }
+
+            // There should be no more tokens in this string.
+            th.LastTokenRequired();
+
+            return value;
+        }
+
+        /// <summary>
         /// Calls the specified conversion action.
         /// </summary>
         /// <typeparam name="TConvertFrom">Type of the value to convert from.</typeparam>
