2575c01337969dc832f3a1a1427dbf6a2f019f60
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-30 07:54:27 +0200

    Create ListControlEmptyItem.cs

---
+++ Source/Alternet.UI.Common/Base/ControlItems/ListControl/ListControlEmptyItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements an empty item which can be used as a spacer.
+    /// </summary>
+    public class ListControlEmptyItem : ListControlItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListControlEmptyItem"/> class.
+        /// </summary>
+        public ListControlEmptyItem()
+        {
+            this.HideSelection = true;
+        }
+    }
+}
