2f34c71515025ec82f00c51c32e0da067ad6fb12
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2024-12-31 10:58:44 +0200

    Update used nuget packages

---
+++ Source/Alternet.UI/nuget.list.old.packages.bat
@@ -0,0 +1,1 @@
+dotnet list package --outdated
---
+++ Source/nuget.list.all.old.packages.bat
@@ -0,0 +1,15 @@
+echo off
+echo =========================
+echo on
+
+dotnet list Alternet.UI.Common\Alternet.UI.Common.csproj package --outdated
+
+echo off
+echo =========================
+echo on
+
+dotnet list Alternet.UI.Maui\Alternet.UI.Maui.csproj package --outdated
+
+echo off
+echo =========================
+echo on
