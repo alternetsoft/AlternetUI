a5090bdf5f45aeefeb02b3131d34072efa6e5cc7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-09 12:38:52 +0200

    UnhandledExceptionMode: new members

--- Source/Alternet.UI.Interfaces/Enums/DebugAndLogs/UnhandledExceptionMode.cs
+++ Source/Alternet.UI.Interfaces/Enums/DebugAndLogs/UnhandledExceptionMode.cs
@@ -7,12 +7,38 @@ namespace Alternet.UI
     {
         /// <summary>
         /// Always route exceptions to the event handler.
+        /// If no event handler is assigned, application will
+        /// show default error dialog
+        /// with exception details, 'Continue' and 'Quit' buttons.
         /// </summary>
         CatchException,
 
         /// <summary>
-        /// Never route exceptions to the event handler.
+        /// Never route exceptions to the event handler. Application will throw the exceptions.
         /// </summary>
         ThrowException,
+
+        /// <summary>
+        /// Route exceptions to the event handler.
+        /// If exceptions are not handled there, application will
+        /// show default error dialog
+        /// with exception details, 'Continue' and 'Quit' buttons.
+        /// </summary>
+        CatchWithDialog,
+
+        /// <summary>
+        /// Route exceptions to the event handler.
+        /// If exceptions are not handled there, application will
+        /// show default error dialog
+        /// with exception details, 'Continue' and 'Quit' buttons.
+        /// If user presses 'Continue', exception will be shrown.
+        /// </summary>
+        CatchWithDialogAndThrow,
+
+        /// <summary>
+        /// Route exceptions to the event handler. If exceptions are not handled there
+        /// they will be shrown.
+        /// </summary>
+        CatchWithThrow,
     }
 }
