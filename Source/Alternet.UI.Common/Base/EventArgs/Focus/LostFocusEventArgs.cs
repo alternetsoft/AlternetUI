ea834afd2f3698517c76f4b0764eedf1c15b587f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-10 04:37:30 +0200

    Create focus event arguments

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Focus/LostFocusEventArgs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides additional data for the <see cref="AbstractControl.LostFocus"/>
+    /// event of the <see cref="AbstractControl"/>.
+    /// </summary>
+    public class LostFocusEventArgs : BaseFocusEventArgs
+    {
+        /// <summary>
+        /// Gets an empty <see cref="LostFocusEventArgs"/> instance with default parameters.
+        /// </summary>
+        public static readonly new LostFocusEventArgs Empty = new EmptyLostFocusEventArgs();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LostFocusEventArgs"/> class.
+        /// </summary>
+        public LostFocusEventArgs()
+            : base(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LostFocusEventArgs"/> class.
+        /// </summary>
+        /// <param name="newFocusedControl">
+        /// The control that will be focused. If Null, some native
+        /// control will be focused (possibly outside the application) and it can not be casted to
+        /// the <see cref="AbstractControl"/>.
+        /// </param>
+        public LostFocusEventArgs(AbstractControl? newFocusedControl)
+            : base(newFocusedControl)
+        {
+        }
+
+        private class EmptyLostFocusEventArgs : LostFocusEventArgs
+        {
+            public override AbstractControl? Value
+            {
+                get => base.Value;
+
+                set
+                {
+                }
+            }
+        }
+    }
+}
