c944ec3e3b442488e030a78481a9d58406ee5db0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-10 14:17:04 +0200

    Create IValueContainer{T}.cs

---
+++ Source/Alternet.UI.Common/Interfaces/IValueContainer{T}.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides access to the value of the specified type.
+    /// </summary>
+    /// <typeparam name="T">Type of the value.</typeparam>
+    public interface IValueContainer<T>
+    {
+        /// <summary>
+        /// Occurs when the value is changed.
+        /// </summary>
+        event EventHandler? ValueChanged;
+
+        /// <summary>
+        /// Gets or sets value.
+        /// </summary>
+        /// <returns></returns>
+        T? Value { get; set; }
+    }
+}
