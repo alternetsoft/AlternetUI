193e2549186e37a9596d970a8d7f66c38386c066
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-12 16:03:37 +0200

    Create IDoCommand.cs

---
+++ Source/Alternet.UI.Common/Interfaces/IDoCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Allows to execute named command with parameters.
+    /// </summary>
+    public interface IDoCommand
+    {
+        /// <summary>
+        /// Executes a command with the specified name and parameters.
+        /// </summary>
+        /// <param name="cmdName">
+        /// Name of the command to execute.
+        /// </param>
+        /// <param name="args">
+        /// Parameters of the command.
+        /// </param>
+        /// <returns>
+        /// A <see cref="object"/> representing the result of the command execution.
+        /// </returns>
+        object? DoCommand(string cmdName, params object?[] args);
+    }
+}
