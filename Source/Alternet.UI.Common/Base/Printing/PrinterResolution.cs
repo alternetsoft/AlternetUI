e15464d0be41c3feeb454530aef03a190e54a6e7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-12 14:22:59 +0200

    PrinterResolution: Removed exception

--- Source/Alternet.UI.Common/Base/Printing/PrinterResolution.cs
+++ Source/Alternet.UI.Common/Base/Printing/PrinterResolution.cs
@@ -7,24 +7,25 @@ namespace Alternet.Drawing.Printing
     /// </summary>
     public class PrinterResolution
     {
+        private PrinterResolutionKind printerResolutionKind;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PrinterResolution"/> class with
         /// the specified printer resolution kind.
         /// </summary>
         /// <param name="printerResolutionKind">The printer resolution kind.</param>
-#pragma warning disable
         public PrinterResolution(PrinterResolutionKind printerResolutionKind)
-#pragma warning restore
         {
+            this.printerResolutionKind = printerResolutionKind;
         }
 
         /// <summary>
-        /// Gets the printer resolution kind.
+        /// Gets or sets the printer resolution kind.
         /// </summary>
-        public PrinterResolutionKind Kind
+        public virtual PrinterResolutionKind Kind
         {
-            get => throw new Exception();
-            set => throw new Exception();
+            get => printerResolutionKind;
+            set => printerResolutionKind = value;
         }
     }
 }
