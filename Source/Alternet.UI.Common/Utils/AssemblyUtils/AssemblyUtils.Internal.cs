1b4a2f60e7c8b3790272ab58c9b92aa5a3c63f32
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-13 12:54:20 +0200

    AssemblyUtils: new members
    EnumMethods, GetTypeNameUsingCodeDom, GetTypeDisplayName

---
+++ Source/Alternet.UI.Common/Utils/AssemblyUtils/AssemblyUtils.Internal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+namespace Alternet.UI
+{
+    public static partial class AssemblyUtils
+    {
+        internal static string? GetTypeConverterAttributeData(Type? type, out Type? converterType)
+        {
+            bool foundTC = false;
+            return GetCustomAttributeData(
+                type,
+                typeof(TypeConverterAttribute),
+                true,
+                ref foundTC,
+                out converterType);
+        }
+
+        internal static string GetTypeConverterAttributeData(MemberInfo mi, out Type? converterType)
+        {
+            return GetCustomAttributeData(
+                mi,
+                typeof(TypeConverterAttribute),
+                out converterType);
+        }
+
+        // Special version of type-based GetCustomAttributeData that does two
+        //  additional tasks:
+        //  1) Retrieves the attributes even if it's defined on a base type, and
+        //  2) Distinguishes between "attribute found and said null" and
+        //     "no attribute found at all" via the ref bool.
+        internal static string? GetCustomAttributeData(
+            Type? t,
+            Type attrType,
+            bool allowTypeAlso,
+            ref bool attributeDataFound,
+            out Type? typeValue)
+        {
+            typeValue = null;
+            attributeDataFound = false;
+            Type? currentType = t;
+            string? attributeDataString = null;
+            CustomAttributeData cad;
+
+            while (currentType != null && !attributeDataFound)
+            {
+                IList<CustomAttributeData> list = CustomAttributeData.GetCustomAttributes(currentType);
+
+                for (int j = 0; j < list.Count && !attributeDataFound; j++)
+                {
+                    cad = list[j];
+
+                    if (cad.Constructor.ReflectedType == attrType)
+                    {
+                        attributeDataFound = true;
+                        attributeDataString = GetCustomAttributeData(
+                            cad,
+                            attrType,
+                            out typeValue,
+                            allowTypeAlso,
+                            false,
+                            false);
+                    }
+                }
+
+                if (!attributeDataFound)
+                {
+                    currentType = currentType.BaseType;
+
+                    // object.BaseType is null, used as terminating condition for the while() loop.
+                }
+            }
+
+            return attributeDataString;
+        }
+    }
+}
---
+++ Source/Alternet.UI.Common/Utils/AssemblyUtils/AssemblyUtils.Private.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Alternet.UI
+{
+    public static partial class AssemblyUtils
+    {
+        // Helper that inspects a specific CustomAttributeData obtained via ReflectionOnlyLoad, and
+        // returns its value if the Type of the attribiutes matches the passed in attrType. It only
+        // looks for attributes with no values or a single value of Type string that is passed in via
+        // a ctor. If allowTypeAlso is true, then it looks for values of typeof(Type) as well in the
+        // single value case. If noArgs == false and zeroArgsAllowed = true, that means 0 or 1 args
+        // are permissible.
+        private static string? GetCustomAttributeData(
+            CustomAttributeData cad,
+            Type attrType,
+            out Type? typeValue,
+            bool allowTypeAlso,
+            bool noArgs,
+            bool zeroArgsAllowed)
+        {
+            string? attrValue = null;
+            typeValue = null;
+
+            // get the Constructor info
+            ConstructorInfo cinfo = cad.Constructor;
+            if (cinfo.ReflectedType == attrType)
+            {
+                // typedConstructorArguments (the Attribute constructor arguments)
+                // [MyAttribute("test", Name=Hello)]
+                // "test" is the Constructor Argument
+                IList<CustomAttributeTypedArgument> constructorArguments = cad.ConstructorArguments;
+                if (constructorArguments.Count == 1 && !noArgs)
+                {
+                    CustomAttributeTypedArgument tca = constructorArguments[0];
+                    attrValue = tca.Value as string;
+                    if (attrValue == null && allowTypeAlso && tca.ArgumentType == typeof(Type))
+                    {
+                        typeValue = tca.Value as Type;
+                        attrValue = typeValue?.AssemblyQualifiedName;
+                    }
+
+                    if (attrValue == null)
+                    {
+                        throw new ArgumentException(SR.Get(SRID.ParserAttributeArgsLow, attrType.Name));
+                    }
+                }
+                else if (constructorArguments.Count == 0)
+                {
+                    // zeroArgsAllowed = true for CPA for example.
+                    // CPA with no args is valid and would mean that this type is overriding a base CPA
+                    if (noArgs || zeroArgsAllowed)
+                    {
+                        attrValue = string.Empty;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(SR.Get(SRID.ParserAttributeArgsLow, attrType.Name));
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException(SR.Get(SRID.ParserAttributeArgsHigh, attrType.Name));
+                }
+            }
+
+            return attrValue;
+        }
+
+        // Given a ReflectionOnlyLoaded member, returns the value of a metadata attribute of
+        // Type attrType if set on that member. Looks only for attributes that have a ctor with
+        // one parameter that is of Type string or Type.
+        private static string GetCustomAttributeData(MemberInfo mi, Type attrType, out Type? typeValue)
+        {
+            IList<CustomAttributeData> list = CustomAttributeData.GetCustomAttributes(mi);
+            string? attrValue = GetCustomAttributeData(list, attrType, out typeValue, true, false);
+            return attrValue ?? string.Empty;
+        }
+
+        // Helper that enumerates a list of CustomAttributeData obtained
+        // via ReflectionOnlyLoad, and
+        // looks for a specific attribute of Type attrType.
+        // It only looks for attribiutes with a single
+        // value of Type string that is passed in via a ctor.
+        // If allowTypeAlso is true, then it looks for
+        // values of typeof(Type) as well.
+        private static string? GetCustomAttributeData(
+            IList<CustomAttributeData> list,
+            Type attrType,
+            out Type? typeValue,
+            bool allowTypeAlso,
+            bool allowZeroArgs)
+        {
+            typeValue = null;
+            string? attrValue = null;
+            for (int j = 0; j < list.Count; j++)
+            {
+                attrValue = GetCustomAttributeData(
+                    list[j],
+                    attrType,
+                    out typeValue,
+                    allowTypeAlso,
+                    false,
+                    allowZeroArgs);
+                if (attrValue != null)
+                {
+                    break;
+                }
+            }
+
+            return attrValue;
+        }
+    }
+}
