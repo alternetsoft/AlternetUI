27ed4d3aed41a0d3c939554af10f4601a256ceaf
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-14 21:11:45 +0200

    Create KnownProperties.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/Known/KnownProperties.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains property information for some of the properties.
+    /// </summary>
+    public static class KnownProperties
+    {
+        /// <summary>
+        /// Contains property information for some of the <see cref="AbstractControl"/> properties.
+        /// </summary>
+        public static class AbstractControl
+        {
+            private static PropertyInfo? enabled;
+            private static PropertyInfo? visible;
+
+            /// <summary>
+            /// Gets property information for the 'Visible' property of the control.
+            /// </summary>
+            public static PropertyInfo? Visible
+            {
+                get
+                {
+                    return visible ??= GetControlProperty("Visible");
+                }
+            }
+
+            /// <summary>
+            /// Gets property information for the 'Enabled' property of the control.
+            /// </summary>
+            public static PropertyInfo? Enabled
+            {
+                get
+                {
+                    return enabled ??= GetControlProperty("Enabled");
+                }
+            }
+
+            private static PropertyInfo? GetControlProperty(string propName)
+            {
+                return AssemblyUtils.GetPropertySafe(
+                                        typeof(AbstractControl),
+                                        propName,
+                                        BindingFlags.Public | BindingFlags.Instance);
+            }
+        }
+    }
+}
