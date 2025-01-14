eae156467b015608b1207d498d53f05143ca5a3f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-14 21:11:50 +0200

    Create GenericControlProperty.cs

---
+++ Source/Alternet.UI.Interfaces/Enums/Control/GenericControlProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates generic control properties.
+    /// </summary>
+    public enum GenericControlProperty
+    {
+        /// <summary>
+        /// Not a property.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Specifies 'Enabled' property of the control.
+        /// </summary>
+        Enabled,
+
+        /// <summary>
+        /// Specifies 'Visible' property of the control.
+        /// </summary>
+        Visible,
+
+        /// <summary>
+        /// Specifies 'Text' property of the control.
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// Specifies 'Title' property of the control.
+        /// </summary>
+        Title,
+    }
+}
