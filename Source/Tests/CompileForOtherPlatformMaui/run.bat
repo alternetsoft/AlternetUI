340a2c22f11a412d7d1e499a3694e6ab60639a70
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-01-22 17:05:08 +0200

    Create run.bat

---
+++ Source/Tests/CompileForOtherPlatformMaui/run.bat
@@ -0,0 +1,2 @@
+dotnet publish -f net8.0-windows10.0.19041.0 -c Debug -p:PublishReadyToRun=true -p:WindowsPackageType=None 
+.\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\publish\CompileForOtherPlatformMaui.exe
