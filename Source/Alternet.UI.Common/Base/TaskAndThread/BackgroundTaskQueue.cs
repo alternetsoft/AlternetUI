d775eb61d3b0b9a8a599d131f1b854d5dc670c7b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-01 01:22:13 +0200

    Create BackgroundTaskQueue.cs

---
+++ Source/Alternet.UI.Common/Base/TaskAndThread/BackgroundTaskQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alternet.UI.Threading
+{
+    public class BackgroundTaskQueue
+    {
+        private readonly ConcurrentQueue<Func<Task>> tasks = new ConcurrentQueue<Func<Task>>();
+        private readonly SemaphoreSlim signal = new SemaphoreSlim(0);
+
+        // Enqueue a task
+        public void Enqueue(Func<Task> task)
+        {
+            tasks.Enqueue(task);
+            signal.Release(); // Signal that a task is available
+        }
+
+        // Dequeue and execute tasks
+        public async Task<Func<Task>> DequeueAsync(CancellationToken cancellationToken)
+        {
+            await signal.WaitAsync(cancellationToken);
+
+            tasks.TryDequeue(out var task);
+            return task;
+        }
+    }
+}
