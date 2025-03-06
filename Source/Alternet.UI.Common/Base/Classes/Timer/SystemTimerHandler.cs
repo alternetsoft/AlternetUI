de0246ef1e26fa482f1cf1215c24ae54add0e3aa
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-06 05:02:33 +0200

    Create SystemTimerHandler.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/Timer/SystemTimerHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    internal class SystemTimerHandler : DisposableObject, ITimerHandler
+    {
+        public bool Enabled { get; set; }
+
+        public int Interval { get; set; }
+
+        public Action? Tick { get; set; }
+
+        protected override void DisposeManaged()
+        {
+            base.DisposeManaged();
+        }
+    }
+}
