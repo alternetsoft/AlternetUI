be2b52e59e4510c1694da5bd67883ac0d8052b49
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-07 14:08:37 +0200

    Create IRaiseSystemColorsChanged.cs

---
+++ Source/Alternet.UI.Common/Interfaces/IRaiseSystemColorsChanged.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines a method which is raised when the system colors are changed.
+    /// </summary>
+    public interface IRaiseSystemColorsChanged
+    {
+        /// <summary>
+        /// Updates visual style of the control when the system colors are changed.
+        /// </summary>
+        void RaiseSystemColorsChanged();
+    }
+}
