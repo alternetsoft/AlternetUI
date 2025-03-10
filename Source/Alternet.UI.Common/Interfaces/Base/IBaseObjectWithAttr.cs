89e954a11cd100983f35b8c456b9e71ca8fe4f48
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-10 18:05:26 +0200

    Create IBaseObjectWithAttr.cs

---
+++ Source/Alternet.UI.Common/Interfaces/Base/IBaseObjectWithAttr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Interface representing an object with attributes.
+    /// </summary>
+    public interface IBaseObjectWithAttr : IBaseObjectWithId
+    {
+        /// <inheritdoc cref="BaseObjectWithAttr.Tag"/>
+        object? Tag { get; set; }
+
+        /// <inheritdoc cref="BaseObjectWithAttr.IntFlags"/>
+        ICustomIntFlags IntFlags { get; }
+
+        /// <inheritdoc cref="BaseObjectWithAttr.IntFlagsAndAttributes"/>
+        IIntFlagsAndAttributes IntFlagsAndAttributes { get; set; }
+
+        /// <inheritdoc cref="BaseObjectWithAttr.FlagsAndAttributes"/>
+        IFlagsAndAttributes FlagsAndAttributes { get; set; }
+
+        /// <inheritdoc cref="BaseObjectWithAttr.CustomFlags"/>
+        ICustomFlags<string> CustomFlags { get; }
+
+        /// <inheritdoc cref="BaseObjectWithAttr.CustomAttr"/>
+        ICustomAttributes<string, object> CustomAttr { get; }
+    }
+}
