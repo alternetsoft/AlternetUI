f3233e6a3143e2fb0b70eedfcc6ddb4448d8290b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-15 06:45:58 +0200

    Create VerticalLine.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Border/VerticalLine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a vertical line control.
+    /// </summary>
+    public partial class VerticalLine : UserControl
+    {
+        private Coord lineWidth = 1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VerticalLine"/> class.
+        /// </summary>
+        public VerticalLine()
+        {
+            ParentBackColor = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VerticalLine"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public VerticalLine(Control parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <inheritdoc/>
+        public override SizeD SuggestedSize
+        {
+            get
+            {
+                var result = base.SuggestedSize;
+                return (lineWidth, result.Height);
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the vertical line.
+        /// </summary>
+        public virtual Coord LineWidth
+        {
+            get => lineWidth;
+
+            set
+            {
+                if (lineWidth == value)
+                    return;
+                lineWidth = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void DefaultPaint(PaintEventArgs e)
+        {
+            var rect = e.ClipRectangle;
+            var brush = DefaultColors.GetBorderBrush(this);
+            e.Graphics.DrawVertLine(brush, rect.TopLeft, rect.Height, LineWidth);
+        }
+    }
+}
