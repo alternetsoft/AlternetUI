a4c741d12f61cfeec58cd8048cd998eef32fecae
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-15 09:12:48 +0200

    Create BaseContentView.cs

---
+++ Source/Alternet.UI.Maui/Views.Maui/BaseContentView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+
+namespace Alternet.Maui
+{
+    /// <summary>
+    /// Represents a base class for content views in the library.
+    /// </summary>
+    public partial class BaseContentView : ContentView
+    {
+    }
+}
