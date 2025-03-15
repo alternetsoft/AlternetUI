f665e5a2ec57982cc5b2ebcf930a1bd59cdda862
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-15 06:45:55 +0200

    Create HorizontalLine.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Border/HorizontalLine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a horizontal line control.
+    /// </summary>
+    public partial class HorizontalLine : UserControl
+    {
+        private Coord lineHeight = 1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HorizontalLine"/> class.
+        /// </summary>
+        public HorizontalLine()
+        {
+            ParentBackColor = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HorizontalLine"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public HorizontalLine(Control parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <inheritdoc/>
+        public override SizeD SuggestedSize
+        {
+            get
+            {
+                var result = base.SuggestedSize;
+                return (result.Width, lineHeight);
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the height of the horizontal line.
+        /// </summary>
+        public virtual Coord LineHeight
+        {
+            get => lineHeight;
+
+            set
+            {
+                if (lineHeight == value)
+                    return;
+                lineHeight = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void DefaultPaint(PaintEventArgs e)
+        {
+            var rect = e.ClipRectangle;
+            var brush = DefaultColors.GetBorderBrush(this);
+            e.Graphics.DrawHorzLine(brush, rect.TopLeft, rect.Width, LineHeight);
+        }
+    }
+}
