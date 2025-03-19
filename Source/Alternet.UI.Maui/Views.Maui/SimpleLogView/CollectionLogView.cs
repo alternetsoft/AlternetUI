d3dd552f42b7233794ad0f127dacc2cb8098b5d8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-19 11:53:51 +0200

    Create CollectionLogView.cs

---
+++ Source/Alternet.UI.Maui/Views.Maui/SimpleLogView/CollectionLogView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Alternet.Maui
+{
+    /// <summary>
+    /// Represents a log view which internally uses <see cref="CollectionView"/>.
+    /// </summary>
+    public partial class CollectionLogView : BaseLogView
+    {
+        private readonly ObservableCollection<LogItem> logItems = new();
+        private readonly CollectionView collectionView = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionLogView"/> class.
+        /// </summary>
+        public CollectionLogView()
+        {
+            collectionView.SelectionMode = SelectionMode.Single;
+            collectionView.ItemsSource = logItems;
+            collectionView.VerticalScrollBarVisibility = ScrollBarVisibility.Always;
+            collectionView.HorizontalScrollBarVisibility = ScrollBarVisibility.Always;
+
+            Content = new Grid()
+            {
+                Children =
+                {
+                    collectionView,
+                },
+            };
+        }
+
+        /// <inheritdoc/>
+        public override void Clear()
+        {
+            logItems.Clear();
+        }
+
+        /// <inheritdoc/>
+        protected override void AddItem(string s)
+        {
+            logItems.Add(new(s));
+            collectionView.SelectedItem = logItems[logItems.Count - 1];
+            collectionView.ScrollTo(logItems.Count - 1);
+        }
+    }
+}
