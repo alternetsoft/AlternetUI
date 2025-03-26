41eb5b7410fd6f87261c56bd9191b5f2b1988257
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-26 13:44:53 +0200

    Create AssertFailureExceptionData.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/Exceptions/AssertFailureExceptionData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains properties that describe assert failure.
+    /// </summary>
+    public class AssertFailureExceptionData : BaseObject
+    {
+        /// <summary>
+        /// Gets or sets assert failure message.
+        /// </summary>
+        public string? Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets source code file name.
+        /// </summary>
+        public string? File { get; set; }
+
+        /// <summary>
+        /// Gets or sets source code line number in the file.
+        /// </summary>
+        public string? Line { get; set; }
+
+        /// <summary>
+        /// Gets or sets function name where error occured.
+        /// </summary>
+        public string? Function { get; set; }
+
+        /// <summary>
+        /// Gets or sets assert condition.
+        /// </summary>
+        public string? Condition { get; set; }
+
+        internal static void TestSerialize()
+        {
+            AssertFailureExceptionData e = new()
+            {
+                Message = "value",
+                Condition = "value",
+                File = "value",
+                Function = "value",
+                Line = "value",
+            };
+
+            XmlUtils.SerializeToFile(@"e:\sample.xml", e);
+        }
+    }
+}
