e502fbb02b107af62e527c19b789d2e2f06904ff
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-27 14:13:09 +0200

    Create SimpleTreeView.cs

---
+++ Source/Alternet.UI.Maui/Views.Maui/SimpleTreeView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternet.Maui
+{
+    /// <summary>
+    /// Represents a simple tree view.
+    /// </summary>
+    internal partial class SimpleTreeView : BaseContentView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleTreeView"/> class.
+        /// </summary>
+        public SimpleTreeView()
+        {
+        }
+    }
+}
