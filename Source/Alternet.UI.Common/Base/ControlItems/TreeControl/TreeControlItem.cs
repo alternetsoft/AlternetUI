f57d93136cd981e26605d8de6b7e740ba595c241
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-27 08:49:55 +0200

    Create TreeControlItem.cs

---
+++ Source/Alternet.UI.Common/Base/ControlItems/TreeControl/TreeControlItem.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents an item in a tree control.
+    /// </summary>
+    public partial class TreeControlItem : ListControlItem
+    {
+        private List<TreeControlItem>? items;
+
+        /// <summary>
+        /// Gets or sets the parent item of this tree control item.
+        /// </summary>
+        public virtual TreeControlItem? Parent { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this item is visible.
+        /// </summary>
+        public virtual bool IsVisible { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this item is expanded.
+        /// </summary>
+        public virtual bool IsExpanded { get; set; }
+
+        /// <summary>
+        /// Gets the child items of this tree control item.
+        /// </summary>
+        public virtual IEnumerable<TreeControlItem> Items => items ?? [];
+
+        /// <summary>
+        /// Gets a value indicating whether this item has child items.
+        /// </summary>
+        public virtual bool HasItems => items is not null && items.Count > 0;
+
+        /// <summary>
+        /// Gets the indent level of this item.
+        /// </summary>
+        public virtual int IndentLelel
+        {
+            get
+            {
+                if (Parent is null)
+                    return 0;
+                return Parent.IndentLelel + 1;
+            }
+        }
+
+        /// <summary>
+        /// Removes the specified child item from this tree control item.
+        /// </summary>
+        /// <param name="item">The item to remove.</param>
+        /// <returns>true if the item was successfully removed; otherwise, false.</returns>
+        public virtual bool Remove(TreeControlItem item)
+        {
+            return items?.Remove(item) ?? false;
+        }
+
+        /// <summary>
+        /// Adds the specified child item to this tree control item.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        public virtual void Add(TreeControlItem item)
+        {
+            items ??= new();
+            items.Add(item);
+        }
+
+        /// <summary>
+        /// Clears all child items from this tree control item.
+        /// </summary>
+        public virtual void Clear()
+        {
+            items?.Clear();
+        }
+
+        /// <summary>
+        /// Retrieves all child items of this tree control item.
+        /// If a child item is expanded, its children are also included in the result.
+        /// All child items are processed recursively.
+        /// </summary>
+        /// <returns>An enumerable collection containing all child items.</returns>
+        /// <param name="prm">The parameters that define which items should be
+        /// included in the result.</param>
+        public virtual IEnumerable<TreeControlItem> EnumExpandedItems(EnumExpandedItemsParams prm)
+        {
+            if (items is null)
+                yield break;
+
+            foreach (var item in items)
+            {
+                if (!Condition(item))
+                    continue;
+                yield return item;
+
+                if (!prm.Recursive || !item.HasItems || !item.IsExpanded)
+                    continue;
+                var subItems = item.EnumExpandedItems(prm);
+
+                foreach(var subItem in subItems)
+                {
+                    if (Condition(item))
+                        yield return item;
+                }
+            }
+
+            bool Condition(TreeControlItem itm)
+            {
+                if (prm.OnlyVisible)
+                {
+                    if (!itm.IsVisible)
+                        return false;
+                }
+
+                if(prm.Condition is not null)
+                {
+                    return prm.Condition(itm);
+                }
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Represents the parameters used for enumerating expanded tree control items.
+        /// </summary>
+        public record EnumExpandedItemsParams
+        {
+            /// <summary>
+            /// Specifies whether to include only visible items in the enumeration.
+            /// Defaults to <c>true</c>.
+            /// </summary>
+            public bool OnlyVisible = true;
+
+            /// <summary>
+            /// Specifies whether to process all child items recursively.
+            /// Defaults to <c>true</c>.
+            /// </summary>
+            public bool Recursive = true;
+
+            /// <summary>
+            /// A function that determines whether an item should be included
+            /// in the enumeration.
+            /// This function takes a <see cref="TreeControlItem"/> as input and
+            /// returns <c>true</c> if the item meets the condition; otherwise, <c>false</c>.
+            /// </summary>
+            public Func<TreeControlItem, bool>? Condition;
+        }
+    }
+}
