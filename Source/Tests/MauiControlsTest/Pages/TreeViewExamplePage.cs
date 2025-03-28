f0f2e0b1171d76645e79c88a5b23e4a0ecd91bb2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-28 08:51:05 +0200

    Maui: continued SimpleTreeView

---
+++ Source/Tests/MauiControlsTest/Pages/TreeViewExamplePage.cs
@@ -0,0 +1,279 @@
+using Alternet.Maui;
+
+using Microsoft.Maui.Controls;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace AllQuickStarts
+{
+    public partial class TreeViewExamplePage : ContentPage
+    {
+        public static ObservableCollection<Item> SampleItems = new()
+        {
+            new Item
+            {
+                Name = "TaskManager",
+            },
+            new Item
+            {
+                Name = "BudgetPlanner",
+            },
+            new Item
+            {
+                Name = "WeatherApp",
+            },
+            new Item
+            {
+                Name = "CodeEditor",
+            },
+            new Item
+            {
+                Name = "RecipeHelper",
+            }
+        };
+
+        private Item? selectedItem;
+        private int? selectedIndex;
+        private CollectionView collectionView;
+
+        public void CollectionAdd()
+        {
+        }
+
+        public void CollectionRemove()
+        {
+        }
+
+        public void CollectionClear()
+        {
+        }
+
+        public void CollectionRename()
+        {
+        }
+
+        public TreeViewExamplePage()
+        {
+            var menuFlyout = new MenuFlyout();
+            menuFlyout.Add(new MenuFlyoutItem
+            {
+                Text = "Add",
+                Command = new Command(() =>
+                {
+                    CollectionAdd();
+                }),
+            });
+            menuFlyout.Add(new MenuFlyoutItem
+            {
+                Text = "Remove",
+                Command = new Command(() =>
+                {
+                    CollectionRemove();
+                }),
+            });
+            menuFlyout.Add(new MenuFlyoutItem
+            {
+                Text = "Clear",
+                Command = new Command(() =>
+                {
+                    CollectionClear();
+                }),
+            });
+            menuFlyout.Add(new MenuFlyoutItem
+            {
+                Text = "Rename",
+                Command = new Command(() =>
+                {
+                    CollectionRename();
+                }),
+            });
+
+            var tapGesture = new TapGestureRecognizer
+            {
+                Buttons = ButtonsMask.Secondary,
+            };
+            tapGesture.Tapped += (sender, e) =>
+            {
+                if (sender is View tappedFrame && tappedFrame.BindingContext is Item item)
+                {
+                    collectionView!.SelectedItem = item;
+                }
+            };
+
+            collectionView = CreateSampleCollectionView(tapGesture);
+            collectionView.ItemsSource = SampleItems;
+
+            FlyoutBase.SetContextFlyout(collectionView, menuFlyout);
+
+            collectionView.SetBinding(CollectionView.SelectedItemProperty, new Binding(nameof(SelectedItem), source: this));
+            collectionView.SelectionChanged += OnSelectionChanged;
+            collectionView.SelectionMode = SelectionMode.Single;
+
+            var toolbar = new SimpleToolBarView();
+
+            toolbar.AddButton(
+                "Add",
+                "Add new item",
+                Alternet.UI.KnownSvgImages.ImgAdd,
+                () =>
+                {
+                    CollectionAdd();
+                });
+
+            toolbar.AddSeparator();
+
+            toolbar.AddButton(
+                "Remove",
+                "Remove current item",
+                Alternet.UI.KnownSvgImages.ImgRemove,
+                () =>
+                {
+                    CollectionRemove();
+                });
+
+            toolbar.AddButton(
+                "Clear",
+                "Clear items",
+                Alternet.UI.KnownSvgImages.ImgRemoveAll,
+                () =>
+                {
+                    CollectionClear();
+                });
+
+            toolbar.AddButton(
+                "Rename",
+                "Rename current item",
+                Alternet.UI.KnownSvgImages.ImgGear,
+                () =>
+                {
+                    CollectionRename();
+                });
+
+            toolbar.AddExpandingSpace();
+            toolbar.IsTopBorderVisible = true;
+            toolbar.IsBottomBorderVisible = true;
+
+            toolbar.AddButton(
+                null,
+                "More actions",
+                Alternet.UI.KnownSvgImages.ImgMoreActions,
+                () =>
+                {
+                });
+
+            var grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Star }
+                }
+            };
+
+            grid.Add(toolbar, 0, 0);
+            grid.Add(collectionView, 0, 1);
+
+            Content = grid;
+        }
+
+        public int? SelectedIndex
+        {
+            get => selectedIndex;
+            set
+            {
+                if (selectedIndex != value)
+                {
+                    selectedIndex = value;
+                    OnPropertyChanged(nameof(SelectedIndex));
+                }
+            }
+        }
+
+        public static CollectionView CreateSampleCollectionView(GestureRecognizer? tapGesture = null)
+        {
+            var collectionView = new CollectionView
+            {
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    Grid grid = new()
+                    {
+                        Padding = 5,
+                    };
+
+                    grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+                    Label nameLabel = new()
+                    {
+                        Margin = 5,
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        VerticalTextAlignment = TextAlignment.Center,
+                    };
+
+                    nameLabel.SetBinding(Label.TextProperty, static (Item item) => item.Name);
+
+                    grid.Add(nameLabel);
+
+                    if (tapGesture is not null)
+                        grid.GestureRecognizers.Add(tapGesture);
+
+                    return grid;
+                })
+            };
+
+            return collectionView;
+        }
+
+        private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is Item selectedItem)
+            {
+                SelectedIndex = SampleItems.IndexOf(selectedItem);
+            }
+            else
+            {
+                SelectedIndex = null;
+            }
+        }
+
+        public Item? SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    OnPropertyChanged(nameof(SelectedItem));
+                }
+            }
+        }
+
+        public class Item : INotifyPropertyChanged
+        {
+            private string? name;
+
+            public string? Name
+            {
+                get => name;
+                set
+                {
+                    if (name != value)
+                    {
+                        name = value;
+                        OnPropertyChanged(nameof(Name));
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
