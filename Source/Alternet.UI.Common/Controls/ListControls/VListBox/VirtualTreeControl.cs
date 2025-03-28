66ab801c54cffa49e0da8c5dec98f1a413bd52cf
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-28 14:01:51 +0200

    Create VirtualTreeControl.cs

---
+++ Source/Alternet.UI.Common/Controls/ListControls/VListBox/VirtualTreeControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a tree control that inherits from <see cref="VirtualListBox"/>.
+    /// This control is under development, please do not use it.
+    /// </summary>
+    public partial class VirtualTreeControl : VirtualListBox
+    {
+        public static int DefaultLevelMargin = 16;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualTreeControl"/> class.
+        /// </summary>
+        public VirtualTreeControl()
+        {
+        }
+    }
+}
