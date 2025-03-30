1032117d686a24dd4a26ac7236519a0098322026
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-03-30 09:36:38 +0300

    Create ListControlItemWithNotify.cs

---
+++ Source/Alternet.UI.Common/Base/ControlItems/ListControlItemWithNotify.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a list control item with notification capabilities.
+    /// </summary>
+    public partial class ListControlItemWithNotify : ListControlItem
+    {
+        /// <inheritdoc/>
+        public override string Text
+        {
+            get => base.Text;
+            set
+            {
+                if (base.Text == value)
+                    return;
+                base.Text = value;
+                RaisePropertyChanged(nameof(Text));
+            }
+        }
+    }
+}
