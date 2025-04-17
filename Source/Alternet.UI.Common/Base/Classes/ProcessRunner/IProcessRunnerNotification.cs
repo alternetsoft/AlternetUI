8398ea1964417c2ba4f9b45d260f9a649bf70401
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-17 13:24:08 +0300

    Create IProcessRunnerNotification.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/ProcessRunner/IProcessRunnerNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using Alternet.UI;
+
+namespace Alternet.UI
+{
+    public interface IProcessRunnerNotification : IDisposableObject
+    {
+        ObjectUniqueId UniqueId { get; }
+
+        void OnRunningProcessStarted(Process process);
+
+        void OnRunningProcessDisposed(Process process);
+
+        void OnRunningProcessExited(Process process);
+
+        void OnRunningProcessLog(Process process, string data, LogItemKind kind = LogItemKind.Information);
+    }
+}
