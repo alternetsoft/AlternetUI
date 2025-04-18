4a967f7fa4a6f9312c8ae7ac0abe0dc7c7da85f0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-18 07:39:33 +0300

    Add xml comments

--- Source/Alternet.UI.Common/Base/Classes/ProcessRunner/IProcessRunnerNotification.cs
+++ Source/Alternet.UI.Common/Base/Classes/ProcessRunner/IProcessRunnerNotification.cs
@@ -7,16 +7,41 @@ using Alternet.UI;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Defines a contract for receiving notifications about the lifecycle and events of a running process.
+    /// </summary>
     public interface IProcessRunnerNotification : IDisposableObject
     {
+        /// <summary>
+        /// Gets the unique identifier for this notification instance.
+        /// </summary>
         ObjectUniqueId UniqueId { get; }
 
+        /// <summary>
+        /// Called when a process starts running.
+        /// </summary>
+        /// <param name="process">The <see cref="Process"/> that has started.</param>
         void OnRunningProcessStarted(Process process);
 
+        /// <summary>
+        /// Called when a process is disposed.
+        /// </summary>
+        /// <param name="process">The <see cref="Process"/> that has been disposed.</param>
         void OnRunningProcessDisposed(Process process);
 
+        /// <summary>
+        /// Called when a process exits.
+        /// </summary>
+        /// <param name="process">The <see cref="Process"/> that has exited.</param>
         void OnRunningProcessExited(Process process);
 
+        /// <summary>
+        /// Called when a log entry is generated for a running process.
+        /// </summary>
+        /// <param name="process">The <see cref="Process"/> associated with the log entry.</param>
+        /// <param name="data">The log message.</param>
+        /// <param name="kind">The kind of log entry, such as information, warning, or error.
+        /// Defaults to <see cref="LogItemKind.Information"/>.</param>
         void OnRunningProcessLog(Process process, string data, LogItemKind kind = LogItemKind.Information);
     }
 }
--- Source/Alternet.UI.Common/Base/Classes/ProcessRunner/ProcessRunnerWithNotification.cs
+++ Source/Alternet.UI.Common/Base/Classes/ProcessRunner/ProcessRunnerWithNotification.cs
@@ -10,30 +10,69 @@ using Alternet.UI;
 
 namespace Alternet.UI
 {
+    /// <summary>
+    /// Provides functionality to run processes with notifications for their lifecycle events.
+    /// </summary>
     public static class ProcessRunnerWithNotification
     {
+        /// <summary>
+        /// Specifies the default encoding used for standard input, output, and error streams
+        /// when running a process.
+        /// </summary>
+        /// <remarks>
+        /// If this property is not set, <see cref="Encoding.UTF8"/> is used as the default encoding.
+        /// This encoding is applied to the <see cref="ProcessStartInfo.StandardOutputEncoding"/>,
+        /// <see cref="ProcessStartInfo.StandardErrorEncoding"/>, and optionally to the input stream
+        /// if supported by the platform.
+        /// </remarks>
+        public static Encoding? DefaultInputOutputEncoding;
+
+        /// <summary>
+        /// Prefix for error output messages.
+        /// </summary>
         public static string? ErrorOutputPrefix = "Error> ";
 
+        /// <summary>
+        /// Prefix for standard output messages.
+        /// </summary>
         public static string? OutputPrefix = "Output> ";
 
+        /// <summary>
+        /// Separator message added to log when a process exits.
+        /// </summary>
         public static string? ProcessExitedSeparator = "-";
 
-        public static string? ProcessExitedWithResult = "Process exited with result";
+        /// <summary>
+        /// Message template for logging process exit results.
+        /// </summary>
+        public static string? ProcessExitedWithResult = "Process exited with result: {0}";
 
         private static readonly
-            ConcurrentDictionary<ObjectUniqueId, WeakReferenceValue<IProcessRunnerNotification>>
-            subscribers = new();
+            ConcurrentDictionary<ObjectUniqueId, WeakReferenceValue<IProcessRunnerNotification>> subscribers
+            = new();
 
         private static Process? runningProcess;
 
+        /// <summary>
+        /// Initializes static members of the <see cref="ProcessRunnerWithNotification"/> class.
+        /// </summary>
         static ProcessRunnerWithNotification()
         {
         }
 
+        /// <summary>
+        /// Occurs when the currently running process is disposed.
+        /// </summary>
         public static event EventHandler? RunningProcessDisposed;
 
+        /// <summary>
+        /// Occurs when the currently running process exits.
+        /// </summary>
         public static event EventHandler? RunningProcessExited;
 
+        /// <summary>
+        /// Gets or sets the currently running process.
+        /// </summary>
         public static Process? RunningProcess
         {
             get => runningProcess;
@@ -46,6 +85,9 @@ namespace Alternet.UI
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether there is a running process that has not exited.
+        /// </summary>
         public static bool HasRunningProcess
         {
             get
@@ -54,16 +96,27 @@ namespace Alternet.UI
             }
         }
 
+        /// <summary>
+        /// Unbinds a subscriber from receiving process notifications.
+        /// </summary>
+        /// <param name="subscriber">The subscriber to unbind.</param>
         public static void Unbind(IProcessRunnerNotification subscriber)
         {
             subscribers.TryRemove(subscriber.UniqueId, out _);
         }
 
+        /// <summary>
+        /// Binds a subscriber to receive process notifications.
+        /// </summary>
+        /// <param name="subscriber">The subscriber to bind.</param>
         public static void Bind(IProcessRunnerNotification subscriber)
         {
             subscribers.TryAdd(subscriber.UniqueId, new(subscriber));
         }
 
+        /// <summary>
+        /// Kills the currently running process, if any.
+        /// </summary>
         public static void KillRunningProcess()
         {
             if (RunningProcess != null)
@@ -77,7 +130,13 @@ namespace Alternet.UI
             }
         }
 
-        public static int RunProcessOldStyle(ProcessStartInfo startInfo, bool wait)
+        /// <summary>
+        /// Runs a process using the specified start information and optionally waits for it to exit.
+        /// </summary>
+        /// <param name="startInfo">The start information for the process.</param>
+        /// <param name="wait">If <c>true</c>, waits for the process to exit before returning.</param>
+        /// <returns>The exit code of the process if <paramref name="wait"/> is <c>true</c>; otherwise, 0.</returns>
+        public static int RunProcessAndWait(ProcessStartInfo startInfo, bool wait)
         {
             var process = RunProcess(startInfo);
 
@@ -90,25 +149,33 @@ namespace Alternet.UI
             return 0;
         }
 
+        /// <summary>
+        /// Runs a process using the specified start information.
+        /// </summary>
+        /// <param name="startInfo">The start information for the process.</param>
+        /// <returns>The <see cref="Process"/> instance representing the running process.</returns>
         public static Process RunProcess(ProcessStartInfo startInfo)
         {
             KillRunningProcess();
 
             Process Internal()
             {
+                var encoding = DefaultInputOutputEncoding ?? Encoding.UTF8;
+
                 startInfo.CreateNoWindow = true;
                 startInfo.UseShellExecute = false;
-                startInfo.StandardErrorEncoding = Encoding.UTF8;
-                /*startInfo.StandardInputEncoding = Encoding.UTF8;*/
-                startInfo.StandardOutputEncoding = Encoding.UTF8;
+                startInfo.StandardErrorEncoding = encoding;
+
+                var result = AssemblyUtils.TrySetMemberValue(startInfo, "StandardInputEncoding", Encoding.UTF8);
+
+                startInfo.StandardOutputEncoding = encoding;
                 startInfo.RedirectStandardInput = true;
                 startInfo.RedirectStandardOutput = true;
                 startInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 startInfo.RedirectStandardError = true;
                 if (!string.IsNullOrEmpty(startInfo.WorkingDirectory))
                 {
-                    startInfo.WorkingDirectory =
-                        PathUtils.AddDirectorySeparatorChar(startInfo.WorkingDirectory);
+                    startInfo.WorkingDirectory = PathUtils.AddDirectorySeparatorChar(startInfo.WorkingDirectory);
                 }
 
                 var process = new Process();
@@ -144,7 +211,7 @@ namespace Alternet.UI
                     var exitCode = process.ExitCode;
 
                     if (ProcessExitedWithResult is not null)
-                        LogString(process, $"{ProcessExitedWithResult}: {exitCode}");
+                        LogString(process, string.Format(ProcessExitedWithResult, exitCode));
                     LogString(process, ProcessExitedSeparator);
                 };
 
@@ -209,6 +276,10 @@ namespace Alternet.UI
             return Internal();
         }
 
+        /// <summary>
+        /// Notifies all subscribers of a specific action.
+        /// </summary>
+        /// <param name="action">The action to perform for each subscriber.</param>
         private static void Notify(Action<IProcessRunnerNotification> action)
         {
             BaseObject.Invoke(() =>
@@ -223,6 +294,10 @@ namespace Alternet.UI
             });
         }
 
+        /// <summary>
+        /// Notifies all subscribers that a process has exited.
+        /// </summary>
+        /// <param name="process">The process that has exited.</param>
         private static void NotifyExited(Process process)
         {
             BaseObject.Invoke(() =>
@@ -232,6 +307,10 @@ namespace Alternet.UI
             });
         }
 
+        /// <summary>
+        /// Notifies all subscribers that a process has been disposed.
+        /// </summary>
+        /// <param name="process">The process that has been disposed.</param>
         private static void NotifyDisposed(Process process)
         {
             BaseObject.Invoke(() =>
