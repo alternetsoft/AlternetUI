eec2c0da0a1f4860e40bf08b2d454ea1f2ad9dcb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-19 09:29:49 +0300

    Create BaseConcurrentStack.cs

---
+++ Source/Alternet.UI.Common/Base/Collections/BaseConcurrentStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a thread-safe, last-in-first-out (LIFO) collection of objects.
+    /// This class extends <see cref="ConcurrentStack{T}"/> to provide additional functionality or customization.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the stack.</typeparam>
+    public class BaseConcurrentStack<T> : ConcurrentStack<T>
+    {
+    }
+}
