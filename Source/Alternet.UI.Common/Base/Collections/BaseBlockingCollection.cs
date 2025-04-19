397cfd66e28712da0464c48ce17e4e2af62d191c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-19 09:31:41 +0300

    Create BaseBlockingCollection.cs

---
+++ Source/Alternet.UI.Common/Base/Collections/BaseBlockingCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a thread-safe collection that provides blocking and bounding capabilities.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public class BaseBlockingCollection<T> : BlockingCollection<T>
+    {
+        /// <summary>
+        /// Removes all items from the collection.
+        /// </summary>
+        public void Clear()
+        {
+            while (TryTake(out _))
+            {
+            }
+        }
+    }
+}
