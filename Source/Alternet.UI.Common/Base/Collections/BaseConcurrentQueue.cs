778e4c69c6a2789d3404bb100aa46b1f74f3c25e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-19 09:29:45 +0300

    Create BaseConcurrentQueue.cs

---
+++ Source/Alternet.UI.Common/Base/Collections/BaseConcurrentQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a thread-safe first in-first out (FIFO) collection for storing objects.
+    /// Inherits from <see cref="ConcurrentQueue{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the queue.</typeparam>
+    public class BaseConcurrentQueue<T> : ConcurrentQueue<T>
+    {
+        /// <summary>
+        /// Removes all items from the queue in a thread-safe manner.
+        /// </summary>
+        public void Clear()
+        {
+            while (TryDequeue(out _))
+            {
+            }
+        }
+    }
+}
