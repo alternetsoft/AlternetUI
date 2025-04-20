f779adad3d07dafa520529e131d4d8e7d03c774a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-20 07:32:41 +0300

    Create MauiCommands.cs

---
+++ Source/Alternet.UI.Maui/Utils/MauiCommands.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+
+namespace Alternet.Maui
+{
+    /// <summary>
+    /// Provides a collection of reusable commands for .NET MAUI applications.
+    /// </summary>
+    public static class MauiCommands
+    {
+        private static Command? exitCommand;
+
+        /// <summary>
+        /// Gets the command to exit the application.
+        /// </summary>
+        /// <remarks>
+        /// This command executes the <see cref="Alternet.UI.MauiUtils.CloseApplication"/> method
+        /// to close the application. The command is always executable.
+        /// </remarks>
+        public static Command ExitCommand
+        {
+            get
+            {
+                exitCommand ??= new Command(
+                    execute: () =>
+                    {
+                        Alternet.UI.MauiUtils.CloseApplication();
+                    },
+                    canExecute: () =>
+                    {
+                        return true;
+                    });
+                return exitCommand;
+            }
+        }
+    }
+}
