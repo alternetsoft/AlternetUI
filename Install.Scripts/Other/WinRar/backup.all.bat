07ffc7bc2043625464b390625b402bc441f8e8ad
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-24 15:48:40 +0300

    Minor changes

---
+++ Install.Scripts/Other/WinRar/backup.all.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\WinRar\WinRAR" a -r -x*.dll -x*.xml -x*.nupkg -x*.apk -x*.aab -x*.a -x*.dylib -x*.pdb -x*.dcu -x*.jar -x*.zip -x*.dat -x*.dex -x*.apks -x*.so -x*.class -x*.java -x*.blob -x*.cache -x*.lz4 -x*.dll -x*.exe -ag{-}YYYY-MM-DD worksoftw.rar "c:\work\*.*" 
+
