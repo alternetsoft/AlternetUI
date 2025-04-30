b4e34b5fabac2a482c0d3d72d8bc3d24714833b4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-04-30 23:20:05 +0300

    Create ListControlColumn.cs

---
+++ Source/Alternet.UI.Common/Base/ControlItems/ListControl/ListControlColumn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a column within a list control, defining its layout properties.
+    /// </summary>
+    /// <remarks>
+    /// This class provides configuration settings such as suggested width for column rendering.
+    /// It can be extended to accommodate additional visual or functional enhancements.
+    /// </remarks>
+    public class ListControlColumn : BaseControlItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListControlColumn"/> class.
+        /// </summary>
+        public ListControlColumn()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the suggested width for the list control column.
+        /// </summary>
+        /// <remarks>
+        /// The width recommendation is based on layout constraints and user preferences.
+        /// </remarks>
+        public virtual Coord SuggestedWidth { get; set; }
+    }
+}
