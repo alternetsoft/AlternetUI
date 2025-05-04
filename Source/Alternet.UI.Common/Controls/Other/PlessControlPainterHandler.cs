2c53e7202e367e39b24e4d0b87665868a9685d50
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-05-04 08:44:52 +0300

    Implemented PlessControlPainterHandler

--- Source/Alternet.UI.Common/Controls/Other/PlessControlPainterHandler.cs
+++ Source/Alternet.UI.Common/Controls/Other/PlessControlPainterHandler.cs
@@ -13,14 +13,20 @@ namespace Alternet.Drawing
     public class PlessControlPainterHandler : DisposableObject, IControlPainterHandler
     {
         /// <summary>
+        /// Gets or sets the default color of a checkbox in its normal state.
+        /// </summary>
+        public static LightDarkColor DefaultCheckBoxColor
+            = new(light: (0, 103, 192) /*(0, 95, 184)*/, dark: new(76, 194, 255));
+
+        /// <summary>
         /// Gets or sets the default size of a check mark in device-independent units.
         /// </summary>
-        public static SizeD DefaultCheckMarkSize = (16, 16);
+        public static SizeD DefaultCheckMarkSize = (20, 20);
 
         /// <summary>
         /// Gets or sets the default size of a checkbox in device-independent units.
         /// </summary>
-        public static SizeD DefaultCheckBoxSize = (13, 13);
+        public static SizeD DefaultCheckBoxSize = (16, 16);
 
         /// <summary>
         /// Gets or sets the default size of an expander in device-independent units.
@@ -43,6 +49,111 @@ namespace Alternet.Drawing
         /// </summary>
         public static SizeD DefaultCollapseButtonSize = (19, 21);
 
+        /// <summary>
+        /// Gets or sets the SVG image used when the checkbox is unchecked.
+        /// </summary>
+        public static SvgImage? CheckBoxImageUnchecked;
+
+        /// <summary>
+        /// Gets or sets the SVG image used when the checkbox is checked.
+        /// </summary>
+        public static SvgImage? CheckBoxImageChecked;
+
+        /// <summary>
+        /// Gets or sets the SVG image used when the checkbox is in an indeterminate state.
+        /// </summary>
+        public static SvgImage? CheckBoxImageIndeterminate;
+
+        /// <summary>
+        /// Gets or sets the colors associated with different visual states of a checkbox.
+        /// </summary>
+        public static EnumArray<VisualControlState, Color?> CheckBoxStateColors = new();
+
+        /// <summary>
+        /// Gets the SVG image corresponding to the specified checkbox state.
+        /// </summary>
+        /// <param name="checkState">The state of the checkbox (unchecked, checked, or indeterminate).</param>
+        /// <returns>The SVG image representing the checkbox state.</returns>
+        public static SvgImage GetCheckBoxSvg(CheckState checkState)
+        {
+            SvgImage svgImage;
+
+            switch (checkState)
+            {
+                case CheckState.Unchecked:
+                default:
+                    svgImage = CheckBoxImageUnchecked ?? KnownSvgImages.ImgSquare;
+                    break;
+                case CheckState.Checked:
+                    svgImage = CheckBoxImageChecked ?? KnownSvgImages.ImgSquareCheckFilled;
+                    break;
+                case CheckState.Indeterminate:
+                    svgImage = CheckBoxImageIndeterminate ?? KnownSvgImages.ImgSquareMinusFilled;
+                    break;
+            }
+
+            return svgImage;
+        }
+
+        /// <summary>
+        /// Draws a checkbox using SVG images.
+        /// </summary>
+        /// <param name="control">The control associated with the checkbox.</param>
+        /// <param name="canvas">The graphics context where the checkbox will be drawn.</param>
+        /// <param name="rect">The rectangle defining the bounds of the checkbox.</param>
+        /// <param name="checkState">The state of the checkbox (checked, unchecked, or indeterminate).</param>
+        /// <param name="controlState">The visual state of the control (e.g., normal, hovered, pressed).</param>
+        public static void DrawCheckBoxSvg(
+            AbstractControl control,
+            Graphics canvas,
+            RectD rect,
+            CheckState checkState,
+            VisualControlState controlState)
+        {
+            var svg = GetCheckBoxSvg(checkState);
+            var size = control.PixelFromDip(rect.Size.Width);
+
+            Image? image;
+
+            var color = CheckBoxStateColors[controlState];
+
+            if(color is null)
+            {
+                switch (controlState)
+                {
+                    case VisualControlState.Disabled:
+                        image = svg.AsDisabledImage(size, control.IsDarkBackground);
+                        break;
+                    case VisualControlState.Selected:
+                        color = DefaultCheckBoxColor.LightOrDark(isDark: true);
+                        image = svg.ImageWithColor(size, color);
+                        break;
+                    default:
+                        if (checkState == CheckState.Unchecked)
+                        {
+                            color = DefaultColors.GetBorderColor(control.IsDarkBackground);
+                        }
+                        else
+                        {
+                            color = DefaultCheckBoxColor.LightOrDark(control.IsDarkBackground);
+                        }
+
+                        image = svg.ImageWithColor(size, color);
+                        break;
+                }
+            }
+            else
+            {
+                image = svg.ImageWithColor(size, color);
+            }
+
+            if (image is not null)
+            {
+                canvas.DrawImage(image, rect.Location);
+                return;
+            }
+        }
+
         /// <inheritdoc/>
         public virtual void DrawCheckBox(
             AbstractControl control,
@@ -51,7 +162,7 @@ namespace Alternet.Drawing
             CheckState checkState,
             VisualControlState controlState)
         {
-            throw new NotImplementedException();
+            DrawCheckBoxSvg(control, canvas, rect, checkState, controlState);
         }
 
         /// <inheritdoc/>
