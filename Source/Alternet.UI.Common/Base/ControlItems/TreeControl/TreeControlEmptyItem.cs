184523ee16941625d5fe934e9c464b34f814ae9d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-05-09 11:50:02 +0300

    Create TreeControlEmptyItem.cs

---
+++ Source/Alternet.UI.Common/Base/ControlItems/TreeControl/TreeControlEmptyItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements an empty item which can be used as a spacer.
+    /// </summary>
+    public class TreeControlEmptyItem : TreeControlItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreeControlEmptyItem"/> class.
+        /// </summary>
+        public TreeControlEmptyItem()
+        {
+            this.HideSelection = true;
+        }
+    }
+}
