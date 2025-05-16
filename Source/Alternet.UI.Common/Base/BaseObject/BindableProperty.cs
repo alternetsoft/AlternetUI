d3d5bf1255312a4e22655fded933e00c5ba7b2a2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-05-16 19:28:52 +0300

    BindableProperty prototype

---
+++ Source/Alternet.UI.Common/Base/BaseObject/BindableProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    internal delegate object? BindablePropertyGetValueDelegate(object? element);
+
+    internal delegate void BindablePropertySetValueDelegate(object? element, object? value);
+
+    internal class BindableProperty : ImmutableObject
+    {
+        public BindableProperty()
+        {
+        }
+
+        public virtual string? PropertyName { get; set; }
+
+        public virtual Type? PropertyType { get; set; }
+
+        public virtual Type? ElementType { get; set; }
+
+        public virtual BindablePropertyGetValueDelegate? GetValue { get; set; }
+
+        public virtual BindablePropertySetValueDelegate? SetValue { get; set; }
+    }
+}
---
+++ Source/Alternet.UI.Common/Controls/Abstract.Props/AbstractControl.Bindable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    public partial class AbstractControl
+    {
+        internal static readonly BindableProperty TextProperty = new()
+        {
+            PropertyName = nameof(Text),
+            PropertyType = typeof(string),
+            ElementType = typeof(AbstractControl),
+            GetValue = (instance) =>
+            {
+                return ((AbstractControl?)instance)?.Text;
+            },
+            SetValue = (instance, value) =>
+            {
+                if (instance is AbstractControl control)
+                    control.Text = value?.ToString() ?? string.Empty;
+            },
+        };
+    }
+}
