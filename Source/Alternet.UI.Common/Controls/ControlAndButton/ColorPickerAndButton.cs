57d1fe32ce7e1572add5f1c2625c5f489d271b7b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-22 12:09:16 +0300

    Create ColorPickerAndButton.cs

---
+++ Source/Alternet.UI.Common/Controls/ControlAndButton/ColorPickerAndButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+using Alternet.UI.Localization;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements <see cref="ColorPicker"/> with side buttons.
+    /// </summary>
+    public partial class ColorPickerAndButton : ControlAndButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorPickerAndButton"/> class.
+        /// </summary>
+        public ColorPickerAndButton()
+        {
+        }
+
+        /// <summary>
+        /// Gets main child control.
+        /// </summary>
+        [Browsable(false)]
+        public new ColorPicker MainControl => (ColorPicker)base.MainControl;
+
+        /// <summary>
+        /// Gets main child control, same as <see cref="MainControl"/>.
+        /// </summary>
+        [Browsable(false)]
+        public ColorPicker ColorPicker => (ColorPicker)base.MainControl;
+
+        /// <inheritdoc/>
+        protected override AbstractControl CreateControl()
+        {
+            return new ColorPicker();
+        }
+    }
+}
