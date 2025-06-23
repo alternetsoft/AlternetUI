526b88eb7dd5f10dbcd6073c13911a2ebbf40ae3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-23 13:40:15 +0300

    Create FontSizePicker.cs

---
+++ Source/Alternet.UI.Common/Controls/SpeedButton/FontSizePicker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a picker control for selecting font sizes.
+    /// </summary>
+    /// <remarks>The <see cref="FontSizePicker"/> class provides functionality
+    /// to display a list of available
+    /// font sizes and allows the user to select one.
+    /// By default, the control initializes with a predefined set of font sizes.</remarks>
+    public partial class FontSizePicker : ListPicker
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FontSizePicker"/> class.
+        /// </summary>
+        public FontSizePicker()
+        {
+            AddFontSizesAndSelect(true);
+        }
+    }
+}
