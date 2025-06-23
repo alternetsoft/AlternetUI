de0f95354ca075f518763e4a7e973d689ee8101d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-23 13:40:20 +0300

    Create ListPicker.cs

---
+++ Source/Alternet.UI.Common/Controls/SpeedButton/ListPicker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents control that displays a selected item and allows to change it
+    /// with the popup window with virtual list box.
+    /// </summary>
+    [ControlCategory("Other")]
+    public partial class ListPicker : SpeedButtonWithListPopup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListPicker"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public ListPicker(Control parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListPicker"/> class.
+        /// </summary>
+        public ListPicker()
+        {
+            UseTheme = KnownTheme.StaticBorder;
+            SetContentHorizontalAlignment(HorizontalAlignment.Left);
+        }
+
+        /// <inheritdoc/>
+        public override ControlTypeId ControlKind => ControlTypeId.Other;
+    }
+}
