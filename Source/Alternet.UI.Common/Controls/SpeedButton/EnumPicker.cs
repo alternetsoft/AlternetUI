3f290e200fc5d4cf1e7c6933bc8b93b6f890215c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-23 10:45:00 +0300

    Create EnumPicker.cs

---
+++ Source/Alternet.UI.Common/Controls/SpeedButton/EnumPicker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents control that displays a selected enum value and allows to change it
+    /// with the popup window.
+    /// </summary>
+    [ControlCategory("Other")]
+    public partial class EnumPicker : SpeedEnumButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumPicker"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public EnumPicker(Control parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumPicker"/> class.
+        /// </summary>
+        public EnumPicker()
+        {
+            UseTheme = KnownTheme.StaticBorder;
+            SetContentHorizontalAlignment(HorizontalAlignment.Left);
+        }
+
+        /// <inheritdoc/>
+        public override ControlTypeId ControlKind => ControlTypeId.ColorPicker;
+    }
+}
