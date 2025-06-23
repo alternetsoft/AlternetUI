807730f17ed521e3e80e4c7cacc27cfb17124531
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-23 13:40:12 +0300

    Create FontNamePicker.cs

---
+++ Source/Alternet.UI.Common/Controls/SpeedButton/FontNamePicker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a picker control for selecting font names.
+    /// </summary>
+    /// <remarks>The <see cref="FontNamePicker"/> class provides functionality
+    /// to display a list of available
+    /// font names  and allows the user to select one.
+    /// By default, the control initializes with a predefined set of font names.</remarks>
+    public partial class FontNamePicker : ListPicker
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FontNamePicker"/> class.
+        /// </summary>
+        public FontNamePicker()
+        {
+            AddFontNamesAndSelect(true);
+        }
+    }
+}
