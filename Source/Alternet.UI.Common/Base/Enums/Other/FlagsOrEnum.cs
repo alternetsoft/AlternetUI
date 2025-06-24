0245bd5e478768edc13809ad50d4bda499a0f178
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-24 07:04:00 +0300

    Organize enums

---
+++ Source/Alternet.UI.Common/Base/Enums/Other/FlagsOrEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents the classification of an object as either a standard enumeration,
+    /// a flags enumeration, or neither.
+    /// </summary>
+    /// <remarks>This enumeration is used to distinguish between objects that are standard enums,
+    /// enums marked with the <see cref="System.FlagsAttribute"/>, or objects that are
+    /// not enums at all.</remarks>
+    public enum FlagsOrEnum
+    {
+        /// <summary>
+        /// The object is not an enum or flags.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The object is a standard enum.
+        /// </summary>
+        Enum,
+
+        /// <summary>
+        /// The object is an enum marked with [Flags] attribute.
+        /// </summary>
+        Flags,
+    }
+}
