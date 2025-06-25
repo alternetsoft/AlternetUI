8a92e08579e828df27d31145895759bd1130e32d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-25 17:54:36 +0300

    Create LabelAndButton.cs

---
+++ Source/Alternet.UI.Common/Controls/ControlAndButton/LabelAndButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+using Alternet.UI.Localization;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Implements <see cref="GenericLabel"/> with side buttons.
+    /// </summary>
+    public partial class LabelAndButton : ControlAndButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LabelAndButton"/> class.
+        /// </summary>
+        public LabelAndButton()
+        {
+        }
+
+        /// <summary>
+        /// Gets main child control.
+        /// </summary>
+        [Browsable(false)]
+        public new GenericLabel MainControl => (GenericLabel)base.MainControl;
+
+        /// <summary>
+        /// Gets main child control, same as <see cref="MainControl"/>.
+        /// </summary>
+        [Browsable(false)]
+        public GenericLabel Label => (GenericLabel)base.MainControl;
+
+        /// <summary>
+        /// Gets or sets <see cref="AbstractControl.Text"/> property of the main child control.
+        /// </summary>
+        [Browsable(true)]
+        public override string Text
+        {
+            get
+            {
+                return Label.Text;
+            }
+
+            set
+            {
+                Label.Text = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override AbstractControl CreateControl()
+        {
+            return new GenericLabel();
+        }
+    }
+}
