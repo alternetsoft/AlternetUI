8652a2c6c632c662c249a0162c70bfbf4f9d8012
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-25 15:29:13 +0300

    ImageList: AddResized, AddImageStrip and 5 new constructors

--- Source/Alternet.UI.Common/Drawing/ImageList/ImageList.cs
+++ Source/Alternet.UI.Common/Drawing/ImageList/ImageList.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Alternet.Base.Collections;
 using Alternet.UI;
 
@@ -27,6 +30,65 @@ namespace Alternet.Drawing
         public static int DefaultSize = 16;
 
         private SizeI size = DefaultSize;
+
+        /// <summary>
+        /// Initializes a new empty instance of the <c>ImageList</c> class with
+        /// the specified image size.
+        /// </summary>
+        public ImageList(SizeI size)
+            : this(false)
+        {
+            ImageSize = size;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>ImageList</c> class with specified image list.
+        /// </summary>
+        /// <param name="images">Collection of images to be stored in this new instance.</param>
+        public ImageList(IEnumerable<Image> images)
+            : this(false)
+        {
+            var firstImage = images.FirstOrDefault();
+            if (firstImage == null)
+            {
+                ImageSize = new(16, 16);
+                return;
+            }
+
+            Initialize(images, firstImage.Size);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>ImageList</c> class with specified parameters.
+        /// </summary>
+        /// <param name="images">Collection of images to be stored in this new instance.</param>
+        /// <param name="size">New size of the images.</param>
+        public ImageList(IEnumerable<Image> images, SizeI size)
+            : this(false)
+        {
+            Initialize(images, size);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>ImageList</c> class with specified image strip.
+        /// </summary>
+        /// <param name="stripImage">List of images concatenated in a single image strip.</param>
+        public ImageList(Image stripImage)
+            : this(stripImage, stripImage.Height)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <c>ImageList</c> class with specified image strip.
+        /// </summary>
+        /// <param name="stripImage">List of images concatenated in a single image strip.</param>
+        /// <param name="imageWidth">Width of the particular image in the strip.</param>
+        public ImageList(Image stripImage, int imageWidth)
+            : this(false)
+        {
+            ImageSize = new(imageWidth, stripImage.Height);
+            AddImageStrip(stripImage);
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ImageList"/> with default values.
@@ -93,6 +155,38 @@ namespace Alternet.Drawing
         }
 
         /// <summary>
+        /// Adds the image to the image list, resizing it if necessary.
+        /// </summary>
+        /// <param name="image">The image to be added.</param>
+        /// <returns>Returns true if the image was added successfully.</returns>
+        public virtual bool AddResized(Image image)
+        {
+            if (image.Size != ImageSize)
+            {
+                var resizedImage = new Bitmap(image, ImageSize);
+                return Add(resizedImage);
+            }
+
+            return Add(image);
+        }
+
+        /// <summary>
+        /// Adds image strip to the image list.
+        /// </summary>
+        /// <param name="strip">The image strip containing multiple images.</param>
+        public virtual void AddImageStrip(Image strip)
+        {
+            var width = ImageSize.Width;
+            int imageCount = strip.Width / width;
+            for (int i = 0; i < imageCount; i++)
+            {
+                var sourceRectangle = new RectI(i * width, 0, width, ImageSize.Height);
+                var image = strip.GetSubBitmap(sourceRectangle);
+                Add(image);
+            }
+        }
+
+        /// <summary>
         /// Adds svg to the image list with the default normal color for the specified theme.
         /// </summary>
         /// <param name="svg">Svg to add.</param>
@@ -143,7 +237,12 @@ namespace Alternet.Drawing
             if (index < 0 || index >= Images.Count)
                 return false;
 
-            g.DrawImageUnscaled(Images[index], new(x, y));
+            var image = Images[index];
+
+            if (image is null)
+                return false;
+
+            g.DrawImageUnscaled(image, new(x, y));
             return true;
         }
 
@@ -193,6 +292,17 @@ namespace Alternet.Drawing
         protected override IImageListHandler CreateHandler()
         {
             return GraphicsFactory.Handler.CreateImageListHandler() ?? new PlessImageListHandler();
+        }
+
+        /// <summary>
+        /// Initializes the image list with the specified images and size.
+        /// </summary>
+        /// <param name="images">The images to initialize with.</param>
+        /// <param name="size">The size to set for the images.</param>
+        protected virtual void Initialize(IEnumerable<Image> images, SizeI size)
+        {
+            ImageSize = size;
+            Images.AddRange(images);
         }
     }
 }
