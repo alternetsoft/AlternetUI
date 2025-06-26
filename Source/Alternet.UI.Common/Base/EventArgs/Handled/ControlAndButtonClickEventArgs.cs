6518f2394e9af9999ccdc9c274165f3c6907f37c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-26 08:01:07 +0300

    ControlAndButtonClickEventArgs: new members
     IsButtonCombo, IsButtonPlus, IsButtonMinus, isButtonEllispsis

--- Source/Alternet.UI.Common/Base/EventArgs/Handled/ControlAndButtonClickEventArgs.cs
+++ Source/Alternet.UI.Common/Base/EventArgs/Handled/ControlAndButtonClickEventArgs.cs
@@ -13,5 +13,45 @@ namespace Alternet.UI
         /// Gets id of the clicked button.
         /// </summary>
         public virtual ObjectUniqueId? ButtonId { get; set; }
+
+        /// <summary>
+        /// Determines if the button is a combo button.
+        /// </summary>
+        public virtual bool IsButtonCombo(AbstractControl buttonOwner)
+        {
+            if(buttonOwner is ControlAndButton controlAndButton)
+                return ButtonId == controlAndButton.IdButtonCombo;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if the button is a plus button.
+        /// </summary>
+        public virtual bool IsButtonPlus(AbstractControl buttonOwner)
+        {
+            if(buttonOwner is ControlAndButton controlAndButton)
+                return ButtonId == controlAndButton.IdButtonPlus;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if the button is a minus button.
+        /// </summary>
+        public virtual bool IsButtonMinus(AbstractControl buttonOwner)
+        {
+            if(buttonOwner is ControlAndButton controlAndButton)
+                return ButtonId == controlAndButton.IdButtonMinus;
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if the button is an ellipsis button.
+        /// </summary>
+        public virtual bool IsButtonEllipsis(AbstractControl buttonOwner)
+        {
+            if(buttonOwner is ControlAndButton controlAndButton)
+                return ButtonId == controlAndButton.IdButtonEllipsis;
+            return false;
+        }
     }
 }
