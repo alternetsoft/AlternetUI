da78115f36d3abec1ed78f9960f81bac58cab652
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-27 13:07:03 +0300

    Improve documentation related samples

--- Source/Samples/ApiDocDll/AllWindows/TabControlWindow.uixml.cs
+++ Source/Samples/ApiDocDll/AllWindows/TabControlWindow.uixml.cs
@@ -8,20 +8,43 @@ namespace ApiDoc
         public TabControlWindow()
         {
             InitializeComponent();
+            MinWidth = 600;
             TabControlExample1();
+            SetSizeToContent();
         }
 
         public void TabControlExample1()
         {
             #region TabControlCSharpCreation
             var tc = new Alternet.UI.TabControl();
-            tc.SuggestedSize = (250, 300);
-            tc.Pages.Add(new TabPage { Title = "Page 1"});
+            tc.SuggestedHeight = 300;
+            tc.Margin = 10;
+
+            var page1 = new TabPage
+            {
+                Title = "Page 1",
+            };
+
+            var content1 = new ListBox
+            {
+                HasBorder = false,
+                Items = new string[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" },
+            };
+
+            content1.Parent = page1;
+
+            tc.Pages.Add(page1);
             tc.Pages.Add(new TabPage { Title = "Page 2" });
             tc.Pages.Add(new TabPage { Title = "Page 3" });
             tc.Pages.Add(new TabPage { Title = "Page 4" });
             tc.Pages.Add(new TabPage { Title = "Page 5" });
             tc.Parent = mainPanel;
+
+            tc.SelectedPageChanged += (s, e) =>
+            {
+                Title = $"Selected page: {tc.SelectedPage?.Title}";
+            };
+
             #endregion
         }
     }
