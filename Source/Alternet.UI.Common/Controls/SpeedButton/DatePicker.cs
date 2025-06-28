33eab9e17b57375b6ebdf4cc89ee2d6303744004
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-28 11:09:12 +0300

    Create DatePicker.cs

---
+++ Source/Alternet.UI.Common/Controls/SpeedButton/DatePicker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a control that displays a date and shows popup calendar when it is clicked.
+    /// </summary>
+    [ControlCategory("Other")]
+    public partial class DatePicker : SpeedDateButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatePicker"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public DatePicker(Control parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatePicker"/> class.
+        /// </summary>
+        public DatePicker()
+        {
+            UseTheme = KnownTheme.StaticBorder;
+        }
+
+        /// <inheritdoc/>
+        public override ControlTypeId ControlKind => ControlTypeId.Other;
+    }
+}
