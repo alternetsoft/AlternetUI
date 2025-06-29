684746481654142cfedd70df42ad07d4c16a45d7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-29 13:35:04 +0300

    Create TimePickerHourFormat.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/TimePickerHourFormat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents the hour format for the <c>TimePicker</c> and other controls.
+    /// </summary>
+    public enum TimePickerHourFormat
+    {
+        /// <summary>
+        /// Use operating system settings.
+        /// </summary>
+        System,
+
+        /// <summary>
+        /// 12-hour format (AM/PM).
+        /// </summary>
+        Hour12,
+
+        /// <summary>
+        /// 24-hour format.
+        /// </summary>
+        Hour24,
+    }
+}
