7e71ed3d4a2d33a269026a202d88e138c6e5eafb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-06-29 13:35:08 +0300

    Create TimePickerValuePart.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/TimePickerValuePart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the individual components of a time value that
+    /// a time picker control can expose or manipulate.
+    /// </summary>
+    public enum TimePickerValuePart
+    {
+        /// <summary>
+        /// Represents the hour component of the time value.
+        /// </summary>
+        Hour,
+
+        /// <summary>
+        /// Represents the minute component of the time value.
+        /// </summary>
+        Minute,
+
+        /// <summary>
+        /// Represents the second component of the time value.
+        /// </summary>
+        Second,
+
+        /// <summary>
+        /// Represents the AM/PM designator of the time value in 12-hour format.
+        /// </summary>
+        AmPm,
+    }
+}
