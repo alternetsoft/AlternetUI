8d17ce7e279184d3ebad11e62d5e4d7124920b7f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-02 00:30:58 +0300

    PopupListBox.Default

--- Source/Alternet.UI.Common/Controls/PopupWindow/PopupListBox.cs
+++ Source/Alternet.UI.Common/Controls/PopupWindow/PopupListBox.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 namespace Alternet.UI
 {
     /// <summary>
-    /// Popup window with <see cref="ListBox"/> control.
+    /// Popup window with <see cref="VirtualListBox"/> control.
     /// </summary>
     public partial class PopupListBox : PopupListBox<VirtualListBox>
     {
+        private static PopupListBox? defaultInstance;
+
+        /// <summary>
+        /// Gets or sets the default instance of <see cref="PopupListBox"/>.
+        /// </summary>
+        public static new PopupListBox Default
+        {
+            get => defaultInstance ??= new PopupListBox();
+            set => defaultInstance = value;
+        }
     }
 }
