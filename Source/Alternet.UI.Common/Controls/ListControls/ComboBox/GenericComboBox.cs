c924913dcb4f097bdb2fe09c216a4c53d060db4f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-03 14:36:20 +0300

    Add GenericComboBox control implementation
    Introduces the GenericComboBox class, extending TextBoxWithListPopup to provide a combo box control supporting both text input and item selection.

---
+++ Source/Alternet.UI.Common/Controls/ListControls/ComboBox/GenericComboBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a text box with an associated list popup that provides
+    /// a generic combo box functionality.
+    /// </summary>
+    /// <remarks>The <see cref="GenericComboBox"/> class extends
+    /// <see cref="TextBoxWithListPopup"/> to allow
+    /// users  to input text and select items from a dropdown list.
+    /// This control is suitable for scenarios where both
+    /// free-form text input and predefined item selection are required.</remarks>
+    public class GenericComboBox : TextBoxWithListPopup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericComboBox"/> class.
+        /// </summary>
+        public GenericComboBox()
+        {
+            SyncTextAndComboButton();
+        }
+    }
+}
