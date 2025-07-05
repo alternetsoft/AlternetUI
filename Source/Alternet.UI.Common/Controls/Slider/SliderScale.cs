e8ca9270572bf75170b455df56201f21903323cf
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-05 10:54:25 +0300

    Update SliderScale.cs

--- Source/Alternet.UI.Common/Controls/Slider/SliderScale.cs
+++ Source/Alternet.UI.Common/Controls/Slider/SliderScale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Alternet.UI
@@ -9,7 +10,7 @@ namespace Alternet.UI
     /// </summary>
     /// <remarks>The <see cref="SliderScale"/> class provides functionality for
     /// rendering a slider scale. It is intended to be used as part of a
-    /// <see cref="Slider"/> and can be customized or extended as needed.</remarks>
+    /// <see cref="Container"/> and can be customized or extended as needed.</remarks>
     public partial class SliderScale : Spacer
     {
         /// <summary>
@@ -20,6 +21,123 @@ namespace Alternet.UI
         /// Use this to set up a slider scale with default settings.</remarks>
         public SliderScale()
         {
+        }
+
+        /// <summary>
+        /// Gets the container that holds the slider scale.
+        /// In the default implementation, this property returns the parent control
+        /// that implements the <see cref="ISliderScaleContainer"/> interface.
+        /// </summary>
+        [Browsable(false)]
+        public virtual ISliderScaleContainer? Container
+        {
+            get
+            {
+                return Parent as ISliderScaleContainer;
+            }
+        }
+
+        /// <summary>
+        /// Gets the current value of the slider.
+        /// </summary>
+        [Browsable(false)]
+        public int Value
+        {
+            get
+            {
+                return Container?.Value ?? 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets the orientation of the slider.
+        /// </summary>
+        [Browsable(false)]
+        public SliderOrientation Orientation
+        {
+            get
+            {
+                return Container?.Orientation ?? SliderOrientation.Horizontal;
+            }
+        }
+
+        /// <summary>
+        /// Gets the minimum value of the slider.
+        /// </summary>
+        [Browsable(false)]
+        public int Minimum
+        {
+            get
+            {
+                return Container?.Minimum ?? 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets the maximum value that the slider can represent.
+        /// </summary>
+        [Browsable(false)]
+        public int Maximum
+        {
+            get
+            {
+                return Container?.Maximum ?? 100;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the slider's orientation is horizontal.
+        /// </summary>
+        public bool IsHorizontal
+        {
+            get
+            {
+                return Orientation == SliderOrientation.Horizontal;
+            }
+        }
+
+        /// <summary>
+        /// Gets the frequency of ticks on the slider scale.
+        /// </summary>
+        [Browsable(false)]
+        public int TickFrequency
+        {
+            get
+            {
+                return Container?.TickFrequency ?? 1;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void DefaultPaint(PaintEventArgs e)
+        {
+            if(Container == null)
+            {
+                base.DefaultPaint(e);
+                return;
+            }
+
+            var min = Minimum;
+            var max = Maximum;
+            var value = Value;
+            var frequency = TickFrequency;
+
+            if (IsHorizontal)
+            {
+                HorizontalDefaultPaint();
+            }
+            else
+            {
+                VerticalDefaultPaint();
+            }
+
+            void HorizontalDefaultPaint()
+            {
+            }
+
+            void VerticalDefaultPaint()
+            {
+            }
         }
     }
 }
