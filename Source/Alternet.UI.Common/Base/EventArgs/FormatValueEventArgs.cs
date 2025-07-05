87156ad3374937d8bac4ad6872f93a6fe9ffb632
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-05 10:53:24 +0300

    Create FormatValueEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/FormatValueEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents event data for formatting a value into a display-friendly string.
+    /// </summary>
+    /// <typeparam name="T">Type of the value to format.</typeparam>
+    public class FormatValueEventArgs<T> : BaseEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormatValueEventArgs{T}"/> class.
+        /// </summary>
+        /// <param name="value">The value to format.</param>
+        public FormatValueEventArgs(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Gets the raw value to format.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the formatted representation of the value.
+        /// </summary>
+        public string? FormattedValue { get; set; }
+    }
+}
