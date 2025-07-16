85c076262c17adfab6cc1d0323cde9247f6867da
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-16 06:44:31 +0300

    Move old control and handler files to archive folder
    Renamed various control and handler source files to the archive folder.

---
+++ Old/Removed 2025-07-16-OldLinkLabel/ILinkLabelHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Contains methods and properties which allow to work with link label control.
+    /// </summary>
+    public interface ILinkLabelHandler
+    {
+        /// <inheritdoc cref="LinkLabel.Url"/>
+        string Url { get; set; }
+
+        /// <inheritdoc cref="LinkLabel.HoverColor"/>
+        Color HoverColor { get; set; }
+
+        /// <inheritdoc cref="LinkLabel.NormalColor"/>
+        Color NormalColor { get; set; }
+
+        /// <inheritdoc cref="LinkLabel.VisitedColor"/>
+        Color VisitedColor { get; set; }
+
+        /// <inheritdoc cref="LinkLabel.Visited"/>
+        bool Visited { get; set; }
+    }
+}
