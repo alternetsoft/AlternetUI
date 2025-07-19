680debe46bf1db23f83e0d2035e5342f2c840354
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-19 12:34:12 +0300

    Merge GenericTextControl to GenericWrappedTextControl
    Deleted GenericTextControl.cs and moved its core logic (text painting, measuring, preferred size calculation, and debug drawing) into GenericWrappedTextControl.cs. GenericWrappedTextControl now directly inherits from GenericControl and implements all necessary text control functionality, supporting both wrapped and unwrapped text scenarios.

--- Source/Alternet.UI.Common/Controls/Abstract.Generic/GenericWrappedTextControl.cs
+++ Source/Alternet.UI.Common/Controls/Abstract.Generic/GenericWrappedTextControl.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 
 using Alternet.Drawing;
@@ -10,8 +12,13 @@ namespace Alternet.UI
     /// Implements simple text control which can draw text with new line
     /// characters and wrapping.
     /// </summary>
-    public partial class GenericWrappedTextControl : GenericTextControl
+    public partial class GenericWrappedTextControl : GenericControl
     {
+        /// <summary>
+        /// Gets or sets whether to show debug corners when control is painted.
+        /// </summary>
+        public static bool ShowDebugCorners = false;
+
         private TextFormat.Record textFormat;
         private IEnumerable<string>? wrappedText;
 
@@ -29,6 +36,18 @@ namespace Alternet.UI
         /// </summary>
         public GenericWrappedTextControl()
         {
+        }
+
+        /// <summary>
+        /// Gets text for painting.
+        /// </summary>
+        [Browsable(false)]
+        public virtual string TextForPaint
+        {
+            get
+            {
+                return Text;
+            }
         }
 
         /// <summary>
@@ -132,23 +151,17 @@ namespace Alternet.UI
             });
         }
 
-        /// <inheritdoc/>
-        public override SizeD MeasureText(Graphics dc, Font font, SizeD availableSize)
-        {
-            var wrappedWidth = availableSize.Width;
-
-            wrappedText = DrawingUtils.WrapTextToList(
-                    TextForPaint,
-                    wrappedWidth,
-                    font,
-                    dc);
-
-            var result = DrawInternal(dc, (PointD.Empty, availableSize), font);
-            return result;
-        }
-
-        /// <inheritdoc/>
-        public override void DrawText(
+        /// <summary>
+        /// Draws text within a specified rectangle on the provided graphics context.
+        /// </summary>
+        /// <remarks>The text is aligned within the specified rectangle according to the current text
+        /// alignment settings.</remarks>
+        /// <param name="dc">The graphics context used for drawing the text.</param>
+        /// <param name="rect">The rectangle that defines the area where the text will be drawn.</param>
+        /// <param name="font">The font used to render the text.</param>
+        /// <param name="foreColor">The color of the text.</param>
+        /// <param name="backColor">The background color behind the text.</param>
+        public virtual void DrawText(
             Graphics dc,
             RectD rect,
             Font font,
@@ -168,6 +181,131 @@ namespace Alternet.UI
                 vertAlignment);
 
             DrawInternal(dc, alignedItemRect, font, foreColor, backColor);
+        }
+
+        /// <inheritdoc/>
+        public override void DefaultPaint(PaintEventArgs e)
+        {
+            var rect = ClientRectangle;
+            var dc = e.Graphics;
+            var state = VisualState;
+            RectD paddedRect = (
+                rect.Location + Padding.LeftTop,
+                rect.Size - Padding.Size);
+
+            var labelFont = GetLabelFont(state);
+            var labelForeColor = GetLabelForeColor(state);
+            var labelBackColor = GetLabelBackColor(state);
+
+            DrawText(
+                dc,
+                paddedRect,
+                labelFont,
+                labelForeColor,
+                labelBackColor);
+
+            DefaultPaintDebug(e);
+        }
+
+        /// <summary>
+        /// Measures text size.
+        /// </summary>
+        /// <param name="dc">Graphics context.</param>
+        /// <param name="font">Font used to draw the text.</param>
+        /// <param name="availableSize">Available size.</param>
+        public virtual SizeD MeasureText(Graphics dc, Font font, SizeD availableSize)
+        {
+            var wrappedWidth = availableSize.Width;
+
+            wrappedText = DrawingUtils.WrapTextToList(
+                    TextForPaint,
+                    wrappedWidth,
+                    font,
+                    dc);
+
+            var result = DrawInternal(dc, (PointD.Empty, availableSize), font);
+            return result;
+        }
+
+        /// <summary>
+        /// Default method for calculating preferred size.
+        /// </summary>
+        public virtual SizeD GetDefaultPreferredSize(
+            SizeD availableSize,
+            bool withPadding,
+            Func<SizeD, SizeD> func)
+        {
+            var suggested = SuggestedSize;
+
+            var isNanSuggestedWidth = suggested.IsNanWidth;
+            var isNanSuggestedHeight = suggested.IsNanHeight;
+
+            var containerSize = suggested;
+
+            if (isNanSuggestedWidth)
+                containerSize.Width = availableSize.Width;
+
+            if (isNanSuggestedHeight)
+                containerSize.Height = availableSize.Height;
+
+            var paddingSize = Padding.Size;
+
+            containerSize -= paddingSize;
+
+            var measured = func(containerSize);
+
+            if (!isNanSuggestedWidth)
+                measured.Width = suggested.Width;
+            else
+                measured.Width += paddingSize.Width;
+
+            if (!isNanSuggestedHeight)
+                measured.Height = suggested.Height;
+            else
+                measured.Height += paddingSize.Height;
+
+            return measured;
+        }
+
+        /// <inheritdoc/>
+        public override SizeD GetPreferredSize(SizeD availableSize)
+        {
+            if (availableSize.AnyIsEmptyOrNegative)
+                return SizeD.Empty;
+
+            var result = GetDefaultPreferredSize(
+                        availableSize,
+                        withPadding: true,
+                        (size) =>
+                        {
+                            var measured = MeasureText(
+                                MeasureCanvas,
+                                GetLabelFont(VisualControlState.Normal),
+                                size);
+                            return measured;
+                        });
+            return result;
+        }
+
+        /// <inheritdoc/>
+        protected override void OnVisualStateChanged(EventArgs e)
+        {
+            base.OnVisualStateChanged(e);
+            UserControl.HandleOnVisualStateChanged(this, RefreshOptions);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            PerformLayoutAndInvalidate();
+        }
+
+        [Conditional("DEBUG")]
+        private void DefaultPaintDebug(PaintEventArgs e)
+        {
+            if (ShowDebugCorners)
+                BorderSettings.DrawDesignCorners(e.Graphics, e.ClipRectangle);
         }
 
         private SizeD DrawInternal(
