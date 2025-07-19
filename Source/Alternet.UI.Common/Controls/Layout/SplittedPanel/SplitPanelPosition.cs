c00d75eda90279d4457e2477fc7f343de0ef7519
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-19 08:11:20 +0300

    Create SplitPanelPosition.cs

---
+++ Source/Alternet.UI.Common/Controls/Layout/SplittedPanel/SplitPanelPosition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the position of a panel within a split container.
+    /// </summary>
+    public enum SplitPanelPosition
+    {
+        /// <summary>
+        /// Represents the left position of the panel.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// Represents the right position of the panel.
+        /// </summary>
+        Right,
+
+        /// <summary>
+        /// Represents the top position of the panel.
+        /// </summary>
+        Top,
+
+        /// <summary>
+        /// Represents the bottom position of the panel.
+        /// </summary>
+        Bottom,
+
+        /// <summary>
+        /// Represents the center alignment of the panel.
+        /// </summary>
+        Center,
+    }
+}
