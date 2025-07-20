59e7f17acc7a7479d7f2deacf5e2003c0c79eeee
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-07-20 19:05:03 +0300

    Update Window.cs

--- Source/Alternet.UI.Common/Controls/Window/Window.cs
+++ Source/Alternet.UI.Common/Controls/Window/Window.cs
@@ -1032,7 +1032,9 @@ namespace Alternet.UI
                 {
                     ApplyStartLocationOnce(null);
                     RaiseLoadedOnce();
-                    Refresh();
+
+                    if(App.IsLinuxOS)
+                        Refresh();
                 }
 
                 base.Visible = value;
