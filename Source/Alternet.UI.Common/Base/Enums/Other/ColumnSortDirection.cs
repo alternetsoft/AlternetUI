f276bee4eb5e7489e9b36f15cf584c327677cc7f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-03 14:53:14 +0300

    Create ColumnSortDirection.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Other/ColumnSortDirection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the direction in which a column is sorted.
+    /// </summary>
+    public enum ColumnSortDirection
+    {
+        /// <summary>
+        /// The column is sorted in ascending order.
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// The column is sorted in descending order.
+        /// </summary>
+        Descending,
+
+        /// <summary>
+        /// The sort direction is toggled or flipped.
+        /// </summary>
+        Flip,
+    }
+}
