98d9b903be5bf9fe06f86fec0c72cdc8620db908
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-03 14:53:11 +0300

    Create FileListBoxColumn.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/FileListBoxColumn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the columns that can be displayed in a file list box.
+    /// </summary>
+    public enum FileListBoxColumn
+    {
+        /// <summary>
+        /// The file name column.
+        /// </summary>
+        Name,
+
+        /// <summary>
+        /// The date and time the file was last modified.
+        /// </summary>
+        DateModified,
+
+        /// <summary>
+        /// The file size column.
+        /// </summary>
+        Size,
+    }
+}
