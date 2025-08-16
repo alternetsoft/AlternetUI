decb4662516e30ac8be111041895a484803c4c94
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-16 11:22:34 +0300

    Organize enums

---
+++ Source/Alternet.UI.Common/Base/Enums/ToolTip/OverlayToolTipFlags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines a set of flags that specify options or behaviors for the tooltip overlay.
+    /// </summary>
+    /// <remarks>This enumeration supports bitwise combination of its member
+    /// values using the bitwise OR operator. Use the <see cref="None"/> value
+    /// to represent the absence of any options.</remarks>
+    [Flags]
+    public enum OverlayToolTipFlags
+    {
+        /// <summary>
+        /// Represents the absence of any specific value or option.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Specifies that the overlay should be automatically dismissed
+        /// after a predefined time interval.
+        /// </summary>
+        DismissAfterInterval = 1 << 0,
+
+        /// <summary>
+        /// Specifies that the system colors should be used for rendering the overlay.
+        /// </summary>
+        UseSystemColors = 1 << 1,
+    }
+}
