1ce3dca6922c3be3ad14cf05f9092c970eabc13a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-16 11:23:12 +0300

    Create IAssignable{T}.cs

---
+++ Source/Alternet.UI.Common/Interfaces/Base/IAssignable{T}.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines a method to assign values from another instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to assign from.</typeparam>
+    public interface IAssignable<T>
+    {
+        /// <summary>
+        /// Assigns values from the specified source instance to the current instance.
+        /// </summary>
+        /// <param name="source">The source instance to assign values from.</param>
+        void AssignFrom(T source);
+    }
+}
