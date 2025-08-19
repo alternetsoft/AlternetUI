77ac435cf139e026d3b10afb57919d04bd73174a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-19 10:50:42 +0300

    Create ControlOverlayFlags.cs

---
+++ Source/Alternet.UI.Common/Controls/ControlOverlay/ControlOverlayFlags.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies flags that define the behavior or appearance of a control overlay.
+    /// </summary>
+    /// <remarks>This enumeration is marked with the <see cref="FlagsAttribute"/>,
+    /// allowing  bitwise combination of its member values to represent
+    /// multiple overlay options.</remarks>
+    [Flags]
+    public enum ControlOverlayFlags
+    {
+        /// <summary>
+        /// Represents the absence of any value or state.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Indicates that the overlay should be removed when the 'Escape' key is pressed.
+        /// </summary>
+        RemoveOnEscape = 1 << 0,
+
+        /// <summary>
+        /// Indicates that the overlay should be removed when the 'Enter' key is pressed.
+        /// </summary>
+        RemoveOnEnter = 1 << 1,
+    }
+}
