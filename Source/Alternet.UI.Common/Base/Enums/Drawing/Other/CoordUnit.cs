5d5a3e16b96786625c70a70fc3562f211235cc96
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-20 18:28:58 +0300

    Create CoordUnit.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Other/CoordUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Specifies the unit of measurement for coordinates.
+    /// </summary>
+    public enum CoordUnit
+    {
+        /// <summary>
+        /// The coordinate is measured in physical pixels.
+        /// </summary>
+        Pixel,
+
+        /// <summary>
+        /// The coordinate is measured in device-independent pixels.
+        /// </summary>
+        Dip,
+
+        /// <summary>
+        /// The coordinate is specified as a percentage.
+        /// </summary>
+        Percent,
+    }
+}
