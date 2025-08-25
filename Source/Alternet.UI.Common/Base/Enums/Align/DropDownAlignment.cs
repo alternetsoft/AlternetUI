aad4f8f05000456b53cdd1f5761077ca24529c41
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-25 12:38:19 +0300

    Create DropDownAlignment.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Align/DropDownAlignment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the alignment of a drop-down relative to a reference element.
+    /// </summary>
+    public enum DropDownAlignment
+    {
+        /// <summary>
+        /// Aligns the drop-down before the start edge of the reference element.
+        /// </summary>
+        BeforeStart,
+
+        /// <summary>
+        /// Aligns the drop-down after the start edge of the reference element.
+        /// </summary>
+        AfterStart,
+
+        /// <summary>
+        /// Aligns the drop-down before the end edge of the reference element.
+        /// </summary>
+        BeforeEnd,
+
+        /// <summary>
+        /// Aligns the drop-down after the end edge of the reference element.
+        /// </summary>
+        AfterEnd,
+
+        /// <summary>
+        /// Aligns the drop-down centered relative to the reference element.
+        /// </summary>
+        Center,
+    }
+}
