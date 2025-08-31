aa939b920e554cb6b0838778fd5fdfe280e14bd4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-08-31 11:28:19 +0300

    Refactor Menu to use ItemContainerElement base class
    Moved common item container logic from Menu to new generic ItemContainerElement<T> base class. Updated Menu to inherit from ItemContainerElement<MenuItem> and removed redundant code for item management, improving code reuse and maintainability.

---
+++ Source/Alternet.UI.Common/Base/BaseElement/ItemContainerElement.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a container element that manages a collection of items of
+    /// type <typeparamref name="T"/>.
+    /// </summary>
+    /// <remarks>This class provides functionality to manage a collection of items,
+    /// including querying, observing, and accessing child elements.
+    /// It supports operations such as retrieving items by tag and exposing
+    /// logical and content elements for the hosted framework.</remarks>
+    /// <typeparam name="T">The type of items contained in the collection.
+    /// Must derive from <see cref="FrameworkElement"/>.</typeparam>
+    public partial class ItemContainerElement<T>
+        : HostedFrameworkElement, ICollectionObserver<object>
+        where T : FrameworkElement
+    {
+        private BaseCollection<T>? items;
+
+        /// <summary>
+        /// Gets a value indicating whether the collection contains any items.
+        /// </summary>
+        [Browsable(false)]
+        public bool HasItems
+        {
+            get
+            {
+                return items != null && items.Count > 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether items have been allocated.
+        /// </summary>
+        [Browsable(false)]
+        public bool ItemsAllocated
+        {
+            get
+            {
+                return items != null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of items in the current instance.
+        /// </summary>
+        [Content]
+        public virtual BaseCollection<T> Items
+        {
+            get
+            {
+                if (items == null)
+                {
+                    items = new() { ThrowOnNullAdd = true };
+                }
+
+                return items;
+            }
+        }
+
+        /// <summary>
+        /// Gets the total number of child items currently in the collection.
+        /// </summary>
+        [Browsable(false)]
+        int ICollectionObserver<object>.Count
+        {
+            get
+            {
+                if (HasItems)
+                {
+                    return Items.Count;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public override IReadOnlyList<FrameworkElement> ContentElements
+        {
+            get
+            {
+                if (items == null)
+                    return Array.Empty<FrameworkElement>();
+                return items;
+            }
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<FrameworkElement> LogicalChildrenCollection => ContentElements;
+
+        INotifyCollectionChanged ICollectionObserver<object>.Notification
+        {
+            get => Items;
+        }
+
+        object ICollectionObserver<object>.GetItem(int index)
+        {
+            return Items[index];
+        }
+
+        /// <summary>
+        /// Searches for an item whose <see cref="BaseObjectWithAttr.Tag"/>
+        /// matches the specified value.
+        /// </summary>
+        /// <param name="value">The object to compare with each item's <c>Tag</c> property.</param>
+        /// <returns>
+        /// The first item with a matching <c>Tag</c>, or <c>null</c> if
+        /// no match is found or <paramref name="value"/> is <c>null</c>.
+        /// </returns>
+        /// <remarks>
+        /// Useful for retrieving an item based on externally assigned metadata or identifiers.
+        /// </remarks>
+        public virtual T? FindItemWithTag(object? value)
+        {
+            if (!HasItems)
+                return null;
+            if (value is null)
+                return null;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                var item = Items[i];
+
+                if (value.Equals(item.Tag))
+                    return item;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Adds the specified item to the collection of items.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        /// <returns>value of <paramref name="item"/> parameter to allow method chaining.</returns>
+        public virtual T Add(T item)
+        {
+            Items.Add(item);
+            return item;
+        }
+    }
+}
--- Source/Alternet.UI.Common/Controls/Menu/Menu.cs
+++ Source/Alternet.UI.Common/Controls/Menu/Menu.cs
@@ -10,10 +10,8 @@ namespace Alternet.UI
     /// <summary>
     /// Represents the base functionality for all menus.
     /// </summary>
-    public abstract partial class Menu : HostedFrameworkElement, IMenuProperties
+    public abstract partial class Menu : ItemContainerElement<MenuItem>, IMenuProperties
     {
-        private BaseCollection<MenuItem>? items;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Menu"/> class.
         /// </summary>
@@ -22,78 +20,7 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Indicates whether this <see cref="MenuItem"/> contains any child items.
-        /// </summary>
-        /// <remarks>
-        /// Returns <c>true</c> if the internal <c>Items</c> collection has been initialized;
-        /// otherwise, <c>false</c>.
-        /// </remarks>
-        [Browsable(false)]
-        public bool HasItems
-        {
-            get
-            {
-                return items != null;
-            }
-        }
-
-        /// <summary>
-        /// Gets a collection of <see cref="MenuItem"/> objects associated with the menu.
-        /// </summary>
-        [Content]
-        public virtual BaseCollection<MenuItem> Items
-        {
-            get
-            {
-                if(items == null)
-                {
-                    items = new() { ThrowOnNullAdd = true };
-                }
-
-                return items;
-            }
-        }
-
-        /// <summary>
-        /// Gets the total number of child items currently in the collection.
-        /// </summary>
-        [Browsable(false)]
-        int ICollectionObserver<object>.Count
-        {
-            get
-            {
-                if (HasItems)
-                {
-                    return Items.Count;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
-        /// <inheritdoc/>
-        public override IReadOnlyList<FrameworkElement> ContentElements
-        {
-            get
-            {
-                if (items == null)
-                    return Array.Empty<FrameworkElement>();
-                return items;
-            }
-        }
-
-        /// <inheritdoc />
-        public override IEnumerable<FrameworkElement> LogicalChildrenCollection => ContentElements;
-
-        INotifyCollectionChanged ICollectionObserver<object>.Notification
-        {
-            get => Items;
-        }
-
-        /// <summary>
-        /// Performs some action for the each element in <see cref="Items"/>.
+        /// Performs some action for the each element in <see cref="ItemContainerElement{T}.Items"/>.
         /// </summary>
         /// <param name="action">Specifies action which will be called for the
         /// each item.</param>
@@ -101,7 +28,7 @@ namespace Alternet.UI
         /// sub-items recursively or only for the direct sub-items.</param>
         public virtual void ForEachItem(Action<MenuItem> action, bool recursive = false)
         {
-            if (items is null)
+            if (!HasItems)
                 return;
             foreach (var child in Items)
             {
@@ -111,47 +38,13 @@ namespace Alternet.UI
             }
         }
 
-        object ICollectionObserver<object>.GetItem(int index)
-        {
-            return Items[index];
-        }
-
-        /// <summary>
-        /// Searches the menu for an item whose <see cref="BaseObjectWithAttr.Tag"/>
-        /// matches the specified value.
-        /// </summary>
-        /// <param name="value">The object to compare with each item's <c>Tag</c> property.</param>
-        /// <returns>
-        /// The first <see cref="MenuItem"/> with a matching <c>Tag</c>, or <c>null</c> if
-        /// no match is found or <paramref name="value"/> is <c>null</c>.
-        /// </returns>
-        /// <remarks>
-        /// Useful for retrieving a menu item based on externally assigned metadata or identifiers.
-        /// </remarks>
-        public virtual MenuItem? FindItemWithTag(object? value)
-        {
-            if (items is null)
-                return null;
-            if (value is null)
-                return null;
-
-            for (int i = 0; i < Items.Count; i++)
-            {
-                var item = Items[i];
-
-                if (value.Equals(item.Tag))
-                    return item;
-            }
-
-            return null;
-        }
-
         /// <summary>
         /// Checks the menu item whose <c>Tag</c> matches the specified value, and unchecks all others.
         /// </summary>
         /// <param name="value">The value to search for in each item's <c>Tag</c>.</param>
         /// <remarks>
-        /// Uses <see cref="FindItemWithTag(object?)"/> to locate the target item, then
+        /// Uses <see cref="ItemContainerElement{T}.FindItemWithTag(object?)"/>
+        /// to locate the target item, then
         /// delegates to <see cref="CheckSingleItem(MenuItem?)"/>.
         /// Ensures exclusive checked state among sibling items.
         /// </remarks>
@@ -181,14 +74,14 @@ namespace Alternet.UI
         /// Unchecks all child menu items by setting their <c>Checked</c> property to <c>false</c>.
         /// </summary>
         /// <remarks>
-        /// Iterates through the <see cref="Items"/> collection and clears the checked state
+        /// Iterates through the items collection and clears the checked state
         /// of each item.
         /// If the <c>Items</c> collection is not initialized, the method exits
         /// without performing any action.
         /// </remarks>
         public virtual void UncheckItems()
         {
-            if (items is null)
+            if (!HasItems)
                 return;
             for (int i = 0; i < Items.Count; i++)
             {
@@ -199,18 +92,7 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Adds item to <see cref="Items"/>.
-        /// </summary>
-        /// <param name="item">Menu item.</param>
-        /// <returns><paramref name="item"/> parameter.</returns>
-        public virtual MenuItem Add(MenuItem item)
-        {
-            Items.Add(item);
-            return item;
-        }
-
-        /// <summary>
-        /// Adds a separator item to the <see cref="Items"/> collection.
+        /// Adds a separator item to the <see cref="ItemContainerElement{T}.Items"/> collection.
         /// </summary>
         /// <returns>The created separator item.</returns>
         public virtual MenuItem AddSeparator()
@@ -221,7 +103,7 @@ namespace Alternet.UI
         }
 
         /// <summary>
-        /// Creates and adds new item to <see cref="Items"/>.
+        /// Creates and adds new item to <see cref="ItemContainerElement{T}.Items"/> collection.
         /// </summary>
         /// <returns>Created item.</returns>
         /// <param name="title">Item title.</param>
