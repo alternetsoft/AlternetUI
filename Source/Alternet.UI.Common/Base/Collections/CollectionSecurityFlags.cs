3081af13c3bff52858b38ed878d261dac7bcdb65
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-05 13:12:59 +0300

    Create CollectionSecurityFlags.cs

---
+++ Source/Alternet.UI.Common/Base/Collections/CollectionSecurityFlags.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies security-related flags for collection operations.
+    /// </summary>
+    [Flags]
+    public enum CollectionSecurityFlags
+    {
+        /// <summary>
+        /// No security flags are set.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Prevents replacing items in the collection.
+        /// </summary>
+        NoReplace = 1 << 0,
+
+        /// <summary>
+        /// Prevents adding <c>null</c> items to the collection.
+        /// </summary>
+        NoNull = 1 << 1,
+
+        /// <summary>
+        /// Specifies a combination of flags that enforce non-null values
+        /// and prevent replacement of existing values.
+        /// </summary>
+        /// <remarks>This enumeration value is a bitwise combination of the <see cref="NoNull"/>
+        /// and <see cref="NoReplace"/> flags.
+        /// It is used to ensure that values are neither null nor replaced during
+        /// operations.</remarks>
+        NoNullOrReplace = NoNull | NoReplace,
+    }
+}
