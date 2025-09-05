80282da8d2f4abebfc42e2025c9a975b74b5c567
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-05 18:24:07 +0300

    Create MenuItemType.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Other/MenuItemType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the type of a menu item.
+    /// </summary>
+    public enum MenuItemType
+    {
+        /// <summary>
+        /// A standard menu item.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        /// A menu item that can be checked or unchecked.
+        /// </summary>
+        Check,
+
+        /// <summary>
+        /// A menu item that behaves as a radio button within a group.
+        /// </summary>
+        Radio,
+
+        /// <summary>
+        /// A menu item that acts as a separator between other items.
+        /// </summary>
+        Separator,
+    }
+}
