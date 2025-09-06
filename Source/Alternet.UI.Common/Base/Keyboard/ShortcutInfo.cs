35d2e43e9334c4b67da25d698b5396402904b751
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-06 09:00:59 +0300

    Add formatting options for ShortcutInfo string output
    Introduces the FormatOptions struct and a new ToString overload in ShortcutInfo to support customizable string representations, including template-based formatting and user-specific preferences.

--- Source/Alternet.UI.Common/Base/Keyboard/ShortcutInfo.cs
+++ Source/Alternet.UI.Common/Base/Keyboard/ShortcutInfo.cs
@@ -200,5 +200,69 @@ namespace Alternet.UI
 
             return shortcut;
         }
+
+        /// <summary>
+        /// Converts the object to its string representation based on the specified formatting options.
+        /// </summary>
+        /// <remarks>The method applies the specified formatting options to the first valid key in the
+        /// filtered key information. If a template is specified in the <paramref name="options"/>,
+        /// it is used to format the output string.</remarks>
+        /// <param name="options">The formatting options to apply when generating the string representation.
+        /// This includes settings such as
+        /// whether to use a template and user-specific formatting preferences.</param>
+        /// <returns>A string representation of the object based on the provided <paramref name="options"/>,
+        /// or <see langword="null"/> if no valid key information is available.</returns>
+        public virtual string? ToString(FormatOptions options)
+        {
+            var filteredKeys = Alternet.UI.KeyInfo.FilterBackendOs(KeyInfo);
+            if (filteredKeys is not null && filteredKeys.Length > 0)
+            {
+                var key = filteredKeys[0];
+
+                if (key is null)
+                    return null;
+
+                if (options.UseTemplate)
+                {
+                    var result = string.Format(options.Template, key.ToString(options.ForUser));
+                    return result;
+                }
+                else
+                {
+                    return key.ToString(options.ForUser);
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Specifies formatting options for converting shortcut information to a string.
+        /// </summary>
+        public struct FormatOptions
+        {
+            /// <summary>
+            /// Gets or sets a value indicating whether to use a template for formatting.
+            /// </summary>
+            public bool UseTemplate { get; set; }
+
+            /// <summary>
+            /// Gets or sets a value indicating whether the string representation should be formatted for user display.
+            /// </summary>
+            public bool ForUser { get; set; }
+
+            /// <summary>
+            /// Gets or sets the template string used for formatting, if <see cref="UseTemplate"/> is <c>true</c>.
+            /// </summary>
+            public string? Template { get; set; }
+
+            /// <summary>
+            /// Gets or sets an object that contains data associated with this instance.
+            /// </summary>
+            /// <remarks>This property can be used to store additional information related to the
+            /// instance, such as metadata or user-defined data. The type is <see cref="object"/>,
+            /// allowing any type of data to be assigned.</remarks>
+            public object? Tag { get; set; }
+        }
     }
 }
