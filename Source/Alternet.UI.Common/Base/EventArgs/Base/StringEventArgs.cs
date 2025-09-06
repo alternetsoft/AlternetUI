d03bad3973a29c43911afa04815364889e3878c8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-06 09:00:45 +0300

    Add StringEventArgs for string event data
    Introduces StringEventArgs, a specialized event argument class for passing string values in event-driven scenarios. This class inherits from BaseEventArgs<string> and provides a convenient way to encapsulate string event data.

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Base/StringEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for events that include a string value as event data.
+    /// </summary>
+    /// <remarks>This class is a specialized implementation of <see cref="BaseEventArgs{T}"/> where the event
+    /// data is of type <see cref="string"/>. It can be used to pass string-based information in event-driven
+    /// programming scenarios.</remarks>
+    public class StringEventArgs : BaseEventArgs<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringEventArgs"/> class with the specified string value.
+        /// </summary>
+        /// <param name="value">The string value associated with the event.</param>
+        public StringEventArgs(string value)
+            : base(value)
+        {
+        }
+    }
+}
