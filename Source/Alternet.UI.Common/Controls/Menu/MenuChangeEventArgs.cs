9af177257de30c7a2d354614469d2462bcbccc53
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-07 11:37:17 +0300

    Create MenuChangeEventArgs.cs

---
+++ Source/Alternet.UI.Common/Controls/Menu/MenuChangeEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for menu change events, indicating which menu item was changed and the type of change.
+    /// </summary>
+    public class MenuChangeEventArgs : BaseEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuChangeEventArgs"/> class.
+        /// </summary>
+        /// <param name="item">The menu item that was changed.</param>
+        /// <param name="action">The type of change that occurred.</param>
+        public MenuChangeEventArgs(Menu item, MenuChangeKind action)
+        {
+            Item = item;
+            Action = action;
+        }
+
+        /// <summary>
+        /// Gets or sets the menu item that was changed.
+        /// </summary>
+        public Menu Item { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of change that occurred.
+        /// </summary>
+        public MenuChangeKind Action { get; set; }
+    }
+}
