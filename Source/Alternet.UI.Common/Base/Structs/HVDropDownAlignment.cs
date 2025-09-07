20b5c40f1115a75bb8b14169d23b92aadcc6f495
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-07 12:34:41 +0300

    Add position-based alignment to HVDropDownAlignment
    Introduced constructors and properties to support dropdown alignment by a specific position using PointD. Added logic to determine if position-based alignment is used and whether the position coordinates are negative.

--- Source/Alternet.UI.Common/Base/Structs/HVDropDownAlignment.cs
+++ Source/Alternet.UI.Common/Base/Structs/HVDropDownAlignment.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Alternet.Drawing;
+
 namespace Alternet.UI
 {
     /// <summary>
@@ -30,11 +32,84 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Represents the alignment settings for a dropdown positioned at a specific point.
+        /// </summary>
+        /// <param name="point">The position of the dropdown as a <see cref="PointD"/>.</param>
+        public HVDropDownAlignment(PointD point)
+        {
+            Horizontal = DropDownAlignment.Position;
+            Vertical = DropDownAlignment.Position;
+            Position = point;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HVDropDownAlignment"/> class with
+        /// the specified horizontal and vertical alignment, and position.
+        /// </summary>
+        /// <param name="horizontal">The horizontal alignment of the dropdown.</param>
+        /// <param name="vertical">The vertical alignment of the dropdown.</param>
+        /// <param name="point">The position of the dropdown as a <see cref="PointD"/>.</param>
+        public HVDropDownAlignment(
+            DropDownAlignment horizontal,
+            DropDownAlignment vertical,
+            PointD point)
+        {
+            Horizontal = horizontal;
+            Vertical = vertical;
+            Position = point;
+        }
+
+        /// <summary>
         /// Gets the default drop-down alignment.
         /// </summary>
         public static HVDropDownAlignment Default => new(
             DropDownAlignment.AfterStart,
             DropDownAlignment.AfterEnd);
+
+        /// <summary>
+        /// Gets a value indicating whether the horizontal alignment
+        /// is set to <see cref="DropDownAlignment.Position"/>.
+        /// </summary>
+        public bool IsHorzPositionUsed => Horizontal == DropDownAlignment.Position;
+
+        /// <summary>
+        /// Gets a value indicating whether the vertical alignment
+        /// is set to <see cref="DropDownAlignment.Position"/>.
+        /// </summary>
+        public bool IsVertPositionUsed => Vertical == DropDownAlignment.Position;
+
+        /// <summary>
+        /// Gets a value indicating whether either the horizontal or vertical
+        /// alignment is set to <see cref="DropDownAlignment.Position"/>.
+        /// </summary>
+        public bool IsPositionUsed => IsHorzPositionUsed || IsVertPositionUsed;
+
+        /// <summary>
+        /// Gets a value indicating whether the vertical position is negative.
+        /// Returns <c>true</c> if <see cref="IsVertPositionUsed"/> is <c>true</c> and
+        /// <see cref="Position"/> has a negative Y coordinate.
+        /// </summary>
+        public bool IsVertPositionNegative => IsVertPositionUsed && Position.Y < 0;
+
+        /// <summary>
+        /// Gets a value indicating whether the horizontal position is negative.
+        /// Returns <c>true</c> if <see cref="IsHorzPositionUsed"/> is <c>true</c>
+        /// and <see cref="Position"/> has a negative X coordinate.
+        /// </summary>
+        public bool IsHorzPositionNegative => IsHorzPositionUsed && Position.X < 0;
+
+        /// <summary>
+        /// Gets a value indicating whether either the horizontal or vertical position is negative.
+        /// Returns <c>true</c> if <see cref="IsPositionUsed"/> is <c>true</c> and <see cref="Position"/>
+        /// has a negative X or Y coordinate.
+        /// </summary>
+        public bool IsPositionNegative => IsPositionUsed && (Position.X < 0 || Position.Y < 0);
+
+        /// <summary>
+        /// Gets the position of the object in a two-dimensional space.
+        /// It is used when Horizontal or Vertical alignment is set to <see cref="DropDownAlignment.Position"/>.
+        /// </summary>
+        public PointD Position { get; }
 
         /// <summary>
         /// Gets the horizontal drop-down alignment.
