2a6893407e047568a734aa718573d1fe0ce6cdaa
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-11 16:04:55 +0300

    Add phantom window utility methods to FormUtils
    Introduces GetPhantomWindow and CreatePhantomWindow methods to provide a hidden, borderless window positioned off-screen. This is useful for scenarios requiring an invisible window for background processing or interoperation purposes.

--- Source/Alternet.UI.Common/Utils/ControlUtils/FormUtils.cs
+++ Source/Alternet.UI.Common/Utils/ControlUtils/FormUtils.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Alternet.Drawing;
+
 namespace Alternet.UI
 {
     /// <summary>
@@ -10,8 +12,57 @@ namespace Alternet.UI
     /// </summary>
     public static class FormUtils
     {
+        private static WeakReferenceValue<Window> phantomWindow = new();
+
         static FormUtils()
         {
+        }
+
+        /// <summary>
+        /// Gets a hidden, borderless window positioned off-screen.
+        /// If such a window is not already created, it creates one.
+        /// </summary>
+        /// <remarks>This phantom window is not shown in the taskbar and is intended for scenarios where
+        /// an invisible or off-screen window is required, such as for background processing or interoperation
+        /// purposes.</remarks>
+        /// <returns>A <see cref="Window"/> instance configured with minimal dimensions, no border,
+        /// and positioned outside the visible screen area.</returns>
+        public static Window GetPhantomWindow()
+        {
+            if(phantomWindow.Value == null)
+                phantomWindow.Value = CreatePhantomWindow();
+            return phantomWindow.Value;
+        }
+
+        /// <summary>
+        /// Creates a hidden, borderless window positioned off-screen.
+        /// </summary>
+        /// <remarks>This method creates a window with minimal dimensions and ensures it does not appear
+        /// in the taskbar. The window is positioned off-screen and is intended for scenarios
+        /// where a window handle is
+        /// required without displaying the window to the user.
+        /// Use <see cref="GetPhantomWindow"/> to retrieve a singleton instance of such a window.
+        /// </remarks>
+        /// <returns>A <see cref="Window"/> instance configured as a hidden, off-screen window.</returns>
+        public static Window CreatePhantomWindow()
+        {
+            var window = new Window
+            {
+                Width = 1,
+                Height = 1,
+                ShowInTaskbar = false,
+                HasBorder = false,
+                StartLocation = WindowStartLocation.Manual,
+                Location = new PointD(-32000, -32000),
+            };
+
+            window.HandleNeeded();
+
+#pragma warning disable
+            var a = window.Handler.GetHandle();
+#pragma warning restore
+
+            return window;
         }
 
         /// <summary>
