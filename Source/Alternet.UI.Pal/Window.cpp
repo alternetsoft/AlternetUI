6adb6d78416701c7cecfeb753aceb67a24856a9b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-12 11:08:55 +0300

    Update Window.cpp

--- Source/Alternet.UI.Pal/Window.cpp
+++ Source/Alternet.UI.Pal/Window.cpp
@@ -129,10 +129,6 @@ namespace Alternet::UI
     void Window::OnActivate(wxActivateEvent& event)
     {
         Control::OnActivate(event);
-        /*
-        if (!HasEnabledChilds())
-            event.Skip(false);
-            */
     }
 
     void Window::OnBeforeDestroyWxWindow()
