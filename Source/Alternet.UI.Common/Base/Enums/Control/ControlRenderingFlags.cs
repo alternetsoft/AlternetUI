623723b36d194363f99ee4373935e9cad3f20865
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-12 11:07:40 +0300

    Create ControlRenderingFlags.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/ControlRenderingFlags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies rendering options for a control.
+    /// </summary>
+    [Flags]
+    public enum ControlRenderingFlags
+    {
+        /// <summary>
+        /// No rendering options are set.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Use SkiaSharp for rendering.
+        /// </summary>
+        UseSkiaSharp = 1 << 0,
+
+        /// <summary>
+        /// Use OpenGL for rendering.
+        /// </summary>
+        UseOpenGL = 1 << 1,
+    }
+}
