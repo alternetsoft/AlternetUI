a1d00d724540fe9a6313cc7487a55dcec8a54ed5
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-13 07:18:33 +0300

    Create MenuItemRoleType.cs

---
+++ Source/Alternet.UI.Common/Controls/Menu/Role/MenuItemRoleType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the role type for a menu item.
+    /// </summary>
+    public enum MenuItemRoleType
+    {
+        /// <summary>
+        /// No specific role assigned.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Role is determined automatically.
+        /// </summary>
+        Auto,
+
+        /// <summary>
+        /// Represents an "About" menu item.
+        /// </summary>
+        About,
+
+        /// <summary>
+        /// Represents an "Exit" menu item.
+        /// </summary>
+        Exit,
+
+        /// <summary>
+        /// Represents a "Preferences" menu item.
+        /// </summary>
+        Preferences,
+
+        /// <summary>
+        /// Represents any other menu item role which is not explicitly defined.
+        /// </summary>
+        Other,
+    }
+
+}
