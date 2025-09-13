1b173223212a2c902dbd98c825159f60db138356
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-13 08:33:44 +0300

    Add index support to MenuChangeEventArgs
    Introduced an optional Index property and a new constructor to MenuChangeEventArgs to specify the menu item's position in change events. This allows tracking the index for insert and remove actions, improving event context.

--- Source/Alternet.UI.Common/Controls/Menu/MenuChangeEventArgs.cs
+++ Source/Alternet.UI.Common/Controls/Menu/MenuChangeEventArgs.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace Alternet.UI
 {
     /// <summary>
-    /// Provides data for menu change events, indicating which menu item was changed and the type of change.
+    /// Provides data for menu change events, indicating which menu
+    /// item was changed and the type of change.
     /// </summary>
     public class MenuChangeEventArgs : BaseEventArgs
     {
@@ -21,9 +22,34 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="MenuChangeEventArgs"/>
+        /// class with the specified menu item, action, and index.
+        /// </summary>
+        /// <remarks>This constructor allows specifying the index of the menu item in
+        /// addition to the item and the action.</remarks>
+        /// <param name="item">The <see cref="Menu"/> item associated with the event.</param>
+        /// <param name="action">The kind of change that occurred, represented by
+        /// a <see cref="MenuChangeKind"/> value.</param>
+        /// <param name="index">The zero-based index of the menu item in the collection.
+        /// Must be greater than or equal to 0.</param>
+        public MenuChangeEventArgs(Menu item, MenuChangeKind action, int index)
+            : this(item, action)
+        {
+            Index = index;
+        }
+
+        /// <summary>
         /// Gets or sets the menu item that was changed.
         /// </summary>
         public Menu Item { get; set; }
+
+        /// <summary>
+        /// Gets or sets the zero-based index of the item. This is optional and may be null.
+        /// <see cref="Index"/> is typically used to indicate the position where item was inserted
+        /// or removed in the case of <see cref="MenuChangeKind.ItemInserted"/> or
+        /// <see cref="MenuChangeKind.ItemRemoved"/>.
+        /// </summary>
+        public int? Index { get; set; }
 
         /// <summary>
         /// Gets or sets the type of change that occurred.
