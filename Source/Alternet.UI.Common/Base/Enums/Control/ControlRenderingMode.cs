349e4015b5dc9918a435b6290a5c58ce2c81c5f6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-16 17:49:40 +0300

    Create ControlRenderingMode.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/ControlRenderingMode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the rendering mode for the control.
+    /// </summary>
+    public enum ControlRenderingMode
+    {
+        /// <summary>
+        /// Control is rendered using software mode with clipping.
+        /// </summary>
+        SoftwareClipped,
+
+        /// <summary>
+        /// Control is rendered using software mode with double buffering.
+        /// </summary>
+        SoftwareDoubleBuffered,
+
+        /// <summary>
+        /// Control is rendered using SkiaSharp.
+        /// </summary>
+        SkiaSharp,
+
+        /// <summary>
+        /// Control is rendered using SkiaSharp with OpenGL acceleration.
+        /// </summary>
+        SkiaSharpWithOpenGL,
+    }
+}
