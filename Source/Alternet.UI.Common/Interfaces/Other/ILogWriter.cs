60355aed49a9055b33f1fd75fb55fad7b6ddafb0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-17 11:14:57 +0300

    Create ILogWriter.cs

---
+++ Source/Alternet.UI.Common/Interfaces/Other/ILogWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines methods for logging with indentation and other features.
+    /// </summary>
+    public interface ILogWriter
+    {
+        /// <summary>
+        /// Increases the indentation level for subsequent log messages.
+        /// </summary>
+        void Indent();
+
+        /// <summary>
+        /// Decreases the indentation level for subsequent log messages.
+        /// </summary>
+        void Unindent();
+
+        /// <summary>
+        /// Writes a message to the log, followed by a line terminator.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        void WriteLine(string message);
+    }
+}
