98891176c09a96afaf10ea504a3f48d9221b6109
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-18 13:20:05 +0300

    Create IMacOsApplicationHandler.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/Application/IMacOsApplicationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines methods and properties for handling macOS-specific application functionality.
+    /// </summary>
+    public interface IMacOsApplicationHandler : IApplicationHandler
+    {
+        /// <summary>
+        /// Sets the common menu bar for the application on macOS.
+        /// </summary>
+        /// <param name="menuBar">
+        /// The <see cref="MainMenu"/> to set as the common menu bar, or <c>null</c> to remove it.
+        /// </param>
+        void MacSetCommonMenuBar(MainMenu? menuBar);
+
+        /// <summary>
+        /// Gets the title name of the Help menu for macOS.
+        /// </summary>
+        string? HelpMenuTitleName { get; }
+
+        /// <summary>
+        /// Gets the title name of the Window menu for macOS.
+        /// </summary>
+        string? WindowMenuTitleName { get; }
+    }
+}
