a3491f48019c2ee47b2d284efaa8d8373ac40e5e
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-19 12:12:55 +0300

    Create GraphicsBackendType.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Other/GraphicsBackendType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Specifies the graphics backend used for rendering.
+    /// </summary>
+    public enum GraphicsBackendType
+    {
+        /// <summary>
+        /// Uses the SkiaSharp graphics backend.
+        /// </summary>
+        SkiaSharp,
+
+        /// <summary>
+        /// Uses the WxWidgets graphics backend.
+        /// </summary>
+        WxWidgets,
+
+        /// <summary>
+        /// Represents a null graphics backend which does not perform any rendering.
+        /// </summary>
+        Null,
+
+        /// <summary>
+        /// Other or unknown graphics backend.
+        /// </summary>
+        Other,
+    }
+}
