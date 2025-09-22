37a970478ae2a0c926c51ca984cd10c63763a1e3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-22 09:03:51 +0300

    Refactor LengthConverter for improved clarity and API
    Reorganized LengthConverter by making unit strings and factors static readonly, improving XML documentation, and updating method signatures for nullability and clarity. Moved FromString and ToString methods to public static, removed redundant internal methods, and streamlined conversion logic for better maintainability and usability.

--- Source/Alternet.UI.Common/Base/TypeConverters/LengthConverter.cs
+++ Source/Alternet.UI.Common/Base/TypeConverters/LengthConverter.cs
@@ -1,32 +1,133 @@
-#pragma warning disable
-#nullable disable
-
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
 using System.Globalization;
 using System.Reflection;
 
+using Alternet.Drawing;
+
 namespace Alternet.UI
 {
     /// <summary>
     /// Converter class for converting instances of other types to and from coordinate representing length.
-    /// </summary> 
+    /// </summary>
     public class LengthConverter : BaseTypeConverter
     {
-        #region Public Methods
+        // This array contains strings for unit types
+        // These are effectively "TypeConverter only" units.
+        // They are all expressed in terms of the Pixel unit type and a conversion factor.
+        private static readonly string[] pixelUnitStrings =
+        {
+            "px",
+            "in",
+            "cm",
+            "pt",
+        };
+
+        private static readonly Coord[] pixelUnitFactors =
+        {
+            // Pixel itself
+            CoordD.One,
+
+            // Pixels per Inch
+            CoordD.Coord96,
+
+            // Pixels per Centimeter
+            CoordD.Coord96 / CoordD.Coord2And54,
+
+            // Pixels per Point
+            CoordD.Coord96 / CoordD.Coord72,
+        };
 
         /// <summary>
-        /// CanConvertFrom - Returns whether or not this class can convert from a given type.
+        /// Converts the specified coordinate to its string representation using
+        /// the specified culture-specific formatting.
         /// </summary>
-        /// <returns>
-        /// bool - True if this converter can convert from the provided type, false if not.
-        /// </returns>
-        /// <param name="typeDescriptorContext"> The ITypeDescriptorContext for this call. </param>
-        /// <param name="sourceType"> The Type being queried for support. </param>
+        /// <param name="l">The coordinate to convert. If the value is not a number (NaN),
+        /// the method returns "Auto".</param>
+        /// <param name="cultureInfo">The culture-specific formatting information to use for
+        /// the conversion.</param>
+        /// <returns>A string representation of the coordinate formatted using the specified
+        /// culture, or "Auto" if the coordinate
+        /// is not a number (NaN).</returns>
+        public static string ToString(Coord l, CultureInfo cultureInfo)
+        {
+            if (MathUtils.IsNaN(l))
+                return "Auto";
+            return Convert.ToString(l, cultureInfo);
+        }
+
+        /// <summary>
+        /// Converts a string representation of a coordinate value, including an optional unit,
+        /// into a <see cref="Coord"/> object.
+        /// </summary>
+        /// <remarks>This method is culture-sensitive for parsing the numeric value but uses a fixed,
+        /// culture-invariant set of unit strings. Supported unit strings include "px" (pixels)
+        /// and other predefined units. If the string does not end with a recognized unit,
+        /// it is assumed to represent pixels.</remarks>
+        /// <param name="s">The string to parse. The string should be in the format
+        /// "[value][unit]", where <c>[value]</c> is a numeric
+        /// value and <c>[unit]</c> is an optional unit specifier (e.g., "px", "in").
+        /// If no unit is specified, the value
+        /// is interpreted as pixels. The special value "auto" is interpreted
+        /// as <see cref="Coord.NaN"/>.</param>
+        /// <param name="cultureInfo">The <see cref="CultureInfo"/> used to interpret
+        /// the numeric value in the string.</param>
+        /// <returns>A <see cref="Coord"/> object representing the parsed value.
+        /// If the string specifies "auto", <see cref="Coord.NaN"/> is returned.</returns>
+        /// <exception cref="FormatException">Thrown if the string cannot be parsed
+        /// into a valid coordinate value.</exception>
+        public static Coord FromString(string s, CultureInfo cultureInfo)
+        {
+            /* NOTE - This code is called from FontSizeConverter, so changes will affect both. */
+
+            string valueString = s.Trim();
+            string goodString = valueString.ToLowerInvariant();
+            int strLen = goodString.Length;
+            int strLenUnit = 0;
+            Coord unitFactor = CoordD.One;
+
+            // Auto is represented and Coord.NaN
+            // properties that do not want Auto and NaN to be in their legit values,
+            // should disallow NaN in validation callbacks (same goes for negative values)
+            if (goodString == "auto") return Coord.NaN;
+
+            for (int i = 0; i < pixelUnitStrings.Length; i++)
+            {
+                // NOTE: This is NOT a culture specific comparison.
+                // This is by design: we want the same unit string table to work across all cultures.
+                if (goodString.EndsWith(pixelUnitStrings[i], StringComparison.Ordinal))
+                {
+                    strLenUnit = pixelUnitStrings[i].Length;
+                    unitFactor = pixelUnitFactors[i];
+                    break;
+                }
+            }
+
+            // important to substring original non-lowered string
+            // this allows case sensitive ToDouble/ToSingle below handle
+            // "NaN" and "Infinity" correctly.
+            // this addresses windows bug 1177408
+            valueString = valueString.Substring(0, strLen - strLenUnit);
+
+            // FormatException errors thrown by Convert.ToDouble are pretty uninformative.
+            // Throw a more meaningful error in this case that tells that we were attempting
+            // to create a Length instance from a string.  This addresses windows bug 968884
+            try
+            {
+                Coord result = Convert.ToSingle(valueString, cultureInfo) * unitFactor;
+                return result;
+            }
+            catch (FormatException)
+            {
+                throw new FormatException();
+            }
+        }
+
+        /// <inheritdoc/>
         public override bool CanConvertFrom(
-            ITypeDescriptorContext typeDescriptorContext,
-            Type sourceType)
+            ITypeDescriptorContext? typeDescriptorContext,
+            Type? sourceType)
         {
             // We can only handle strings, integral and floating types
             TypeCode tc = Type.GetTypeCode(sourceType);
@@ -48,19 +149,11 @@ namespace Alternet.UI
             }
         }
 
-        /// <summary>
-        /// CanConvertTo - Returns whether or not this class can convert to a given type.
-        /// </summary>
-        /// <returns>
-        /// bool - True if this converter can convert to the provided type, false if not.
-        /// </returns>
-        /// <param name="typeDescriptorContext"> The ITypeDescriptorContext for this call. </param>
-        /// <param name="destinationType"> The Type being queried for support. </param>
+        /// <inheritdoc/>
         public override bool CanConvertTo(
-            ITypeDescriptorContext typeDescriptorContext,
-            Type destinationType)
+            ITypeDescriptorContext? typeDescriptorContext,
+            Type? destinationType)
         {
-            // We can convert to an InstanceDescriptor or to a string.
             if (destinationType == typeof(InstanceDescriptor) ||
                 destinationType == typeof(string))
             {
@@ -72,66 +165,42 @@ namespace Alternet.UI
             }
         }
 
-        /// <summary>
-        /// ConvertFrom - Attempt to convert to a length from the given object
-        /// </summary>
-        /// <returns>
-        /// The value representing the size in device-independent units.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// An ArgumentNullException is thrown if the example object is null.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// An ArgumentException is thrown if the example object is not null and is not a valid type
-        /// which can be converted to a coordinate.
-        /// </exception>
-        /// <param name="typeDescriptorContext"> The ITypeDescriptorContext for this call. </param>
-        /// <param name="cultureInfo"> The CultureInfo which is respected when converting. </param>
-        /// <param name="source"> The object to convert to a coordinate. </param>
-        public override object ConvertFrom(ITypeDescriptorContext typeDescriptorContext,
-                                           CultureInfo cultureInfo,
-                                           object source)
+        /// <inheritdoc/>
+        public override object ConvertFrom(
+            ITypeDescriptorContext? typeDescriptorContext,
+            CultureInfo cultureInfo,
+            object source)
         {
             if (source != null)
             {
-                if (source is string) { return FromString((string)source, cultureInfo); }
-                else { return (Coord)(Convert.ToSingle(source, cultureInfo)); }
+                if (source is string v)
+                {
+                    return FromString(v, cultureInfo);
+                }
+                else
+                {
+                    return (Coord)Convert.ToSingle(source, cultureInfo);
+                }
             }
 
             throw GetConvertFromException(source);
         }
 
-        /// <summary>
-        /// Attempt to convert a coordinate to the given type
-        /// </summary>
-        /// <returns>
-        /// The object which was constructed.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// An ArgumentNullException is thrown if the example object is null.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// An ArgumentException is thrown if the object is not null,
-        /// or if the destinationType isn't one of the valid destination types.
-        /// </exception>
-        /// <param name="typeDescriptorContext"> The ITypeDescriptorContext for this call. </param>
-        /// <param name="cultureInfo"> The CultureInfo which is respected when converting. </param>
-        /// <param name="value"> The value to convert. </param>
-        /// <param name="destinationType">The type to which to convert the value. </param>
-        public override object ConvertTo(ITypeDescriptorContext typeDescriptorContext,
-                                         CultureInfo cultureInfo,
-                                         object value,
-                                         Type destinationType)
+        /// <inheritdoc/>
+        public override object ConvertTo(
+            ITypeDescriptorContext? typeDescriptorContext,
+            CultureInfo cultureInfo,
+            object value,
+            Type destinationType)
         {
             if (destinationType == null)
             {
                 throw new ArgumentNullException("destinationType");
             }
 
-            if (value != null
-                && value is Coord)
+            if (value != null && value is Coord v)
             {
-                Coord l = (Coord)value;
+                Coord l = v;
                 if (destinationType == typeof(string))
                 {
                     if (MathUtils.IsNaN(l))
@@ -145,82 +214,8 @@ namespace Alternet.UI
                     return new InstanceDescriptor(ci, new object[] { l });
                 }
             }
+
             throw GetConvertToException(value, destinationType);
         }
-        #endregion 
-
-        #region Internal Methods
-
-        // Parse a Length from a string given the CultureInfo.
-        // Formats: 
-        //"[value][unit]"
-        //   [value] is a coordinate
-        //   [unit] is a string specifying the unit, like 'in' or 'px', or nothing (means pixels)
-        // NOTE - This code is called from FontSizeConverter, so changes will affect both.
-        static internal Coord FromString(string s, CultureInfo cultureInfo)
-        {
-            string valueString = s.Trim();
-            string goodString = valueString.ToLowerInvariant();
-            int strLen = goodString.Length;
-            int strLenUnit = 0;
-            Coord unitFactor = 1.0;
-
-            //Auto is represented and Coord.NaN
-            //properties that do not want Auto and NaN to be in their legit values,
-            //should disallow NaN in validation callbacks (same goes for negative values)
-            if (goodString == "auto") return Coord.NaN;
-
-            for (int i = 0; i < PixelUnitStrings.Length; i++)
-            {
-                // NOTE: This is NOT a culture specific comparison.
-                // This is by design: we want the same unit string table to work across all cultures.
-                if (goodString.EndsWith(PixelUnitStrings[i], StringComparison.Ordinal))
-                {
-                    strLenUnit = PixelUnitStrings[i].Length;
-                    unitFactor = PixelUnitFactors[i];
-                    break;
-                }
-            }
-
-            //  important to substring original non-lowered string 
-            //  this allows case sensitive ToDouble/ToSingle below handle
-            //  "NaN" and "Infinity" correctly. 
-            //  this addresses windows bug 1177408
-            valueString = valueString.Substring(0, strLen - strLenUnit);
-
-            // FormatException errors thrown by Convert.ToDouble are pretty uninformative.
-            // Throw a more meaningful error in this case that tells that we were attempting
-            // to create a Length instance from a string.  This addresses windows bug 968884
-            try
-            {
-                Coord result = Convert.ToSingle(valueString, cultureInfo) * unitFactor;
-                return result;
-            }
-            catch (FormatException)
-            {
-                throw new FormatException();
-            }
-        }
-
-        // This array contains strings for unit types 
-        // These are effectively "TypeConverter only" units.
-        // They are all expressed in terms of the Pixel unit type and a conversion factor.
-        static private string[] PixelUnitStrings = { "px", "in", "cm", "pt" };
-        static private Coord[] PixelUnitFactors =
-        {
-            1.0,              // Pixel itself
-            96.0,             // Pixels per Inch
-            96.0 / 2.54,      // Pixels per Centimeter
-            96.0 / 72.0,      // Pixels per Point
-        };
-
-        static internal string ToString(Coord l, CultureInfo cultureInfo)
-        {
-            if (MathUtils.IsNaN(l)) return "Auto";
-            return Convert.ToString(l, cultureInfo);
-        }
-
-        #endregion
-
     }
 }
