3f3e472dec2333ecaa69c65b82f7e26bd1bc3921
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-23 12:01:13 +0300

    Create LibraryUtils.cs

---
+++ Source/Alternet.UI.Common/Utils/OSUtils/LibraryUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides utility methods and functionality for working with native libraries.
+    /// </summary>
+    /// <remarks>This static class serves as a collection of helper methods and utilities designed to
+    /// simplify common operations related to native libraries.
+    /// All methods are static and can be accessed without instantiating the class.</remarks>
+    public static class LibraryUtils
+    {
+    }
+}
