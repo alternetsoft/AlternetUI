a1d378af8dafc3d7f5810fbdc7e074c0f12eb7e4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-27 06:54:26 +0300

    Add SystemDrawingColorCache for cached color retrieval
    Introduces SystemDrawingColorCache, a static class that caches System.Drawing.Color instances for all KnownColor values to improve performance when retrieving colors.

---
+++ Source/Alternet.UI.Common/Drawing/Color/SystemDrawingColorCache.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Provides a cached collection of <see cref="System.Drawing.Color"/>
+    /// instances corresponding to all <see cref="System.Drawing.KnownColor"/> values.
+    /// </summary>
+    /// <remarks>This class improves performance by precomputing and caching all
+    /// <see cref="System.Drawing.Color"/> instances derived
+    /// from <see cref="System.Drawing.KnownColor"/> values. Use the
+    /// <see cref="Get"/>  method to retrieve
+    /// a cached <see cref="System.Drawing.Color"/> instance.</remarks>
+    public static class SystemDrawingColorCache
+    {
+        private static readonly System.Drawing.Color[] colors;
+
+        static SystemDrawingColorCache()
+        {
+            var values = Enum.GetValues(typeof(System.Drawing.KnownColor));
+            colors = new System.Drawing.Color[values.Length];
+
+            foreach (System.Drawing.KnownColor kc in values)
+            {
+                colors[(int)kc] = System.Drawing.Color.FromKnownColor(kc);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the <see cref="System.Drawing.Color"/> associated with
+        /// the specified <see cref="System.Drawing.KnownColor"/>.
+        /// This method uses a cached collection of colors for improved performance.
+        /// </summary>
+        /// <param name="kc">A value from the <see cref="System.Drawing.KnownColor"/>
+        /// enumeration representing the color to retrieve.</param>
+        /// <returns>The <see cref="System.Drawing.Color"/> corresponding
+        /// to the specified <paramref name="kc"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static System.Drawing.Color Get(System.Drawing.KnownColor kc)
+        {
+            return colors[(int)kc];
+        }
+    }
+}
