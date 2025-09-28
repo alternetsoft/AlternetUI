fa2b3c4d6d4a9e9edbf0e69f4f9eed5df7d1b5e0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-28 20:10:00 +0300

    Add SkiaSharp.SkiaApi type reference to KnownTypes
    Introduces a lazily initialized reference to the SkiaSharp.SkiaApi type in KnownTypes, allowing runtime access if available. Also updates existing LazyStruct fields to be readonly and corrects a typo in the summary comment.

--- Source/Alternet.UI.Common/Base/Classes/Known/KnownTypes.cs
+++ Source/Alternet.UI.Common/Base/Classes/Known/KnownTypes.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 
+using SkiaSharp;
+
 namespace Alternet.UI
 {
     /// <summary>
-    /// Contains refrences to some of the known types.
+    /// Contains references to some of the known types.
     /// </summary>
     public static class KnownTypes
     {
         /// <summary>
+        /// Provides a lazily initialized reference to the SkiaSharp.SkiaApi type, if available.
+        /// </summary>
+        /// <remarks>This property attempts to retrieve the type <c>SkiaSharp.SkiaApi</c> from the
+        /// assembly containing the <see cref="SKCanvas"/> class.
+        /// The value will be <see langword="null"/> if the type
+        /// cannot be found.</remarks>
+        public static readonly LazyStruct<Type?> SkiaSharpSkiaApi = new (() =>
+        {
+            var result = typeof(SKCanvas).Assembly.GetType("SkiaSharp.SkiaApi");
+            return result;
+        });
+
+        /// <summary>
         /// Gets 'Alternet.UI.MauiUtils' type.
         /// </summary>
-        public static LazyStruct<Type?> MauiUtils = new(() =>
+        public static readonly LazyStruct<Type?> MauiUtils = new(() =>
         {
             var result = KnownAssemblies.LibraryMaui.Value?.GetType("Alternet.UI.MauiUtils");
             return result;
@@ -21,7 +36,7 @@ namespace Alternet.UI
         /// <summary>
         /// Gets 'System.Runtime.InteropServices.NativeLibrary' type.
         /// </summary>
-        public static LazyStruct<Type?> InteropServicesNativeLibrary = new(() =>
+        public static readonly LazyStruct<Type?> InteropServicesNativeLibrary = new(() =>
         {
             var asm = KnownAssemblies.LibraryInteropServices.Value;
             var result = asm?.GetType("System.Runtime.InteropServices.NativeLibrary");
@@ -31,7 +46,7 @@ namespace Alternet.UI
         /// <summary>
         /// Gets 'System.Runtime.InteropServices.DllImportResolver' type.
         /// </summary>
-        public static LazyStruct<Type?> InteropServicesDllImportResolver = new(() =>
+        public static readonly LazyStruct<Type?> InteropServicesDllImportResolver = new(() =>
         {
             var asm = KnownAssemblies.LibraryInteropServices.Value;
             var result = asm?.GetType("System.Runtime.InteropServices.DllImportResolver");
