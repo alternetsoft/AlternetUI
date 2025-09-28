d56459dcb3e57072c802e422ba646cc6fc8cd286
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-09-28 14:50:45 +0300

    Create SkiaDirectPaintMacOsPage.cs

---
+++ Source/Samples/ControlsSampleDll/Pages/ControlsTest/SkiaDirectPaintMacOsPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.UI;
+using Alternet.Drawing;
+
+namespace ControlsSampleDll
+{
+    internal class SkiaDirectPaintMacOsPage : Panel
+    {
+        public SkiaDirectPaintMacOsPage()
+        {
+            if (!App.IsMacOS)
+            {
+                var label = new Label();
+                label.Text = "This page is available only on macOs.";
+                label.Parent = this;
+                label.Dock = DockStyle.Fill;
+                label.HorizontalAlignment = HorizontalAlignment.Center;
+                label.VerticalAlignment = VerticalAlignment.Center;
+            }
+            else
+            {
+                var skiaControl = new SkiaDirectPaintMacOs();
+                skiaControl.Parent = this;
+                skiaControl.Dock = DockStyle.Fill;
+            }
+        }
+
+        private class SkiaDirectPaintMacOs : UserControl        
+        {
+            public override void DefaultPaint(PaintEventArgs e)
+            {
+                if (!App.IsMacOS)
+                    return;
+                IntPtr nsViewPtr = Handler.GetHandle();
+                if (nsViewPtr == IntPtr.Zero)
+                    return;
+            }
+        }
+    }
+}
