7792853f33cc97ae12a56169f540ac1bc5a76504
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-10-01 07:22:24 +0300

    Add SVG image support and management methods
    Introduces storage and handling for SVG images, including methods to set, get, reload, and reset SVG images and their cached data. Adds functionality to load SVG images from resources, apply color overrides, and regenerate small and large image variants from SVG sources.

--- Source/Alternet.UI.Common/Drawing/Image/EnumImages.cs
+++ Source/Alternet.UI.Common/Drawing/Image/EnumImages.cs
@@ -21,6 +21,7 @@ namespace Alternet.UI
         private EnumArray<TKey, Image?> smallImages = new();
         private EnumArray<TKey, Image?> largeImages = new();
         private EnumArray<TKey, Color?> svgColors = new();
+        private EnumArray<TKey, SvgImage?> svgImages = new();
 
         /// <summary>
         /// Gets or sets size of the small svg image.
@@ -39,7 +40,35 @@ namespace Alternet.UI
         /// <param name="value"></param>
         public virtual void SetSvgColor(TKey index, Color? value)
         {
+            GetSvgImage(index)?.SetColorOverride(KnownSvgColor.Normal, value);
             svgColors[index] = value;
+        }
+
+        /// <summary>
+        /// Associates the specified SVG image with the given key in the collection.
+        /// </summary>
+        /// <remarks>If an SVG image is already associated with the specified key, it will be replaced
+        /// with the new value.</remarks>
+        /// <param name="index">The key used to identify the SVG image in the collection.
+        /// Cannot be null.</param>
+        /// <param name="value">The SVG image to associate with the specified key.
+        /// Can be null to remove the association.</param>
+        public virtual void SetSvgImage(TKey index, SvgImage? value)
+        {
+            svgImages[index] = value;
+        }
+
+        /// <summary>
+        /// Retrieves the <see cref="SvgImage"/> associated with the specified key.
+        /// </summary>
+        /// <param name="index">The key used to locate the <see cref="SvgImage"/>.
+        /// Must not be null.</param>
+        /// <returns>The <see cref="SvgImage"/> associated with the specified key,
+        /// or <see langword="null"/> if the key does not
+        /// exist.</returns>
+        public virtual SvgImage? GetSvgImage(TKey index)
+        {
+            return svgImages[index];
         }
 
         /// <summary>
@@ -177,6 +206,109 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Reloads an image from an SVG source and updates it with the specified size and color.
+        /// </summary>
+        /// <remarks>This method retrieves the SVG image associated with the specified
+        /// <paramref name="imageKey"/>, applies the corresponding color, and resizes it based
+        /// on the <paramref name="isSmallImage"/> parameter.
+        /// The updated image is then set in the appropriate storage.</remarks>
+        /// <param name="imageKey">The key identifying the SVG image to reload.</param>
+        /// <param name="isSmallImage">A value indicating whether the
+        /// image should be reloaded as a small image. If <see langword="true"/>, the
+        /// image is resized to a smaller dimension; otherwise,
+        /// it is resized to a larger dimension.</param>
+        public virtual void ReloadFromSvg(TKey imageKey, bool isSmallImage)
+        {
+            var svg = svgImages[imageKey];
+            if (svg is not null)
+            {
+                var size = GetSvgSize(isSmallImage);
+                var svgColor = svgColors[imageKey];
+                var image = svg.ImageWithColor(size, svgColor);
+                SetImage(imageKey, image, isSmallImage);
+            }
+        }
+
+        /// <summary>
+        /// Reloads all images from their corresponding SVG sources.
+        /// </summary>
+        /// <remarks>This method clears the current collections of small
+        /// and large images and reloads them
+        /// from the SVG sources. It ensures that both small and large
+        /// versions of each image are  reloaded. Use this
+        /// method to refresh all images when the underlying SVG sources
+        /// have changed.</remarks>
+        public virtual void ReloadAllFromSvg()
+        {
+            ReloadSmallImagesFromSvg();
+            ReloadLargeImagesFromSvg();
+        }
+
+        /// <summary>
+        /// Reloads and regenerates large images from their corresponding SVG sources.
+        /// </summary>
+        /// <remarks>This method clears the current collection of large images
+        /// and regenerates them based
+        /// on the existing SVG image sources.
+        /// It processes each SVG source and updates the large image collection
+        /// accordingly.</remarks>
+        public virtual void ReloadLargeImagesFromSvg()
+        {
+            largeImages = new();
+            svgImages.ForEachKey((key) => ReloadFromSvg(key, false));
+        }
+
+        /// <summary>
+        /// Reloads and regenerates the small-sized images from their corresponding SVG sources.
+        /// </summary>
+        /// <remarks>This method clears the current collection of small images
+        /// and regenerates them based
+        /// on the existing SVG image sources. It processes each SVG source and creates
+        /// a corresponding small-sized image.</remarks>
+        public virtual void ReloadSmallImagesFromSvg()
+        {
+            smallImages = new();
+            svgImages.ForEachKey((key) => ReloadFromSvg(key, true));
+        }
+
+        /// <summary>
+        /// Determines whether the collection contains any non-null SVG images.
+        /// </summary>
+        /// <returns><see langword="true"/> if the collection contains
+        /// at least one non-null SVG image; otherwise, <see langword="false"/>.</returns>
+        public virtual bool HasSvgImages()
+        {
+            bool result = false;
+            svgImages.ForEachKey((key) =>
+            {
+                if (svgImages[key] != null)
+                    result = true;
+            });
+
+            return result;
+        }
+
+        /// <summary>
+        /// Resets all cached images within the current instance and its child elements.
+        /// This is done only if the current instance contains any SVG images.
+        /// </summary>
+        /// <remarks>This method clears any cached image data associated with
+        /// the current instance and
+        /// recursively resets cached images for all child elements.
+        /// It is typically used to ensure that updated or
+        /// refreshed image data is loaded the next time the images are accessed.</remarks>
+        public virtual void ResetCachedImages()
+        {
+            svgImages.ForEachKey((key) =>
+            {
+                var svg = svgImages[key];
+                svg?.ResetCachedImages();
+            });
+
+            ReloadAllFromSvg();
+        }
+
+        /// <summary>
         /// Loads the specified image from the assembly resource.
         /// </summary>
         /// <param name="assembly">The assembly from where image is loaded.</param>
@@ -204,8 +336,10 @@ namespace Alternet.UI
                 if (url.EndsWith(".svg"))
                 {
                     var svg = new MonoSvgImage(url, SvgImageDataKind.Url, false);
+                    SetSvgImage(imageKey, svg);
                     var size = GetSvgSize(isSmallImage);
                     var svgColor = svgColors[imageKey];
+                    svg.SetColorOverride(KnownSvgColor.Normal, svgColor);
                     image = svg.ImageWithColor(size, svgColor);
                 }
                 else
@@ -221,6 +355,42 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Loads an SVG image from an embedded resource within the specified assembly.
+        /// </summary>
+        /// <remarks>The method resolves the resource path based on the provided
+        /// <paramref
+        /// name="imageKey"/> and  <paramref name="isSmallImage"/> flag.
+        /// If the resource path is null or the resource is
+        /// not an SVG file,  the method returns <see langword="false"/>.</remarks>
+        /// <param name="assembly">The assembly containing the embedded resource.</param>
+        /// <param name="imageKey">The key used to identify the image.</param>
+        /// <param name="isSmallImage">A value indicating whether to
+        /// load the small version of the image.  If <see langword="true"/>, the method
+        /// attempts to load the small image; otherwise, the regular image.</param>
+        /// <returns><see langword="true"/> if the SVG image was successfully loaded and set;
+        /// otherwise, <see langword="false"/>.</returns>
+        public virtual bool LoadSvgFromResource(Assembly assembly, TKey imageKey, bool isSmallImage = true)
+        {
+            var relativePath = GetImageName(imageKey, isSmallImage);
+
+            if (relativePath is null)
+                return false;
+
+            string url = AssemblyUtils.GetImageUrlInAssembly(assembly, relativePath);
+
+            if (url.EndsWith(".svg"))
+            {
+                var svg = new MonoSvgImage(url, SvgImageDataKind.Url, false);
+                var svgColor = svgColors[imageKey];
+                svg.SetColorOverride(KnownSvgColor.Normal, svgColor);
+                SetSvgImage(imageKey, svg);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
         /// Gets size of the svg image.
         /// </summary>
         /// <param name="isSmallImage">Whether returned size is for small or large image.</param>
@@ -231,6 +401,64 @@ namespace Alternet.UI
                 return SmallSvgSize;
             else
                 return LargeSvgSize;
+        }
+
+        /// <summary>
+        /// Loads both small and large images from the specified assembly's embedded resources.
+        /// </summary>
+        /// <remarks>This method loads two sets of images: small and large.
+        /// The specific resources to load
+        /// are determined by the implementation of the resource-loading logic.
+        /// Ensure that the provided assembly
+        /// contains the required resources in the expected format.</remarks>
+        /// <param name="assembly">The assembly containing the embedded
+        /// resources to load the images from.</param>
+        public virtual void LoadSmallAndLargeImagesFromResource(Assembly assembly)
+        {
+            LoadImagesFromResource(assembly, true);
+            LoadImagesFromResource(assembly, false);
+        }
+
+        /// <summary>
+        /// Loads large images from the specified assembly's embedded resources.
+        /// </summary>
+        /// <remarks>This method loads only the large images from the assembly's resources.
+        /// Ensure that the assembly contains the expected resource format.</remarks>
+        /// <param name="assembly">The assembly containing the embedded resources
+        /// to load images from. Cannot be <see langword="null"/>.</param>
+        public void LoadLargeImagesFromResource(Assembly assembly)
+        {
+            LoadImagesFromResource(assembly, loadSmallImageNames: false);
+        }
+
+        /// <summary>
+        /// Loads small images from the specified assembly's embedded resources.
+        /// </summary>
+        /// <remarks>This method loads only the small images from the assembly's resources.
+        /// Ensure that the assembly contains the expected resource format.</remarks>
+        /// <param name="assembly">The assembly containing the embedded resources
+        /// to load images from. Cannot be <see langword="null"/>.</param>
+        public void LoadSmallImagesFromResource(Assembly assembly)
+        {
+            LoadImagesFromResource(assembly, loadSmallImageNames: true);
+        }
+
+        /// <summary>
+        /// Loads SVG images from the specified assembly and processes them based
+        /// on the provided size option.
+        /// </summary>
+        /// <remarks>This method iterates through all keys in the internal collection
+        /// of SVG images and
+        /// loads the corresponding SVG resources from the specified assembly.
+        /// The size of the images is  determined by
+        /// the <paramref name="isSmallImage"/> parameter.</remarks>
+        /// <param name="assembly">The assembly containing the embedded SVG resources to load.</param>
+        /// <param name="isSmallImage">A boolean value indicating whether
+        /// to load the SVG images as small-sized images.  <see langword="true"/> to
+        /// load small images; otherwise, <see langword="false"/>.</param>
+        public void LoadSvgFromResource(Assembly assembly, bool isSmallImage = true)
+        {
+            svgImages.ForEachKey((key) => LoadSvgFromResource(assembly, key, isSmallImage));
         }
 
         /// <summary>
