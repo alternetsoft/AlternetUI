391c6c1d2dd2bd23fe952944ecb4c9a78060d039
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-10-11 14:00:37 +0300

    Handle system color changes in ColorPicker
    Added override for OnSystemColorsChanged to update control colors when system colors change, ensuring the ColorPicker reflects the current system theme.

--- Source/Alternet.UI.Common/Controls/Other/ColorPicker.cs
+++ Source/Alternet.UI.Common/Controls/Other/ColorPicker.cs
@@ -39,5 +39,12 @@ namespace Alternet.UI
 
         /// <inheritdoc/>
         public override ControlTypeId ControlKind => ControlTypeId.ColorPicker;
+
+        /// <inheritdoc/>
+        protected override void OnSystemColorsChanged(EventArgs e)
+        {
+            UseControlColors(DefaultUseControlColors);
+            base.OnSystemColorsChanged(e);
+        }
     }
 }
