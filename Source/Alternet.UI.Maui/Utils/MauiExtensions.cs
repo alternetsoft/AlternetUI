484060c9aef7de185e3860d3b91bcf800ce0102f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-10-27 07:16:59 +0200

    Add extension methods for menu item conversion and state update
    Introduced extension methods to convert Alternet.UI.MenuItem to MenuBarItem and MenuFlyoutItem, and to update the execution state (IsEnabled) of MenuBarItem and MenuFlyoutItem based on command availability. These utilities facilitate integration and dynamic state management of menu items in Maui applications.

--- Source/Alternet.UI.Maui/Utils/MauiExtensions.cs
+++ Source/Alternet.UI.Maui/Utils/MauiExtensions.cs
@@ -115,5 +115,97 @@ namespace Alternet.Maui.Extensions
         {
             return new(value.X, value.Y, value.Width, value.Height);
         }
+
+        /// <summary>
+        /// Converts an <see cref="Alternet.UI.MenuItem"/> to a <see cref="MenuBarItem"/>.
+        /// </summary>
+        /// <param name="menuItem">The <see cref="Alternet.UI.MenuItem"/> to convert. Must not be <see langword="null"/>.</param>
+        /// <returns>A <see cref="MenuBarItem"/> that represents the converted menu item, including its text, enabled state, and
+        /// child items if present.</returns>
+        public static MenuBarItem ToMenuBarItem(this Alternet.UI.MenuItem menuItem)
+        {
+            var result = new MenuBarItem
+            {
+                Text = menuItem.Text,
+                IsEnabled = menuItem.Enabled,
+            };
+
+            if (menuItem.HasItems)
+            {
+                foreach (var child in menuItem.Items)
+                {
+                    result.Add(child.ToMenuFlyoutItem());
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts an <see cref="Alternet.UI.MenuItem"/> to a corresponding
+        /// <see cref="Microsoft.Maui.Controls.MenuFlyoutItem"/>.
+        /// </summary>
+        /// <param name="menuItem">The <see cref="Alternet.UI.MenuItem"/> to convert. Cannot be <c>null</c>.</param>
+        /// <returns>A <see cref="Microsoft.Maui.Controls.MenuFlyoutItem"/> that represents the converted menu item.</returns>
+        public static MenuFlyoutItem ToMenuFlyoutItem(this Alternet.UI.MenuItem menuItem)
+        {
+            if (menuItem.IsSeparator)
+            {
+                var separator = new MenuFlyoutSeparator();
+                return separator;
+            }
+
+            var result = new MenuFlyoutItem
+            {
+                Text = menuItem.Text,
+            };
+
+            if (menuItem.Command is not null)
+            {
+                result.Command = menuItem.Command.ToMaui();
+                result.CommandParameter = menuItem.CommandParameter;
+            }
+            else
+            {
+                result.Clicked += (s, e) => menuItem.RaiseClick();
+                result.IsEnabled = menuItem.Enabled;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Updates the execution state of the specified <see cref="MenuBarItem"/> and its child items.
+        /// </summary>
+        /// <remarks>This method recursively updates the execution state of all child items within the
+        /// specified <see cref="MenuBarItem"/>. If a child item is a <see cref="MenuFlyoutItem"/>, its execution state
+        /// is updated by calling its <c>UpdateCanExecute</c> method. If a child item is another <see
+        /// cref="MenuBarItem"/>, this method is called recursively on that item.</remarks>
+        /// <param name="menuItem">The <see cref="MenuBarItem"/> whose execution state is to be updated.</param>
+        public static void UpdateCanExecute(this MenuBarItem menuItem)
+        {
+            for (int i = 0; i < menuItem.Count; i++)
+            {
+                var child = menuItem[i];
+                if (child is MenuFlyoutItem menuFlyoutItem)
+                    menuFlyoutItem.UpdateCanExecute();
+                else
+                if (child is MenuBarItem menuBarItem)
+                    menuBarItem.UpdateCanExecute();
+            }
+        }
+
+        /// <summary>
+        /// Updates 'IsEnabled' property of <see cref="MenuFlyoutItem"/> based on the result of the
+        /// <see cref="ICommand.CanExecute"/> method of the associated command.
+        /// </summary>
+        /// <param name="menuFlyoutItem">The <see cref="MenuFlyoutItem"/> whose state will be updated.
+        /// The item must have an associated  <see cref="ICommand"/> for this method to take effect.</param>
+        public static void UpdateCanExecute(this MenuFlyoutItem menuFlyoutItem)
+        {
+            if (menuFlyoutItem.Command is null)
+                return;
+            menuFlyoutItem.IsEnabled = menuFlyoutItem.Command.CanExecute(menuFlyoutItem.CommandParameter);
+        }
     }
 }
