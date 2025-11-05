93cb00f265b55070fd9467244632684eb81933bf
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-11-05 12:33:15 +0200

    Install scripts

---
+++ Install.Scripts/MSW.Publish.2b.Unzip.Vsix.bat
@@ -0,0 +1,28 @@
+SETLOCAL EnableDelayedExpansion
+
+set SCRIPT_HOME=%~dp0
+set SOURCE_DIR=%SCRIPT_HOME%\..\Source
+set PublishRoot=%SOURCE_DIR%\..\Publish\
+
+:: Set up publish folder
+
+if not exist "%PublishRoot%" (mkdir "%PublishRoot%")
+if not !ERRORLEVEL! EQU 0 (exit /b !ERRORLEVEL!)
+
+set PackagesPublishDirectory=%PublishRoot%\Packages\
+::REM set SourcePublishDirectory=%PublishRoot%\Sources\
+
+echo ====================================5
+
+pushd "%PackagesPublishDirectory%"
+
+for /R %%f in (*.vsix) do call "C:\Program Files\WinRar\WinRAR.exe" x "%%f" "%%f.Folder/"
+
+popd
+
+
+
+echo ====================================5
+
+
+exit /b
