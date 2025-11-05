8321f7053a0efc35608f2dabaee178eaa110f789
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-11-05 06:41:54 +0200

    Demo: run scripts

---
+++ Source/Samples/ControlsSample/run.net9.sh
@@ -0,0 +1,1 @@
+dotnet run --framework net9.0
--- Source/Samples/ControlsSample/run.release.net8.nooptimize.bat
+++
@@ -1,3 +0,0 @@
-dotnet clean
-dotnet build --framework net8.0 -c Release /p:Optimize=false /p:DebugType=portable /p:DebugSymbols=true --no-incremental
-dotnet run --framework net8.0 -c Release /p:Optimize=false /p:DebugType=portable /p:DebugSymbols=true --no-incremental
