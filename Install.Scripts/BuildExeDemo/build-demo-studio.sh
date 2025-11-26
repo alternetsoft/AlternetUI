23da4376161b93b22b4276020fba56e93e75fe9f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-11-26 15:30:12 +0200

    Update build-demo-studio.sh

--- Install.Scripts/BuildExeDemo/build-demo-studio.sh
+++ Install.Scripts/BuildExeDemo/build-demo-studio.sh
@@ -5,6 +5,6 @@ SCRIPT_HOME=$(cd "$(dirname "$0")"; pwd -P)
 pushd $SCRIPT_HOME
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-dotnet msbuild /t:BuildExeDemo -tl:off -property:DEMO_NAME_CODE=studio-ui -property:VERSION_SUFFIX=10.0.0 -property:TARGET_CONFIG=Debug build-demo.proj
+dotnet msbuild /t:BuildExeDemo -tl:off -property:DEMO_NAME_CODE=studio-ui -property:VERSION_SUFFIX=10.0.5 -property:TARGET_CONFIG=Debug build-demo.proj
 
 popd
