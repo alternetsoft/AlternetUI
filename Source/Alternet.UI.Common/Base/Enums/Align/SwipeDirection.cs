4db895ce67b2a0172d801170f450028a3e6fc3e7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2025-12-04 07:18:57 +0200

    Add composite values to SwipeDirection enum
    Introduced All, LeftRight, and UpDown composite flags to the SwipeDirection enum for easier handling of multiple swipe directions. Updated XML documentation for clarity.

--- Source/Alternet.UI.Common/Base/Enums/Align/SwipeDirection.cs
+++ Source/Alternet.UI.Common/Base/Enums/Align/SwipeDirection.cs
@@ -11,17 +11,40 @@ namespace Alternet.UI
     [Flags]
     public enum SwipeDirection
     {
-        /// <summary>Indicates a presence of rightward swipe.</summary>
+        /// <summary>
+        /// Indicates a presence of rightward swipe.
+        /// </summary>
         Right = 1,
 
-        /// <summary>Indicates a presence of leftward swipe.</summary>
+        /// <summary>
+        /// Indicates a presence of leftward swipe.
+        /// </summary>
         Left = 2,
 
-        /// <summary>Indicates a presence of upward swipe.</summary>
+        /// <summary>
+        /// Indicates a presence of upward swipe.
+        /// </summary>
         Up = 4,
 
-        /// <summary>Indicates a presence of downward swipe.</summary>
-        Down = 8
+        /// <summary>
+        /// Indicates a presence of downward swipe.
+        /// </summary>
+        Down = 8,
+
+        /// <summary>
+        /// Indicates a presence of all swipe directions.
+        /// </summary>
+        All = Right | Left | Up | Down,
+
+        /// <summary>
+        /// Indicates a presence of horizontal swipe directions.
+        /// </summary>
+        LeftRight = Left | Right,
+
+        /// <summary>
+        /// Indicates a presence of vertical swipe directions.
+        /// </summary>
+        UpDown = Up | Down,
     }
 
     /// <summary>
