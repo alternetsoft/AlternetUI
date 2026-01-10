b3cefd6c298d31c3c7a4dd2f2d8e3d0616a83e9d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-01-10 09:22:02 +0200

    Add Reset and EnsureCreated methods to MeasureItemEventArgs
    Introduces Reset and EnsureCreated methods for reinitializing MeasureItemEventArgs instances with new graphics contexts, indices, and item heights. Also adds ItemSize property for unified width/height access and updates constructors to use Reset for improved reusability.

--- Source/Alternet.UI.Common/Base/EventArgs/Paint/MeasureItemEventArgs.cs
+++ Source/Alternet.UI.Common/Base/EventArgs/Paint/MeasureItemEventArgs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 using Alternet.Drawing;
@@ -31,25 +32,26 @@ namespace Alternet.UI
         /// <param name="index">The index of the item for which you need the height or width.</param>
         /// <param name="itemHeight">The height of the item to measure relative to
         /// the <paramref name="graphics" /> object.</param>
-        public MeasureItemEventArgs(Graphics graphics, int index, int itemHeight)
+#pragma warning disable
+        public MeasureItemEventArgs(Graphics graphics, int index, Coord itemHeight)
+#pragma warning restore
         {
-            this.graphics = graphics;
-            this.index = index;
-            this.itemHeight = itemHeight;
+            Reset(graphics, index, itemHeight);
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MeasureItemEventArgs" /> class.</summary>
         /// <param name="graphics">The <see cref="Graphics" /> object being written to.</param>
         /// <param name="index">The index of the item for which you need the height or width.</param>
+#pragma warning disable
         public MeasureItemEventArgs(Graphics graphics, int index = 0)
+#pragma warning restore
         {
-            this.graphics = graphics;
-            this.index = index;
+            Reset(graphics, index);
         }
 
         /// <summary>
-        /// Gets the <see cref="Graphics" /> object to measure against.
+        /// Gets or sets the <see cref="Graphics" /> object to measure against.
         /// </summary>
         /// <returns>
         /// The <see cref="Graphics" /> object to use to determine the scale of the item
@@ -106,6 +108,24 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Gets or sets the size of the item.
+        /// This is an alias for the <see cref="ItemWidth"/> and <see cref="ItemHeight"/> properties.
+        /// </summary>
+        public SizeD ItemSize
+        {
+            get
+            {
+                return new SizeD(itemWidth, itemHeight);
+            }
+
+            set
+            {
+                itemWidth = value.Width;
+                itemHeight = value.Height;
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the width of the item specified by the
         /// <see cref="MeasureItemEventArgs.Index" />.
         /// </summary>
@@ -121,6 +141,73 @@ namespace Alternet.UI
             {
                 itemWidth = value;
             }
+        }
+
+        /// <summary>
+        /// Ensures that the specified <see cref="MeasureItemEventArgs"/> instance is initialized and associated with
+        /// the given <see cref="Graphics"/> context and item index.
+        /// </summary>
+        /// <param name="e">A reference to the <see cref="MeasureItemEventArgs"/> instance to initialize or reset.
+        /// If <see langword="null"/>, a new instance will be created and assigned.</param>
+        /// <param name="graphics">The <see cref="Graphics"/> context to associate with the event arguments.
+        /// Cannot be <see langword="null"/>.</param>
+        /// <param name="index">The index of the item to be measured. The default value is 0.</param>
+        public static void EnsureCreated([NotNull] ref MeasureItemEventArgs? e, Graphics graphics, int index = 0)
+        {
+            if (e == null)
+                e = new MeasureItemEventArgs(graphics, index);
+            else
+                e.Reset(graphics, index);
+        }
+
+        /// <summary>
+        /// Ensures that the specified <see cref="MeasureItemEventArgs"/> instance is initialized with the provided
+        /// graphics context, item index, and item height.
+        /// </summary>
+        /// <remarks>If <paramref name="e"/> is already initialized, its state is reset with the new
+        /// parameters. Otherwise, a new <see cref="MeasureItemEventArgs"/> is created and assigned to <paramref
+        /// name="e"/>.</remarks>
+        /// <param name="e">A reference to the <see cref="MeasureItemEventArgs"/> instance to initialize.
+        /// If <see langword="null"/>, a new instance is created and assigned.</param>
+        /// <param name="graphics">The <see cref="Graphics"/> context used for measuring the item.
+        /// Cannot be <see langword="null"/>.</param>
+        /// <param name="index">The zero-based index of the item to measure.</param>
+        /// <param name="itemHeight">The height of the item to be measured.</param>
+        public static void EnsureCreated([NotNull] ref MeasureItemEventArgs? e, Graphics graphics, int index, Coord itemHeight)
+        {
+            if (e == null)
+                e = new MeasureItemEventArgs(graphics, index, itemHeight);
+            else
+                e.Reset(graphics, index, itemHeight);
+        }
+
+        /// <summary>
+        /// Assigns the specified graphics context, item index, and item height to the current instance for subsequent
+        /// drawing operations.
+        /// </summary>
+        /// <param name="graphics">The graphics context to use for rendering. Cannot be null.</param>
+        /// <param name="index">The zero-based index of the item to assign. Must be greater than or equal to 0.</param>
+        /// <param name="itemHeight">The height, in pixels, of the item to assign. Must be greater than 0.</param>
+        public virtual void Reset(Graphics graphics, int index, Coord itemHeight)
+        {
+            this.graphics = graphics;
+            this.index = index;
+            this.itemHeight = itemHeight;
+            this.itemWidth = 0;
+        }
+
+        /// <summary>
+        /// Assigns the specified graphics context and item index to the current instance for subsequent
+        /// drawing operations.
+        /// </summary>
+        /// <param name="graphics">The graphics context to use for rendering. Cannot be null.</param>
+        /// <param name="index">The zero-based index of the item to assign. Must be greater than or equal to 0.</param>
+        public virtual void Reset(Graphics graphics, int index = 0)
+        {
+            this.graphics = graphics;
+            this.index = index;
+            this.itemHeight = 0;
+            this.itemWidth = 0;
         }
     }
 }
