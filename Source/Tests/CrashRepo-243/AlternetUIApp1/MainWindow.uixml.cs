5e22e535d246fe14e4f87793d3de00988e0a3bf8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-01-12 06:24:57 +0200

    Add CrashRepo #243

---
+++ Source/Tests/CrashRepo-243/AlternetUIApp1/MainWindow.uixml.cs
@@ -0,0 +1,12 @@
+using Alternet.UI;
+
+namespace AlternetUIApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Tests/CrashRepo-243/AlternetUIApp1/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using Alternet.UI;
+
+namespace AlternetUIApp1
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var application = new Application();
+            var window = new MainWindow();
+
+            application.Run(window);
+
+            window.Dispose();
+            application.Dispose();
+        }
+    }
+}
