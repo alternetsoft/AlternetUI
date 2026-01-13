90eb04515957f88b4a0262b649eb174df7cd4dcd
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-01-13 14:12:45 +0200

    Add selection, paint, and image helpers to ListBoxItemPaintEventArgs
    Introduces HasSelection, PaintRectangle, and VisibleTextForDisplay properties to enhance item rendering logic. Adds GetItemImages and GetImage methods for retrieving state-dependent images, improving support for custom drawing and selection visibility scenarios.

--- Source/Alternet.UI.Common/Controls/ListControls/EventArgs/ListBoxItemPaintEventArgs.cs
+++ Source/Alternet.UI.Common/Controls/ListControls/EventArgs/ListBoxItemPaintEventArgs.cs
@@ -45,6 +45,47 @@ namespace Alternet.UI
         public virtual bool IsSelected { get; set; }
 
         /// <summary>
+        /// Gets a value indicating whether the current item is selected, taking into account any selection visibility
+        /// settings.
+        /// </summary>
+        /// <remarks>If the associated item has selection visibility disabled, this property returns false
+        /// regardless of the actual selection state.</remarks>
+        public virtual bool HasSelection
+        {
+            get
+            {
+                var hideSelection = Item?.HideSelection ?? false;
+
+                bool isSelected;
+                if (hideSelection)
+                    isSelected = false;
+                else
+                    isSelected = IsSelected;
+                return isSelected;
+            }
+        }
+
+        /// <summary>
+        /// Gets the rectangle, in client coordinates, that defines the area available for painting the item's content,
+        /// excluding any padding. This property uses the padding settings defined by the associated list control to
+        /// determine the available painting area. <see cref="PaintEventArgs.ClientRectangle"/> is used as the base rectangle from which
+        /// the padding is applied.
+        /// </summary>
+        /// <remarks>The returned rectangle accounts for the item's padding as determined by the
+        /// associated list control. Use this property when performing custom drawing to ensure content is rendered
+        /// within the correct bounds.</remarks>
+        public virtual RectD PaintRectangle
+        {
+            get
+            {
+                Thickness padding = ListControlItem.GetPadding(Item, ListBox);
+                var paintRectangle = ClientRectangle;
+                paintRectangle.ApplyMargin(padding);
+                return paintRectangle;
+            }
+        }
+
+        /// <summary>
         /// Gets whether item is current.
         /// </summary>
         public virtual bool IsCurrent { get; set; }
@@ -91,6 +132,23 @@ namespace Alternet.UI
             get
             {
                 return ListBox.GetItemText(ItemIndex, false);
+            }
+        }
+
+        /// <summary>
+        /// Gets the text to display for the item, taking into account the current visibility settings.
+        /// </summary>
+        /// <remarks>If the associated list box is configured to hide text, this property returns an empty
+        /// string. Otherwise, it returns the display text for the item. This property is intended for use in UI
+        /// scenarios where the visibility of item text may be toggled.</remarks>
+        public virtual string VisibleTextForDisplay
+        {
+            get
+            {
+                var textVisible = ListBox?.Defaults.TextVisible ?? true;
+
+                var s = textVisible ? ItemTextForDisplay : string.Empty;
+                return s;
             }
         }
 
@@ -152,15 +210,72 @@ namespace Alternet.UI
         {
             get
             {
-                var color = ListControlItem.GetSelectedTextColor(Item, ListBox);
-                return ListControlItem.GetItemImages(Item, ListBox, color);
+                return GetItemImages(Item, ListBox);
             }
         }
 
         /// <summary>
-        /// Gets control which item is painted.
+        /// Gets container control in which item is painted.
         /// </summary>
         public virtual IListControlItemContainer ListBox { get; set; }
+
+        /// <summary>
+        /// Retrieves the set of images associated with the specified list control item, taking into account its
+        /// selection state and the containing list box.
+        /// </summary>
+        /// <param name="item">The list control item for which to obtain images.
+        /// This parameter can be null to indicate no specific item.</param>
+        /// <param name="listBox">The container that holds the list control item.
+        /// This parameter can be null if the item is not associated
+        /// with a container.</param>
+        /// <returns>An object containing the images representing the visual states of the specified item. The returned value
+        /// reflects the item's current state within the provided container.</returns>
+        public virtual EnumArrayStateImages GetItemImages(ListControlItem? item, IListControlItemContainer? listBox)
+        {
+            var color = ListControlItem.GetSelectedTextColor(item, listBox);
+            return ListControlItem.GetItemImages(item, listBox, color);
+        }
+
+        /// <summary>
+        /// Retrieves the image associated with the specified list control item, based on its selection and enabled
+        /// state.
+        /// </summary>
+        /// <remarks>The returned image reflects the item's visual state: selected, normal, or disabled,
+        /// depending on the parameters provided and the enabled state of the container.</remarks>
+        /// <param name="item">The list control item for which to retrieve the image. Can be null if no item is specified.</param>
+        /// <param name="listBox">The container that holds the list control item. Can be null if
+        /// the item is not associated with a container.</param>
+        /// <param name="isSelected">true if the item is currently selected; otherwise, false.</param>
+        /// <returns>The image corresponding to the item's current state, or null if no image is available for the specified item
+        /// and state.</returns>
+        public virtual Image? GetImage(ListControlItem? item, IListControlItemContainer? listBox, bool isSelected)
+        {
+            var itemImages = GetItemImages(item, listBox);
+            var normalImage = itemImages[VisualControlState.Normal];
+            var disabledImage = itemImages[VisualControlState.Disabled];
+            var selectedImage = itemImages[VisualControlState.Selected];
+
+            var image = ListControlItem.IsContainerEnabled(listBox)
+                ? (isSelected ? selectedImage : normalImage) : disabledImage;
+            return image;
+        }
+
+        /// <summary>
+        /// Retrieves the image associated with the current item state, based on whether the item is selected and
+        /// enabled.
+        /// </summary>
+        /// <remarks>If the item is disabled, the disabled image is returned regardless of the selection
+        /// state. If the item is enabled, the selected or normal image is returned based on the value of
+        /// isSelected.</remarks>
+        /// <param name="isSelected">true to retrieve the image for the selected state;
+        /// false to retrieve the image for the normal or disabled
+        /// state.</param>
+        /// <returns>An Image representing the item's visual state. Returns null if no image
+        /// is defined for the current state.</returns>
+        public virtual Image? GetImage(bool isSelected)
+        {
+            return GetImage(Item, ListBox, isSelected);
+        }
 
         /// <summary>
         /// Gets text color.
