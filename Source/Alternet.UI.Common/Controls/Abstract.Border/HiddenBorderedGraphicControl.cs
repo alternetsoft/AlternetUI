5da34ff34e1271133f126dbc9184446376d27539
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-01-18 13:24:30 +0200

    Create HiddenBorderedGraphicControl.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Border/HiddenBorderedGraphicControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// This is the <see cref="BorderedGraphicControl"/> descendant which is initialized with the hidden border.
+    /// </summary>
+    public partial class HiddenBorderedGraphicControl : Border
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HiddenBorder"/> class.
+        /// </summary>
+        public HiddenBorderedGraphicControl()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override bool GetDefaultHasBorder() => false;
+    }
+}
