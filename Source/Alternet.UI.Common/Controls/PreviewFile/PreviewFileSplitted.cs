bfbcf2c77fe83e6be953a5bf95e0665e8061bace
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-02-23 17:29:26 +0200

    PreviewFileSplitted: Add alignment API for second preview panel
    Replace the previous bool-based positioning with ElementContentAlign to control the second preview's placement. Introduces DefaultSecondAlignment static field, a new AlignSecondPanel(ElementContentAlign) method, and explicit SecondPanelToTop/Left helpers. Constructor signature changed from (..., bool isRight) to (..., ElementContentAlign alignment) and now uses DefaultSecondAlignment by default. Also exposes MainPanel property, initializes left/top panel sizes, and ensures panel visibility flags are set consistently when moving the second panel.

--- Source/Alternet.UI.Common/Controls/PreviewFile/PreviewFileSplitted.cs
+++ Source/Alternet.UI.Common/Controls/PreviewFile/PreviewFileSplitted.cs
@@ -13,6 +13,13 @@ namespace Alternet.UI
     /// </summary>
     public partial class PreviewFileSplitted : HiddenBorder, IFilePreview
     {
+        /// <summary>
+        /// Specifies the default alignment for the content of the second preview element.
+        /// </summary>
+        /// <remarks>The default value is set to right alignment. This field can be used to ensure
+        /// consistent alignment behavior for the second element's content across the application.</remarks>
+        public static ElementContentAlign DefaultSecondAlignment = ElementContentAlign.Right;
+
         private readonly SplittedPanel panel = new()
         {
         };
@@ -34,7 +41,7 @@ namespace Alternet.UI
         /// Initializes a new instance of the <see cref="PreviewFileSplitted"/> class.
         /// </summary>
         public PreviewFileSplitted()
-            : this(new PreviewFile(), new PreviewFileDummy(), true)
+            : this(new PreviewFile(), new PreviewFileDummy(), DefaultSecondAlignment)
         {
         }
 
@@ -43,8 +50,8 @@ namespace Alternet.UI
         /// </summary>
         /// <param name="centerPanel">Preview control which is docked in the center.</param>
         /// <param name="secondPanel">Second preview control.</param>
-        /// <param name="isRight">Whether second panel is shown in the right side bar.</param>
-        public PreviewFileSplitted(IFilePreview centerPanel, IFilePreview secondPanel, bool isRight)
+        /// <param name="alignment">Specifies the alignment of the second panel.</param>
+        public PreviewFileSplitted(IFilePreview centerPanel, IFilePreview secondPanel, ElementContentAlign alignment)
         {
             this.first = centerPanel;
             this.second = secondPanel;
@@ -55,27 +62,32 @@ namespace Alternet.UI
                 panel.BottomVisible = false;
                 panel.LeftVisible = false;
                 panel.RightPanel.Width = 300;
+                panel.LeftPanel.Width = 300;
                 panel.BottomPanel.Height = 300;
+                panel.TopPanel.Height = 300;
             });
             first.Control.Parent = panel.CenterPanel;
-            if(isRight)
-                SecondPanelToRight();
-            else
-                SecondPanelToBottom();
+
+            AlignSecondPanel(alignment);
         }
+
+        /// <summary>
+        /// Gets the main panel used for arranging and displaying first and second preview panels.
+        /// </summary>
+        public SplittedPanel MainPanel => panel;
 
         /// <summary>
         /// First preview sub-control which default docking position is at the center.
         /// </summary>
-        public virtual IFilePreview First
+        public IFilePreview First
         {
             get => first;
         }
 
         /// <summary>
-        /// First preview sub-control which default docking position is at the right.
+        /// Second preview sub-control which default docking position is at the right or bottom.
         /// </summary>
-        public virtual IFilePreview Second
+        public IFilePreview Second
         {
             get => second;
         }
@@ -103,11 +115,72 @@ namespace Alternet.UI
         AbstractControl IFilePreview.Control => this;
 
         /// <summary>
+        /// Aligns the second panel to the specified edge of the container.
+        /// </summary>
+        /// <remarks>Use this method to reposition the second panel within the container according to the
+        /// desired alignment. Supplying an invalid alignment value may result in no action or undefined
+        /// behavior.</remarks>
+        /// <param name="alignment">One of the <see cref="ElementContentAlign"/>
+        /// values that specifies the edge (Left, Right, Top, or Bottom) to
+        /// which the second panel will be aligned.</param>
+        public virtual void AlignSecondPanel(ElementContentAlign alignment)
+        {
+            switch (alignment)
+            {
+                case ElementContentAlign.Left:
+                    SecondPanelToLeft();
+                    break;
+                case ElementContentAlign.Right:
+                    SecondPanelToRight();
+                    break;
+                case ElementContentAlign.Top:
+                    SecondPanelToTop();
+                    break;
+                case ElementContentAlign.Bottom:
+                    SecondPanelToBottom();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Moves the second panel to the top position within the main panel and updates the visibility of other panel
+        /// sides accordingly.
+        /// </summary>
+        /// <remarks>Call this method to display the second panel at the top of the main panel while
+        /// hiding the bottom, left, and right panels. Ensure that the second panel is properly initialized and assigned
+        /// before invoking this method.</remarks>
+        public virtual void SecondPanelToTop()
+        {
+            panel.BottomVisible = false;
+            panel.LeftVisible = false;
+            panel.RightVisible = false;
+            second.Control.Parent = panel.TopPanel;
+            panel.TopVisible = true;
+        }
+
+        /// <summary>
+        /// Displays the second panel on the left side of the main panel by adjusting visibility settings.
+        /// </summary>
+        /// <remarks>This method hides the bottom, top, and right panels, and makes the left panel visible
+        /// to accommodate the second panel. Ensure that the second panel is properly initialized before calling this
+        /// method.</remarks>
+        public virtual void SecondPanelToLeft()
+        {
+            panel.BottomVisible = false;
+            panel.TopVisible = false;
+            panel.RightVisible = false;
+            second.Control.Parent = panel.LeftPanel;
+            panel.LeftVisible = true;
+        }
+
+        /// <summary>
         /// Moves second preview panel to the right side bar.
         /// </summary>
         public virtual void SecondPanelToRight()
         {
             panel.BottomVisible = false;
+            panel.TopVisible = false;
+            panel.LeftVisible = false;
             second.Control.Parent = panel.RightPanel;
             panel.RightVisible = true;
         }
@@ -118,6 +191,8 @@ namespace Alternet.UI
         public virtual void SecondPanelToBottom()
         {
             panel.RightVisible = false;
+            panel.TopVisible = false;
+            panel.LeftVisible = false;
             second.Control.Parent = panel.BottomPanel;
             panel.BottomVisible = true;
         }
