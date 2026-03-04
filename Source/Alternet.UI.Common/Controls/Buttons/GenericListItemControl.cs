1ec7cee93abb85099b04c173f446ed15d17baa1b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-04 09:45:45 +0200

    Create GenericListItemControl.cs

---
+++ Source/Alternet.UI.Common/Controls/Buttons/GenericListItemControl.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a user interface control that displays a single <see cref="ListControlItem"/> item.
+    /// </summary>
+    public partial class GenericListItemControl : GenericControl, IListControlItemContainer
+    {
+        private ListControlItemDefaults itemDefaults = new();
+        private ListItemDrawable itemDrawable = new();
+        private ListControlItem? item;
+        private IFormatProvider? formatProvider;
+        private SvgImage? checkImageUnchecked;
+        private SvgImage? checkImageChecked;
+        private SvgImage? checkImageIndeterminate;
+        private ImageList? imageList;
+
+        /// <summary>
+        /// Initializes a new instance of the GenericListItemControl class and associates it with the specified parent
+        /// control, if provided.
+        /// </summary>
+        /// <remarks>Establishing a parent-child relationship between controls can be useful for managing
+        /// layout, event routing, and resource ownership within a user interface.</remarks>
+        /// <param name="parent">The parent control to associate with this item. This parameter can be null if the item does not have a
+        /// parent control.</param>
+        public GenericListItemControl(AbstractControl? parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the GenericListItemControl class.
+        /// </summary>
+        public GenericListItemControl()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the item associated with the control.
+        /// If the item has not been created, it is instantiated when accessed.
+        /// </summary>
+        /// <remarks>When a new item is assigned, the control's layout is updated to reflect the change.</remarks>
+        [Browsable(false)]
+        public virtual ListControlItem Item
+        {
+            get
+            {
+                item ??= CreateItem();
+                OnItemCreatedOrAssigned(item);
+                return item;
+            }
+
+            set
+            {
+                if (value == null || value == item)
+                    return;
+                item = value;
+                OnItemCreatedOrAssigned(item);
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the settings used when item is painted.
+        /// </summary>
+        /// <remarks>Assigning a new value updates the layout to reflect the changed item defaults.</remarks>
+        [Browsable(false)]
+        public virtual ListControlItemDefaults ItemDefaults
+        {
+            get
+            {
+                return itemDefaults;
+            }
+
+            set
+            {
+                if (value == null || value == itemDefaults)
+                    return;
+                itemDefaults = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <inheritdoc/>
+        ObjectUniqueId IListControlItemContainer.UniqueId => UniqueId;
+
+        /// <inheritdoc/>
+        AbstractControl? IListControlItemContainer.Control => this;
+
+        /// <inheritdoc/>
+        IListControlItemDefaults IListControlItemContainer.Defaults => itemDefaults;
+
+        /// <inheritdoc/>
+        float? IListControlItemContainer.ColumnSeparatorWidth => null;
+
+        /// <inheritdoc/>
+        bool IListControlItemContainer.Focused => Focused;
+
+        /// <inheritdoc/>
+        bool IListControlItemContainer.HasColumns => false;
+
+        /// <inheritdoc/>
+        IReadOnlyList<ListControlColumn> IListControlItemContainer.Columns => Array.Empty<ListControlColumn>();
+
+        /// <summary>
+        /// Gets or sets the format provider used to control formatting operations for this control.
+        /// When this property is set, the control will use the specified format provider for any culture-specific formatting operations,
+        /// such as displaying dates, numbers, or other formatted values. If the format provider is null, the control will use
+        /// the default formatting behavior based on the current culture or invariant culture.
+        /// Setting this property allows for customization of how values are formatted and displayed by the control,
+        /// providing flexibility in localization and presentation of data.
+        /// </summary>
+        [Browsable(false)]
+        public virtual IFormatProvider? FormatProvider
+        {
+            get
+            {
+                return formatProvider;
+            }
+
+            set
+            {
+                formatProvider = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the image displayed when the checkbox is in the unchecked state.
+        /// This is used when checkboxes are shown in the item.
+        /// </summary>
+        /// <remarks>Setting this property to null displays the default image for the unchecked state. Use
+        /// this property to customize the appearance of the checkbox.</remarks>
+        [Browsable(false)]
+        public virtual SvgImage? CheckImageUnchecked
+        {
+            get => checkImageUnchecked;
+            set
+            {
+                if (checkImageUnchecked == value)
+                    return;
+                checkImageUnchecked = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the image displayed when the checkbox is in the checked state.
+        /// This is used when checkboxes are shown in the item.
+        /// </summary>
+        /// <remarks>Setting this property to null displays the default image for the checked state. Use
+        /// this property to customize the appearance of the checkbox.</remarks>
+        [Browsable(false)]
+        public virtual SvgImage? CheckImageChecked
+        {
+            get => checkImageChecked;
+            set
+            {
+                if (checkImageChecked == value)
+                    return;
+                checkImageChecked = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the image displayed when the checkbox is in the indeterminate state.
+        /// This is used when checkboxes are shown in the item.
+        /// </summary>
+        /// <remarks>Setting this property to null displays the default image for the indeterminate state. Use
+        /// this property to customize the appearance of the checkbox.</remarks>
+        [Browsable(false)]
+        public virtual SvgImage? CheckImageIndeterminate
+        {
+            get => checkImageIndeterminate;
+            set
+            {
+                if (checkImageIndeterminate == value)
+                    return;
+                checkImageIndeterminate = value;
+                PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the collection of images associated with the control.
+        /// </summary>
+        /// <remarks>Setting this property updates the control's layout and refreshes its display. This
+        /// property is not visible in the property grid.</remarks>
+        [Browsable(false)]
+        public virtual ImageList? ImageList
+        {
+            get => imageList;
+            set
+            {
+                if (imageList == value)
+                    return;
+                imageList = value;
+                if (Item.HasValidImageIndex)
+                    PerformLayoutAndInvalidate();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new instance of a ListControlItem. Derived classes can override
+        /// this method to provide custom item creation logic.
+        /// </summary>
+        /// <returns>A new instance of ListControlItem.</returns>
+        protected virtual ListControlItem CreateItem()
+        {
+            return new();
+        }
+
+        /// <summary>
+        /// Invoked when a list item is created or assigned, allowing derived classes to perform custom processing or
+        /// initialization for the item.
+        /// </summary>
+        /// <remarks>Override this method in a derived class to implement additional behavior when a new
+        /// item is added to the control or an existing item is assigned. This method is called each time an item is
+        /// created or assigned to the control.</remarks>
+        /// <param name="item">The list item that has been created or assigned and will be associated with the control.</param>
+        protected virtual void OnItemCreatedOrAssigned(ListControlItem item)
+        {
+            itemDrawable.Item = item;
+        }
+
+        ListControlItem? IListControlItemContainer.SafeItem(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IListControlItemContainer.GetItemText(int index, bool forDisplay)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IListControlItemContainer.GetItemText(ListControlItem? item, bool forDisplay)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IListControlItemContainer.GetItemCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
