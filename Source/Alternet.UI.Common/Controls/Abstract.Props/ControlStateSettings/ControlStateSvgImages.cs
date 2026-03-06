c909d6545415e92e955e5d2376c56671b6904c69
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-06 18:30:09 +0200

    Create ControlStateSvgImages.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Props/ControlStateSettings/ControlStateSvgImages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies a set of <see cref="SvgImage"/> for different control states.
+    /// </summary>
+    public class ControlStateSvgImages : ControlStateObjects<SvgImage>
+    {
+        /// <summary>
+        /// Gets <see cref="ControlStateSvgImages"/> with empty state images.
+        /// </summary>
+        public static readonly ControlStateSvgImages Empty;
+
+        static ControlStateSvgImages()
+        {
+            Empty = new();
+            Empty.SetImmutable();
+        }
+
+        /// <summary>
+        /// Creates clone of this object.
+        /// </summary>
+        /// <returns></returns>
+        public virtual ControlStateSvgImages Clone()
+        {
+            var result = new ControlStateSvgImages();
+            result.Normal = Normal;
+            result.Hovered = Hovered;
+            result.Pressed = Pressed;
+            result.Disabled = Disabled;
+            result.Focused = Focused;
+            return result;
+        }
+    }
+}
