ae7cc44672f8146ea2cde1e64f16b61edca948a9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-08 19:53:37 +0200

    Update StdButton.cs

--- Source/Alternet.UI.Common/Controls/Buttons/StdButton.cs
+++ Source/Alternet.UI.Common/Controls/Buttons/StdButton.cs
@@ -45,7 +45,7 @@ namespace Alternet.UI
         /// </summary>
         public static bool DefaultCornerRadiusIsPercent = true;
 
-        private static LightDarkColor? defaultHotBorderColor;
+        private static LightDarkColor? defaultHoveredBorderColor;
         private static LightDarkColor? defaultBorderColor;
         private static LightDarkColor? defaultBorderColorIsd;
         private static LightDarkColor? defaultHoveredBorderColorIsd;
@@ -53,6 +53,9 @@ namespace Alternet.UI
         private static LightDarkColor? defaultForeColorIsd;
         private static LightDarkColor? defaultBackColor;
         private static LightDarkColor? defaultForeColor;
+        private static LightDarkColor? defaultHoveredBackColorIsd;
+        private static LightDarkColor? defaultHoveredBackColor;
+        private static LightDarkColor? defaultPressedBackColor;
 
         private bool isDefault;
         private bool isCancel;
@@ -165,11 +168,21 @@ namespace Alternet.UI
 
         /// <summary>
         /// Gets or sets the default background color used for <see cref="StdButton"/> control when
-        /// it's <see cref="IsDefault"/> property is set to true.
+        /// it is in hovered state and its <see cref="IsDefault"/> property is set to true.
         /// </summary>
-        public static LightDarkColor DefaultBackColorIsd
+        public static LightDarkColor DefaultHoveredBackColorIsd
         {
-            get => defaultBackColorIsd ?? DefaultBackColor;
+            get => defaultHoveredBackColorIsd ?? DefaultHoveredBackColor;
+            set => defaultHoveredBackColorIsd = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the default background color used for <see cref="StdButton"/> control when
+        /// its <see cref="IsDefault"/> property is set to true.
+        /// </summary>
+        public static LightDarkColor DefaultNormalBackColorIsd
+        {
+            get => defaultBackColorIsd ?? DefaultNormalBackColor;
             set => defaultBackColorIsd = value;
         }
 
@@ -179,23 +192,42 @@ namespace Alternet.UI
         /// </summary>
         public static LightDarkColor DefaultForeColorIsd
         {
-            get => defaultForeColorIsd ?? DefaultForeColor;
+            get => defaultForeColorIsd ?? DefaultNormalForeColor;
             set => defaultForeColorIsd = value;
         }
 
         /// <summary>
         /// Gets or sets the default background color used for <see cref="StdButton"/> control.
         /// </summary>
-        public static new LightDarkColor DefaultBackColor
+        public static LightDarkColor DefaultNormalBackColor
         {
             get => defaultBackColor ??= new (light: (245, 245, 245), dark: (63, 63, 63));
             set => defaultBackColor = value;
         }
 
         /// <summary>
+        /// Gets or sets the default background color used for the pressed state of buttons.
+        /// </summary>
+        public static LightDarkColor DefaultPressedBackColor
+        {
+            get => defaultPressedBackColor ?? DefaultColors.ControlBackColor;
+            set => defaultPressedBackColor = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the default background color used for <see cref="StdButton"/> control when it is in a hovered state.
+        /// </summary>
+        public static LightDarkColor DefaultHoveredBackColor
+        {
+            get => defaultHoveredBackColor ??= new(light: (235, 235, 235), dark: (69, 69, 69));
+            set => defaultHoveredBackColor = value;
+        }
+
+
+        /// <summary>
         /// Gets or sets the default foreground color used for <see cref="StdButton"/> control.
         /// </summary>
-        public static new LightDarkColor DefaultForeColor
+        public static LightDarkColor DefaultNormalForeColor
         {
             get => defaultForeColor ?? DefaultColors.ControlForeColor;
             set => defaultForeColor = value;
@@ -209,12 +241,12 @@ namespace Alternet.UI
         {
             get
             {
-                return defaultHotBorderColor ?? DefaultColors.AccentColor;
+                return defaultHoveredBorderColor ?? DefaultBorderColor;
             }
 
             set
             {
-                defaultHotBorderColor = value;
+                defaultHoveredBorderColor = value;
             }
         }
 
@@ -260,7 +292,7 @@ namespace Alternet.UI
         {
             get
             {
-                return defaultHoveredBorderColorIsd ?? DefaultColors.AccentColor;
+                return defaultHoveredBorderColorIsd ?? DefaultBorderColor;
             }
 
             set
@@ -499,7 +531,7 @@ namespace Alternet.UI
             }
             else
             {
-                return DefaultForeColor.LightOrDark(isd);
+                return DefaultNormalForeColor.LightOrDark(isd);
             }
         }
 
@@ -516,11 +548,47 @@ namespace Alternet.UI
 
             if (IsDefault)
             {
-                return DefaultBackColorIsd.LightOrDark(isd);
+                return DefaultNormalBackColorIsd.LightOrDark(isd);
             }
             else
             {
-                return DefaultBackColor.LightOrDark(isd);
+                return DefaultNormalBackColor.LightOrDark(isd);
+            }
+        }
+
+        /// <summary>
+        /// Gets the effective background color for the pressed state, adjusted for the specified or current appearance setting.
+        /// </summary>
+        /// <remarks>Use this method to obtain the appropriate pressed background color for a button,
+        /// ensuring consistency with the application's light or dark mode.</remarks>
+        /// <param name="isDark">An optional value indicating whether to use dark appearance. If null, the method uses the system's current
+        /// appearance setting.</param>
+        /// <returns>A Color representing the background color to use when the button is pressed, based on the effective
+        /// appearance.</returns>
+        public virtual Color GetEffectivePressedBackColor(bool? isDark = null)
+        {
+            var isd = isDark ?? SystemSettings.AppearanceIsDark;
+            return DefaultPressedBackColor.LightOrDark(isd);
+        }
+
+        /// <summary>
+        /// Gets the effective background color for the hovered state based on the specified dark mode and other settings.
+        /// </summary>
+        /// <param name="isDark">A value indicating whether the application is in dark mode. If <see langword="true"/>, a color suitable for
+        /// dark backgrounds is returned; otherwise, a color suitable for light backgrounds is returned.</param>
+        /// <returns>A <see cref="Color"/> representing the effective background color for the hovered state, determined by the current default
+        /// settings and the dark mode parameter.</returns>
+        public virtual Color GetEffectiveHoveredBackColor(bool? isDark = null)
+        {
+            var isd = isDark ?? SystemSettings.AppearanceIsDark;
+
+            if (IsDefault)
+            {
+                return DefaultHoveredBackColorIsd.LightOrDark(isd);
+            }
+            else
+            {
+                return DefaultHoveredBackColor.LightOrDark(isd);
             }
         }
 
@@ -538,6 +606,34 @@ namespace Alternet.UI
         public virtual void SetColorThemeToLight(ColorThemeApplyOptions opt = ColorThemeApplyOptions.All)
         {
             SetColorThemeToLightOrDark(isDark: false, opt);
+        }
+
+        /// <inheritdoc/>
+        public override Brush? GetBackground(VisualControlState state)
+        {
+            Color color;
+
+            if (state == VisualControlState.Hovered)
+            {
+                color = GetEffectiveHoveredBackColor();
+            }
+            else
+            if (state == VisualControlState.Pressed)
+            {
+                color = GetEffectivePressedBackColor();
+            }
+            else
+            {
+                color = GetEffectiveBackColor();
+            }
+
+            return color.AsBrush;
+        }
+
+        /// <inheritdoc/>
+        public override void DefaultPaint(PaintEventArgs e)
+        {
+            base.DefaultPaint(e);
         }
 
         /// <inheritdoc/>
