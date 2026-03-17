49f7fcd1de00750547adf288eadde0d8b202381b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-17 06:23:40 +0200

    Update nuget.source.add.bat

--- Install.Scripts/AdditionalLocalPackages/nuget.source.add.bat
+++ Install.Scripts/AdditionalLocalPackages/nuget.source.add.bat
@@ -3,5 +3,4 @@ set SCRIPT_HOME=%~dp0.
 mkdir "c:\AlternetTest.6AB52"
 dotnet nuget add source "c:\AlternetTest.6AB52" -n AlternetTest
 
-call Info.DotNet.Nuget.Source.List.bat
 echo ================
