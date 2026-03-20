256e3dd9a17fb2a7b850a688171505be954714e9
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-20 12:29:14 +0200

    Create GenericPanel.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Derived/GenericPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Used as a container for generic other controls.
+    /// </summary>
+    [ControlCategory("Containers")]
+    public partial class GenericPanel : GenericContainerControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericPanel"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public GenericPanel(AbstractControl parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericPanel"/> class.
+        /// </summary>
+        public GenericPanel()
+        {
+            TabStop = false;
+            CanSelect = false;
+            ParentBackColor = true;
+            ParentForeColor = true;
+        }
+
+        /// <inheritdoc/>
+        public override ControlTypeId ControlKind => ControlTypeId.Panel;
+
+        [Browsable(false)]
+        internal new string Text
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+    }
+}
