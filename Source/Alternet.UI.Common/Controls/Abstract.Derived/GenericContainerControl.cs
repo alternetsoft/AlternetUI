ae4c0f27d9b5c133a6ae2b74a03ecaa311e46df7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-20 12:29:07 +0200

    Create GenericContainerControl.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.Derived/GenericContainerControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Alternet.Drawing;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Base class for all generic container controls.
+    /// </summary>
+    public partial class GenericContainerControl : HiddenGenericBorder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericContainerControl"/> class.
+        /// </summary>
+        /// <param name="parent">Parent of the control.</param>
+        public GenericContainerControl(AbstractControl parent)
+            : this()
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericContainerControl"/> class.
+        /// </summary>
+        public GenericContainerControl()
+        {
+            TabStop = false;
+            CanSelect = false;
+            ParentBackColor = true;
+            ParentForeColor = true;
+        }
+    }
+}
