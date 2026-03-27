1af6ad597db06a53d84e49d3527def17863f171b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-03-27 17:40:51 +0200

    Update CopyAdditionalFilesToSite.bat

--- Documentation/Alternet.UI.Documentation/CopyAdditionalFilesToSite.bat
+++ Documentation/Alternet.UI.Documentation/CopyAdditionalFilesToSite.bat
@@ -2,7 +2,7 @@ SETLOCAL EnableDelayedExpansion
 
 set SCRIPT_HOME=%~dp0.
 
-set ROBOTS=%SCRIPT_HOME%\..\..\..\AlternetStudio\Documentation\Alternet.Studio.Documentation\robots.txt
+set ROBOTS=%SCRIPT_HOME%\robots.txt
 set ROBOTSRESULT=%SCRIPT_HOME%\site\robots.txt
 
 del "%ROBOTSRESULT%"
