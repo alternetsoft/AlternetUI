8c1950a0c648fb139d80cb0dde9281321d650c77
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-05-15 00:23:41 +0300

    Create PerformLayoutReason.cs

---
+++ Source/Alternet.UI.Common/Controls/Abstract.LayoutManager/PerformLayoutReason.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines the reason for performing layout.
+    /// </summary>
+    public enum PerformLayoutReason
+    {
+        /// <summary>
+        /// The reason for performing layout is not specified.
+        /// </summary>
+        NotSpecified,
+
+        /// <summary>
+        /// The reason for performing layout is that the control's <see cref="AbstractControl.SuggestedSize"/> property has changed.
+        /// </summary>
+        SuggestedSizeChanged,
+    }
+}
