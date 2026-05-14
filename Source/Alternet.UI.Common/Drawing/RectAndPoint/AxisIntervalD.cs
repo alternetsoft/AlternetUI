aaeee2a897d2e0ce0331e8aa79a96fab8d1fc261
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-05-14 13:37:22 +0300

    Implement equality and ToString for AxisIntervalD

--- Source/Alternet.UI.Common/Drawing/RectAndPoint/AxisIntervalD.cs
+++ Source/Alternet.UI.Common/Drawing/RectAndPoint/AxisIntervalD.cs
@@ -9,7 +9,7 @@ namespace Alternet.Drawing
     /// commonly used in layout calculations to define the starting point and the extent of a layout
     /// item along a specific axis (horizontal or vertical).
     /// </summary>
-    public readonly struct AxisIntervalD
+    public readonly struct AxisIntervalD : IEquatable<AxisIntervalD>
     {
         /// <summary>
         /// Represents an empty start and length, where both start and length are set to zero.
@@ -56,5 +56,47 @@ namespace Alternet.Drawing
         /// Returns true if <paramref name="value"/> lies within the interval using closed semantics: [Start, End].
         /// </summary>
         public bool ContainsClosed(Coord value) => value >= Start && value <= End;
+
+        /// <summary>
+        /// Determines whether two specified <see cref="AxisIntervalD"/> values are equal.
+        /// </summary>
+        /// <param name="left">The first value to compare.</param>
+        /// <param name="right">The second value to compare.</param>
+        /// <returns><see langword="true"/> if <paramref name="left"/> is equal to <paramref name="right"/>; otherwise, <see
+        /// langword="false"/>.</returns>
+        public static bool operator ==(AxisIntervalD left, AxisIntervalD right) => left.Equals(right);
+
+        /// <summary>
+        /// Determines whether two specified <see cref="AxisIntervalD"/> instances are not equal.
+        /// </summary>
+        /// <param name="left">The first instance to compare.</param>
+        /// <param name="right">The second instance to compare.</param>
+        /// <returns><see langword="true"/> if <paramref name="left"/> and <paramref name="right"/> are not equal; otherwise,
+        /// <see langword="false"/>.</returns>
+        public static bool operator !=(AxisIntervalD left, AxisIntervalD right) => !left.Equals(right);
+
+        /// <inheritdoc/>
+        public override string? ToString()
+        {
+            return $"Start: {Start}, Length: {Length}, End: {End}";
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj)
+        {
+            return obj is AxisIntervalD other && Start == other.Start && Length == other.Length;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Start, Length);
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(AxisIntervalD other)
+        {
+            return Start == other.Start && Length == other.Length;
+        }
     }
 }
