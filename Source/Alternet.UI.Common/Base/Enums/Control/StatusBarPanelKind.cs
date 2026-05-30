8997f869345802c3f8b3869542b001aed1494dcc
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-05-30 14:45:12 +0300

    Create StatusBarPanelKind.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/StatusBarPanelKind.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Enumerates the types of the individual panel in the status bar.
+    /// </summary>
+    public enum StatusBarPanelKind
+    {
+        /// <summary>
+        /// The panel is a text panel that displays text.
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// The panel is a separator that displays a vertical line to separate other panels.
+        /// The <see cref="StatusBarPanel.Text"/> and other properties are ignored for this panel.
+        /// </summary>
+        Separator,
+    }
+}
