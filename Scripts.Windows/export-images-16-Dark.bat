b0ea977bc1c610ebae16c8110d045d092c3711a6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-05-30 11:45:56 +0300

    Create svg image export scripts

---
+++ Scripts.Windows/export-images-16-Dark.bat
@@ -0,0 +1,7 @@
+
+
+pushd ..\Source\Tools\Alternet.UI.RunCmdAdv\
+                                            
+dotnet run -- -r=exportSvg Size=16 Result="E:\Images" IsDark=true
+
+popd
---
+++ Scripts.Windows/export-images-32-Dark.bat
@@ -0,0 +1,7 @@
+
+
+pushd ..\Source\Tools\Alternet.UI.RunCmdAdv\
+                                            
+dotnet run -- -r=exportSvg Size=32 Result="E:\Images" IsDark=true
+
+popd
