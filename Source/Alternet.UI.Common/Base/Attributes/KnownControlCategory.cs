a2ad4fc575b40cb6faab5cbe32c0359408b31925
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-03 14:15:14 +0300

    Create KnownControlCategory.cs

---
+++ Source/Alternet.UI.Common/Base/Attributes/KnownControlCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines known control categories for design-time support.
+    /// </summary>
+    public static class KnownControlCategory
+    {
+        /// <summary>
+        /// Indicates that the control is hidden and should not be displayed in the designer toolbox.
+        /// </summary>
+        public const string Hidden = "Hidden";
+
+        /// <summary>
+        /// Indicates that the control is for internal use only and should not be displayed in the designer toolbox.
+        /// </summary>
+        public const string Internal = "Internal";
+    }
+}
