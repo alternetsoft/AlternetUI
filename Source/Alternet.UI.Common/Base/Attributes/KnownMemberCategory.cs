50055191bfa49740995efe1b2b9137c70b83f772
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-03 14:15:19 +0300

    Create KnownMemberCategory.cs

---
+++ Source/Alternet.UI.Common/Base/Attributes/KnownMemberCategory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines known categories for members of objects, such as properties and events.
+    /// </summary>
+    public static class KnownMemberCategory
+    {
+        /// <summary>
+        /// Indicates that the member belongs to the "Layout" category,
+        /// which includes properties and events related to the layout of controls.
+        /// </summary>
+        public const string Layout = "Layout";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Behavior" category,
+        /// which includes properties and events related to the behavior of controls.
+        /// </summary>
+        public const string Behavior = "Behavior";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Mouse" category,
+        /// which includes properties and events related to mouse interactions.
+        /// </summary>
+        public const string Mouse = "Mouse";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Focus" category,
+        /// which includes properties and events related to focus management.
+        /// </summary>
+        public const string Focus = "Focus";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Property Changed" category,
+        /// which includes properties and events related to property changes.
+        /// </summary>
+        public const string PropertyChanged = "Property Changed";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Private" category,
+        /// which includes properties and events that are intended for internal use only.
+        /// </summary>
+        public const string Private = "Private";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Drag Drop" category,
+        /// which includes properties and events related to drag-and-drop operations.
+        /// </summary>
+        public const string DragDrop = "Drag Drop";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Key" category,
+        /// which includes properties and events related to keyboard interactions.
+        /// </summary>
+        public const string Key = "Key";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Action" category,
+        /// which includes properties and events related to actions performed by the control.
+        /// </summary>
+        public const string Action = "Action";
+        
+        /// <summary>
+        /// Indicates that the member belongs to the "Appearance" category,
+        /// which includes properties and events related to the visual appearance of the control.
+        /// </summary>
+        public const string Appearance = "Appearance";       
+    }
+}
+
+
