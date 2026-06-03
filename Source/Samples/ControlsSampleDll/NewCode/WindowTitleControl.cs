801eec108e9a703186ca65169892edd2fc698f6d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-03 17:25:29 +0300

    Create WindowTitleControl.cs

---
+++ Source/Samples/ControlsSampleDll/NewCode/WindowTitleControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a control that can be used as a window title bar.
+    /// </summary>
+    public partial class WindowTitleControl : ToolBar
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindowTitleControl"/> class.
+        /// </summary>
+        public WindowTitleControl()
+        {
+        }
+    }
+}
