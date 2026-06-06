c511fe641b1a4bbe99755653bf8a86aecaabd561
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-06 12:13:29 +0300

    Create SvgImageSet.cs

---
+++ Source/Alternet.UI.Common/Drawing/ImageContainer/SvgImageSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Provides a set of images in different resolutions based on the same svg image.
+    /// </summary>
+    public class SvgImageSet : ImageSet
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SvgImageSet"/> class.
+        /// </summary>
+        public SvgImageSet()
+        {
+        }
+    }
+}
