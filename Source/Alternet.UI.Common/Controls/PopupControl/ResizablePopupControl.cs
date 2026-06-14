e23bc221e931cb5230c0f9b8a65b277ba67a1e88
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-14 09:20:44 +0300

    Create ResizablePopupControl.cs

---
+++ Source/Alternet.UI.Common/Controls/PopupControl/ResizablePopupControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a resizable popup control with a window like border and title bar.
+    /// </summary>
+    public partial class ResizablePopupControl : PopupControl
+    {
+        private readonly ResizableWindowBorder border = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResizablePopupControl"/> class.
+        /// </summary>
+        public ResizablePopupControl()
+        {
+            border.ParentFont = true;
+            border.Parent = this;
+        }
+
+        /// <summary>
+        /// Gets the border control of the popup.
+        /// </summary>
+        public ResizableWindowBorder BorderControl => border;
+    }
+}
