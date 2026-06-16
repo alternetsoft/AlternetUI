4b47f735f6317cfb79c93132b3bb83b65a327ef1
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-16 09:33:29 +0300

    Create InnerPopupTreeView.cs

---
+++ Source/Alternet.UI.Common/Controls/PopupControl/InnerPopupTreeView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a tree view control used in inner popups.
+    /// This control is supposed to be shown inside the client area of another control.
+    /// </summary>
+    public partial class InnerPopupTreeView : ResizablePopupControl<StdTreeView>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InnerPopupTreeView"/> class.
+        /// </summary>
+        public InnerPopupTreeView()
+        {
+        }
+    }
+}
