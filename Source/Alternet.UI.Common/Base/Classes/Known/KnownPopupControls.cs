e617d2b125a450e93028fad6da89df46069ec872
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-19 16:47:41 +0300

    Create KnownPopupControls.cs

---
+++ Source/Alternet.UI.Common/Base/Classes/Known/KnownPopupControls.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a class that provides factory methods for creating instances of inner popup controls.
+    /// </summary>
+    public class KnownPopupControls
+    {
+        /// <summary>
+        /// Gets or sets the default instance of <see cref="KnownPopupControls"/> used by the framework.
+        /// </summary>
+        public static KnownPopupControls Default { get; set; } = new ();
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="InnerPopupToolBar"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="InnerPopupToolBar"/> class.</returns>
+        public virtual InnerPopupToolBar CreateInnerPopupToolBar()
+        {
+            return new InnerPopupToolBar();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="InnerPopupTreeView"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="InnerPopupTreeView"/> class.</returns>
+        public virtual InnerPopupTreeView CreateInnerPopupTreeView()
+        {
+            return new InnerPopupTreeView();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="PopupControl{T}"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="PopupControl{T}"/> class.</returns>
+        public virtual PopupControl<T> CreatePopupControl<T>()
+            where T : AbstractControl, new()
+        {
+            return new PopupControl<T>();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="PopupControl"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="PopupControl"/> class.</returns>
+        public virtual PopupControl CreatePopupControl()
+        {
+            return new PopupControl();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ResizablePopupControl{T}"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="ResizablePopupControl{T}"/> class.</returns>
+        public virtual ResizablePopupControl<T> CreateResizablePopupControl<T>(bool useScrollViewer)
+            where T : AbstractControl, new()
+        {
+            return new ResizablePopupControl<T>(useScrollViewer);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ResizablePopupControl"/> class.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="ResizablePopupControl"/> class.</returns>
+        public virtual ResizablePopupControl CreateResizablePopupControl()
+        {
+            return new ResizablePopupControl();
+        }
+    }
+}
