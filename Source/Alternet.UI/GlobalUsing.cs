2fa8eb6725ca2cb582f4c398e441ef1cad424dfc
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-23 15:38:41 +0300

    Move native controls out of the common library

--- Source/Alternet.UI/GlobalUsing.cs
+++ Source/Alternet.UI/GlobalUsing.cs
@@ -1,5 +1,7 @@
 ﻿global using Coord = float;
 
+[assembly: Alternet.UI.XmlnsDefinition("http://schemas.alternetsoft.com/ui/2021", "Alternet.UI")]
+
 #pragma warning disable
 namespace Alternet.Base.Collections
 {
