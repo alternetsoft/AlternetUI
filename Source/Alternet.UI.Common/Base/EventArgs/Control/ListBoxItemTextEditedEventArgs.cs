6bebb31e2f51eece916f39e15fdcd41220873213
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-26 19:41:00 +0300

    Create ListBoxItemTextEditedEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Control/ListBoxItemTextEditedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the <see cref="VirtualListBox.ItemTextEdited"/> event.
+    /// </summary>
+    public class ListBoxItemTextEditedEventArgs : ListBoxItemEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListBoxItemTextEditedEventArgs"/>
+        /// class with the empty new text.
+        /// </summary>
+        public ListBoxItemTextEditedEventArgs()
+        {
+            NewText = string.Empty;
+        }
+
+        /// <summary>
+        /// Gets or sets the new text of the item after editing.
+        /// </summary>
+        public virtual string NewText { get; set; }
+    }
+}
