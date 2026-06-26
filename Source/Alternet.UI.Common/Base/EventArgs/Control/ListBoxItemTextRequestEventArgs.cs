6a7e6233079f37d18371faf49f1ffebdd84b0852
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-26 19:41:03 +0300

    Create ListBoxItemTextRequestEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Control/ListBoxItemTextRequestEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the <see cref="VirtualListBox.EditorTextRequested"/> event.
+    /// </summary>
+    public class ListBoxItemTextRequestEventArgs : ListBoxItemEventArgs
+    {
+        /// <summary>
+        /// Gets or sets the text for the item editor.
+        /// </summary>
+        public string? Text { get; set; }
+    }
+}
