5ab670357b00888c37c3c47e7272e2eaa01fefb0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-26 19:40:57 +0300

    Create ListBoxItemEventArgs.cs

---
+++ Source/Alternet.UI.Common/Base/EventArgs/Control/ListBoxItemEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Provides data for the events related to a list control item.
+    /// Includes the item itself and its index in the list control.
+    /// </summary>
+    public class ListBoxItemEventArgs : BaseEventArgs
+    {
+        /// <summary>
+        /// Gets or sets the index of the item for which the event is raised.
+        /// </summary>
+        public int ItemIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item for which the event is raised.
+        /// </summary>
+        public ListControlItem? Item { get; set; }
+    }
+}
