f19f478f0e98b40f4032e7c3d5dcd63a1f7ee64c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-29 14:00:08 +0300

    Create StdListView.cs

---
+++ Source/Alternet.UI.Common/Controls/ListControls/StdListView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a generic list view control implemented inside the library.
+    /// </summary>
+    public partial class StdListView : StdTreeView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StdListView"/> class.
+        /// </summary>
+        public StdListView()
+        {
+        }
+    }
+}
