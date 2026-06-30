0a8e048ecafc4b2f46a0f10313088af34d74b0a0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-06-30 07:02:21 +0300

    Create ImageLayout.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Image/ImageLayout.cs
@@ -0,0 +1,32 @@
+﻿namespace Alternet.Drawing;
+
+/// <summary>
+///  Specifies the position of the image on the drawing surface.
+/// </summary>
+public enum ImageLayout
+{
+    /// <summary>
+    ///  The image is left-aligned at the top across the drawing surface's bounds.
+    /// </summary>
+    None,
+
+    /// <summary>
+    ///  The image is tiled across the drawing surface's bounds.
+    /// </summary>
+    Tile,
+
+    /// <summary>
+    ///  The image is centered within the drawing surface's bounds.
+    /// </summary>
+    Center,
+
+    /// <summary>
+    ///  The image is stretched across the drawing surface's bounds.
+    /// </summary>
+    Stretch,
+
+    /// <summary>
+    ///  The image is enlarged within the drawing surface's bounds.
+    /// </summary>
+    Zoom,
+}
