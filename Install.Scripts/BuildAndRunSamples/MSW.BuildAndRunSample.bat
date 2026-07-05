96799f229bfd4c94033490c38d7cba4d2fc4a5bb
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-05 16:22:21 +0300

    Update run scripts

--- Install.Scripts/BuildAndRunSamples/MSW.BuildAndRunSample.bat
+++ Install.Scripts/BuildAndRunSamples/MSW.BuildAndRunSample.bat
@@ -2,13 +2,6 @@ SETLOCAL EnableDelayedExpansion
 set SCRIPT_HOME=%~dp0.
 set sampleHome=%1
 set sampleName=%2
-set platform=%3
-
-if "%platform%"==x64 goto ok
-if "%platform%"==x86 goto ok
-set platform=x64
-:ok
-ECHO platform: %platform%
 
 pushd %sampleHome%
 pushd %sampleName%
