005a96fa179bad7fc946bb3e393168c6146c3621
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-28 14:18:25 +0300

    Create LinearGradientMode.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Brush/LinearGradientMode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Specifies the direction of a linear gradient.
+    /// </summary>
+    public enum LinearGradientMode
+    {
+        /// <summary>
+        /// Specifies a horizontal linear gradient.
+        /// </summary>
+        Horizontal,
+
+        /// <summary>
+        /// Specifies a vertical linear gradient.
+        /// </summary>
+        Vertical,
+
+        /// <summary>
+        /// Specifies a diagonal linear gradient from upper-left to lower-right.
+        /// </summary>
+        ForwardDiagonal,
+
+        /// <summary>
+        /// Specifies a diagonal linear gradient from upper-right to lower-left.
+        /// </summary>
+        BackwardDiagonal
+    }
+}
