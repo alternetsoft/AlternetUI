09f26e89433fc3d0cb510a2e79b1656cefa441ab
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-29 08:07:50 +0300

    Create SmoothingMode.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Other/SmoothingMode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Specifies the smoothing mode for rendering.
+    /// </summary>
+    public enum SmoothingMode
+    {
+        /// <summary>
+        /// Specifies an invalid smoothing mode.
+        /// </summary>
+        Invalid = -1,
+
+        /// <summary>
+        /// Specifies no antialiasing smoothing mode.
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        /// Specifies no antialiasing smoothing mode.
+        /// </summary>
+        HighSpeed = 1,
+
+        /// <summary>
+        /// Specifies antialiased smoothing mode.
+        /// </summary>
+        HighQuality = 2,
+
+        /// <summary>
+        /// Specifies no antialiasing smoothing mode.
+        /// </summary>
+        None = 3,
+
+        /// <summary>
+        /// Specifies antialiased smoothing mode.
+        /// </summary>
+        AntiAlias = 4
+    }
+}
