4e59775179f0ae3a5552c5134bee956667ff0496
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-29 20:52:43 +0300

    Create ToolTipIcon.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/ToolTip/ToolTipIcon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines the icon type for a tooltip.
+    /// </summary>
+    public enum ToolTipIcon
+    {
+        /// <summary>
+        /// No Icon.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// An Information Icon.
+        /// </summary>
+        Info = 1,
+
+        /// <summary>
+        /// A Warning Icon.
+        /// </summary>
+        Warning = 2,
+
+        /// <summary>
+        /// An Error Icon.
+        /// </summary>
+        Error = 3,
+    }
+}
