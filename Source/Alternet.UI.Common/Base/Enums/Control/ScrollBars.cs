5ca16d3bb8beb9462909ac636e7191667d05edf6
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-29 08:24:52 +0300

    Create ScrollBars.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/ScrollBars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the scroll bars to be displayed in a control or container.
+    /// </summary>
+    public enum ScrollBars
+    {
+        /// <summary>
+        /// No scroll bars are shown.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Only horizontal scroll bars are shown.
+        /// </summary>
+        Horizontal,
+
+        /// <summary>
+        /// Only vertical scroll bars are shown.
+        /// </summary>
+        Vertical,
+
+        /// <summary>
+        /// Both horizontal and vertical scroll bars are shown.
+        /// </summary>
+        Both,
+    }
+}
