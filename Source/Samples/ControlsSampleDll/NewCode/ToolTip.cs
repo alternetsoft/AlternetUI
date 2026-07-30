b6bcb9f583d0d5dfb7b9f88d3179cdc011c95dc0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-30 19:33:29 +0300

    Update ToolTip.cs

--- Source/Samples/ControlsSampleDll/NewCode/ToolTip.cs
+++ Source/Samples/ControlsSampleDll/NewCode/ToolTip.cs
@@ -9,13 +9,29 @@ namespace Alternet.UI;
 [DefaultEvent(nameof(Popup))]
 public partial class ToolTip : Component
 {
+    private bool active;
+    private int automaticDelay;
+    private int autoPopDelay;
+    private Color backColor;
+    private Color foreColor;
+    private bool isBalloon;
+    private int initialDelay;
+    private bool ownerDraw;
+    private int reshowDelay;
+    private bool showAlways;
+    private bool stripAmpersands;
+    private ToolTipIcon toolTipIcon;
+    private string? toolTipTitle;
+    private bool useAnimation;
+    private bool useFading;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="ToolTip"/> class in its default state.
     /// </summary>
     public ToolTip()
     {
-        BackColor = SystemColors.Info;
-        ForeColor = SystemColors.InfoText;
+        backColor = SystemColors.Info;
+        foreColor = SystemColors.InfoText;
     }
 
     /// <summary>
@@ -24,8 +40,8 @@ public partial class ToolTip : Component
     [DefaultValue(true)]
     public virtual bool Active
     {
-        get;
-        set;
+        get => active;
+        set => active = value;
     }
 
     /// <summary>
@@ -34,8 +50,8 @@ public partial class ToolTip : Component
     [RefreshProperties(RefreshProperties.All)]
     public virtual int AutomaticDelay
     {
-        get;
-        set;
+        get => automaticDelay;
+        set => automaticDelay = value;
     }
 
     /// <summary>
@@ -44,8 +60,8 @@ public partial class ToolTip : Component
     [RefreshProperties(RefreshProperties.All)]
     public virtual int AutoPopDelay
     {
-        get;
-        set;
+        get => autoPopDelay;
+        set => autoPopDelay = value;
     }
 
     /// <summary>
@@ -54,8 +70,8 @@ public partial class ToolTip : Component
     [DefaultValue(typeof(Color), "Info")]
     public virtual Color BackColor
     {
-        get;
-        set;
+        get => backColor;
+        set => backColor = value;
     }
 
     /// <summary>
@@ -64,8 +80,8 @@ public partial class ToolTip : Component
     [DefaultValue(typeof(Color), "InfoText")]
     public virtual Color ForeColor
     {
-        get;
-        set;
+        get => foreColor;
+        set => foreColor = value;
     }
 
     /// <summary>
@@ -74,8 +90,8 @@ public partial class ToolTip : Component
     [DefaultValue(false)]
     public virtual bool IsBalloon
     {
-        get;
-        set;
+        get => isBalloon;
+        set => isBalloon = value;
     }
 
     /// <summary>
@@ -84,15 +100,19 @@ public partial class ToolTip : Component
     [RefreshProperties(RefreshProperties.All)]
     public virtual int InitialDelay
     {
-        get;
-        set;
+        get => initialDelay;
+        set => initialDelay = value;
     }
 
     /// <summary>
     /// Indicates whether the ToolTip will be drawn by the system or the user.
     /// </summary>
     [DefaultValue(false)]
-    public virtual bool OwnerDraw { get; set; }
+    public virtual bool OwnerDraw
+    {
+        get => ownerDraw;
+        set => ownerDraw = value;
+    }
 
     /// <summary>
     /// Gets or sets the length of time (in milliseconds) that it takes subsequent ToolTip
@@ -101,8 +121,8 @@ public partial class ToolTip : Component
     [RefreshProperties(RefreshProperties.All)]
     public virtual int ReshowDelay
     {
-        get;
-        set;
+        get => reshowDelay;
+        set => reshowDelay = value;
     }
 
     /// <summary>
@@ -112,8 +132,8 @@ public partial class ToolTip : Component
     [DefaultValue(false)]
     public virtual bool ShowAlways
     {
-        get;
-        set;
+        get => showAlways;
+        set => showAlways = value;
     }
 
     /// <summary>
@@ -123,8 +143,8 @@ public partial class ToolTip : Component
     [DefaultValue(false)]
     public virtual bool StripAmpersands
     {
-        get;
-        set;
+        get => stripAmpersands;
+        set => stripAmpersands = value;
     }
 
     [Localizable(false)]
@@ -139,8 +159,8 @@ public partial class ToolTip : Component
     [DefaultValue(ToolTipIcon.None)]
     public virtual ToolTipIcon ToolTipIcon
     {
-        get;
-        set;
+        get => toolTipIcon;
+        set => toolTipIcon = value;
     }
 
     /// <summary>
@@ -150,8 +170,8 @@ public partial class ToolTip : Component
     [AllowNull]
     public virtual string? ToolTipTitle
     {
-        get;
-        set;
+        get => toolTipTitle;
+        set => toolTipTitle = value;
     }
 
     /// <summary>
@@ -159,14 +179,22 @@ public partial class ToolTip : Component
     /// </summary>
     [Browsable(true)]
     [DefaultValue(true)]
-    public virtual bool UseAnimation { get; set; }
+    public virtual bool UseAnimation
+    {
+        get => useAnimation;
+        set => useAnimation = value;
+    }
 
     /// <summary>
     /// When set to true, a fade effect is used when tooltips are shown or hidden.
     /// </summary>
     [Browsable(true)]
     [DefaultValue(true)]
-    public virtual bool UseFading { get; set; }
+    public virtual bool UseFading
+    {
+        get => useFading;
+        set => useFading = value;
+    }
 
     /// <summary>
     /// Fires in OwnerDraw mode when the tooltip needs to be drawn.
@@ -176,12 +204,12 @@ public partial class ToolTip : Component
     /// <summary>
     /// Fires when the tooltip is just about to be shown.
     /// </summary>
-    public event PopupEventHandler? Popup;
+    public event PopupToolTipEventHandler? Popup;
 
     /// <summary>
     /// Returns true if the tooltip can offer an extender property to the specified target component.
     /// </summary>
-    public bool CanExtend(object target) => target is Control;
+    public virtual bool CanExtend(object target) => target is Control;
 
     /// <summary>
     /// Retrieves the tooltip text associated with the specified control.
@@ -258,5 +286,23 @@ public partial class ToolTip : Component
     {
         string s = base.ToString();
         return $"{s} InitialDelay: {InitialDelay}, ShowAlways: {ShowAlways}";
+    }
+
+    /// <summary>
+    /// Raises the <see cref="Draw"/> event.
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void RaiseDraw(DrawToolTipEventArgs e)
+    {
+        Draw?.Invoke(this, e);
+    }
+
+    /// <summary>
+    /// Raises the <see cref="Popup"/> event.
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void RaisePopup(PopupToolTipEventArgs e)
+    {
+        Popup?.Invoke(this, e);
     }
 }
