4210c9de73b04bee83ea47d5934324a2883195b3
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-07-31 13:35:16 +0300

    Create MatrixOrder.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Other/MatrixOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Specifies the order for matrix transform operations.
+    /// </summary>
+    public enum MatrixOrder
+    {
+        /// <summary>
+        /// The new operation is applied before the old operation.
+        /// </summary>
+        Prepend,
+
+        /// <summary>
+        /// The new operation is applied after the old operation.
+        /// </summary>
+        Append,
+    }
+}
