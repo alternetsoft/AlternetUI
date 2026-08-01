367047d4b26ed7f0b74f1ea25a00cc659cbd0c78
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-01 11:01:21 +0300

    Create GraphicsState.cs

---
+++ Source/Alternet.UI.Common/Drawing/Graphics/GraphicsState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Alternet.UI;
+
+namespace Alternet.Drawing
+{
+    /// <summary>
+    /// Represents the state of a <see cref="Graphics"/> object.
+    /// This object is returned by a call to the <see cref="Graphics.Save"/> methods.
+    /// </summary>
+    public readonly struct GraphicsState
+    {
+        internal GraphicsState(int state)
+        {
+            State = state;
+        }
+
+        internal int State { get; }
+    }
+}
