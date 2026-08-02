9a538e31079c5f277c7bd33670d4e672fb6337e2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-02 17:21:50 +0300

    Reorder elements in BrushHatchStyle to make it compatible with WinForms

--- Source/Alternet.UI.Common/Base/Enums/Drawing/Brush/BrushHatchStyle.cs
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Brush/BrushHatchStyle.cs
@@ -6,33 +6,33 @@
     public enum BrushHatchStyle
     {
         /// <summary>
-        /// A pattern of lines on a diagonal from upper right to lower left.
-        /// </summary>
-        BackwardDiagonal,
-
-        /// <summary>
-        /// A pattern of lines on a diagonal from upper left to lower right.
-        /// </summary>
-        ForwardDiagonal,
-
-        /// <summary>
-        /// A pattern of crisscross diagonal lines.
-        /// </summary>
-        DiagonalCross,
-
-        /// <summary>
-        /// Specifies horizontal and vertical lines that cross.
-        /// </summary>
-        Cross,
-
-        /// <summary>
         /// A pattern of horizontal lines.
         /// </summary>
-        Horizontal,
+        Horizontal = 0,
 
         /// <summary>
         /// A pattern of vertical lines.
         /// </summary>
-        Vertical,
+        Vertical = 1,
+
+        /// <summary>
+        /// A pattern of lines on a diagonal from upper left to lower right.
+        /// </summary>
+        ForwardDiagonal = 2,
+
+        /// <summary>
+        /// A pattern of lines on a diagonal from upper right to lower left.
+        /// </summary>
+        BackwardDiagonal = 3,
+
+        /// <summary>
+        /// Specifies horizontal and vertical lines that cross.
+        /// </summary>
+        Cross = 4,
+
+        /// <summary>
+        /// A pattern of crisscross diagonal lines.
+        /// </summary>
+        DiagonalCross = 5,
     }
 }
