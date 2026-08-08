521f1d641a503222247a04ff78bd83e7bf5ab17d
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-08 10:21:49 +0300

    Update Enums.h

--- Source/Alternet.UI.Pal/Api/Enums.h
+++ Source/Alternet.UI.Pal/Api/Enums.h
@@ -46,12 +46,12 @@ namespace Alternet::UI
     
     enum class BrushHatchStyle
     {
-        BackwardDiagonal = 0,
-        ForwardDiagonal = 1,
-        DiagonalCross = 2,
-        Cross = 3,
-        Horizontal = 4,
-        Vertical = 5,
+        Horizontal = 0,
+        Vertical = 1,
+        ForwardDiagonal = 2,
+        BackwardDiagonal = 3,
+        Cross = 4,
+        DiagonalCross = 5,
     };
     
     enum class BrushType
@@ -609,6 +609,12 @@ namespace Alternet::UI
         Dip = 7,
     };
     
+    enum class MatrixOrder
+    {
+        Prepend = 0,
+        Append = 1,
+    };
+    
     enum class RasterOperationMode
     {
         Clear = 0,
@@ -694,7 +700,6 @@ namespace Alternet::UI
     
     enum class TextFormatFlags
     {
-        GlyphOverhangPadding = 0,
         Default = 0,
         Left = 0,
         Top = 0,
@@ -702,26 +707,7 @@ namespace Alternet::UI
         Right = 2,
         VerticalCenter = 4,
         Bottom = 8,
-        WordBreak = 16,
-        SingleLine = 32,
-        ExpandTabs = 64,
-        NoClipping = 256,
-        ExternalLeading = 512,
-        NoPrefix = 2048,
-        Internal = 4096,
-        TextBoxControl = 8192,
-        PathEllipsis = 16384,
-        EndEllipsis = 32768,
-        ModifyString = 65536,
-        RightToLeft = 131072,
         WordEllipsis = 262144,
-        NoFullWidthCharacterBreak = 524288,
-        HidePrefix = 1048576,
-        PrefixOnly = 2097152,
-        PreserveGraphicsClipping = 16777216,
-        PreserveGraphicsTranslateTransform = 33554432,
-        NoPadding = 268435456,
-        LeftAndRightPadding = 536870912,
     };
     
     enum class Duplex
