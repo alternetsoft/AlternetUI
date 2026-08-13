e77a22f5a56f34cf7931b49accad5450aa1d50a4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-13 10:08:51 +0300

    Create VerticalTextDirection.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Text/VerticalTextDirection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies how vertical text should be drawn.
+    /// </summary>
+    public enum VerticalTextDirection
+    {
+        /// <summary>
+        /// Characters flow from bottom to top.
+        /// </summary>
+        BottomToTop,
+
+        /// <summary>
+        /// Characters flow from top to bottom.
+        /// </summary>
+        TopToBottom,
+    }
+}
