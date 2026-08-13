89cbf707372a2b8fc115a17122fcb54ce6209c54
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-13 11:05:51 +0300

    HelloWorldUseNuget

---
+++ Source/Tests/HelloWorldUseNuget/MainWindow.uixml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using Alternet.Drawing;
+using Alternet.UI;
+
+namespace HelloWorldSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Icon = new("embres:HelloWorldSample.Sample.ico");
+
+            InitializeComponent();
+
+            SetSizeToContent();
+        }
+
+        private void HelloButton_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
+    }
+}
