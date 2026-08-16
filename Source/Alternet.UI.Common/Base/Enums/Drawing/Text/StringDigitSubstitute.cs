39d491ad5d7f1321b0291b66242d86869c7f3400
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-16 11:53:16 +0300

    Create StringDigitSubstitute.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Text/StringDigitSubstitute.cs
@@ -0,0 +1,27 @@
+﻿namespace Alternet.Drawing;
+
+/// <summary>
+/// Specifies the digit substitution method for a particular language.
+/// </summary>
+public enum StringDigitSubstitute
+{
+    /// <summary>
+    /// The digit substitution method is determined by the user locale settings.
+    /// </summary>
+    User = 0,
+
+    /// <summary>
+    /// The digit substitution method is determined by the language specified in the <see cref='StringFormat'/> instance.
+    /// </summary>
+    None = 1,
+
+    /// <summary>
+    /// The digit substitution method is determined by the national language settings.
+    /// </summary>
+    National = 2,
+
+    /// <summary>
+    /// The digit substitution method is determined by the traditional digit shapes for the specified language.
+    /// </summary>
+    Traditional = 3,
+}
