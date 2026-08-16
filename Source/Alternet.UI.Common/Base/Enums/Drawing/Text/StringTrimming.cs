939b11d956c1bbfad84be4725bae5ebb3b78f6f0
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-16 11:53:23 +0300

    Create StringTrimming.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Drawing/Text/StringTrimming.cs
@@ -0,0 +1,39 @@
+﻿namespace Alternet.Drawing;
+
+/// <summary>
+/// Specifies how text is trimmed when it exceeds the layout region.
+/// </summary>
+public enum StringTrimming
+{
+    /// <summary>
+    /// No trimming is done. Text is displayed entirety, even if it overflows the layout region.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// The text is broken at the boundary of the last character that is inside the layout region.
+    /// </summary>
+    Character = 1,
+
+    /// <summary>
+    /// The text is broken at the boundary of the last word that is inside the layout region.
+    /// </summary>
+    Word = 2,
+
+    /// <summary>
+    /// The text is broken at the last character that is inside
+    /// the layout region and an ellipsis (...) is inserted after the character.
+    /// </summary>
+    EllipsisCharacter = 3,
+
+    /// <summary>
+    /// The text is broken at the last word that is inside the
+    /// layout region and an ellipsis (...) is inserted after the word.
+    /// </summary>
+    EllipsisWord = 4,
+
+    /// <summary>
+    /// The center is removed from the text and replaced by an ellipsis characters.
+    /// </summary>
+    EllipsisPath = 5,
+}
