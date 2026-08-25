25f82696db833a96234860f40245c4da4c0ecd0b
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-25 12:41:46 +0300

    Create RelativeWeekday.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/RelativeWeekday.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the relative occurrence of a weekday within a month.
+    /// Used to represent rules like "first Monday" or "last Friday".
+    /// </summary>
+    public enum RelativeWeekday
+    {
+        /// <summary>
+        /// The first occurrence of the specified weekday in a month.
+        /// </summary>
+        First,
+
+        /// <summary>
+        /// The second occurrence of the specified weekday in a month.
+        /// </summary>
+        Second,
+
+        /// <summary>
+        /// The third occurrence of the specified weekday in a month.
+        /// </summary>
+        Third,
+
+        /// <summary>
+        /// The fourth occurrence of the specified weekday in a month.
+        /// </summary>
+        Fourth,
+
+        /// <summary>
+        /// The last occurrence of the specified weekday in a month.
+        /// </summary>
+        Last
+    }
+}
