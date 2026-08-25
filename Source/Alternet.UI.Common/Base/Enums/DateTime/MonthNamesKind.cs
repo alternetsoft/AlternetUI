4de9eb0d472b9e4c2b8b3c6c5d833ee09bed33bc
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-25 14:51:47 +0300

    Create MonthNamesKind.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/MonthNamesKind.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the kind of month names to be used in date-related controls.
+    /// </summary>
+    public enum MonthNamesKind
+    {
+        /// <summary>
+        /// Represents the full names of the months (e.g., "January", "February").
+        /// </summary>
+        Full,
+
+        /// <summary>
+        /// Represents the abbreviated names of the months (e.g., "Jan", "Feb").
+        /// </summary>
+        Abbreviated,
+    }
+}
