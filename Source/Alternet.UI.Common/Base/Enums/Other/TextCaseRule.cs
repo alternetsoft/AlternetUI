85fe84229e8cdc4576ba27b2a0c46577b7331e33
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-25 12:43:24 +0300

    Create TextCaseRule.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Other/TextCaseRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines rules for changing the case of text.
+    /// </summary>
+    public enum TextCaseRule
+    {
+        /// <summary>
+        /// Convert all characters to lowercase using the current culture.
+        /// </summary>
+        Lower,
+
+        /// <summary>
+        /// Convert all characters to uppercase using the current culture.
+        /// </summary>
+        Upper,
+
+        /// <summary>
+        /// Convert all characters to lowercase using the invariant culture.
+        /// </summary>
+        LowerInvariant,
+
+        /// <summary>
+        /// Convert all characters to uppercase using the invariant culture.
+        /// </summary>
+        UpperInvariant,
+
+        /// <summary>
+        /// Capitalize only the first character of the string, leaving the rest unchanged.
+        /// </summary>
+        SentenceCase,
+    }
+}
