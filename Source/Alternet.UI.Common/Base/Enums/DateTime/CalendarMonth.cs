aeb5524725f321f0452be4f613ace77f7525f4f4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-25 13:00:50 +0300

    Create CalendarMonth.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/CalendarMonth.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents the months of the Gregorian calendar.
+    /// </summary>
+    public enum CalendarMonth
+    {
+        /// <summary>
+        /// Represents January, the first month of the year (1).
+        /// </summary>
+        January = 1,
+
+        /// <summary>
+        /// Represents February, the second month of the year (2).
+        /// </summary>
+        February = 2,
+
+        /// <summary>
+        /// Represents March, the third month of the year (3).
+        /// </summary>
+        March = 3,
+
+        /// <summary>
+        /// Represents April, the fourth month of the year (4).
+        /// </summary>
+        April = 4,
+
+        /// <summary>
+        /// Represents May, the fifth month of the year (5).
+        /// </summary>
+        May = 5,
+
+        /// <summary>
+        /// Represents June, the sixth month of the year (6).
+        /// </summary>
+        June = 6,
+
+        /// <summary>
+        /// Represents July, the seventh month of the year (7).
+        /// </summary>
+        July = 7,
+
+        /// <summary>
+        /// Represents August, the eighth month of the year (8).
+        /// </summary>
+        August = 8,
+
+        /// <summary>
+        /// Represents September, the ninth month of the year (9).
+        /// </summary>
+        September = 9,
+
+        /// <summary>
+        /// Represents October, the tenth month of the year (10).
+        /// </summary>
+        October = 10,
+
+        /// <summary>
+        /// Represents November, the eleventh month of the year (11).
+        /// </summary>
+        November = 11,
+
+        /// <summary>
+        /// Represents December, the twelfth month of the year (12).
+        /// </summary>
+        December = 12,
+    }
+}
