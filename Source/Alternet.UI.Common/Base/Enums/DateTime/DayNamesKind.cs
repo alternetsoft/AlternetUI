6c71d9d05d603fa20b92349a0feb3000e8c0a6e7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-25 14:51:40 +0300

    Create DayNamesKind.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/DayNamesKind.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the kind of day names to be used in date-related controls.
+    /// </summary>
+    public enum DayNamesKind
+    {
+        /// <summary>
+        /// Represents the full names of the days of the week (e.g., "Monday", "Tuesday").
+        /// </summary>
+        Full,
+
+        /// <summary>
+        /// Represents the abbreviated names of the days of the week (e.g., "Mon", "Tue").
+        /// </summary>
+        Abbreviated,
+
+        /// <summary>
+        /// Represents the shortest unique names of the days of the week (e.g., "M", "T").
+        /// </summary>
+        Shortest,
+    }
+}
