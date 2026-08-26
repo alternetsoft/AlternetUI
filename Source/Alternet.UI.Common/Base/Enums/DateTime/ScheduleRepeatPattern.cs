6d0a7c2f245f008d92a109d1837fab2aebc971ae
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-26 11:30:10 +0300

    Create ScheduleRepeatPattern.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/DateTime/ScheduleRepeatPattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a repeat pattern for scheduling events or tasks.
+    /// </summary>
+    public enum ScheduleRepeatPattern
+    {
+        /// <summary>
+        /// Represents no repeat pattern.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Represents a daily repeat pattern.
+        /// </summary>
+        Daily,
+
+        /// <summary>
+        /// Represents a weekly repeat pattern.
+        /// </summary>
+        Weekly,
+
+        /// <summary>
+        /// Represents a monthly repeat pattern.
+        /// </summary>
+        Monthly,
+
+        /// <summary>
+        /// Represents a yearly repeat pattern.
+        /// </summary>
+        Yearly,
+    }
+}
