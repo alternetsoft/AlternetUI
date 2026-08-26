01b07839e70184cf99cebf95cbbe0789c82106e4
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-26 10:58:23 +0300

    Create RepeatPatternPicker.cs

---
+++ Source/Alternet.UI.Common/Controls/DateTime/RepeatPatternPicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a control that allows users to select a repeat pattern for an event or task.
+    /// </summary>
+    public partial class RepeatPatternPicker
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepeatPatternPicker"/> class.
+        /// </summary>
+        public RepeatPatternPicker()
+        {
+        }
+    }
+}
