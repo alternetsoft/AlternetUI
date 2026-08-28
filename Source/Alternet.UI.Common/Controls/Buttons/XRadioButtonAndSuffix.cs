374d3e922e89d3e931c8b65dc9129b1c75b36666
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-28 15:18:39 +0300

    Create XRadioButtonAndSuffix.cs

---
+++ Source/Alternet.UI.Common/Controls/Buttons/XRadioButtonAndSuffix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a generic panel with inner radio button and suffix controls. 
+    /// </summary>
+    public partial class XRadioButtonAndSuffix : ControlAndSuffix<XRadioButton, GenericControl>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XRadioButtonAndSuffix"/> class
+        /// </summary>
+        /// <param name="suffix">The suffix control.</param>
+        public XRadioButtonAndSuffix(GenericControl? suffix)
+            : base(null, suffix)
+        {
+            MainControl.TextVisible = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XRadioButtonAndSuffix"/> class.
+        /// </summary>
+        public XRadioButtonAndSuffix()
+            : this(null)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override GenericControl CreateSuffixControl()
+        {
+            var result = new Label();
+
+            result.Click += (s, e) =>
+            {
+                MainControl.IsChecked = true;
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the <see cref="XRadioButton"/> is checked.
+        /// </summary>
+        public bool IsChecked
+        {
+            get => MainControl.IsChecked;
+            set => MainControl.IsChecked = value;
+        }
+    }
+}
