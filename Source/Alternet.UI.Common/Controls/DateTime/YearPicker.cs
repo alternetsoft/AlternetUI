9cd41cea0c48726f0f2e28ad1823acffa1f0c7ea
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-29 16:46:10 +0300

    Create YearPicker.cs

---
+++ Source/Alternet.UI.Common/Controls/DateTime/YearPicker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Represents a control that allows users to select a year.
+    /// </summary>
+    public partial class YearPicker : XIntPicker
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="YearPicker"/> class.
+        /// </summary>
+        public YearPicker()
+        {
+            Minimum = DateUtils.MinimumDateTime.Year;
+            Maximum = DateUtils.MaximumDateTime.Year;
+            Value = DateTime.Now.Year;
+        }
+    }
+}
