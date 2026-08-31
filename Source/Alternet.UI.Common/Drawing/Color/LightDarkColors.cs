d95636c792729f398fe6a9efe118d511f16793f5
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-08-31 16:35:39 +0300

    Add text-friendly background color groups
    Introduce curated background color lists in `LightDarkColors` for light text, dark text, and colors that work with both. This makes it easier to pick readable color combinations from a documented API instead of manually filtering named colors.

--- Source/Alternet.UI.Common/Drawing/Color/LightDarkColors.cs
+++ Source/Alternet.UI.Common/Drawing/Color/LightDarkColors.cs
@@ -98,5 +98,449 @@ namespace Alternet.Drawing
         }
 
         internal static LightDarkColor White { get; } = new (Color.White);
+
+        /// <summary>
+        /// Contains background colors which look good with light text.
+        /// </summary>
+        public static class LightTextBackgrounds
+        {
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Black => Color.Black;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Blue => Color.Blue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color BlueViolet => Color.BlueViolet;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Brown => Color.Brown;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color CadetBlue => Color.CadetBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Chocolate => Color.Chocolate;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color CornflowerBlue => Color.CornflowerBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Crimson => Color.Crimson;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkBlue => Color.DarkBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkCyan => Color.DarkCyan;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkGoldenrod => Color.DarkGoldenrod;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkGreen => Color.DarkGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkMagenta => Color.DarkMagenta;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkOliveGreen => Color.DarkOliveGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkOrchid => Color.DarkOrchid;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkRed => Color.DarkRed;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkSlateBlue => Color.DarkSlateBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkSlateGray => Color.DarkSlateGray;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DarkViolet => Color.DarkViolet;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color DimGray => Color.DimGray;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Firebrick => Color.Firebrick;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color ForestGreen => Color.ForestGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Green => Color.Green;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color IndianRed => Color.IndianRed;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Indigo => Color.Indigo;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Maroon => Color.Maroon;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MediumBlue => Color.MediumBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MediumOrchid => Color.MediumOrchid;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MediumPurple => Color.MediumPurple;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MediumSlateBlue => Color.MediumSlateBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MediumVioletRed => Color.MediumVioletRed;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color MidnightBlue => Color.MidnightBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Olive => Color.Olive;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color OrangeRed => Color.OrangeRed;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color RebeccaPurple => Color.RebeccaPurple;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color RoyalBlue => Color.RoyalBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color SaddleBrown => Color.SaddleBrown;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color SeaGreen => Color.SeaGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Sienna => Color.Sienna;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color SlateBlue => Color.SlateBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color SlateGray => Color.SlateGray;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color SteelBlue => Color.SteelBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light text.
+            /// </summary>
+            public static Color Teal => Color.Teal;
+        }
+
+        /// <summary>
+        /// Contains background colors which look good with dark text.
+        /// </summary>
+        public static class DarkTextBackgrounds
+        {
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color AntiqueWhite => Color.AntiqueWhite;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Aquamarine => Color.Aquamarine;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Bisque => Color.Bisque;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Gold => Color.Gold;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Khaki => Color.Khaki;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Lavender => Color.Lavender;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightBlue => Color.LightBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightCyan => Color.LightCyan;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightGray => Color.LightGray;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightGreen => Color.LightGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightPink => Color.LightPink;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightSalmon => Color.LightSalmon;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightSkyBlue => Color.LightSkyBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color LightYellow => Color.LightYellow;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Linen => Color.Linen;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color MintCream => Color.MintCream;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color MistyRose => Color.MistyRose;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Moccasin => Color.Moccasin;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color NavajoWhite => Color.NavajoWhite;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color OldLace => Color.OldLace;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PaleGoldenrod => Color.PaleGoldenrod;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PaleGreen => Color.PaleGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PaleTurquoise => Color.PaleTurquoise;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PapayaWhip => Color.PapayaWhip;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PeachPuff => Color.PeachPuff;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Pink => Color.Pink;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color PowderBlue => Color.PowderBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color SeaShell => Color.SeaShell;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color SkyBlue => Color.SkyBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Snow => Color.Snow;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Wheat => Color.Wheat;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color WhiteSmoke => Color.WhiteSmoke;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color Yellow => Color.Yellow;
+
+            /// <summary>
+            /// Gets a background color which looks good with dark text.
+            /// </summary>
+            public static Color YellowGreen => Color.YellowGreen;
+        }
+
+        /// <summary>
+        /// Contains background colors which look good with light and dark text.
+        /// </summary>
+        public static class LightDarkTextBackgrounds
+        {
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color Chocolate => Color.Chocolate;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color DarkCyan => Color.DarkCyan;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color DarkGoldenrod => Color.DarkGoldenrod;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color DarkGreen => Color.DarkGreen;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color Olive => Color.Olive;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color RoyalBlue => Color.RoyalBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color SlateBlue => Color.SlateBlue;
+
+            /// <summary>
+            /// Gets a background color which looks good with light and dark text.
+            /// </summary>
+            public static Color Teal => Color.Teal;
+        }
+
     }
 }
