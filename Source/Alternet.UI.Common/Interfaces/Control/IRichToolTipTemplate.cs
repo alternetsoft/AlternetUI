67c613e26c7be42a278df9cab3555baa80f18b48
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-03 10:27:49 +0300

    Create IRichToolTipTemplate.cs

---
+++ Source/Alternet.UI.Common/Interfaces/Control/IRichToolTipTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Defines a template for the <see cref="RichToolTip"/> control.
+    /// </summary>
+    public interface IRichToolTipTemplate
+    {
+        /// <summary>
+        /// Gets control which contains title.
+        /// </summary>
+        Label TitleLabel { get; }
+
+        /// <summary>
+        /// Gets control which contains message.
+        /// </summary>
+        Label MessageLabel { get; }
+
+        /// <summary>
+        /// Gets control which contains image.
+        /// </summary>
+        PictureBox PictureBox { get; }
+
+        /// <summary>
+        /// Gets the root control for the template.
+        /// </summary>
+        TemplateControl RootControl { get; }
+    }
+}
