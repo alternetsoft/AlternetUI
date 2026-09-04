2653e5d3024277050548fd78abb68d52690d251f
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-04 08:58:41 +0300

    Implement stateful PlessCalendarDateAttr
    Reworked `PlessCalendarDateAttr` from auto-properties to backed fields that use `SetProperty`, aligning updates with `ImmutableObject` change tracking behavior. Added constructors for optional border initialization and text-color initialization, and expanded XML docs to describe supported date attributes.

--- Source/Alternet.UI.Common/Controls/DateTime/PlessCalendarDateAttr.cs
+++ Source/Alternet.UI.Common/Controls/DateTime/PlessCalendarDateAttr.cs
@@ -10,31 +10,85 @@ using Alternet.Drawing;
 namespace Alternet.UI
 {
     /// <summary>
-    /// <see cref="ICalendarDateAttr"/> interface implementation that does nothing.
+    /// Represents the attributes of a calendar date, including text color, background color,
+    /// border color, holiday status, and border style.
     /// </summary>
     public partial class PlessCalendarDateAttr : ImmutableObject, ICalendarDateAttr
     {
+        private Color? textColor;
+        private Color? backgroundColor;
+        private Color? borderColor;
+        private bool isHoliday;
+        private CalendarDateBorder border;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PlessCalendarDateAttr"/> class.
         /// </summary>
-        public PlessCalendarDateAttr()
+        /// <param name="border">The border settings for the calendar date. Optional. If not specified,
+        /// the default value is <see cref="CalendarDateBorder.None"/>.</param>
+        public PlessCalendarDateAttr(CalendarDateBorder border = 0)
         {
+            this.border = border;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlessCalendarDateAttr"/> class with the specified text color.
+        /// </summary>
+        /// <param name="textColor">The text color for the calendar date.</param>
+        public PlessCalendarDateAttr(Color textColor)
+        {
+            this.textColor = textColor;
         }
 
         /// <inheritdoc/>
-        public virtual Color? TextColor { get; set; }
+        public virtual Color? TextColor
+        {
+            get => textColor;
+            set
+            {
+                SetProperty(ref textColor, value);
+            }
+        }
 
         /// <inheritdoc/>
-        public virtual Color? BackgroundColor { get; set; }
+        public virtual Color? BackgroundColor
+        {
+            get => backgroundColor;
+            set
+            {
+                SetProperty(ref backgroundColor, value);
+            }
+        }
 
         /// <inheritdoc/>
-        public virtual Color? BorderColor { get; set; }
+        public virtual Color? BorderColor
+        {
+            get => borderColor;
+            set
+            {
+                SetProperty(ref borderColor, value);
+            }
+        }
 
         /// <inheritdoc/>
-        public virtual bool IsHoliday { get; set; }
+        public virtual bool IsHoliday
+        {
+            get => isHoliday;
+            set
+            {
+                SetProperty(ref isHoliday, value);
+            }
+        }
 
         /// <inheritdoc/>
-        public virtual CalendarDateBorder Border { get; set; }
+        public virtual CalendarDateBorder Border
+        {
+            get => border;
+            set
+            {
+                SetProperty(ref border, value);
+            }
+        }
 
         /// <inheritdoc/>
         public bool HasTextColor => TextColor != null;
