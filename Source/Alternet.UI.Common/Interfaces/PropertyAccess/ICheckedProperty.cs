0295fe2641dc06f6506db481fb90b216f13947e8
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-06 13:34:24 +0300

    Organize folders

---
+++ Source/Alternet.UI.Common/Interfaces/PropertyAccess/ICheckedProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Gives access to the 'Checked' property of the object.
+    /// </summary>
+    public interface ICheckedProperty
+    {
+        /// <summary>
+        /// Gets or sets the 'Checked' property of the object.
+        /// </summary>
+        bool Checked { get; set; }
+
+        /// <summary>
+        /// Occurs when the 'Checked' property of the object changes.
+        /// </summary>
+        event EventHandler? CheckedChanged;
+    }
+}
