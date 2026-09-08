24c5937efc76138435b81c210a0697c1873f70f7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-08 20:00:36 +0300

    Create ShowMethod.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/ShowMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the method used to show a control.
+    /// </summary>
+    public enum ShowMethod
+    {
+        /// <summary>
+        /// Indicates that the control should be shown using the default method,
+        /// which is determined by the control's implementation.
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        /// Indicates that the control should not be shown.
+        /// </summary>
+        None = 1,
+    }
+}
