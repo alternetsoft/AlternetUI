1d6076d66a21603e40db4449764aa8641d95306c
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-09 10:01:26 +0300

    Create ControlColorMode.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Control/ControlColorMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the color mode of the control.
+    /// </summary>
+    public enum ControlColorMode
+    {
+        /// <summary>
+        /// Light color mode.
+        /// </summary>
+        Light,
+
+        /// <summary>
+        /// Dark color mode.
+        /// </summary>
+        Dark,
+    }
+}
