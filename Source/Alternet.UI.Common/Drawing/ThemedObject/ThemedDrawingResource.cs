fe27bdb4aa941b7e7a793054cc5b7b46471ed019
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-15 09:02:29 +0300

    Add title support to ThemedDrawingResource
    Expose a shared `Title` property on themed drawing resources and add constructor overloads that accept a title alongside light/dark brushes, pens, colors, and themed values. This keeps resource metadata synchronized across both theme variants when creating or updating a themed resource.

--- Source/Alternet.UI.Common/Drawing/ThemedObject/ThemedDrawingResource.cs
+++ Source/Alternet.UI.Common/Drawing/ThemedObject/ThemedDrawingResource.cs
@@ -9,6 +9,18 @@ namespace Alternet.Drawing
     /// </summary>
     public interface IThemedDrawingResource : IThemedDrawingObject<IDrawingResource>
     {
+        /// <summary>
+        /// Gets or sets the title of the themed drawing resource.
+        /// </summary>
+        public string? Title
+        {
+            get => Light.Title;
+            set
+            {
+                Light.Title = value;
+                Dark.Title = value;
+            }
+        }
     }
 
     /// <summary>
@@ -33,6 +45,94 @@ namespace Alternet.Drawing
         public ThemedDrawingResource(IDrawingResource light, IDrawingResource dark)
             : base(light, dark)
         {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified light and dark values.
+        /// </summary>
+        /// <param name="light">The value to use for the light theme variant.</param>
+        /// <param name="dark">The value to use for the dark theme variant.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(Brush light, Brush dark, string title)
+            : this(new DrawingResource(light), new DrawingResource(dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified light and dark values.
+        /// </summary>
+        /// <param name="light">The value to use for the light theme variant.</param>
+        /// <param name="dark">The value to use for the dark theme variant.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(Pen light, Pen dark, string title)
+                    : this(new DrawingResource(light), new DrawingResource(dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified light and dark values.
+        /// </summary>
+        /// <param name="light">The value to use for the light theme variant.</param>
+        /// <param name="dark">The value to use for the dark theme variant.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(Color light, Color dark, string title)
+                    : this(new DrawingResource(light), new DrawingResource(dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified themed color.
+        /// </summary>
+        /// <param name="color">The themed color to use for the light and dark theme variants.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(ThemedColor color, string title)
+                    : this(new DrawingResource(color.Light), new DrawingResource(color.Dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified themed brush.
+        /// </summary>
+        /// <param name="brush">The themed brush to use for the light and dark theme variants.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(ThemedBrush brush, string title)
+                    : this(new DrawingResource(brush.Light), new DrawingResource(brush.Dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThemedDrawingResource"/> class with the specified themed pen.
+        /// </summary>
+        /// <param name="pen">The themed pen to use for the light and dark theme variants.</param>
+        /// <param name="title">The title of the themed drawing resource.</param>
+        public ThemedDrawingResource(ThemedPen pen, string title)
+                    : this(new DrawingResource(pen.Light), new DrawingResource(pen.Dark))
+        {
+            Light.Title = title;
+            Dark.Title = title;
+        }
+
+        /// <summary>
+        /// Gets or sets the title of the themed drawing resource.
+        /// </summary>
+        public string? Title
+        {
+            get => Light.Title;
+            set
+            {
+                Light.Title = value;
+                Dark.Title = value;
+            }
         }
     }
 }
