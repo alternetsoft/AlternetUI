074625286354d69512227b348af19bee0310b3ed
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-18 17:37:52 +0300

    Create KeyboardEditMode.cs

---
+++ Source/Alternet.UI.Common/Base/Enums/Input/KeyboardEditMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alternet.UI
+{
+    /// <summary>
+    /// Specifies the keyboard edit mode for a control.
+    /// </summary>
+    public enum KeyboardEditMode
+    {
+        /// <summary>
+        /// No keyboard edit mode is specified.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The control enters edit mode when the Enter key is pressed.
+        /// </summary>
+        EditOnEnter,
+
+        /// <summary>
+        /// The control enters edit mode when the F2 key is pressed.
+        /// </summary>
+        EditOnF2,
+    }
+}
