e7e464ea99a17cde5fca55fa56541eaeefa3c9c7
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-19 11:17:41 +0300

    update to use wxWidgets 3.3.3
    and microsoft.web.webview2.1.0.4191.47

--- Install.Scripts/MSW.Update.WxWidgets.SubTool.4.WinRar.bat
+++ Install.Scripts/MSW.Update.WxWidgets.SubTool.4.WinRar.bat
@@ -5,7 +5,7 @@ set PROJ=%SCRIPT_HOME%\..\Source\Build\Alternet.UI.Pal\Alternet.UI.Pal.proj
 
 set EXTERNAL=%SCRIPT_HOME%\..\External\
 set WINRAR="C:\Program Files\WinRAR\WinRAR.exe"
-set RESULT=wxWidgets-bin-noobjpch-3.3.0.zip
+set RESULT=wxWidgets-bin-noobjpch-3.3.3.zip
 pushd "%EXTERNAL%"
 del "%RESULT%"
 %WINRAR% a -s -r -x*.obj -x*.pch "%RESULT%" "WxWidgets\*.*"
