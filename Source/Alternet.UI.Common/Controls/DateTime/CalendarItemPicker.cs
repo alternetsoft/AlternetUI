f839db31de092b157dcd458475fd557eec45250a
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-24 18:53:22 +0300

    Update CalendarItemPicker.cs

--- Source/Alternet.UI.Common/Controls/DateTime/CalendarItemPicker.cs
+++ Source/Alternet.UI.Common/Controls/DateTime/CalendarItemPicker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Alternet.UI
@@ -11,6 +12,16 @@ namespace Alternet.UI
     {
         private CalendarItem? calendarItem;
 
+        /*
+                private string? title;
+                private string? description;
+                private object? location;
+                private CalendarItemCategory? category;
+                private CalendarItemStatus? status;
+                private object? userData;
+                private string? userName;
+        */
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CalendarItemPicker"/> class.
         /// </summary>
@@ -19,17 +30,53 @@ namespace Alternet.UI
         }
 
         /// <summary>
+        /// Occurs when the <see cref="CalendarItem"/> being edited has changed.
+        /// </summary>
+        public event EventHandler? ValueChanged;
+
+        /// <summary>
         /// Gets or sets the <see cref="CalendarItem"/> that is being edited by the picker.
         /// </summary>
-        public virtual CalendarItem CalendarItem
+        public virtual CalendarItem Value
         {
-            get => calendarItem ??= CreateCalendarItem();
+            get
+            {
+                if (calendarItem == null)
+                {
+                    calendarItem = CreateCalendarItem();
+                    calendarItem.PropertyChanged += OnCalendarItemChanged;
+                }
+
+                return calendarItem;
+            }
+
             set
             {
                 if (calendarItem == value)
                     return;
+                if (calendarItem != null)
+                {
+                    calendarItem.PropertyChanged -= OnCalendarItemChanged;
+                }
+
                 calendarItem = value;
+
+                if (calendarItem != null)
+                {
+                    calendarItem.PropertyChanged += OnCalendarItemChanged;
+                }
             }
+        }
+
+        /// <summary>
+        /// Called when a property of the <see cref="CalendarItem"/> being edited has changed.
+        /// This method can be overridden in derived classes to provide custom behavior when a property changes.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">A <see cref="PropertyChangedEventArgs"/> that contains the event data.</param>
+        protected virtual void OnCalendarItemChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            ValueChanged?.Invoke(this, EventArgs.Empty);
         }
 
         /// <summary>
