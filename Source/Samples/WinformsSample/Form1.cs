3c1dfa66350220ba213965abdc3a1794f714b8b2
Sergiy Kurinny <59140005+generalloki@users.noreply.github.com>
2026-09-26 10:27:02 +0300

    Update Form1.cs

--- Source/Samples/WinformsSample/Form1.cs
+++ Source/Samples/WinformsSample/Form1.cs
@@ -14,13 +14,16 @@ public partial class Form1 : Form
         var svg = Alternet.UI.KnownSvgImages.ImgAngleUp;
         var bitmap = svg.ToNormalBitmap(32, 32, isDark: false);
 
-        var svg2 = Alternet.UI.KnownSvgImages.ImgAngleDown;
+        var svg2 = Alternet.UI.KnownSvgImages.ImgAngleLeft;
         var bitmap2 = svg2.ToDisabledBitmap(32, 32, isDark: false);
 
         pictureBox.Image = bitmap;
+        pictureBox.Size = new Size(32, 32);
         this.Controls.Add(pictureBox);
 
         pictureBox2.Image = bitmap2;
+        pictureBox2.Size = new Size(32, 32);
+        pictureBox2.Location = new Point(128, 128);
         this.Controls.Add(pictureBox2);
     }
 }
